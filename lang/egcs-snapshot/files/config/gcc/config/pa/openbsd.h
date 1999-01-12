96b033bd0d63814611dfd201215d2266243db693
espie <espie@openbsd.org>
1999-01-12 17:18:31 +0000

    Better...

--- lang/egcs-snapshot/files/config/gcc/config/pa/openbsd.h
+++ lang/egcs-snapshot/files/config/gcc/config/pa/openbsd.h
@@ -8,6 +8,12 @@
 /* run-time target specifications */
 #define CPP_PREDEFINES "-D__unix__ -D__ANSI_COMPAT -Asystem(unix) -Asystem(OpenBSD) -Amachine(hppa) -D__OpenBSD__ -D__hppa__ -D__hppa"
 
+#define OVERRIDE_OPTIONS		\
+{										\
+	override_options();			\
+	if (! flag_pic) target_flags |= TARGET_NONPIC_FLAGS;	\
+}
+	
 /* XXX why doesn't PA support -R  like everyone ??? */
 #undef LINK_SPEC
 #define LINK_SPEC \
@@ -54,3 +60,6 @@ do { fputs ("\t.SPACE $PRIVATE$\n\
        output_file_directive ((FILE), main_input_filename); \
    } while (0)
 
+/* remove hpux specific pa defines */
+#undef LDD_SUFFIX
+#undef PARSE_LDD_OUTPUT
