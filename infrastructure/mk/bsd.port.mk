d486876c6f580c389e9c357da4f0736ea5cb2116
espie <espie@openbsd.org>
1999-09-26 22:20:46 +0000

    Buglet, reversed default for CLEANDEPENDS

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Fundamental; tab-width: 4; -*-
 # ex:ts=4
-FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.122 1999/09/26 10:51:34 espie Exp $$
+FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.123 1999/09/26 22:20:46 espie Exp $$
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -408,7 +408,7 @@ CLEANDEPENDS?=Yes
 CLEANDEPENDS?=No
 .endif
 .else
-CLEANDEPENDS?=Yes
+CLEANDEPENDS?=No
 .endif
 NOMANCOMPRESS?=	Yes
 DEF_UMASK?=		022
