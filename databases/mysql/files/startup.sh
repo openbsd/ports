b0ac3f25cdcb381dbd0cc2541cf1c7561a0802b9
brad <brad@openbsd.org>
1999-12-02 03:47:36 +0000

    - remove now unnecesary USE_EGCC and USE_EGXX tags - split out startup.sh cretion from Makefile and just include it in the   files subdir - remove CONFIGURE_ENV, CFLAGS and CXXFLAGS do not require overriding with   -current pthreads, the autoconf script properly detects and uses them - cleanup Makefile

---
+++ databases/mysql/files/startup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+PREFIX=@PREFIX@
+
+if [ -x /sbin/ldconfig ]; then
+ /sbin/ldconfig -m ${PREFIX}/lib/mysql
+fi
+
+if [ -x ${PREFIX}/bin/safe_mysqld ]; then
+ ${PREFIX}/bin/safe_mysqld > /dev/null & && echo -n ' mysql'
+fi
