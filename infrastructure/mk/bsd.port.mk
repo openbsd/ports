158908fdcecf251abf71173b8846cfb7f8609276
espie <espie@openbsd.org>
2000-02-02 18:20:28 +0000

    I am a moron

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Fundamental; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.177 2000/02/02 15:28:17 espie Exp $$
+FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.178 2000/02/02 18:20:28 espie Exp $$
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -848,7 +848,7 @@ PATCH_SITES${_I}:=	${_PATCH_SITES${_I}} ${MASTER_SITE_BACKUP}
 PATCH_SITES${_I}:= ${MASTER_SITE_OVERRIDE} ${_PATCH_SITES${_I}}
 .    endif
 .  elsif defined(MASTER_SITES${_I})
-_PATCH_SITES${_I}:= ${MASTER_SITES${_I}}
+PATCH_SITES${_I}:= ${MASTER_SITES${_I}}
 .  endif
 .endfor
 
