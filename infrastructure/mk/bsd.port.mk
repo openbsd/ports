8b20f37bef4d46753b47f80852ebd274176f3d2e
espie <espie@openbsd.org>
2000-02-09 00:54:33 +0000

    and of course, nobody spotted the obvious typo in the patch I gave on ports...

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Fundamental; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.189 2000/02/09 00:42:39 espie Exp $$
+FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.190 2000/02/09 00:54:33 espie Exp $$
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -633,7 +633,7 @@ WRKDIST?=		${WRKDIR}
 WRKDIST?=		${WRKDIR}/${DISTNAME}
 .endif
 
-WRKSRC?=	   ${WRKDIR}
+WRKSRC?=	   ${WRKDIST}
 
 .if defined(SEPARATE_BUILD)
 WRKBUILD?=		${WRKDIR}/build-${ARCH}
