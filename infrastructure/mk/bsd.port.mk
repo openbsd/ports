7c8bf5098c50cee1da6dccfbc18b46ce26b9e851
espie <espie@openbsd.org>
2000-02-09 00:42:39 +0000

    Temporary work-around for ports that define WRKSRC manually...

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Fundamental; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.188 2000/02/09 00:26:32 espie Exp $$
+FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.189 2000/02/09 00:42:39 espie Exp $$
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -625,7 +625,9 @@ WRKDIR?=		${.CURDIR}/work.${MACHINE_ARCH}
 WRKDIR?=		${.CURDIR}/work
 .endif
 
-.if defined(NO_WRKSUBDIR)
+.if defined(WRKSRC)
+WRKDIST?=		${WRKSRC}
+.elif defined(NO_WRKSUBDIR)
 WRKDIST?=		${WRKDIR}
 .else
 WRKDIST?=		${WRKDIR}/${DISTNAME}
