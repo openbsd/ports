c6da62630a04fe32da5df7a90181b9253a174b8d
espie <espie@openbsd.org>
2000-02-12 00:49:17 +0000

    Oops, left `build' line visible.

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Fundamental; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.195 2000/02/12 00:43:58 espie Exp $$
+FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.196 2000/02/12 00:49:17 espie Exp $$
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -1354,7 +1354,7 @@ ${_BUILD_COOKIE}: ${_CONFIGURE_COOKIE}
 	@cd ${.CURDIR} && make do-build
 .  else
 # What BUILD normally does:
-	cd ${WRKBUILD} && ${SETENV} ${MAKE_ENV} ${MAKE_PROGRAM} ${MAKE_FLAGS} -f ${MAKEFILE} ${ALL_TARGET}
+	@cd ${WRKBUILD} && ${SETENV} ${MAKE_ENV} ${MAKE_PROGRAM} ${MAKE_FLAGS} -f ${MAKEFILE} ${ALL_TARGET}
 # End of BUILD
 .  endif
 .  if target(post-build)
