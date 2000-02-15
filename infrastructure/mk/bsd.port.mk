eb9439da6306a1f3423ac58cdbbf663ea44d42e0
turan <turan@openbsd.org>
2000-02-15 07:28:20 +0000

    DISTF -> DISTFILES, PKG -> PACKAGE

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Fundamental; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.198 2000/02/12 13:52:04 espie Exp $$
+FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.199 2000/02/15 07:28:20 turan Exp $$
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -1200,9 +1200,9 @@ ${_EXTRACT_COOKIE}:
 
 # You need to define LICENCE_TYPE and PERMIT_* to make the warning go away.
 # See ports/infrastructure/templates/Makefile.template
-.if !defined(LICENSE_TYPE) || !defined(PERMIT_PKG_CDROM) || \
-!defined(PERMIT_PKG_FTP) || !defined(PERMIT_DISTF_CDROM) || \
-!defined( PERMIT_DISTF_FTP)
+.if !defined(LICENSE_TYPE) || !defined(PERMIT_PACKAGE_CDROM) || \
+!defined(PERMIT_PACKAGE_FTP) || !defined(PERMIT_DISTFILES_CDROM) || \
+!defined(PERMIT_DISTFILES_FTP)
 	@echo >&2 "*** LICENSING INFO INCOMPLETE!  NOTIFY MAINTAINER! ***"
 .endif
 
