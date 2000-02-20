bbaf95cb8cb7c29f71d2aea77eaf5e637d0dd539
espie <espie@openbsd.org>
2000-02-20 17:05:40 +0000

    The MAINTAINER variable is here to be used...

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Fundamental; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.203 2000/02/19 04:16:23 turan Exp $$
+FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.204 2000/02/20 17:05:40 espie Exp $$
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -1203,8 +1203,8 @@ ${_EXTRACT_COOKIE}:
 .if !defined(LICENSE_TYPE) || !defined(PERMIT_PACKAGE_CDROM) || \
 !defined(PERMIT_PACKAGE_FTP) || !defined(PERMIT_DISTFILES_CDROM) || \
 !defined(PERMIT_DISTFILES_FTP)
-	@echo >&2 "*** The licensing info for this port is incomplete. ***"
-	@echo >&2 "*** Please notify the OpenBSD port maintainer.      ***"
+	@echo >&2 "*** The licensing info for this port is incomplete."
+	@echo >&2 "*** Please notify the OpenBSD port maintainer <${MAINTAINER}>"
 .endif
 
 	@${_MAKE_COOKIE} ${_EXTRACT_COOKIE}
