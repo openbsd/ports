be9e9278639777828e109586ac9772428846e7d3
marc <marc@openbsd.org>
2000-04-08 23:11:15 +0000

    Change PACKAGES so the arch part of the name.  Allows nfs mounted ports tree to be shared by hosts of different types.   Note: you may want to do something like this to move any existing packages: cd /usr/ports/packages mkdir .tmp mv * .tmp mv .tmp sparc patch approved by espie@

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Fundamental; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.252 2000/04/03 17:32:42 espie Exp $$
+FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.253 2000/04/08 23:11:15 marc Exp $$
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -453,7 +453,7 @@ FULLDISTDIR?=	${DISTDIR}/${DIST_SUBDIR}
 .else
 FULLDISTDIR?=	${DISTDIR}
 .endif
-PACKAGES?=		${PORTSDIR}/packages
+PACKAGES?=		${PORTSDIR}/packages/${ARCH}
 TEMPLATES?=		${PORTSDIR}/infrastructure/templates
 
 .if exists(${.CURDIR}/patches.${ARCH})
