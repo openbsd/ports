fd5775bd3185202bc0ca0def30ba1249bdf16357
espie <espie@openbsd.org>
2000-04-09 15:24:13 +0000

    Need to include Makefile.inc sooner.

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Fundamental; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.257 2000/04/09 15:05:52 espie Exp $$
+FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.258 2000/04/09 15:24:13 espie Exp $$
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -401,6 +401,10 @@ _REVISION_NEEDED=${NEED_VERSION:C/.*\.//}
 # NEVER override the "regular" targets unless you want to open
 # a major can of worms.
 
+.if exists(${.CURDIR}/../Makefile.inc)
+.include "${.CURDIR}/../Makefile.inc"
+.endif
+
 FAKE?=No
 .if ${FAKE:U} == "YES"
 WRKINST?=${WRKDIR}/fake-${ARCH}${_FEXT}
@@ -413,9 +417,6 @@ ARCH!=	uname -m
 OPSYS=	OpenBSD
 OPSYS_VER=	${OSREV}
 
-.if exists(${.CURDIR}/../Makefile.inc)
-.include "${.CURDIR}/../Makefile.inc"
-.endif
 
 # Define NO_SHARED_LIBS for those machines that don't support shared libraries.
 #
