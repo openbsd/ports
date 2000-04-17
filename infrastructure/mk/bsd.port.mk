999cccb78eb20e35ed0f58e02a2230f2fd90b209
espie <espie@openbsd.org>
2000-04-17 20:14:33 +0000

    buglet fix: let the port+flavor directory show up in a form suitable for direct SUBDIR reuse.

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Fundamental; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.265 2000/04/17 20:12:03 espie Exp $$
+FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.266 2000/04/17 20:14:33 espie Exp $$
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -2247,7 +2247,7 @@ clean-depends:
 describe:
 .if !defined(NO_DESCRIBE) 
 .  if !empty(FLAVOR)
-	@echo -n "${PKGNAME}|${.CURDIR:S,^${PORTSDIR}/,,}:${FLAVOR}|"
+	@echo -n "${PKGNAME}|${.CURDIR:S,^${PORTSDIR}/,,}${_FEXT:S/-/,/g}|"
 .  else
 	@echo -n "${PKGNAME}|${.CURDIR:S,^${PORTSDIR}/,,}|"
 .  endif
