e597cb37f06fda7a2ecf21d9f3a1bd6c2c4d16d4
espie <espie@openbsd.org>
2000-06-28 13:51:23 +0000

    Kill PLIST.sed Add @comment
    Simplify ruby build, now that bsd.port.mk has proper support
    for variable substitutions.

--- lang/python/pkg/PFRAG.mm
+++ lang/python/pkg/PFRAG.mm
@@ -1,3 +1,4 @@
+@comment $OpenBSD: PFRAG.mm,v 1.2 2000/06/28 13:51:24 espie Exp $
 lib/python1.5/lib-dynload/audioop.so
 lib/python1.5/lib-dynload/imageop.so
 lib/python1.5/lib-dynload/rgbimgmodule.so
