4bb4f21763cddd08e724861e7153585f37cc75af
brad <brad@openbsd.org>
2000-07-10 01:28:38 +0000

    upgrade to dopewars 1.4.8

--- games/dopewars/patches/patch-dopeos.h
+++ games/dopewars/patches/patch-dopeos.h
@@ -1,6 +1,6 @@
---- dopeos.h.orig	Sat Mar 18 14:44:57 2000
-+++ dopeos.h	Sat Mar 18 14:45:26 2000
-@@ -127,6 +127,7 @@
+--- dopeos.h.orig	Sun Jul  9 16:07:11 2000
++++ dopeos.h	Sun Jul  9 21:16:26 2000
+@@ -126,6 +126,7 @@
  #else /* Definitions for Unix build */
  
  #if NETWORKING
