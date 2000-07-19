4d89af4f753c8373ea24ed41cc526b9477f93a7b
naddy <naddy@openbsd.org>
2000-07-19 22:36:32 +0000

    fix "show" target to correctly display variables that contain shell metacharacters; ok espie@

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Fundamental; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.317 2000/07/17 07:11:50 espie Exp $$
+FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.318 2000/07/19 22:36:32 naddy Exp $$
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -2690,7 +2690,7 @@ fake-pkg: ${_PKG_PREREQ}
 .if defined(VARNAME)
 show:
 .  for _var in ${VARNAME}
-	@echo ${${_var}}
+	@echo ${${_var}:Q}
 .  endfor
 .endif
 # Depend is generally meaningless for arbitrary ports, but if someone wants
