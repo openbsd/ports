641cff9b9c647d58f5160b7ca3a383bf24f30178
matt <matt@openbsd.org>
2000-07-27 00:23:33 +0000

    - update to 1.0.3 - clean up Makefile and wrapper scripts some more - better INSTALL/DEINSTALL script - be able to compile w/ old nessus pkg installed (thx rdump@river.com) - make it think nmap is always present to avoid dependency (espie@)

---
+++ security/nessus/files/nmap.h
@@ -0,0 +1,14 @@
+/* Replace the configure-generated nmap.h so that we can convince
+ * nmap_wrapper.nes to build regardless of nmap's presence.
+ */
+
+#ifndef NMAP_H__
+#define NMAP_H__
+
+#define NMAP "/usr/local/bin/nmap"
+#define NMAP_VERSION "2.3"
+#define NMAP_MAJOR 2
+#define NMAP_MINOR 3
+#define NMAP_BETA 
+
+#endif
