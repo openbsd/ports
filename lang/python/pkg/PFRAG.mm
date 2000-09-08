2cbc80d7706a7c45a162066f76e361c60263b7e2
jasoni <jasoni@openbsd.org>
2000-09-08 07:55:54 +0000

    - Update to Python 1.6

--- lang/python/pkg/PFRAG.mm
+++ lang/python/pkg/PFRAG.mm
@@ -1,4 +1,4 @@
-@comment $OpenBSD: PFRAG.mm,v 1.2 2000/06/28 13:51:24 espie Exp $
-lib/python1.5/lib-dynload/audioop.so
-lib/python1.5/lib-dynload/imageop.so
-lib/python1.5/lib-dynload/rgbimgmodule.so
+@comment $OpenBSD: PFRAG.mm,v 1.3 2000/09/08 07:55:55 jasoni Exp $
+lib/python1.6/lib-dynload/audioop.so
+lib/python1.6/lib-dynload/imageop.so
+lib/python1.6/lib-dynload/rgbimgmodule.so
