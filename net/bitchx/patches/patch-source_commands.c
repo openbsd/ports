070f7ef6ed4f8b21681a78c6c89367debde7c9d0
brad <brad@openbsd.org>
2000-09-16 04:04:27 +0000

    upgrade to BitchX 1.0c17

--- net/bitchx/patches/patch-source_commands.c
+++ net/bitchx/patches/patch-source_commands.c
@@ -1,79 +1,62 @@
---- source/commands.c.orig	Wed Jan 19 13:01:51 2000
-+++ source/commands.c	Wed Jun 21 22:31:33 2000
-@@ -2595,13 +2595,20 @@
- 			return;
- 		}
- 		sprintf(comm, "%s -in >%s", p, filename);
--#else
-+#elif defined(__linux__)
- 		if (!(p = path_search("ifconfig", "/sbin:/usr/sbin:/bin:/usr/bin")))
- 		{
- 			yell("Can't find ifconfig");
- 			return;
- 		}
-                 sprintf(comm, "%s -a >%s", p, filename);
-+#else
-+		if (!(p = path_search("netstat", "/sbin:/usr/sbin:/bin:/usr/bin")))            
-+		{
-+			yell("No Netstat to be found");
-+			return;
-+		}
-+		sprintf(comm, "%s -in >%s", p, filename);
+--- source/commands.c.orig	Fri Sep 15 22:45:24 2000
++++ source/commands.c	Fri Sep 15 22:48:11 2000
+@@ -2603,13 +2603,13 @@
+ 		int i;
+ 		char *newhost = NULL;
+ #if !defined(__linux__) && !defined(__EMX__)
+-#if _BSDI_VERSION < 199701
++#if defined(_BSDI_VERSION) && _BSDI_VERSION < 199701 || defined(__OpenBSD__)
+ 		char device[80];
  #endif
- 		system(comm);
+ #endif
  
-@@ -2612,12 +2619,22 @@
+ 		tmpnam(filename);
+-#if defined(_BSDI_VERSION) && _BSDI_VERSION < 199701
++#if defined(_BSDI_VERSION) && _BSDI_VERSION < 199701 || defined(__OpenBSD__)
+ 		if (!(p = path_search("netstat", "/sbin:/usr/sbin:/bin:/usr/bin")))
+ 		{
+ 			yell("No Netstat to be found");
+@@ -2636,10 +2636,16 @@
+ 			unlink(filename);
+ 			return;
  		}
- #if defined(__linux__)
- 		bitchsay("Looking for hostnames on device eth0");
--#elif _BSDI_VERSION < 199701
-+#elif defined(_BSDI_VERSION) && _BSDI_VERSION < 199701
+-#if defined(_BSDI_VERSION) && _BSDI_VERSION < 199701
++#if defined(_BSDI_VERSION) && _BSDI_VERSION < 199701 || defined(__OpenBSD__)
  		fgets(comm, 200, fptr);
  		fgets(comm, 200, fptr);
  		p = next_arg(comm, &q);
- 		strncpy(device, p, 79);
- 		bitchsay("Looking for hostnames on device %s", device);
-+#else
-+		fgets(comm, 200, fptr);
-+		fgets(comm, 200, fptr);
-+		p = next_arg(comm, &q);
++#ifdef __OpenBSD__
 +		while ((*p == 'l') && (*(p+1) == 'o')) {
 +			fgets(comm, 200, fptr);
 +			p = next_arg(comm, &q);
 +		}
-+		strncpy(device, p , 79);
-+		bitchsay("Looking for hostnames on device %s", device);
- #endif
- 		while((fgets(comm, 200, fptr)))
- 		{
-@@ -2649,9 +2666,9 @@
- 				q = strchr(p, ' ');
- 				*q = 0;
- 				if ((p && !*p) || (p && !strcmp(p, "127.0.0.1"))) continue;
++#endif
+ 		strncpy(device, p, 79);
+ 		bitchsay("Looking for hostnames on device %s", device);
+ #else
+@@ -2654,7 +2660,7 @@
+ 			if (strstr(comm, "inet6 addr") || strstr(comm, "inet addr"))
+ #else
+ 			if (strstr(comm, "inet addr"))
 -#endif
 +#endif /* ifdef IPV6 */
+ 			{
+ 				/* inet addr:127.0.0.1  Mask:... */
+ 				/* inet6 addr: ::1/128 Scope:... */
+@@ -2671,11 +2677,15 @@
+ 				}
+ 				*q = 0;
  
--#elif _BSDI_VERSION < 199701
-+#elif defined(_BSDI_VERION) && _BSDI_VERSION < 199701
+-#elif defined(_BSDI_VERSION) && _BSDI_VERSION < 199701
++#elif defined(_BSDI_VERSION) && _BSDI_VERSION < 199701 || defined(__OpenBSD__)
  			if (!strncmp(comm, device, strlen(device)))
  			{
  				p = comm;
-@@ -2661,13 +2678,15 @@
- 				*q = 0;
- 				if ((p && !*p) || (p && !strcmp(p, "127.0.0.1"))) continue;
- #else
--			if ((p = strstr(comm, "inet")))
-+			if (!strncmp(comm, device, strlen(device)))
- 			{
--				p += 5;
-+				p = comm;
++#if defined(_BSDI_VERSION) && _BSDI_VERSION < 199701
+ 				p += 24;
++#elif defined(__OpenBSD__)
 +				p += strlen("Name    Mtu   Network     ")-1;
-+				while (*p && *p == ' ') p++;
++#endif
+ 				while (*p && *p == ' ') p++;
  				q = strchr(p, ' ');
  				*q = 0;
- 				if ((p && !*p) || (p && !strcmp(p, "127.0.0.1"))) continue;
--#endif
-+#endif /* if defined(__linux__) */
- 
- #ifdef IPV6
- 				{
