feec3659ceec338bbfc4373b35c273fb0fa15fd9
brad <brad@openbsd.org>
2000-09-28 17:29:47 +0000

    upgrade to Curl 7.3

---
+++ net/curl/patches/patch-lib_hostip.c
@@ -0,0 +1,10 @@
+--- lib/hostip.c.orig	Thu Sep 28 12:41:36 2000
++++ lib/hostip.c	Thu Sep 28 12:41:43 2000
+@@ -41,7 +41,6 @@
+ #include "setup.h"
+ 
+ #include <string.h>
+-#include <malloc.h>
+ #include <errno.h>
+ 
+ #define _REENTRANT
---
+++ net/curl/patches/patch-lib_krb4.c
@@ -0,0 +1,11 @@
+--- lib/krb4.c.orig	Thu Sep 28 12:52:39 2000
++++ lib/krb4.c	Thu Sep 28 12:52:52 2000
+@@ -40,7 +40,7 @@
+ #ifdef KRB4
+ 
+ #include "security.h"
+-#include "base64_krb.h"
++#include "base64.h"
+ #include <stdlib.h>
+ #include <netdb.h>
+ #include <syslog.h>
