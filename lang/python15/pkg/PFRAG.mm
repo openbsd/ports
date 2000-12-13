4d66b3c7481dd85ae75f54a53b58ad6da37b8864
jasoni <jasoni@openbsd.org>
2000-12-13 05:09:46 +0000

    Import Python 1.5.2.

---
+++ lang/python15/pkg/PFRAG.mm
@@ -0,0 +1,4 @@
+@comment $OpenBSD: PFRAG.mm,v 1.1.1.1 2000/12/13 05:09:48 jasoni Exp $
+lib/python1.5/lib-dynload/audioop.so
+lib/python1.5/lib-dynload/imageop.so
+lib/python1.5/lib-dynload/rgbimgmodule.so
