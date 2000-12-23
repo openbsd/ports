c754691700553322bca5ba22e47efdb8181fdb0c
espie <espie@openbsd.org>
2000-12-23 12:25:57 +0000

    take the old non-fake code out of line, for greater clarity (and since it's so seldom used now).
    Remove a few targets that are not really needed (mirror-distfiles, use
    mirror-maker instead).
    
    More changes to fake to come, once they've been properly tested.

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Fundamental; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.346 2000/12/16 15:49:12 espie Exp $$
+FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.347 2000/12/23 12:25:57 espie Exp $$
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -160,7 +160,6 @@ _REVISION_NEEDED=${NEED_VERSION:C/.*\.//}
 #
 # fetch			- Retrieves ${DISTFILES} (and ${PATCHFILES} if defined, and
 #                 ${SUPDISTFILES} if needed) into ${DISTDIR} as necessary.
-# fetch-list	- Show list of files that would be retrieved by fetch
 # extract		- Unpacks ${EXTRACT_ONLY} (${DISTFILES} by default)
 #                 into ${WRKDIR}.
 # patch			- Apply any provided patches to the source.
@@ -178,13 +177,6 @@ _REVISION_NEEDED=${NEED_VERSION:C/.*\.//}
 # There is also a special pre-fake target that gets run after mtree but
 # before making the INSTALL_PRECOOKIE, to finish setting up the fake tree.
 #
-# If ${FAKE} == No
-# install		- Install the results of a build.
-# reinstall		- Install the results of a build, ignoring "already installed"
-#				  flag.
-# deinstall		- Remove the installation.  Alias: uninstall
-# package		- Create a package from an _installed_ port.
-#
 # plist			- create a file suitable for use as a packing list.  This
 #				  is for port maintainers.
 # describe		- Try to generate a one-line description for each port for
@@ -197,10 +189,6 @@ _REVISION_NEEDED=${NEED_VERSION:C/.*\.//}
 # addsum		- update ${CHECKSUM_FILE} in a non-destructive way 
 #				  (your own ports only!)
 # readme		- Create a README.html file describing the category or package
-# mirror-distfiles	- Mirror the distfile(s) if they are freely redistributable
-#				  Setting MIRROR_DISTFILE to "No" in the package Makefile
-#				  will override the default "Yes", and the distfile will
-#				  not be fetched.
 # list-distfiles- list the distribution and patch files used by a port.
 #				  Typical use is (from the top level of the ports tree)
 #				  make ECHO_MSG=: list-distfiles | tee some-file
@@ -408,9 +396,6 @@ MAKE_ENV+=		PATH='${PORTPATH}' PREFIX='${PREFIX}' \
 	TRUEPREFIX='${PREFIX}' DESTDIR=''
 
 FETCH_CMD?=		/usr/bin/ftp
-
-# By default, distfiles have no restrictions placed on them
-MIRROR_DISTFILE?=	Yes
 
 DISTORIG?=	.bak.orig
 PATCH?=			/usr/bin/patch
@@ -1474,91 +1459,14 @@ ${_INSTALL_COOKIE}:  ${_PACKAGE_COOKIE}
 	@cd ${.CURDIR} && DEPENDS_TARGET=package exec ${MAKE} run-depends lib-depends
 	@${ECHO_MSG} "===>  Installing ${PKGNAME} from ${PKGFILE}"
 # Kludge
-.if ${CONFIGURE_STYLE:Mimake}
+.  if ${CONFIGURE_STYLE:Mimake}
 	@${SUDO} mkdir -p /usr/local/lib/X11
 	@if [ ! -e /usr/local/lib/X11/app-defaults ]; then \
 		${SUDO} ln -sf /var/X11/app-defaults /usr/local/lib/X11/app-defaults; \
 	fi
-.endif
+.  endif
 	@${SUDO} ${SETENV} PKG_PATH=${PKGREPOSITORY}:${PKG_PATH} pkg_add ${PKGFILE}
 	@${SUDO} ${_MAKE_COOKIE} ${_INSTALL_COOKIE}
-
-.else
-
-${_FAKE_COOKIE}: ${_BUILD_COOKIE}
-	@echo 1>&2 "*** ${PKGNAME} does not use fake installation yet"
-
-# The real install, old version
-${_INSTALL_COOKIE}: ${_BUILD_COOKIE} 
-	@cd ${.CURDIR} && exec ${MAKE} run-depends lib-depends 
-.  if !defined(NO_INSTALL)
-	@${ECHO_MSG} "===>  Installing for ${PKGNAME}"
-# Kludge
-.    if ${CONFIGURE_STYLE:Mimake}
-	@mkdir -p /usr/local/lib/X11
-	@if [ ! -e /usr/local/lib/X11/app-defaults ]; then \
-		ln -sf /var/X11/app-defaults /usr/local/lib/X11/app-defaults; \
-	fi
-.    endif
-.    if !defined(NO_PKG_REGISTER) && !defined(FORCE_PKG_REGISTER)
-	@if [ -d ${PKG_DBDIR}/${PKGNAME} -o "X$$(ls -d ${PKG_DBDIR}/${PKGNAME:C/-[0-9].*//g}-* 2> /dev/null)" != "X" ]; then \
-		echo "===>  ${PKGNAME} is already installed - perhaps an older version?"; \
-		echo "      If so, you may wish to \`\`make deinstall'' and install"; \
-		echo "      this port again by \`\`make reinstall'' to upgrade it properly."; \
-		echo "      If you really wish to overwrite the old port of ${PKGNAME}"; \
-		echo "      without deleting it first, set the variable \"FORCE_PKG_REGISTER\""; \
-		echo "      in your environment or the \"make install\" command line."; \
-		exit 1; \
-	fi
-.    endif
-	@if [ `${SH} -c umask` != ${DEF_UMASK} ]; then \
-		${ECHO_MSG} "===>  Warning: your umask is \"`${SH} -c umask`"\".; \
-		${ECHO_MSG} "      If this is not desired, set it to an appropriate value"; \
-		${ECHO_MSG} "      and install this port again by \`\`make reinstall''."; \
-	fi
-	@${_MAKE_COOKIE} ${_INSTALL_PRE_COOKIE}
-.    if target(pre-install)
-	@cd ${.CURDIR} && exec ${MAKE} pre-install
-.    endif
-.    if target(do-install)
-	@cd ${.CURDIR} && exec ${MAKE} do-install
-.    else
-# What INSTALL normally does:
-	@cd ${WRKBUILD} && ${SETENV} ${MAKE_ENV} ${MAKE_PROGRAM} ${MAKE_FLAGS} -f ${MAKE_FILE} ${INSTALL_TARGET}
-# End of INSTALL.
-.    endif
-.    if target(post-install)
-	@cd ${.CURDIR} && exec ${MAKE} post-install
-.    endif
-.    if defined(_MANPAGES) || defined(_CATPAGES)
-.      if defined(MANCOMPRESSED) && defined(NOMANCOMPRESS)
-	@${ECHO_MSG} "===>   Uncompressing manual pages for ${PKGNAME}"
-.        for manpage in ${_MANPAGES} ${_CATPAGES}
-	@${GUNZIP_CMD} ${manpage}.gz
-.        endfor
-.      elif !defined(MANCOMPRESSED) && !defined(NOMANCOMPRESS)
-	@${ECHO_MSG} "===>   Compressing manual pages for ${PKGNAME}"
-.        for manpage in ${_MANPAGES} ${_CATPAGES}
-	@if [ -L ${manpage} ]; then \
-		set - `file ${manpage}`; \
-		shift `expr $$# - 1`; \
-		ln -sf $${1}.gz ${manpage}.gz; \
-		rm ${manpage}; \
-	else \
-		${GZIP_CMD} ${manpage}; \
-	fi
-.          endfor
-.      endif
-.    endif
-.    if defined(${MESSAGE})
-	@cat	${WRKBUILD}/MESSAGE${SUBPACKAGE}
-.    endif
-.    if !defined(NO_PKG_REGISTER)
-	@cd ${.CURDIR} && exec ${MAKE} fake-pkg
-.    endif
-.  endif
-	@${_MAKE_COOKIE} ${_INSTALL_COOKIE}
-
 .endif 
 
 _SUBPACKAGE_COOKIES=
@@ -1648,14 +1556,6 @@ ${_F}:
 		fi; \
 	done; exit 1
 .endfor
-
-# This is for the use of sites which store distfiles which others may
-# fetch - only fetch the distfile if it is allowed to be
-# re-distributed freely
-mirror-distfiles:
-.if (${MIRROR_DISTFILE:L} == "yes")
-	@cd ${.CURDIR} && exec ${MAKE} __FETCH_ALL=Yes __ARCH_OK=Yes NO_IGNORE=Yes NO_WARNINGS=Yes fetch
-.endif
 
 all-packages: ${PKGFILE}
 .if defined(MULTI_PACKAGES) && empty(SUBPACKAGE)
@@ -1840,43 +1740,7 @@ distclean: pre-distclean clean
 .  endif
 .endif
 
-# Prints out a list of files to fetch (useful to do a batch fetch)
-
 RECURSIVE_FETCH_LIST?=	Yes
-
-.if !target(fetch-list)
-fetch-list:
-	@cd ${.CURDIR} && ${MAKE} fetch-list-recursive RECURSIVE_FETCH_LIST=${RECURSIVE_FETCH_LIST} | sort -u
-.endif # !target(fetch-list)
-
-.if !target(fetch-list-recursive)
-fetch-list-recursive:
-	@cd ${.CURDIR} && exec ${MAKE} fetch-list-one-pkg 
-.  if ${RECURSIVE_FETCH_LIST:L} != "no"
-	@for dir in `echo ${_ALWAYS_DEP} ${_BUILD_DEP} ${_RUN_DEP} \
-	| tr '\040' '\012' | sort -u`; do \
-		cd ${PORTSDIR} && cd $$dir && PKGPATH="$$dir" ${MAKE} fetch-list-recursive; \
-	done
-.  endif # ${RECURSIVE_FETCH_LIST} != "NO"
-.endif # !target(fetch-list-recursive)
-
-.if !target(fetch-list-one-pkg)
-fetch-list-one-pkg:
-	@mkdir -p ${FULLDISTDIR}
-	@[ -z "${FULLDISTDIR}" ] || echo "mkdir -p ${FULLDISTDIR}"
-	@cd ${FULLDISTDIR}; \
-	 for select in ${_EVERYTHING}; do \
-	 	file=`echo $$select|sed -e 's,:[0-9]$$,,'`; \
-		if [ ! -f $$file -a ! -f `basename $$file` ]; then \
-			echo -n "cd ${FULLDISTDIR} && [ -f $$file -o -f `basename $$file` ] || " ; \
-			${_SITE_SELECTOR}; \
-			for site in $$sites ; do \
-				echo -n ${FETCH_CMD} ${FETCH_BEFORE_ARGS} $${site}$${file} "${FETCH_AFTER_ARGS}" '|| ' ; \
-			done; \
-			echo "echo $${file} not fetched" ; \
-		fi \
-	done
-.endif # !target(fetch-list-one-pkg)
 
 # packing list utilities.  This generates a packing list from a recently
 # installed port.  Not perfect, but pretty close.  The generated file
@@ -1888,19 +1752,6 @@ fetch-list-one-pkg:
 plist: fake
 	@DESTDIR=${WRKINST} PREFIX=${WRKINST}${PREFIX} LDCONFIG="${LDCONFIG}" \
 	MTREE_FILE=${PORTSDIR}/infrastructure/db/fake.mtree \
-	INSTALL_PRE_COOKIE=${_INSTALL_PRE_COOKIE} \
-	perl ${PORTSDIR}/infrastructure/install/make-plist ${PLIST}
-.else
-
-# Figure out where the local mtree file is
-.  if ${PREFIX} == "/usr/local"
-MTREE_FILE?=	/etc/mtree/BSD.local.dist
-.  else
-MTREE_FILE?=	/etc/mtree/BSD.x11.dist
-.  endif
-
-plist: install
-	@DESTDIR=${PREFIX} PREFIX=${PREFIX} LDCONFIG="${LDCONFIG}" MTREE_FILE=${MTREE_FILE} \
 	INSTALL_PRE_COOKIE=${_INSTALL_PRE_COOKIE} \
 	perl ${PORTSDIR}/infrastructure/install/make-plist ${PLIST}
 .endif
@@ -2463,49 +2314,6 @@ print-depends:
 	@cd ${.CURDIR} && exec ${MAKE} FULL_PACKAGE_NAME=Yes print-depends-list print-package-depends
 .endif
 
-# Fake installation of package so that user can pkg_delete it later.
-# Also, make sure that an installed port is recognized correctly in
-# accordance to the @pkgdep directive in the packing lists
-
-.if !target(fake-pkg)
-fake-pkg: ${_PKG_PREREQ}
-	@if [ `/bin/ls -l ${COMMENT} | awk '{print $$5}'` -gt 60 ]; then \
-	    echo "** ${COMMENT} too large - installation not recorded."; \
-	    exit 1; \
-	 fi
-	@if [ ! -d ${PKG_DBDIR} ]; then rm -f ${PKG_DBDIR}; mkdir -p ${PKG_DBDIR}; fi
-.  if defined(FORCE_PKG_REGISTER)
-	@rm -rf ${PKG_DBDIR}/${PKGNAME}
-.  endif
-	@if [ ! -d ${PKG_DBDIR}/${PKGNAME} ]; then \
-		${ECHO_MSG} "===>  Registering installation for ${PKGNAME}"; \
-		mkdir -p ${PKG_DBDIR}/${PKGNAME}; \
-		${PKG_CMD} ${PKG_ARGS} -O ${PKGFILE} > ${PKG_DBDIR}/${PKGNAME}/+CONTENTS; \
-		cp ${WRKPKG}/DESCR${SUBPACKAGE} ${PKG_DBDIR}/${PKGNAME}/+DESC; \
-		cp ${COMMENT} ${PKG_DBDIR}/${PKGNAME}/+COMMENT; \
-		if [ -f ${WRKPKG}/INSTALL${SUBPACKAGE} ]; then \
-			cp ${WRKPKG}/INSTALL${SUBPACKAGE} ${PKG_DBDIR}/${PKGNAME}/+INSTALL; \
-		fi; \
-		if [ -f ${WRKPKG}/DEINSTALL${SUBPACKAGE} ]; then \
-			cp ${WRKPKG}/DEINSTALL${SUBPACKAGE} ${PKG_DBDIR}/${PKGNAME}/+DEINSTALL; \
-		fi; \
-		if [ -f ${WRKPKG}/REQ${SUBPACKAGE} ]; then \
-			cp ${WRKPKG}}/REQ${SUBPACKAGE} ${PKG_DBDIR}/${PKGNAME}/+REQ; \
-		fi; \
-		if [ -f ${WRKPKG}/MESSAGE${SUBPACKAGE} ]; then \
-			cp ${WRKPKG}/MESSAGE${SUBPACKAGE} ${PKG_DBDIR}/${PKGNAME}/+DISPLAY; \
-		fi; \
-		for dep in `cd ${.CURDIR} && ${MAKE} package-depends ECHO_MSG=true | ${_SORT_DEPENDS}`; do \
-			if [ -d ${PKG_DBDIR}/$$dep ]; then \
-				if ! grep ^${PKGNAME}$$ ${PKG_DBDIR}/$$dep/+REQUIRED_BY \
-					>/dev/null 2>&1; then \
-					echo ${PKGNAME} >> ${PKG_DBDIR}/$$dep/+REQUIRED_BY; \
-				fi; \
-			fi; \
-		done; \
-	fi
-.endif
-
 .if defined(VARNAME)
 show:
 	@echo ${${VARNAME}:Q}
@@ -2546,14 +2354,17 @@ unlink-categories:
 	fi
 .endfor
     
+.if ${FAKE:U} == "NO"
+.  include "${PORTSDIR}/infrastructure/mk/old-install.mk"
+.endif
+
 .PHONY: \
    addsum all build build-depends checkpatch \
    checksum clean clean-depends configure deinstall \
    delete-package delete-package-links depend depends depends-list \
    describe distclean do-build do-configure do-extract \
    do-fetch do-install do-package do-patch extract list-distfiles \
-   fake-pkg fetch fetch-depends fetch-list fetch-list-one-pkg \
-   fetch-list-recursive install lib-depends makesum mirror-distfiles \
+   fetch fetch-depends install lib-depends makesum \
    cdrom-packages ftp-packages \
    misc-depends package package-depends package-links package-name \
    package-noinstall patch plist update-patches post-build \
---
+++ infrastructure/mk/old-install.mk
@@ -0,0 +1,139 @@
+# $OpenBSD: old-install.mk,v 1.1 2000/12/23 12:25:58 espie Exp $
+# Stuff that is needed for old, pre-fake, port installations.
+
+# If ${FAKE} == No
+# install		- Install the results of a build.
+# reinstall		- Install the results of a build, ignoring "already installed"
+#				  flag.
+# deinstall		- Remove the installation.  Alias: uninstall
+# package		- Create a package from an _installed_ port.
+
+
+${_FAKE_COOKIE}: ${_BUILD_COOKIE}
+	@echo 1>&2 "*** ${PKGNAME} does not use fake installation yet"
+
+# The real install, old version
+${_INSTALL_COOKIE}: ${_BUILD_COOKIE} 
+	@cd ${.CURDIR} && exec ${MAKE} run-depends lib-depends 
+.if !defined(NO_INSTALL)
+	@${ECHO_MSG} "===>  Installing for ${PKGNAME}"
+# Kludge
+.  if ${CONFIGURE_STYLE:Mimake}
+	@mkdir -p /usr/local/lib/X11
+	@if [ ! -e /usr/local/lib/X11/app-defaults ]; then \
+		ln -sf /var/X11/app-defaults /usr/local/lib/X11/app-defaults; \
+	fi
+.  endif
+.  if !defined(NO_PKG_REGISTER) && !defined(FORCE_PKG_REGISTER)
+	@if [ -d ${PKG_DBDIR}/${PKGNAME} -o "X$$(ls -d ${PKG_DBDIR}/${PKGNAME:C/-[0-9].*//g}-* 2> /dev/null)" != "X" ]; then \
+		echo "===>  ${PKGNAME} is already installed - perhaps an older version?"; \
+		echo "      If so, you may wish to \`\`make deinstall'' and install"; \
+		echo "      this port again by \`\`make reinstall'' to upgrade it properly."; \
+		echo "      If you really wish to overwrite the old port of ${PKGNAME}"; \
+		echo "      without deleting it first, set the variable \"FORCE_PKG_REGISTER\""; \
+		echo "      in your environment or the \"make install\" command line."; \
+		exit 1; \
+	fi
+.  endif
+	@if [ `${SH} -c umask` != ${DEF_UMASK} ]; then \
+		${ECHO_MSG} "===>  Warning: your umask is \"`${SH} -c umask`"\".; \
+		${ECHO_MSG} "      If this is not desired, set it to an appropriate value"; \
+		${ECHO_MSG} "      and install this port again by \`\`make reinstall''."; \
+	fi
+	@${_MAKE_COOKIE} ${_INSTALL_PRE_COOKIE}
+.  if target(pre-install)
+	@cd ${.CURDIR} && exec ${MAKE} pre-install
+.  endif
+.  if target(do-install)
+	@cd ${.CURDIR} && exec ${MAKE} do-install
+.  else
+# What INSTALL normally does:
+	@cd ${WRKBUILD} && ${SETENV} ${MAKE_ENV} ${MAKE_PROGRAM} ${MAKE_FLAGS} -f ${MAKE_FILE} ${INSTALL_TARGET}
+# End of INSTALL.
+.  endif
+.  if target(post-install)
+	@cd ${.CURDIR} && exec ${MAKE} post-install
+.  endif
+.  if defined(_MANPAGES) || defined(_CATPAGES)
+.    if defined(MANCOMPRESSED) && defined(NOMANCOMPRESS)
+	@${ECHO_MSG} "===>   Uncompressing manual pages for ${PKGNAME}"
+.      for manpage in ${_MANPAGES} ${_CATPAGES}
+	@${GUNZIP_CMD} ${manpage}.gz
+.      endfor
+.    elif !defined(MANCOMPRESSED) && !defined(NOMANCOMPRESS)
+	@${ECHO_MSG} "===>   Compressing manual pages for ${PKGNAME}"
+.      for manpage in ${_MANPAGES} ${_CATPAGES}
+	@if [ -L ${manpage} ]; then \
+		set - `file ${manpage}`; \
+		shift `expr $$# - 1`; \
+		ln -sf $${1}.gz ${manpage}.gz; \
+		rm ${manpage}; \
+	else \
+		${GZIP_CMD} ${manpage}; \
+	fi
+.        endfor
+.    endif
+.  endif
+.  if defined(${MESSAGE})
+	@cat	${WRKBUILD}/MESSAGE${SUBPACKAGE}
+.  endif
+.  if !defined(NO_PKG_REGISTER)
+	@cd ${.CURDIR} && exec ${MAKE} fake-pkg
+.  endif
+.endif
+	@${_MAKE_COOKIE} ${_INSTALL_COOKIE}
+
+# Figure out where the local mtree file is
+.if ${PREFIX} == "/usr/local"
+MTREE_FILE?=	/etc/mtree/BSD.local.dist
+.else
+MTREE_FILE?=	/etc/mtree/BSD.x11.dist
+.endif
+
+plist: install
+	@DESTDIR=${PREFIX} PREFIX=${PREFIX} LDCONFIG="${LDCONFIG}" MTREE_FILE=${MTREE_FILE} \
+	INSTALL_PRE_COOKIE=${_INSTALL_PRE_COOKIE} \
+	perl ${PORTSDIR}/infrastructure/install/make-plist ${PLIST}
+
+# Fake installation of package so that user can pkg_delete it later.
+# Also, make sure that an installed port is recognized correctly in
+# accordance to the @pkgdep directive in the packing lists
+
+fake-pkg: ${_PKG_PREREQ}
+	@if [ `/bin/ls -l ${COMMENT} | awk '{print $$5}'` -gt 60 ]; then \
+	    echo "** ${COMMENT} too large - installation not recorded."; \
+	    exit 1; \
+	 fi
+	@if [ ! -d ${PKG_DBDIR} ]; then rm -f ${PKG_DBDIR}; mkdir -p ${PKG_DBDIR}; fi
+.if defined(FORCE_PKG_REGISTER)
+	@rm -rf ${PKG_DBDIR}/${PKGNAME}
+.endif
+	@if [ ! -d ${PKG_DBDIR}/${PKGNAME} ]; then \
+		${ECHO_MSG} "===>  Registering installation for ${PKGNAME}"; \
+		mkdir -p ${PKG_DBDIR}/${PKGNAME}; \
+		${PKG_CMD} ${PKG_ARGS} -O ${PKGFILE} > ${PKG_DBDIR}/${PKGNAME}/+CONTENTS; \
+		cp ${WRKPKG}/DESCR${SUBPACKAGE} ${PKG_DBDIR}/${PKGNAME}/+DESC; \
+		cp ${COMMENT} ${PKG_DBDIR}/${PKGNAME}/+COMMENT; \
+		if [ -f ${WRKPKG}/INSTALL${SUBPACKAGE} ]; then \
+			cp ${WRKPKG}/INSTALL${SUBPACKAGE} ${PKG_DBDIR}/${PKGNAME}/+INSTALL; \
+		fi; \
+		if [ -f ${WRKPKG}/DEINSTALL${SUBPACKAGE} ]; then \
+			cp ${WRKPKG}/DEINSTALL${SUBPACKAGE} ${PKG_DBDIR}/${PKGNAME}/+DEINSTALL; \
+		fi; \
+		if [ -f ${WRKPKG}/REQ${SUBPACKAGE} ]; then \
+			cp ${WRKPKG}}/REQ${SUBPACKAGE} ${PKG_DBDIR}/${PKGNAME}/+REQ; \
+		fi; \
+		if [ -f ${WRKPKG}/MESSAGE${SUBPACKAGE} ]; then \
+			cp ${WRKPKG}/MESSAGE${SUBPACKAGE} ${PKG_DBDIR}/${PKGNAME}/+DISPLAY; \
+		fi; \
+		for dep in `cd ${.CURDIR} && ${MAKE} package-depends ECHO_MSG=true | ${_SORT_DEPENDS}`; do \
+			if [ -d ${PKG_DBDIR}/$$dep ]; then \
+				if ! grep ^${PKGNAME}$$ ${PKG_DBDIR}/$$dep/+REQUIRED_BY \
+					>/dev/null 2>&1; then \
+					echo ${PKGNAME} >> ${PKG_DBDIR}/$$dep/+REQUIRED_BY; \
+				fi; \
+			fi; \
+		done; \
+	fi
+
+.PHONY: fake-pkg
