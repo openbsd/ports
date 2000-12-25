25701db117ffcf4aa0ff0f31e6ec66dd928b80ce
brad <brad@openbsd.org>
2000-12-25 03:53:36 +0000

    - fixup Makefile - add libtool patches - stop splitting info page - don't start COMMENT with a capital letter when it's not necessary

---
+++ devel/gmp/patches/patch-ltmain.sh
@@ -0,0 +1,53 @@
+--- ltmain.sh.orig	Mon Mar 20 07:45:01 2000
++++ ltmain.sh	Sun Dec 24 21:04:44 2000
+@@ -1031,6 +1031,9 @@
+ 	  $echo "$modename: warning: assuming \`-no-fast-install' instead" 1>&2
+ 	  fast_install=no
+ 	  ;;
++	*-*-openbsd*)
++	  # do not include libc due to us having libc/libc_r.
++	  ;;
+ 	*)
+ 	  no_install=yes
+ 	  ;;
+@@ -4200,40 +4203,6 @@
+     # Exit here if they wanted silent mode.
+     test "$show" = : && exit 0
+ 
+-    echo "----------------------------------------------------------------------"
+-    echo "Libraries have been installed in:"
+-    for libdir in $libdirs; do
+-      echo "   $libdir"
+-    done
+-    echo
+-    echo "If you ever happen to want to link against installed libraries"
+-    echo "in a given directory, LIBDIR, you must either use libtool, and"
+-    echo "specify the full pathname of the library, or use \`-LLIBDIR'"
+-    echo "flag during linking and do at least one of the following:"
+-    if test -n "$shlibpath_var"; then
+-      echo "   - add LIBDIR to the \`$shlibpath_var' environment variable"
+-      echo "     during execution"
+-    fi
+-    if test -n "$runpath_var"; then
+-      echo "   - add LIBDIR to the \`$runpath_var' environment variable"
+-      echo "     during linking"
+-    fi
+-    if test -n "$hardcode_libdir_flag_spec"; then
+-      libdir=LIBDIR
+-      eval flag=\"$hardcode_libdir_flag_spec\"
+-
+-      echo "   - use the \`$flag' linker flag"
+-    fi
+-    if test -n "$admincmds"; then
+-      echo "   - have your system administrator run these commands:$admincmds"
+-    fi
+-    if test -f /etc/ld.so.conf; then
+-      echo "   - have your system administrator add LIBDIR to \`/etc/ld.so.conf'"
+-    fi
+-    echo
+-    echo "See any operating system documentation about shared libraries for"
+-    echo "more information, such as the ld(1) and ld.so(8) manual pages."
+-    echo "----------------------------------------------------------------------"
+     exit 0
+     ;;
+ 
