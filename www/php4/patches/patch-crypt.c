93d5cc5ae56b22b19aa3bce34d38fa260b882d16
avsm <avsm@openbsd.org>
2000-12-26 23:35:43 +0000

    - update to php-4.0.4 - bump NEED_VERSION - no longer need extra distfile number4.tar.gz since it has   been integrated into the main distribution - ltconfig, mysql socket patches are in main distribution now,   so they are removed.  Note that the ltconfig patch was only   applied to the 4_0_4 branch by the PHP team, so we will have   to resubmit it for the next version, unless libtool-cvs has   been updated with our information. - Since php3/4 conflict with each other anyway, versioning is   not needed.
    ok jakob@

--- www/php4/patches/patch-crypt.c
+++ www/php4/patches/patch-crypt.c
@@ -1,3 +1,4 @@
+$OpenBSD: patch-crypt.c,v 1.2 2000/12/26 23:35:43 avsm Exp $
 --- ext/standard/crypt.c.orig      Sat Jul 29 16:10:54 2000
 +++ ext/standard/crypt.c   Thu Jul 27 23:12:56 2000
 @@ -71,7 +71,7 @@
