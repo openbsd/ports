f16547aeb14975d312264e91edcc75f5530ac0a4
avsm <avsm@openbsd.org>
2001-01-07 21:08:11 +0000

    Add a couple of patches from venaas@php.net to allow PHP-4.0.4 to work with OpenSSL 0.9.5a, instead of requiring only OpenSSL 0.9.6 These patches detect the revelant version and use the additional return values in 0.9.6 only if present.
    This also unbreaks the SNMP flavor on 2.8-stable, which requires SSL
    support, since our UCD-SNMP daemon has OpenSSL compiled into it
    (wierd, but thats how the PHP snmp-config.m4 works)
    
    - Add OpenSSL patches
    - Add note crypt.c patch that its now in 4.0.5-dev and can be removed soon
    - Add --with-openssl to the standard set of configure options

--- www/php4/patches/patch-crypt.c
+++ www/php4/patches/patch-crypt.c
@@ -1,7 +1,11 @@
-$OpenBSD: patch-crypt.c,v 1.2 2000/12/26 23:35:43 avsm Exp $
---- ext/standard/crypt.c.orig      Sat Jul 29 16:10:54 2000
-+++ ext/standard/crypt.c   Thu Jul 27 23:12:56 2000
-@@ -71,7 +71,7 @@
+$OpenBSD: patch-crypt.c,v 1.3 2001/01/07 21:08:12 avsm Exp $
+
+NOTE: This patch has been added to php-4.0.5-dev and can be
+removed in the next release.
+
+--- ext/standard/crypt.c.orig	Mon Nov 27 13:31:21 2000
++++ ext/standard/crypt.c	Sun Jan  7 18:31:47 2001
+@@ -71,7 +71,7 @@ extern char *crypt(char *__key,char *__s
  
  #if PHP_BLOWFISH_CRYPT
  #undef PHP_MAX_SALT_LEN
@@ -10,5 +14,3 @@ $OpenBSD: patch-crypt.c,v 1.2 2000/12/26 23:35:43 avsm Exp $
  #endif
  
   /*
-
-
