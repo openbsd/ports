a6159b295028edb35cf9d461863052472b219049
brad <brad@openbsd.org>
2001-01-22 17:15:28 +0000

    - add ONLY_FOR_ARCHS and list archs that support shared libraries and dynamic loading. - patch autoconf so dante does not compile with debugging symbols - add libtool patches

---
+++ security/dante/patches/patch-ltmain.sh
@@ -0,0 +1,53 @@
+--- ltmain.sh.orig	Thu Jul  8 14:17:30 1999
++++ ltmain.sh	Mon Jan 22 11:21:59 2001
+@@ -1798,6 +1798,9 @@
+ 	*-*-cygwin* | *-*-mingw* | *-*-os2* | *-*-beos*)
+ 	  # these systems don't actually have a c library (as such)!
+ 	  ;;
++	*-*-openbsd*)
++	  # do not include libc due to us having libc/libc_r.
++	  ;;
+ 	*)
+ 	  # Add libc to deplibs on all other systems.
+ 	  deplibs="$deplibs -lc"
+@@ -3531,40 +3534,6 @@
+     # Exit here if they wanted silent mode.
+     test "$show" = : && exit 0
+ 
+-    echo "----------------------------------------------------------------------"
+-    echo "Libraries have been installed in:"
+-    for libdir in $libdirs; do
+-      echo "   $libdir"
+-    done
+-    echo
+-    echo "If you ever happen to want to link against installed libraries"
+-    echo "in a given directory, LIBDIR, you must either use libtool, and"
+-    echo "specify the full pathname of the library, or use \`-LLIBDIR'"
+-    echo "flag during linking and do at least one of the following:"
+-    if test -n "$shlibpath_var"; then
+-      echo "   - add LIBDIR to the \`$shlibpath_var' environment variable"
+-      echo "     during execution"
+-    fi
+-    if test -n "$runpath_var"; then
+-      echo "   - add LIBDIR to the \`$runpath_var' environment variable"
+-      echo "     during linking"
+-    fi
+-    if test -n "$hardcode_libdir_flag_spec"; then
+-      libdir=LIBDIR
+-      eval flag=\"$hardcode_libdir_flag_spec\"
+-
+-      echo "   - use the \`$flag' linker flag"
+-    fi
+-    if test -n "$admincmds"; then
+-      echo "   - have your system administrator run these commands:$admincmds"
+-    fi
+-    if test -f /etc/ld.so.conf; then
+-      echo "   - have your system administrator add LIBDIR to \`/etc/ld.so.conf'"
+-    fi
+-    echo
+-    echo "See any operating system documentation about shared libraries for"
+-    echo "more information, such as the ld(1) and ld.so(8) manual pages."
+-    echo "----------------------------------------------------------------------"
+     exit 0
+     ;;
+ 
