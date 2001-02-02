44d36071ecb0571f918752bde41920d5037bf521
jakob <jakob@openbsd.org>
2001-02-02 13:04:29 +0000

    update checksum and MASTER_SITES, fix makefile and patch

---
+++ textproc/xml2rfc/patches/patch-xml2rfc.tcl
@@ -0,0 +1,10 @@
+--- xml2rfc.tcl.orig	Sat Oct 21 05:18:14 2000
++++ xml2rfc.tcl	Fri Feb  2 14:01:25 2001
+@@ -1,6 +1,4 @@
+-#!/bin/sh
+-# the next line restarts using wish \
+-exec wish8.0 "$0" "$0" "$@"
++#!/usr/local/bin/wish8.3
+ 
+ 
+ #
