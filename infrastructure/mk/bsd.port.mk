74d58c4e8004bb9154daca1394b83fff6630ae71
espie <espie@openbsd.org>
2001-02-10 13:47:54 +0000

    Fix DEF_UMASK check, should take ${SUDO} into account. This ought to please naddy and avsm...

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Fundamental; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.353 2001/02/03 01:14:52 miod Exp $$
+FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.354 2001/02/10 13:47:54 espie Exp $$
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -1379,7 +1379,7 @@ _FAKE_SETUP=TRUEPREFIX=${PREFIX} PREFIX=${WRKINST}${PREFIX} DESTDIR=${WRKINST}
 .if ${FAKE:L} == "yes"
 ${_FAKE_COOKIE}: ${_BUILD_COOKIE} 
 	@${ECHO_MSG} "===>  Faking installation for ${PKGNAME}"
-	@if [ `${SH} -c umask` != ${DEF_UMASK} ]; then \
+	@if [ `${SUDO} ${SH} -c umask` != ${DEF_UMASK} ]; then \
 		echo >&2 "Error: your umask is \"`${SH} -c umask`"\".; \
 		exit 1; \
 	fi
