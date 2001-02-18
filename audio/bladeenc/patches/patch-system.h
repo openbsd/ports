26dc06273286af8a5650fcdd6f1966928a4bcfd5
brad <brad@openbsd.org>
2001-02-18 21:36:00 +0000

    upgrade to bladeenc 0.93.10

--- audio/bladeenc/patches/patch-system.h
+++ audio/bladeenc/patches/patch-system.h
@@ -1,11 +1,16 @@
---- bladeenc/system.h.orig	Thu Jun 15 09:02:23 2000
-+++ bladeenc/system.h	Thu Jul  6 16:26:56 2000
-@@ -248,7 +248,7 @@
+--- bladeenc/system.h.orig	Sun Feb 18 11:34:27 2001
++++ bladeenc/system.h	Sun Feb 18 11:35:04 2001
+@@ -327,12 +327,9 @@
  
  typedef		unsigned	char 	uchar;
  
 -#if !defined(SYS_TYPES_H) && !defined(_SYS_TYPES_H)
-+#if !defined(SYS_TYPES_H) && !defined(_SYS_TYPES_H) && !defined(UNIX_SYSTEM)
++#ifndef UNIX_SYSTEM
  		typedef		unsigned short	ushort;
  		typedef		unsigned int	uint;
  #endif
+-
+-
+-
+ 
+ #endif		/* __SYSTEM__ */
