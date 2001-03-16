5130db70d188c5e61df385431ab32600b0084b5b
espie <espie@openbsd.org>
2001-03-16 14:54:40 +0000

    Simplify MULTI_PACKAGES, allow for PKGNAME-foo=value

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Fundamental; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.359 2001/03/06 18:59:38 espie Exp $$
+FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.360 2001/03/16 14:54:40 espie Exp $$
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -743,7 +743,11 @@ EXTRACT_SUFX?=		.tar.gz
 
 # Derive names so that they're easily overridable.
 DISTFILES?=		${DISTNAME}${EXTRACT_SUFX}
+.if ${SUBPACKAGE} != '' && defined(PKGNAME${SUBPACKAGE})
+PKGNAME?=		${PKGNAME${SUBPACKAGE}}
+.endif
 PKGNAME?=		${DISTNAME}${SUBPACKAGE}
+
 PKGNAME:=		${PKGNAME}${_FEXT}
 
 _EVERYTHING=${DISTFILES}
