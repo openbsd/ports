dc47c1713cb9a59a54557d9d50d062e4dbd5d4a9
jakob <jakob@openbsd.org>
2001-03-19 15:48:38 +0000

    lightweight window manager with emphasis on usability. submitted by Wynand van Dyk <wynand@type01.com>

---
+++ x11/pwm/patches/patch-config.h
@@ -0,0 +1,11 @@
+--- config.h~ Sat Jun  3 22:12:54 2000
++++ config.h Sat Mar 17 20:10:43 2001
+@@ -64,7 +64,7 @@
+ #define PREFIX ""
+ #endif
+ 
+-#define CF_SYS_CONFIG_LOCATION PREFIX"/etc/pwm/"
++#define CF_SYS_CONFIG_LOCATION PREFIX"/share/pwm/"
+ #define CF_GLOBAL_CFGFILE CF_SYS_CONFIG_LOCATION"pwm.conf"
+ #define CF_USER_CONFIG_LOCATION ".pwm/"
+ #define CF_USER_CFGFILE CF_USER_CONFIG_LOCATION"pwm.conf"
---
+++ x11/pwm/patches/patch-system.mk
@@ -0,0 +1,11 @@
+--- system.mk~ Sat Jul  1 12:37:50 2000
++++ system.mk Sat Mar 17 18:44:10 2001
+@@ -74,7 +74,7 @@ MAKE_DEPEND=$(CC) -M $(DEFINES) $(INCLUD
+ ##
+ 
+ AR=ar
+-ARFLAGS=crs
++ARFLAGS=cr
+ 
+ 
+ ##
