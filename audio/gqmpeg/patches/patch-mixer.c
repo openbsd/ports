59f41ce9693d7920c86663bf30dea10c3a2289c5
rohee <rohee@openbsd.org>
2001-03-27 20:48:24 +0000

    update to 0.9.0, work mostly by obecian@ with help from Ibrahim Khalifa

--- audio/gqmpeg/patches/patch-mixer.c
+++ audio/gqmpeg/patches/patch-mixer.c
@@ -1,56 +1,48 @@
---- mixer.c.orig	Sat Jul  3 07:39:09 1999
-+++ mixer.c	Mon Dec  6 18:43:13 1999
-@@ -24,7 +24,7 @@
- #include <linux/soundcard.h>
- #endif
- 
--#if defined(sun) && defined(__svr4__) || defined(__NetBSD__)
-+#if defined(sun) && defined(__svr4__) || defined(__NetBSD__) || defined(__OpenBSD__)
- #include <sys/audioio.h>
- #endif
- 
-@@ -177,7 +177,7 @@
- 	return vol;
- }
- 
--#elif defined(__NetBSD__)
-+#elif defined(__NetBSD__) || defined(__OpenBSD__)
- mixer_devinfo_t *infos;
- mixer_ctrl_t *values;
- void mixer_init(gint init_device_id)
-@@ -189,7 +189,7 @@
- 
-   mixer_device = getenv("MIXERDEVICE");
-   if (mixer_device == NULL)
--    mixer_device = "/dev/mixer0";
-+    mixer_device = "/dev/mixer";
- 
-   if ((fd = open(mixer_device, O_RDWR)) == -1) {
-     perror(mixer_device);
-@@ -259,7 +259,7 @@
- 
-   mixer_device = getenv("MIXERDEVICE");
-   if (mixer_device == NULL)
--    mixer_device = "/dev/mixer0";
-+    mixer_device = "/dev/mixer";
- 
-   if ((fd = open(mixer_device, O_RDWR)) == -1) {
-     perror(mixer_device);
-@@ -303,7 +303,7 @@
- 
-   mixer_device = getenv("MIXERDEVICE");
-   if (mixer_device == NULL)
--    mixer_device = "/dev/mixer0";
-+    mixer_device = "/dev/mixer";
- 
-   if ((fd = open(mixer_device, O_RDWR)) == -1) {
-     perror(mixer_device);
-@@ -595,7 +595,7 @@
- 
- #endif
- 
--#if defined(sun) && defined(__svr4__) || defined(__NetBSD__)
-+#if defined(sun) && defined(__svr4__) || defined(__NetBSD__) || defined(__OpenBSD__)
- /* from 0 through 100% */
- void set_volume(gint vol)
- {
+$OpenBSD: patch-mixer.c,v 1.2 2001/03/27 20:48:25 rohee Exp $
+
+Our mixer device is /dev/mixer not /dex/mixer0
+
+*** src/mixer.c.orig	Tue Mar 20 14:11:50 2001
+--- src/mixer.c	Tue Mar 20 14:14:08 2001
+***************
+*** 275,281 ****
+--- 275,285 ----
+  
+    mixer_device = getenv("MIXERDEVICE");
+    if (mixer_device == NULL)
++ #ifdef __OpenBSD__
++     mixer_device = "/dev/mixer";
++ #else
+      mixer_device = "/dev/mixer0";
++ #endif
+  
+    if ((fd = open(mixer_device, O_RDWR)) == -1) {
+      perror(mixer_device);
+***************
+*** 352,358 ****
+--- 356,366 ----
+  
+    mixer_device = getenv("MIXERDEVICE");
+    if (mixer_device == NULL)
++ #ifdef __OpenBSD__
++     mixer_device = "/dev/mixer";
++ #else
+      mixer_device = "/dev/mixer0";
++ #endif
+  
+    if ((fd = open(mixer_device, O_RDWR)) == -1) {
+      perror(mixer_device);
+***************
+*** 396,402 ****
+--- 404,414 ----
+  
+    mixer_device = getenv("MIXERDEVICE");
+    if (mixer_device == NULL)
++ #ifdef __OpenBSD__
++     mixer_device = "/dev/mixer";
++ #else
+      mixer_device = "/dev/mixer0";
++ #endif
+  
+    if ((fd = open(mixer_device, O_RDWR)) == -1) {
+      perror(mixer_device);
