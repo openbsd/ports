c77f5047eee051e5a46c62d1d6fca7fea7e07bb4
jakob <jakob@openbsd.org>
2001-03-30 15:53:13 +0000

    add no_x11 flavour

--- textproc/xml2rfc/patches/patch-xml2rfc.tcl
+++ textproc/xml2rfc/patches/patch-xml2rfc.tcl
@@ -1,10 +1,11 @@
---- xml2rfc.tcl.orig	Sat Oct 21 05:18:14 2000
-+++ xml2rfc.tcl	Fri Feb  2 14:01:25 2001
-@@ -1,6 +1,4 @@
+--- xml2rfc.tcl.orig	Fri Mar 30 17:09:11 2001
++++ xml2rfc.tcl	Fri Mar 30 17:09:15 2001
+@@ -1,8 +1,3 @@
 -#!/bin/sh
 -# the next line restarts using wish \
 -exec wish8.0 "$0" "$0" "$@"
-+#!/usr/local/bin/wish8.3
- 
- 
+-
+-
+ #
+ # xml2rfc.cgi - convert technical memos written using XML to TXT/HTML/NROFF
  #
