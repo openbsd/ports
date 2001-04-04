4d85ac4a33ab2a295ca41f2e5d8ca738c5da42ce
espie <espie@openbsd.org>
2001-04-04 09:55:34 +0000

    reset IFS so that dir-depends will work correctly.

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Fundamental; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.386 2001/04/04 08:17:31 espie Exp $$
+FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.387 2001/04/04 09:55:34 espie Exp $$
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -1073,7 +1073,7 @@ _flavor_fragment= \
 							flavor="$$flavor $$i";; \
 					esac \
 				fi; \
-			done;; \
+			done; unset IFS;; \
 		esac; \
 		toset="PKGPATH=$$dir"; \
 		case X$$multi in "X");; *) \
