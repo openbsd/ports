41d87e2076577a78dcde2cc100e197f82b0b0893
espie <espie@openbsd.org>
2001-05-19 20:07:00 +0000

    Make sure assignment is unambiguous. Found out by naddy@

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Fundamental; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.408 2001/05/07 15:24:50 espie Exp $$
+FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.409 2001/05/19 20:07:00 espie Exp $$
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -541,7 +541,7 @@ ${_PACKAGE_COOKIE}: ${_INSTALL_COOKIE}
 .endif
 
 .for _sub in ${MULTI_PACKAGES}
-_PACKAGE_COOKIE${_sub}= ${_PACKAGE_COOKIE}${_sub}
+_PACKAGE_COOKIE${_sub} = ${_PACKAGE_COOKIE}${_sub}
 _PACKAGE_COOKIES+=  ${_PACKAGE_COOKIE${_sub}}
 .  if ${FAKE:L} == "yes"
 ${_PACKAGE_COOKIE${_sub}}: ${_FAKE_COOKIE}
