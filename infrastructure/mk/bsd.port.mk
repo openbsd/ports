38bf33944376d5cfc96ce1365323764a30d7868a
espie <espie@openbsd.org>
2001-05-23 15:48:08 +0000

    Fix quoting

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Fundamental; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.412 2001/05/23 14:18:24 espie Exp $$
+FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.413 2001/05/23 15:48:08 espie Exp $$
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -2156,7 +2156,7 @@ describe:
 .    else
 	@echo -n "${PREFIX}|"
 .    endif
-	@echo -n "${_COMMENT}|"; \
+	@echo -n ${_COMMENT}"|"; \
 	if [ -f ${DESCR} ]; then \
 		echo -n "${DESCR:S,^${PORTSDIR}/,,}|"; \
 	else \
