ed3f2b5725bda14a317fd4d98ab2347a729ddac5
espie <espie@openbsd.org>
2001-08-16 15:15:24 +0000

    Fix (used bad patch version...) _PKGFILE->PKGFILE.

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Fundamental; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.437 2001/08/16 15:13:17 espie Exp $$
+FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.438 2001/08/16 15:15:24 espie Exp $$
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -2006,16 +2006,16 @@ clean: pre-clean
 .  if ${clean:L:Minstall}
 .    if ${clean:L:Msub}
 .	   for _s in ${MULTI_PACKAGES}
-	${SUDO} ${PKG_DELETE} ${clean:M-f} ${_FULLPKGNAME${_s}}
+	${SUDO} ${PKG_DELETE} ${clean:M-f} ${FULLPKGNAME${_s}}
 .      endfor
 .    else
-	${SUDO} ${PKG_DELETE} ${clean:M-f} ${_FULLPKGNAME${SUBPACKAGE}}
+	${SUDO} ${PKG_DELETE} ${clean:M-f} ${FULLPKGNAME${SUBPACKAGE}}
 .    endif
 .  endif
 .  if ${clean:L:Mpackages} || ${clean:L:Mpackage} && ${clean:L:Msub}
 	rm -f ${_PACKAGE_COOKIES}
 .  elif ${clean:L:Mpackage}
-	rm -f ${_PKGFILE${SUBPACKAGE}}
+	rm -f ${PKGFILE${SUBPACKAGE}}
 .  endif
 .endif
 
