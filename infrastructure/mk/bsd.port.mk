27888b1ea446e8966eae4abe7668a0b699d5b76c
brad <brad@openbsd.org>
2001-08-17 03:23:04 +0000

    remove some old cruft to do with tar that does not pertain to us.

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Fundamental; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.438 2001/08/16 15:15:24 espie Exp $$
+FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.439 2001/08/17 03:23:04 brad Exp $$
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -547,11 +547,7 @@ PATCH_ARGS+=	-C
 PATCH_DIST_ARGS+=	-C
 .endif
 
-.if exists(/bin/tar)
 TAR?=	/bin/tar
-.else
-TAR?=	/usr/bin/tar
-.endif
 UNZIP?=	unzip
 BZIP2?=	bzip2
 
