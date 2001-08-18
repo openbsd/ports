ddecfd4bad489e867f875536759513cfab5217cb
kevlo <kevlo@openbsd.org>
2001-08-18 03:46:11 +0000

    upgrade to version 2.4.2

--- textproc/libxml/patches/patch-ltmain.sh
+++ textproc/libxml/patches/patch-ltmain.sh
@@ -1,47 +1,29 @@
---- ltmain.sh.orig	Fri Jan  5 07:13:13 2001
-+++ ltmain.sh	Mon Feb 19 10:30:34 2001
-@@ -1079,6 +1079,17 @@
- 	    # These systems don't actually have c library (as such)
- 	    continue
- 	    ;;
-+          *-*-openbsd*)
-+	    # Do not include libc due to us having libc/libc_r.
-+	    continue
-+            ;;
-+          esac
-+        elif test "$arg" = "-lc_r"; then
-+	  case "$host" in
-+	  *-*-openbsd*) 
-+            # Do not include libc_r directly, use -pthread flag. 
-+	    continue
-+	    ;;
- 	  esac
- 	elif test "$arg" = "-lm"; then
- 	  case "$host" in
-@@ -1091,6 +1102,10 @@
- 	deplibs="$deplibs $arg"
+--- ltmain.sh.orig	Fri Aug 17 21:15:18 2001
++++ ltmain.sh	Fri Aug 17 21:20:01 2001
+@@ -1037,6 +1037,10 @@
+ 	continue
  	;;
  
 +      -?thread)
-+       deplibs="$deplibs $arg"
-+       ;;
++	deplibs="$deplibs $arg"
++	;;
 +
        -module)
  	module=yes
  	continue
-@@ -1795,6 +1810,9 @@
- 	*-*-cygwin* | *-*-mingw* | *-*-os2* | *-*-beos*)
- 	  # these systems don't actually have a c library (as such)!
- 	  ;;
-+        *-*-openbsd*) 
-+	  # Do not include libc due to us having libc/libc_r.
-+	  ;;
- 	*)
- 	  # Add libc to deplibs on all other systems.
- 	  deplibs="$deplibs -lc"
-@@ -3555,40 +3573,6 @@
+@@ -2401,6 +2405,9 @@
+ 	  *-*-cygwin* | *-*-mingw* | *-*-pw32* | *-*-os2* | *-*-beos*)
+ 	    # these systems don't actually have a c library (as such)!
+ 	    ;;
++	  *-*-openbsd*)
++	    # Do not include libc due to us having libc/libc_r.
++	    ;;
+ 	  *-*-rhapsody* | *-*-darwin1.[012])
+ 	    # Rhapsody C library is in the System framework
+ 	    deplibs="$deplibs -framework System"
+@@ -4412,40 +4419,6 @@
      # Exit here if they wanted silent mode.
-     test "$show" = : && exit 0
+     test "$show" = ":" && exit 0
  
 -    echo "----------------------------------------------------------------------"
 -    echo "Libraries have been installed in:"
@@ -51,7 +33,7 @@
 -    echo
 -    echo "If you ever happen to want to link against installed libraries"
 -    echo "in a given directory, LIBDIR, you must either use libtool, and"
--    echo "specify the full pathname of the library, or use \`-LLIBDIR'"
+-    echo "specify the full pathname of the library, or use the \`-LLIBDIR'"
 -    echo "flag during linking and do at least one of the following:"
 -    if test -n "$shlibpath_var"; then
 -      echo "   - add LIBDIR to the \`$shlibpath_var' environment variable"
