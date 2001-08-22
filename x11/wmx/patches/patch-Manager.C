c753e3ec1d2ac86f7611e2199752fe120a77f89a
jakob <jakob@openbsd.org>
2001-08-22 09:34:17 +0000

    update patches & compile with gnome support; olof@crt.se

--- x11/wmx/patches/patch-Manager.C
+++ x11/wmx/patches/patch-Manager.C
@@ -1,19 +1,27 @@
-*** Manager.C.orig	Thu Jul 15 11:30:24 1999
---- Manager.C	Thu Jul 15 11:32:40 1999
-***************
-*** 719,725 ****
-      }
-  }
-  
-! #ifdef sgi
-  extern "C" {
-  extern int putenv(char *);	/* not POSIX */
-  }
---- 719,725 ----
-      }
-  }
-  
-! #if defined(sgi) || defined(__OpenBSD__)
-  extern "C" {
-  extern int putenv(char *);	/* not POSIX */
-  }
+$OpenBSD: patch-Manager.C,v 1.2 2001/08/22 09:34:17 jakob Exp $
+
+--- Manager.C.orig	Mon Apr 23 11:10:42 2001
++++ Manager.C	Wed Aug 22 11:18:10 2001
+@@ -916,7 +916,7 @@ Boolean WindowManager::raiseTransients(C
+     }
+ }
+ 
+-#ifdef sgi
++#if defined(sgi) || defined(__OpenBSD__)
+ extern "C" {
+ extern int putenv(char *);	/* not POSIX */
+ }
+
+--- Manager.C.orig	Mon Apr 23 11:10:42 2001
++++ Manager.C	Wed Aug 22 11:18:10 2001
+@@ -1069,6 +1069,11 @@
+ 
+     delete windows;
+ }
++#ifdef __OpenBSD__
++extern "C" {
++       int      snprintf(char *, size_t, const char *, ...);
++}
++#endif
+ 
+ void WindowManager::gnomeUpdateChannelList()
