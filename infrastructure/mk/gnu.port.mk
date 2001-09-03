72b351c4e8cb786d99a762542bc5d4250c4360b1
brad <brad@openbsd.org>
2001-09-03 02:00:48 +0000

    kludge which allows the ports tree to continue working on our newly renamed powerpc systems without needing to patch all copies of config.guess in the tree.

--- infrastructure/mk/gnu.port.mk
+++ infrastructure/mk/gnu.port.mk
@@ -1,8 +1,15 @@
 #-*- mode: Fundamental; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-# $OpenBSD: gnu.port.mk,v 1.1 2001/08/25 11:23:46 espie Exp $
+# $OpenBSD: gnu.port.mk,v 1.2 2001/09/03 02:00:48 brad Exp $
 #	Based on bsd.port.mk, originally by Jordan K. Hubbard.
 #	This file is in the public domain.
+
+# XXX - Kludge for these archictectures so we do not
+#	have to patch all copies of config.guess
+#	throughout the ports tree.
+.if ${MACHINE} == "macppc" || ${MACHINE} == "mvmeppc"
+CONFIGURE_ARGS+=	--host=powerpc-unknown-openbsd${OPSYS_VER}
+.endif
 
 .if ${CONFIGURE_STYLE:L:Mdest}
 CONFIGURE_ARGS+=	--prefix='$${${DESTDIRNAME}}${PREFIX}'
