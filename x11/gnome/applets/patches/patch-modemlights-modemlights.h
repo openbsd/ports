25bd10de43a1973eacaf3ef1c0b459e7a4eae16b
todd <todd@openbsd.org>
2001-09-13 21:23:08 +0000

    applets for use with the GNOME panel

---
+++ x11/gnome/applets/patches/patch-modemlights-modemlights.h
@@ -0,0 +1,11 @@
+--- modemlights/modemlights.h.orig	Fri Jun  8 21:23:34 2001
++++ modemlights/modemlights.h	Fri Jun  8 21:23:41 2001
+@@ -17,6 +17,8 @@
+ #include <net/if.h>
+ 
+ #ifdef __OpenBSD__
++#  include <sys/timeout.h>
++#  include <net/ppp_defs.h>
+ #  include <net/bpf.h>
+ #  include <net/if_pppvar.h>
+ #  include <net/if_ppp.h>
