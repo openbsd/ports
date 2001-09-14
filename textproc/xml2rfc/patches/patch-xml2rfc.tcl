8c961e60cf8156441c7951b0497b15862df53f34
jakob <jakob@openbsd.org>
2001-09-14 06:43:27 +0000

    update to v1.8

--- textproc/xml2rfc/patches/patch-xml2rfc.tcl
+++ textproc/xml2rfc/patches/patch-xml2rfc.tcl
@@ -1,13 +1,11 @@
-$OpenBSD: patch-xml2rfc.tcl,v 1.4 2001/08/19 21:39:09 jakob Exp $
+$OpenBSD: patch-xml2rfc.tcl,v 1.5 2001/09/14 06:43:27 jakob Exp $
 
---- xml2rfc.tcl.orig	Sun Aug 19 19:25:18 2001
-+++ xml2rfc.tcl	Sun Aug 19 19:25:57 2001
-@@ -1,8 +1,3 @@
+--- xml2rfc.tcl.orig	Fri Sep 14 08:35:49 2001
++++ xml2rfc.tcl	Fri Sep 14 08:35:59 2001
+@@ -1,6 +1,3 @@
 -#!/bin/sh
 -# the next line restarts using wish \
 -exec wish "$0" "$0" "$@"
--
--
- #
- # xml2rfc.tcl - convert technical memos written using XML to TXT/HTML/NROFF
+ 
+ 
  #
