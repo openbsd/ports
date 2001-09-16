9771cb3551e9fae6c68d52d78e4ddd50ba026abf
naddy <naddy@openbsd.org>
2001-09-16 17:10:29 +0000

    Update to 5.05TP1: - bug fixes - asynchronous DNS lookups don't leave zombies - support for Netscape plug-ins

--- www/opera/files/opera.sh
+++ www/opera/files/opera.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 #
-# $OpenBSD: opera.sh,v 1.1.1.1 2001/05/16 14:44:27 naddy Exp $
+# $OpenBSD: opera.sh,v 1.2 2001/09/16 17:10:29 naddy Exp $
 
-OPERA_DIR=@PREFIX@/share/opera exec @PREFIX@/libexec/opera "$@"
+PLUGIN_PATH=@PREFIX@/lib/opera/plugins
+PLUGIN_PATH=${PLUGIN_PATH}:${HOME}/.netscape/plugins
+PLUGIN_PATH=${PLUGIN_PATH}:@LOCALBASE@/netscape/plugins
+
+export OPERA_DIR=@PREFIX@/share/opera
+export OPERA_PLUGIN_PATH=${OPERA_PLUGIN_PATH-${PLUGIN_PATH}}
+
+exec @PREFIX@/libexec/opera-static "$@"
