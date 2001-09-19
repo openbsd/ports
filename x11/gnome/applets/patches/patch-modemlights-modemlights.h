e6a221a1bacc16d94160f1218451310d8809889f
naddy <naddy@openbsd.org>
2001-09-19 16:28:58 +0000

    - fix message catalog installation - sync libtool patches - use .bz2 distfile

--- x11/gnome/applets/patches/patch-modemlights-modemlights.h
+++ x11/gnome/applets/patches/patch-modemlights-modemlights.h
@@ -1,3 +1,4 @@
+$OpenBSD: patch-modemlights-modemlights.h,v 1.2 2001/09/19 16:28:58 naddy Exp $
 --- modemlights/modemlights.h.orig	Fri Jun  8 21:23:34 2001
 +++ modemlights/modemlights.h	Fri Jun  8 21:23:41 2001
 @@ -17,6 +17,8 @@
