49f6871211b10f68b30ceac9d6849852bad074a6
jakob <jakob@openbsd.org>
2001-09-24 22:01:49 +0000

    update to version 2001.313 - no longer depend on c-client - enable gssapi (kerberos5) support

---
+++ mail/imap-uw/patches/patch-src_osdep_unix_env_unix.c
@@ -0,0 +1,14 @@
+$OpenBSD: patch-src_osdep_unix_env_unix.c,v 1.1 2001/09/24 22:01:49 jakob Exp $
+
+--- src/osdep/unix/env_unix.c.orig	Thu Dec 21 01:12:13 2000
++++ src/osdep/unix/env_unix.c	Thu Jan 18 16:11:09 2001
+@@ -971,7 +971,8 @@ long dotlock_lock (char *file,DOTLOCK *b
+       }
+       close (pi[0]); close (pi[1]);
+     }
+-    if (lockEaccesError) {	/* punt silently if paranoid site */
++    if (strncmp(base->lock,"/var/mail/",10) && lockEaccesError) {
++	/* punt silently if paranoid site */
+       sprintf (tmp,"Mailbox vulnerable - directory %.80s",base->lock);
+       if (s = strrchr (tmp,'/')) *s = '\0';
+       strcat (tmp," must have 1777 protection");
