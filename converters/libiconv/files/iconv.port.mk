5f7d8adc3a39748a9576e8e979a04935d927030b
brad <brad@openbsd.org>
2001-09-24 17:17:02 +0000

    add comment explaining why we have a RUN_DEPENDS entry here.

--- converters/libiconv/files/iconv.port.mk
+++ converters/libiconv/files/iconv.port.mk
@@ -1,4 +1,8 @@
-# $OpenBSD: iconv.port.mk,v 1.1 2001/09/23 05:29:47 brad Exp $
+# $OpenBSD: iconv.port.mk,v 1.2 2001/09/24 17:17:02 brad Exp $
 
+# The RUN_DEPENDS entry is to ensure libiconv is
+# installed. This is needed so we have charset.alias
+# on static archs.
+# Typically installed in PREFIX/lib.
 LIB_DEPENDS+=	iconv.2:libiconv-*:converters/libiconv
 RUN_DEPENDS+=	:libiconv-*:converters/libiconv
--- devel/gettext/files/gettext.port.mk
+++ devel/gettext/files/gettext.port.mk
@@ -1,4 +1,8 @@
-# $OpenBSD: gettext.port.mk,v 1.1 2001/09/23 05:29:47 brad Exp $
+# $OpenBSD: gettext.port.mk,v 1.2 2001/09/24 17:17:02 brad Exp $
 
+# The RUN_DEPENDS entry is to ensure gettext is
+# installed. This is needed so we have locale.alias
+# on static archs.
+# Typically installed in PREFIX/share/locale.
 LIB_DEPENDS+=	intl.1:gettext->=0.10.37:devel/gettext
 RUN_DEPENDS+=	:gettext->=0.10.37:devel/gettext
