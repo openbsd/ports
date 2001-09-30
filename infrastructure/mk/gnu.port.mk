aac1c8b95cc23b1e0d2a88e4a959b75f456b3e5b
espie <espie@openbsd.org>
2001-09-30 17:14:33 +0000

    make sure config.guess is executable.

--- infrastructure/mk/gnu.port.mk
+++ infrastructure/mk/gnu.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Fundamental; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-# $OpenBSD: gnu.port.mk,v 1.4 2001/09/24 22:30:03 espie Exp $
+# $OpenBSD: gnu.port.mk,v 1.5 2001/09/30 17:14:33 espie Exp $
 #	Based on bsd.port.mk, originally by Jordan K. Hubbard.
 #	This file is in the public domain.
 
@@ -9,6 +9,7 @@ MODGNU_CONFIG_GUESS_DIRS?=${WRKSRC}
 MODGNU_configure =
 .for _d in ${MODGNU_CONFIG_GUESS_DIRS}
 MODGNU_configure += cp -f ${PORTSDIR}/infrastructure/db/config.guess ${_d};
+MODGNU_configure += chmod a+rx ${_d}/config.guess;
 .endfor
 MODGNU_configure += ${MODSIMPLE_configure}
 
@@ -25,3 +26,5 @@ CONFIGURE_ARGS+=	--sysconfdir='$${${DESTDIRNAME}}${SYSCONFDIR}'
 CONFIGURE_ARGS+=	--sysconfdir='${SYSCONFDIR}'
 .  endif
 .endif
+
+REGRESS_TARGET?=	check
