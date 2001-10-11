d5839f2f4a2611a50c46021e7ee99185f9ce0c30
brad <brad@openbsd.org>
2001-10-11 15:48:00 +0000

    uncomment LIB_DEPENDS entry. Suggested by: espie@

--- devel/gettext/files/gettext.port.mk
+++ devel/gettext/files/gettext.port.mk
@@ -1,9 +1,9 @@
-# $OpenBSD: gettext.port.mk,v 1.3 2001/10/11 15:34:15 brad Exp $
+# $OpenBSD: gettext.port.mk,v 1.4 2001/10/11 15:48:00 brad Exp $
 
 # The RUN_DEPENDS entry is to ensure gettext is
 # installed. This is needed so we have locale.alias
 # on static archs.
 # Typically installed in PREFIX/share/locale.
-#LIB_DEPENDS+=	intl.1:gettext->=0.10.38:devel/gettext
+LIB_DEPENDS+=	intl.1:gettext->=0.10.38:devel/gettext
 BUILD_DEPENDS+=	:gettext->=0.10.38:devel/gettext
 RUN_DEPENDS+=	:gettext->=0.10.38:devel/gettext
