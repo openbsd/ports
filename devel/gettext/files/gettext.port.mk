1ab3c7c5fa766b810c8b7460a836efe79dcfa2d3
brad <brad@openbsd.org>
2001-10-11 15:34:15 +0000

    use a BUILD_DEPENDS for now (until TRUST_PACKAGES is enabled for libs) instead of LIB_DEPENDS so that we can ensure we get the right version. Problem pointed out by chris@ that some ports barf when given a msgfmt that comes from anything older than gettext 0.10.38.

--- devel/gettext/files/gettext.port.mk
+++ devel/gettext/files/gettext.port.mk
@@ -1,8 +1,9 @@
-# $OpenBSD: gettext.port.mk,v 1.2 2001/09/24 17:17:02 brad Exp $
+# $OpenBSD: gettext.port.mk,v 1.3 2001/10/11 15:34:15 brad Exp $
 
 # The RUN_DEPENDS entry is to ensure gettext is
 # installed. This is needed so we have locale.alias
 # on static archs.
 # Typically installed in PREFIX/share/locale.
-LIB_DEPENDS+=	intl.1:gettext->=0.10.37:devel/gettext
-RUN_DEPENDS+=	:gettext->=0.10.37:devel/gettext
+#LIB_DEPENDS+=	intl.1:gettext->=0.10.38:devel/gettext
+BUILD_DEPENDS+=	:gettext->=0.10.38:devel/gettext
+RUN_DEPENDS+=	:gettext->=0.10.38:devel/gettext
