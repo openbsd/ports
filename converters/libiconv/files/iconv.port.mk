4aa1a2bd8577518f97e7997e6b27a1314c1f5013
brad <brad@openbsd.org>
2001-10-28 06:34:24 +0000

    better comment.

--- converters/libiconv/files/iconv.port.mk
+++ converters/libiconv/files/iconv.port.mk
@@ -1,8 +1,7 @@
-# $OpenBSD: iconv.port.mk,v 1.2 2001/09/24 17:17:02 brad Exp $
+# $OpenBSD: iconv.port.mk,v 1.3 2001/10/28 06:34:24 brad Exp $
 
-# The RUN_DEPENDS entry is to ensure libiconv is
-# installed. This is needed so we have charset.alias
-# on static archs.
+# The RUN_DEPENDS entry is to ensure libiconv is installed. This is
+# necessary so that we have charset.alias installed on static archs.
 # Typically installed in PREFIX/lib.
 LIB_DEPENDS+=	iconv.2:libiconv-*:converters/libiconv
 RUN_DEPENDS+=	:libiconv-*:converters/libiconv
