762a69e82d222e81011777e8a91d10a720895756
wilfried <wilfried@openbsd.org>
2001-11-13 09:03:25 +0000

    don't use vm/vm.h, from maintainer

--- devel/libgtop/patches/patch-sysdeps_freebsd_procmap.c
+++ devel/libgtop/patches/patch-sysdeps_freebsd_procmap.c
@@ -1,6 +1,6 @@
-$OpenBSD: patch-sysdeps_freebsd_procmap.c,v 1.2 2001/09/19 14:37:49 naddy Exp $
+$OpenBSD: patch-sysdeps_freebsd_procmap.c,v 1.3 2001/11/13 09:03:25 wilfried Exp $
 --- sysdeps/freebsd/procmap.c.orig	Thu May 27 20:56:48 1999
-+++ sysdeps/freebsd/procmap.c	Mon Aug 27 17:26:53 2001
++++ sysdeps/freebsd/procmap.c	Mon Nov 12 20:54:39 2001
 @@ -32,9 +32,6 @@
  #include <sys/param.h>
  #include <sys/proc.h>
@@ -11,16 +11,18 @@ $OpenBSD: patch-sysdeps_freebsd_procmap.c,v 1.2 2001/09/19 14:37:49 naddy Exp $
  
  #include <sys/vnode.h>
  #include <sys/mount.h>
-@@ -48,7 +45,7 @@
+@@ -46,9 +43,8 @@
+ #include <sys/user.h>
+ #endif
  #include <sys/sysctl.h>
- #include <vm/vm.h>
+-#include <vm/vm.h>
  
 -#if defined(__NetBSD__) && (__NetBSD_Version__ >= 104000000)
 +#if (defined(__NetBSD__) && (__NetBSD_Version__ >= 104000000)) || defined(OpenBSD)
  /* Fixme ... */
  #undef _KERNEL
  #define _UVM_UVM_AMAP_I_H_ 1
-@@ -82,7 +79,7 @@ glibtop_get_proc_map_p (glibtop *server,
+@@ -82,7 +78,7 @@ glibtop_get_proc_map_p (glibtop *server,
  	struct kinfo_proc *pinfo;
  	struct vm_map_entry entry, *first;
  	struct vmspace vmspace;
@@ -29,7 +31,7 @@ $OpenBSD: patch-sysdeps_freebsd_procmap.c,v 1.2 2001/09/19 14:37:49 naddy Exp $
  	struct vnode vnode;
  	struct inode inode;
  #else
-@@ -142,7 +139,7 @@ glibtop_get_proc_map_p (glibtop *server,
+@@ -142,7 +138,7 @@ glibtop_get_proc_map_p (glibtop *server,
  
  	/* I tested this a few times with `mmap'; as soon as you write
  	 * to the mmap'ed area, the object type changes from OBJT_VNODE
@@ -38,7 +40,7 @@ $OpenBSD: patch-sysdeps_freebsd_procmap.c,v 1.2 2001/09/19 14:37:49 naddy Exp $
  
  	do {
  		if (update) {
-@@ -163,7 +160,7 @@ glibtop_get_proc_map_p (glibtop *server,
+@@ -163,7 +159,7 @@ glibtop_get_proc_map_p (glibtop *server,
   			continue;
  #endif
  #else
@@ -47,7 +49,7 @@ $OpenBSD: patch-sysdeps_freebsd_procmap.c,v 1.2 2001/09/19 14:37:49 naddy Exp $
   		if (UVM_ET_ISSUBMAP (&entry))
  			continue;
  #else
-@@ -189,7 +186,7 @@ glibtop_get_proc_map_p (glibtop *server,
+@@ -189,7 +185,7 @@ glibtop_get_proc_map_p (glibtop *server,
  
  		i++;
  
@@ -56,7 +58,7 @@ $OpenBSD: patch-sysdeps_freebsd_procmap.c,v 1.2 2001/09/19 14:37:49 naddy Exp $
  		if (!entry.object.uvm_obj)
  			continue;
  
-@@ -213,7 +210,7 @@ glibtop_get_proc_map_p (glibtop *server,
+@@ -213,7 +209,7 @@ glibtop_get_proc_map_p (glibtop *server,
  			glibtop_error_io_r (server, "kvm_read (object)");
  #endif
  
--- devel/libgtop/patches/patch-sysdeps_freebsd_procmem.c
+++ devel/libgtop/patches/patch-sysdeps_freebsd_procmem.c
@@ -1,6 +1,6 @@
-$OpenBSD: patch-sysdeps_freebsd_procmem.c,v 1.2 2001/09/19 14:37:49 naddy Exp $
+$OpenBSD: patch-sysdeps_freebsd_procmem.c,v 1.3 2001/11/13 09:03:25 wilfried Exp $
 --- sysdeps/freebsd/procmem.c.orig	Thu May 27 20:56:49 1999
-+++ sysdeps/freebsd/procmem.c	Mon Aug 27 17:24:33 2001
++++ sysdeps/freebsd/procmem.c	Mon Nov 12 20:53:53 2001
 @@ -31,8 +31,6 @@
  #include <sys/param.h>
  #include <sys/proc.h>
@@ -10,16 +10,18 @@ $OpenBSD: patch-sysdeps_freebsd_procmem.c,v 1.2 2001/09/19 14:37:49 naddy Exp $
  
  #include <sys/vnode.h>
  #include <ufs/ufs/quota.h>
-@@ -45,7 +43,7 @@
+@@ -43,9 +41,8 @@
+ #include <sys/user.h>
+ #endif
  #include <sys/sysctl.h>
- #include <vm/vm.h>
+-#include <vm/vm.h>
  
 -#if defined(__NetBSD__) && (__NetBSD_Version__ >= 104000000)
 +#if (defined(__NetBSD__) && (__NetBSD_Version__ >= 104000000)) || defined(OpenBSD)
  /* Fixme ... */
  #undef _KERNEL
  #define _UVM_UVM_AMAP_I_H_ 1
-@@ -61,9 +59,7 @@ static const unsigned long _glibtop_sysd
+@@ -61,9 +58,7 @@ static const unsigned long _glibtop_sysd
  (1L << GLIBTOP_PROC_MEM_RSS_RLIM);
  
  static const unsigned long _glibtop_sysdeps_proc_mem_share =
@@ -30,7 +32,7 @@ $OpenBSD: patch-sysdeps_freebsd_procmem.c,v 1.2 2001/09/19 14:37:49 naddy Exp $
  (1L << GLIBTOP_PROC_MEM_SHARE);
  #else
  0;
-@@ -111,7 +107,7 @@ glibtop_get_proc_mem_p (glibtop *server,
+@@ -111,7 +106,7 @@ glibtop_get_proc_mem_p (glibtop *server,
  	struct kinfo_proc *pinfo;
  	struct vm_map_entry entry, *first;
  	struct vmspace *vms, vmspace;
@@ -39,7 +41,7 @@ $OpenBSD: patch-sysdeps_freebsd_procmem.c,v 1.2 2001/09/19 14:37:49 naddy Exp $
  	struct vnode vnode;
  	struct inode inode;
  #else
-@@ -196,7 +192,7 @@ glibtop_get_proc_mem_p (glibtop *server,
+@@ -196,7 +191,7 @@ glibtop_get_proc_mem_p (glibtop *server,
   			continue;
  #endif
  #else
@@ -48,7 +50,7 @@ $OpenBSD: patch-sysdeps_freebsd_procmem.c,v 1.2 2001/09/19 14:37:49 naddy Exp $
   		if (UVM_ET_ISSUBMAP (&entry))
  			continue;
  #else
-@@ -205,7 +201,7 @@ glibtop_get_proc_mem_p (glibtop *server,
+@@ -205,7 +200,7 @@ glibtop_get_proc_mem_p (glibtop *server,
  #endif
  #endif
  
@@ -57,7 +59,7 @@ $OpenBSD: patch-sysdeps_freebsd_procmem.c,v 1.2 2001/09/19 14:37:49 naddy Exp $
  		if (!entry.object.uvm_obj)
  			continue;
  
-@@ -232,7 +228,7 @@ glibtop_get_proc_mem_p (glibtop *server,
+@@ -232,7 +227,7 @@ glibtop_get_proc_mem_p (glibtop *server,
  #endif
  		/* If the object is of type vnode, add its size */
  
