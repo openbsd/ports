980f49d11e4c8f6756a404943e15578a031550ae
espie <espie@openbsd.org>
2001-11-15 02:07:05 +0000

    handles subcategories in package-links. Close PR 1731

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Fundamental; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.497 2001/11/13 12:52:22 espie Exp $$
+FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.498 2001/11/15 02:07:05 espie Exp $$
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -2019,7 +2019,8 @@ package-links:
 				exit 1; \
 			fi; \
 		fi; \
-		ln -s ../${PKGREPOSITORYSUBDIR}/${FULLPKGNAME${SUBPACKAGE}}${PKG_SUFX} ${PACKAGES}/$$cat; \
+		case $$cat in */*/*) parent=../../..;; */*) parent=../..;; *) parent=..;; esac; \
+		ln -s $$parent/${PKGREPOSITORYSUBDIR}/${FULLPKGNAME${SUBPACKAGE}}${PKG_SUFX} ${PACKAGES}/$$cat; \
 	done;
 .endif
 
