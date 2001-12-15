5bb9f47eb03edbcba4610a5566cf22b5ceb72be7
brad <brad@openbsd.org>
2001-12-15 18:10:36 +0000

    for the addsum target, check to see if files/md5 exists before trying to move it that way we do not get an error message from mv if it does not already exist. -- Ok'd by: espie@

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Fundamental; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.501 2001/12/13 15:40:27 naddy Exp $$
+FULL_REVISION=$$OpenBSD: bsd.port.mk,v 1.502 2001/12/15 18:10:36 brad Exp $$
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -1166,7 +1166,10 @@ makesum: fetch-all
 
 addsum: fetch-all
 .if !defined(NO_CHECKSUM)
-	@mv ${MD5_FILE} ${CHECKSUM_FILE} || touch ${CHECKSUM_FILE}
+	@if [ -f ${MD5_FILE} ]; then \
+	  mv -f ${MD5_FILE} ${CHECKSUM_FILE}; \
+	fi
+	@touch ${CHECKSUM_FILE}
 	@cd ${DISTDIR} && \
 	 	for cipher in ${_CIPHERS}; do \
 			$$cipher ${_CKSUMFILES} >> ${CHECKSUM_FILE}; \
