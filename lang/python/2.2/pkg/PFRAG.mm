cf056f6c8366bafb3fc8bdd457b2c24a8f32a3ad
matt <matt@openbsd.org>
2002-02-15 19:42:18 +0000

    Python 2.1.2 and Python 2.2.
    Major changes:
    
    - no more threads flavor, threads is now the default.
    
    - subpackage modules that depend on other packages instead of having
    largely redundant flavors (unless platform has no shared libs, then
    flavors count again)
    
    - shared libpython stuff disabled until someone can figure out why
    it makes Zope crash

---
+++ lang/python/2.2/pkg/PFRAG.mm
@@ -0,0 +1,4 @@
+@comment $OpenBSD: PFRAG.mm,v 1.1 2002/02/15 19:42:19 matt Exp $
+lib/python2.2/lib-dynload/audioop.so
+lib/python2.2/lib-dynload/imageop.so
+lib/python2.2/lib-dynload/rgbimgmodule.so
--- lang/python/pkg/PFRAG.mm
+++
@@ -1,4 +0,0 @@
-@comment $OpenBSD: PFRAG.mm,v 1.7 2001/06/05 17:00:25 jasoni Exp $
-lib/python2.1/lib-dynload/audioop.so
-lib/python2.1/lib-dynload/imageop.so
-lib/python2.1/lib-dynload/rgbimgmodule.so
