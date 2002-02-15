1d0df36da036a31b2569e87a60dcdd0488b21f28
mark <mark@openbsd.org>
2002-02-15 08:45:43 +0000

    __ELF__ is defined in OpenBSD 2.9+ (brad@)
    hack filed (#139) with GNU Emacs dev team

--- editors/emacs21/patches/patch-src_s_openbsd.h
+++ editors/emacs21/patches/patch-src_s_openbsd.h
@@ -1,10 +1,10 @@
---- src/s/openbsd.h.orig	Wed Mar 22 07:08:40 2000
+--- src/s/openbsd.h~	Wed Mar 22 07:08:40 2000
 +++ src/s/openbsd.h	Wed Jan 30 18:53:08 2002
 @@ -3,6 +3,10 @@
  /* Get most of the stuff from bsd4.3 */
  #include "bsd4-3.h"
  
-+#if defined(__OpenBSD__) && defined(__powerpc__)
++#if defined(__OpenBSD__) && defined(__powerpc__) && !defined(__ELF__)
 +#define __ELF__
 +#endif
 +
