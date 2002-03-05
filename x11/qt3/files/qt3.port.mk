8d679e5adc9e04e07f0110adf8a46e2270ec6b47
espie <espie@openbsd.org>
2002-03-05 14:46:32 +0000

    More tweaks, I forgot about designer, so some binaries must be renamed to avoid conflicts. This also means more stuff in qt3 module. Plus, create missing pluging directory in PLIST.

--- x11/qt3/files/qt3.port.mk
+++ x11/qt3/files/qt3.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: qt3.port.mk,v 1.1.1.1 2002/03/04 16:13:04 espie Exp $
+# $OpenBSD: qt3.port.mk,v 1.2 2002/03/05 14:46:32 espie Exp $
 
 # This fragment uses MODQT_* variables to make it easier to substitute
 # qt1/qt2/qt3 in a port.
@@ -7,9 +7,10 @@ LIB_DEPENDS+=lib/qt3/qt.3::x11/qt3
 MODQT_LIBDIR=	${LOCALBASE}/lib/qt3
 MODQT_INCDIR=	${LOCALBASE}/include/X11/qt3
 MODQT_MOC=	${LOCALBASE}/bin/moc3
+MODQT_UIC=	${LOCALBASE}/bin/uic3
 MODQT_CONFIGURE_ARGS=	--with-qt-includes=${MODQT_INCDIR} \
 			--with-qt-libraries=${MODQT_LIBDIR}
-_MODQT_SETUP=	MOC=${MODQT_MOC} \
+_MODQT_SETUP=	MOC=${MODQT_MOC} UIC=${MODQT_UIC} \
 		MODQT_INCDIR=${MODQT_INCDIR} \
 		MODQT_LIBDIR=${MODQT_LIBDIR}
 
