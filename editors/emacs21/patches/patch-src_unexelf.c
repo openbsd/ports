c96b34102a249b6b531a2d519fb25ecc673bb40e
pvalchev <pvalchev@openbsd.org>
2002-03-23 19:13:14 +0000

    alpha pieces, not working right yet

--- editors/emacs21/patches/patch-src_unexelf.c
+++ editors/emacs21/patches/patch-src_unexelf.c
@@ -1,15 +1,15 @@
---- src/unexelf.c.orig	Tue Sep 26 09:01:57 2000
-+++ src/unexelf.c	Wed Jan 30 18:56:57 2002
-@@ -466,7 +466,7 @@
+--- src/unexelf.c.orig	Tue Sep 26 07:01:57 2000
++++ src/unexelf.c	Fri Mar 22 23:02:09 2002
+@@ -466,7 +466,7 @@ typedef struct {
  #define hdrNil ((pHDRR)0)
  #endif
  
 -#ifdef __NetBSD__
-+#if __NetBSD__
++#if defined (__NetBSD__) || defined (__OpenBSD__)
  /*
   * NetBSD does not have normal-looking user-land ELF support.
   */
-@@ -520,10 +520,12 @@
+@@ -520,10 +520,12 @@ typedef struct {
  # else
  #  define ElfBitsW(bits, type) Elf/**/bits/**/_/**/type
  # endif
