ceb86778f61412f3bac1a9cde81de8c8efaad311
reinhard <reinhard@openbsd.org>
2002-05-25 10:38:58 +0000

    update to nessus-1.2.0; thx John Benninghoff, Srebrenko Sehic

--- security/nessus/files/nmap.h
+++ security/nessus/files/nmap.h
@@ -1,4 +1,5 @@
-/*
+/* $OpenBSD: nmap.h,v 1.3 2002/05/25 10:38:58 reinhard Exp $
+ *
  *  Replace the configure-generated nmap.h so that we can convince
  *  nmap_wrapper.nes to build regardless of nmap's presence.
  *
@@ -8,10 +9,10 @@
 #ifndef NMAP_H__
 #define NMAP_H__
 
-#define NMAP "/usr/local/bin/nmap"
+#define NMAP "@@PREFIX@@/bin/nmap"
 #define NMAP_VERSION "2.3"
 #define NMAP_MAJOR 2
 #define NMAP_MINOR 3
-#define NMAP_BETA 
+#define NMAP_BETA
 
 #endif
