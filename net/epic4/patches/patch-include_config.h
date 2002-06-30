1965b4de7fced3c25a8e17c97b8925c77c55565a
brad <brad@openbsd.org>
2002-06-30 18:23:31 +0000

    - disable auto rejoin - add RCS ids to patches

--- net/epic4/patches/patch-include_config.h
+++ net/epic4/patches/patch-include_config.h
@@ -1,5 +1,15 @@
---- include/config.h.orig	Thu Dec  7 13:24:34 2000
-+++ include/config.h	Tue Dec 12 17:17:12 2000
+$OpenBSD: patch-include_config.h,v 1.4 2002/06/30 18:23:31 brad Exp $
+--- include/config.h.orig	Sun Jun 30 14:10:23 2002
++++ include/config.h	Sun Jun 30 14:10:10 2002
+@@ -316,7 +316,7 @@
+ #define DEFAULT_AUTO_NEW_NICK 1
+ #define DEFAULT_AUTO_RECONNECT 1
+ #define DEFAULT_AUTO_RECONNECT_DELAY 0
+-#define DEFAULT_AUTO_REJOIN 1
++#define DEFAULT_AUTO_REJOIN 0
+ #define DEFAULT_AUTO_REJOIN_DELAY 0
+ #define DEFAULT_AUTO_UNMARK_AWAY 0
+ #define DEFAULT_AUTO_WHOWAS 1
 @@ -510,7 +510,7 @@
  #undef EPIC_DEBUG		/* force coredump on panic */
  #define EXEC_COMMAND		/* allow /exec comamnd */
