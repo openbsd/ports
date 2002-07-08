6d9afe66ca195d319f754299da90bd76c86812bf
kevlo <kevlo@openbsd.org>
2002-07-08 04:43:42 +0000

    update to version 1.0.3

--- graphics/indexpage/patches/patch-indexpage.pl
+++
@@ -1,8 +0,0 @@
---- indexpage.pl.orig	Wed Jun 13 10:37:14 2001
-+++ indexpage.pl	Wed Jun 13 10:37:25 2001
-@@ -1,4 +1,4 @@
--#!/sw/local/bin/perl -w
-+#!/usr/bin/perl -w
- # $Id: patch-indexpage.pl,v 1.1.1.1 2001/06/13 03:02:36 kevlo Exp $
- #
- # This script generates one or more index pages and the corresponding
