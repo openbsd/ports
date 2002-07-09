36c67a1951d70fdd31ae6893e1672df383dcf71d
jakob <jakob@openbsd.org>
2002-07-09 19:34:49 +0000

    add patch to fix blackbox mode. ok brad@

--- mail/c-client/patches/patch-src_osdep_unix_env_unix.c
+++ mail/c-client/patches/patch-src_osdep_unix_env_unix.c
@@ -1,7 +1,23 @@
-$OpenBSD: patch-src_osdep_unix_env_unix.c,v 1.4 2001/11/19 01:56:13 brad Exp $
---- src/osdep/unix/env_unix.c.orig	Wed Oct 17 23:35:20 2001
-+++ src/osdep/unix/env_unix.c	Sat Nov 17 19:06:47 2001
-@@ -1001,7 +1001,8 @@ long dotlock_lock (char *file,DOTLOCK *b
+$OpenBSD: patch-src_osdep_unix_env_unix.c,v 1.5 2002/07/09 19:34:49 jakob Exp $
+
+--- src/osdep/unix/env_unix.c.orig	Thu Dec 21 01:12:13 2000
++++ src/osdep/unix/env_unix.c	Thu Jan 18 16:11:09 2001
+@@ -767,14 +767,12 @@
+ static char *mymailboxdir ()
+ {
+   char *home = myhomedir ();
+-  if (!myMailboxDir && home) {	/* initialize if first time */
+     if (mailsubdir) {
+       char tmp[MAILTMPLEN];
+       sprintf (tmp,"%s/%s",home,mailsubdir);
+       myMailboxDir = cpystr (tmp);/* use pre-defined subdirectory of home */
+     }
+     else myMailboxDir = cpystr (home);
+-  }
+   return myMailboxDir ? myMailboxDir : "";
+ }
+
+@@ -971,7 +971,8 @@ long dotlock_lock (char *file,DOTLOCK *b
        }
        close (pi[0]); close (pi[1]);
      }
@@ -11,3 +27,4 @@ $OpenBSD: patch-src_osdep_unix_env_unix.c,v 1.4 2001/11/19 01:56:13 brad Exp $
        sprintf (tmp,"Mailbox vulnerable - directory %.80s",base->lock);
        if (s = strrchr (tmp,'/')) *s = '\0';
        strcat (tmp," must have 1777 protection");
+ 
