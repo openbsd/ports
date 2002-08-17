1d8237d1471679cdee5e917e0a4bceea28886fb7
brad <brad@openbsd.org>
2002-08-17 05:11:09 +0000

    - respect SYSCONFDIR - add RCS ids to patches

--- audio/rplay/patches/patch-rplayd_cdrom.c
+++ audio/rplay/patches/patch-rplayd_cdrom.c
@@ -1,6 +1,7 @@
---- rplayd/cdrom.c.orig	Mon Feb  5 15:15:21 2001
-+++ rplayd/cdrom.c	Mon Feb  5 15:19:17 2001
-@@ -46,14 +46,18 @@
+$OpenBSD: patch-rplayd_cdrom.c,v 1.3 2002/08/17 05:11:09 brad Exp $
+--- rplayd/cdrom.c.orig	Wed Mar 10 02:58:02 1999
++++ rplayd/cdrom.c	Sat Aug 17 01:04:17 2002
+@@ -46,14 +46,18 @@ CDROM_TABLE cdrom_table[MAX_CDROMS] =
      {"cdrom1:", "/vol/dev/aliases/cdrom1", 0, 0},
      {"cdrom2:", "/vol/dev/aliases/cdrom2", 0, 0},
      {"cdrom3:", "/vol/dev/aliases/cdrom3", 0, 0},
