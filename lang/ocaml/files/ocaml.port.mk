ce795aa2f0f42d2a963c065c5b53bc7b90183c51
sturm <sturm@openbsd.org>
2002-11-22 15:13:42 +0000

    {RUN,BUILD}_DEPENDS+= :ocaml-*:lang/ocaml -> ::lang/ocaml

--- lang/ocaml/files/ocaml.port.mk
+++ lang/ocaml/files/ocaml.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: ocaml.port.mk,v 1.1 2002/11/13 21:42:15 naddy Exp $
+# $OpenBSD: ocaml.port.mk,v 1.2 2002/11/22 15:13:42 sturm Exp $
 
 # use PFRAG.native for extra files produced by the native code compiler
 
@@ -8,8 +8,8 @@ MODOCAML_NATIVE=Yes
 SED_PLIST+=	|sed -e '/^%%native%%$$/r${PKGDIR}/PFRAG.native' -e '//d'
 .else
 MODOCAML_NATIVE=No
-RUN_DEPENDS+=	:ocaml-*:lang/ocaml
+RUN_DEPENDS+=	::lang/ocaml
 SED_PLIST+=	|sed -e '/^%%native%%$$/d'
 .endif
 
-BUILD_DEPENDS+=	:ocaml-*:lang/ocaml
+BUILD_DEPENDS+=	::lang/ocaml
