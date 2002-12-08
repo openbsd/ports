3a8ee30d3c30f36e6d76ddb9172e1f2ec06db3f7
brad <brad@openbsd.org>
2002-12-08 04:17:39 +0000

    be more flexible with the gmake version we will accept.

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Fundamental; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-#	$OpenBSD: bsd.port.mk,v 1.536 2002/10/10 21:14:11 naddy Exp $
+#	$OpenBSD: bsd.port.mk,v 1.537 2002/12/08 04:17:39 brad Exp $
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -346,7 +346,7 @@ CONFIGURE_STYLE?=
 SYSCONFDIR?=	/etc
 USE_GMAKE?=		No
 .if ${USE_GMAKE:L} == "yes"
-BUILD_DEPENDS+=		::devel/gmake
+BUILD_DEPENDS+=		:gmake-*:devel/gmake
 MAKE_PROGRAM=		${GMAKE}
 .else
 MAKE_PROGRAM=		${MAKE}
