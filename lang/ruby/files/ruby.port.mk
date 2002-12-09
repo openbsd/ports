d354d26f2bda682a31f66cd23499ff45a307dcc5
couderc <couderc@openbsd.org>
2002-12-09 20:26:36 +0000

    Added doc and example directories

--- lang/ruby/files/ruby.port.mk
+++ lang/ruby/files/ruby.port.mk
@@ -1,11 +1,13 @@
-# $OpenBSD: ruby.port.mk,v 1.1 2002/03/17 22:29:02 couderc Exp $
+# $OpenBSD: ruby.port.mk,v 1.2 2002/12/09 20:26:36 couderc Exp $
 
 # ruby module
 
 RUN_DEPENDS+=::lang/ruby
 LIB_DEPENDS+=ruby.1.66::lang/ruby
 
-MODRUBY_LIBDIR=	${LOCALBASE}/lib/ruby
+MODRUBY_LIBDIR=		${LOCALBASE}/lib/ruby
+MODRUBY_DOCDIR=		${PREFIX}/share/doc/ruby
+MODRUBY_EXAMPLEDIR=	${PREFIX}/share/examples/ruby
 
 CONFIGURE_STYLE?=	simple
 CONFIGURE_SCRIPT?=	${LOCALBASE}/bin/ruby extconf.rb
