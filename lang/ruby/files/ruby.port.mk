d7a1517f028194cbaae5fbdcf046efbcdf61bcf3
couderc <couderc@openbsd.org>
2002-12-09 21:04:08 +0000

    add comments

--- lang/ruby/files/ruby.port.mk
+++ lang/ruby/files/ruby.port.mk
@@ -1,11 +1,15 @@
-# $OpenBSD: ruby.port.mk,v 1.2 2002/12/09 20:26:36 couderc Exp $
+# $OpenBSD: ruby.port.mk,v 1.3 2002/12/09 21:04:08 couderc Exp $
 
 # ruby module
 
 RUN_DEPENDS+=::lang/ruby
 LIB_DEPENDS+=ruby.1.66::lang/ruby
 
+# location of ruby libraries
 MODRUBY_LIBDIR=		${LOCALBASE}/lib/ruby
+
+# common directories for ruby extensions
+# used to create docs and examples install path
 MODRUBY_DOCDIR=		${PREFIX}/share/doc/ruby
 MODRUBY_EXAMPLEDIR=	${PREFIX}/share/examples/ruby
 
