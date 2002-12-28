d4b4262c4c2d6bf6cfa24bdab31d98c49d81000e
pvalchev <pvalchev@openbsd.org>
2002-12-28 23:25:32 +0000

    sync linenumbers etc

--- graphics/gtksee/patches/patch-src_detect.c
+++ graphics/gtksee/patches/patch-src_detect.c
@@ -1,6 +1,6 @@
---- src/detect.c.orig	Thu Mar  1 05:40:43 2001
-+++ src/detect.c	Thu Mar  1 05:41:21 2001
-@@ -62,8 +62,6 @@
+--- src/detect.c.orig	Fri Sep 24 09:05:55 1999
++++ src/detect.c	Sat Dec 28 15:15:06 2002
+@@ -62,8 +62,6 @@ detect_jpeg(guchar *header, guchar *file
  {
  #ifdef HAVE_LIBJPEG
  	jpeg_info jinfo;
