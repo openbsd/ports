8b5616f55f0816e0f05826d9360ed3bcf424cf39
espie <espie@openbsd.org>
2002-12-31 02:09:51 +0000

    Bring mt support to MODULE. Fix tricky issue: basic tools must be compiled with -mt as well, otherwise they won't be able to use plugins (and for instance, kde needs uic plugins for its widgets)

--- x11/qt3/files/qt3.port.mk
+++ x11/qt3/files/qt3.port.mk
@@ -1,14 +1,11 @@
-# $OpenBSD: qt3.port.mk,v 1.3 2002/12/29 19:24:53 espie Exp $
+# $OpenBSD: qt3.port.mk,v 1.4 2002/12/31 02:09:51 espie Exp $
 
 # This fragment uses MODQT_* variables to make it easier to substitute
 # qt1/qt2/qt3 in a port.
-LIB_DEPENDS+=lib/qt3/qt.3::x11/qt3
-
 MODQT_LIBDIR=	${LOCALBASE}/lib/qt3
 MODQT_INCDIR=	${LOCALBASE}/include/X11/qt3
-MODQT_MOC=	${LOCALBASE}/bin/moc3
-MODQT_UIC=	${LOCALBASE}/bin/uic3
 MODQT_OVERRIDE_UIC?=Yes
+MODQT_MT?=No
 MODQT_CONFIGURE_ARGS=	--with-qt-includes=${MODQT_INCDIR} \
 			--with-qt-libraries=${MODQT_LIBDIR}
 _MODQT_SETUP=	MOC=${MODQT_MOC} \
@@ -16,6 +13,19 @@ _MODQT_SETUP=	MOC=${MODQT_MOC} \
 		MODQT_LIBDIR=${MODQT_LIBDIR}
 .if ${MODQT_OVERRIDE_UIC:L} == "yes"
 _MODQT_SETUP+=	UIC=${MODQT_UIC}
+.endif
+
+.if ${MODQT_MT:L} == "yes"
+LIB_DEPENDS+=lib/qt3/qt-mt.3::x11/qt3,mt
+# for includes
+BUILD_DEPENDS+=::x11/qt3
+# may be needed to find plugins
+MODQT_MOC=	${LOCALBASE}/bin/moc3-mt
+MODQT_UIC=	${LOCALBASE}/bin/uic3-mt
+.else
+LIB_DEPENDS+=lib/qt3/qt.3::x11/qt3
+MODQT_MOC=	${LOCALBASE}/bin/moc3
+MODQT_UIC=	${LOCALBASE}/bin/uic3
 .endif
 
 CONFIGURE_ENV+=	${_MODQT_SETUP}
