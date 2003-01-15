e92a970941d1a249d3501e49fe6d4e009985a464
mickey <mickey@openbsd.org>
2003-01-15 20:46:56 +0000

    workaround xsnprintf() wackieness found w/ pp; brad@ ok

--- shells/tcsh/patches/patch-sh.exp.c
+++ shells/tcsh/patches/patch-sh.exp.c
@@ -1,4 +1,4 @@
-$OpenBSD: patch-sh.exp.c,v 1.2 2002/07/24 01:12:32 brad Exp $
+$OpenBSD: patch-sh.exp.c,v 1.3 2003/01/15 20:46:56 mickey Exp $
 --- sh.exp.c.orig	Fri Mar  8 12:36:46 2002
 +++ sh.exp.c	Tue Jul 23 20:49:50 2002
 @@ -153,7 +153,7 @@ sh_access(fname, mode)
@@ -10,3 +10,12 @@ $OpenBSD: patch-sh.exp.c,v 1.2 2002/07/24 01:12:32 brad Exp $
  #    define GID_T gid_t
  #   else
  #    define GID_T int
+@@ -661,7 +661,7 @@
+     bool altout = 0;
+     Char *ft = cp, *dp, *ep, *strdev, *strino, *strF, *str, valtest = '\0',
+     *errval = STR0;
+-    char *string, string0[8];
++    char *string, string0[16];
+     time_t footime;
+     struct passwd *pw;
+     struct group *gr;
