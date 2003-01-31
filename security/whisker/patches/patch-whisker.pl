33a87836a6aa0a0c7f61450baa1cfdc64361f839
margarida <margarida@openbsd.org>
2003-01-31 05:11:57 +0000

    Update to version 2.1. From new MAINTAINER Srebrenko Sehic <haver@insecure.dk>
    brad@ ok

--- security/whisker/patches/patch-whisker.pl
+++ security/whisker/patches/patch-whisker.pl
@@ -1,31 +1,20 @@
---- whisker.pl.orig	Sun Aug  6 06:17:49 2000
-+++ whisker.pl	Sun Aug  6 06:23:40 2000
-@@ -2,8 +2,8 @@
- require 5.004; # sorry, just 5.003 has issues...
+--- whisker.pl.orig	Wed Dec 25 16:40:45 2002
++++ whisker.pl	Wed Dec 25 16:41:53 2002
+@@ -32,7 +32,7 @@
+ # Set to 0 if you don't want to see the "Whisker not officially
+ # installed.." message, only seen if the WHISKER_DIR does not exist
  
- $whisker_version="1.4.0";
--$WHEREIS_WHISKER=$0;
--$WHEREIS_WHISKER=~s/[^\/\\]+$//; 
-+# where the db files are
-+$WHEREIS_WHISKER="!!PREFIX!!/share/whisker/";
- $GLOBAL_HTMLOUTPUT=0;
+-$WHISKER_INSTALL_WARNING = 1;
++$WHISKER_INSTALL_WARNING = 0;
  
- use Socket; use Getopt::Std;
-@@ -75,7 +75,7 @@ print qq~
- 	-h+ *scan single host (IP or domain)
- 	-H+ *host list to scan (file)
- 	-F+ *(for unix multi-threaded front end use only)
--	-s+  specifies the script database file (defaults to scan.db)
-+	-s   specifies the script database file (defaults to scan.db)
- 	-V   use virtual hosts when possible
- 	-p+  specify a different default port to use
- 	-S+  force server version (e.g. -S "Apache/1.3.6")
-@@ -574,7 +574,7 @@ if($D{'XXRescanDumb'}>0 && $GLOBAL_WHISK
- $GLOBAL_WHISKER_LOOP_CONTROL=1;
- $nmapfile=$singlehost="";
- $hostsfile="dumb$$.lst"; # we made a list of dumb servers
--$dbfile="dumb.db";
-+$dbfile=$WHEREIS_WHISKER."dumb.db";
- $D{'XXRescanDumb'}=0; $GLOBAL_WHISKER_NOMORE_DUMB=1;}
+ ### END CONFIG SECTION ##################################################
  
- } # this is the $GLOBAL_WHISKER_LOOP_CONTROL while() loop
+@@ -58,7 +58,7 @@
+ eval 'use LW';
+ if($@){	$LW=0;
+     if(-e $WHISKER_DIR.'LW.pm'){
+-	eval "require $WHISKER_DIR".'LW.pm';
++	eval "require \'$WHISKER_DIR".'LW.pm\'';
+ 	if(!$@){$LW++;}}
+ } else { $LW++; }
+ if(!$LW){
