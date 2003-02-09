db749bfd4bebbca27bbb12cd1a1b53c03c7f86ec
lebel <lebel@openbsd.org>
2003-02-09 23:10:11 +0000

    allow g77 as a MODGCC3_LANGS

--- lang/egcs/stable/files/gcc3.port.mk
+++ lang/egcs/stable/files/gcc3.port.mk
@@ -1,9 +1,10 @@
-# $OpenBSD: gcc3.port.mk,v 1.2 2002/11/12 21:14:24 pvalchev Exp $
+# $OpenBSD: gcc3.port.mk,v 1.3 2003/02/09 23:10:11 lebel Exp $
 
 MODGCC3_ARCHES?=
 # Supported languages for now
 _MODGCC3CC=	cc
 _MODGCC3CXX=	c++
+_MODGCC3G77=	g77
 # Always include support for this
 MODGCC3_LANGS+=	${_MODGCC3CC}
 
@@ -22,6 +23,10 @@ LIB_DEPENDS+=	estdc++.5::lang/egcs/stable,-estdc
 MODGCC3_post-patch+= ln -s ${LOCALBASE}/bin/e${_MODGCC3CXX} ${WRKDIR}/bin/g++;
 MODGCC3_post-patch+= ln -s ${LOCALBASE}/bin/e${_MODGCC3CXX} ${WRKDIR}/bin/${_MODGCC3CXX};
 .        endif
+.        if !empty(_MODGCC3G77:L:M${_j})
+BUILD_DEPENDS+=	::lang/egcs/stable,-g77
+MODGCC3_post-patch+= ln -s ${LOCALBASE}/bin/e${_MODGCC3G77} ${WRKDIR}/bin/${_MODGCC3G77};
+.	 endif
 .      endfor
 .    endif
 .  endfor
