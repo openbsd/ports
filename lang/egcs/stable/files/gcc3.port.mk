5ac2ac9ad8ed65c6d98508b3397fa5f3916b3f12
pvalchev <pvalchev@openbsd.org>
2003-02-10 03:29:28 +0000

    make a link to f77 too, not only g77, in case the fortran off gcc3 is desired (since some apps look for it under that name)

--- lang/egcs/stable/files/gcc3.port.mk
+++ lang/egcs/stable/files/gcc3.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: gcc3.port.mk,v 1.3 2003/02/09 23:10:11 lebel Exp $
+# $OpenBSD: gcc3.port.mk,v 1.4 2003/02/10 03:29:28 pvalchev Exp $
 
 MODGCC3_ARCHES?=
 # Supported languages for now
@@ -25,6 +25,7 @@ MODGCC3_post-patch+= ln -s ${LOCALBASE}/bin/e${_MODGCC3CXX} ${WRKDIR}/bin/${_MOD
 .        endif
 .        if !empty(_MODGCC3G77:L:M${_j})
 BUILD_DEPENDS+=	::lang/egcs/stable,-g77
+MODGCC3_post-patch+= ln -s ${LOCALBASE}/bin/e${_MODGCC3G77} ${WRKDIR}/bin/f77;
 MODGCC3_post-patch+= ln -s ${LOCALBASE}/bin/e${_MODGCC3G77} ${WRKDIR}/bin/${_MODGCC3G77};
 .	 endif
 .      endfor
