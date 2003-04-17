39f5133305bb1756b2d292743e3b1992b46b4421
jakob <jakob@openbsd.org>
2003-04-17 08:06:29 +0000

    update to imap-uw release 2002c.

--- mail/imap-uw/patches/patch-src_osdep_unix_env_unix.c
+++ mail/imap-uw/patches/patch-src_osdep_unix_env_unix.c
@@ -1,12 +1,13 @@
-$OpenBSD: patch-src_osdep_unix_env_unix.c,v 1.3 2002/09/18 08:19:06 jakob Exp $
+$OpenBSD: patch-src_osdep_unix_env_unix.c,v 1.4 2003/04/17 08:06:30 jakob Exp $
 
---- src/osdep/unix/env_unix.c.orig	Tue Sep  3 08:39:10 2002
-+++ src/osdep/unix/env_unix.c	Sun Sep  8 14:55:34 2002
-@@ -773,14 +773,12 @@ char *myhomedir ()
+--- src/osdep/unix/env_unix.c.orig	Wed Apr 16 23:03:26 2003
++++ src/osdep/unix/env_unix.c	Thu Apr 17 09:21:27 2003
+@@ -783,15 +783,12 @@ char *myhomedir ()
  static char *mymailboxdir ()
  {
    char *home = myhomedir ();
--  if (!myMailboxDir && home) {	/* initialize if first time */
+-				/* initialize if first time */
+-  if (!myMailboxDir && myHomeDir) {
      if (mailsubdir) {
        char tmp[MAILTMPLEN];
        sprintf (tmp,"%s/%s",home,mailsubdir);
@@ -17,13 +18,3 @@ $OpenBSD: patch-src_osdep_unix_env_unix.c,v 1.3 2002/09/18 08:19:06 jakob Exp $
    return myMailboxDir ? myMailboxDir : "";
  }
  
-@@ -1036,7 +1034,8 @@ long dotlock_lock (char *file,DOTLOCK *b
-       }
-       close (pi[0]); close (pi[1]);
-     }
--    if (lockEaccesError) {	/* punt silently if paranoid site */
-+    if (strncmp(base->lock,"/var/mail/",10) && lockEaccesError) {
-+	/* punt silently if paranoid site */
-       sprintf (tmp,"Mailbox vulnerable - directory %.80s",base->lock);
-       if (s = strrchr (tmp,'/')) *s = '\0';
-       strcat (tmp," must have 1777 protection");
