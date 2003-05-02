d2144109861c2789c9ac214a72031f4c403ba3c2
avsm <avsm@openbsd.org>
2003-05-02 19:50:22 +0000

    all the pear stuff goes into /var/www/pear

--- www/php4/pear/files/pear.port.mk
+++ www/php4/pear/files/pear.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: pear.port.mk,v 1.2 2003/04/03 11:10:40 avsm Exp $
+# $OpenBSD: pear.port.mk,v 1.3 2003/05/02 19:50:22 avsm Exp $
 # PHP PEAR module
 
 RUN_DEPENDS+=    pear:php4-pear-4.3.*:www/php4/pear
@@ -10,3 +10,5 @@ NO_CONFIGURE=	Yes
 
 MAKE_FILE=	${PORTSDIR}/www/php4/pear/files/Makefile.pear
 FAKE_FLAGS+=	WRKINST=${WRKINST} WRKDIR=${WRKDIR}
+
+PREFIX=		/var/www
