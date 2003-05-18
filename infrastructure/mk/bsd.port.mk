9c77c97ed33cad12eb4fdca88183f226cb7308a5
sturm <sturm@openbsd.org>
2003-05-18 23:11:21 +0000

    As "run-depends" is not recursive, only the first layer of pure RUN_DEPENDS packages got created. By setting DEPENDS_TARGET=install, all layers of pure RUN_DEPENDS are installed directly.
    OK espie@, naddy@

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Fundamental; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-#	$OpenBSD: bsd.port.mk,v 1.544 2003/04/06 14:34:36 espie Exp $
+#	$OpenBSD: bsd.port.mk,v 1.545 2003/05/18 23:11:21 sturm Exp $
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -1861,7 +1861,7 @@ ${_FAKE_COOKIE}: ${_BUILD_COOKIE} ${WRKPKG}/mtree.spec
 # The real install
 
 ${_INSTALL_COOKIE}:  ${_PACKAGE_COOKIES}
-	@cd ${.CURDIR} && DEPENDS_TARGET=package exec ${MAKE} run-depends lib-depends
+	@cd ${.CURDIR} && DEPENDS_TARGET=install exec ${MAKE} run-depends lib-depends
 	@${ECHO_MSG} "===>  Installing ${FULLPKGNAME${SUBPACKAGE}} from ${PKGFILE${SUBPACKAGE}}"
 .  for _m in ${MODULES}
 .    if defined(MOD${_m:U}_pre_install)
