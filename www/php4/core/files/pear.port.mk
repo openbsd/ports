430bac2e14b7acc3c355eeac05e43a6ffc62a1b0
avsm <avsm@openbsd.org>
2003-07-04 17:35:57 +0000

    no need for NO_CONFIGURE from naddy@, Xavier Santolaria <xavier@santolaria.net>

--- www/php4/core/files/pear.port.mk
+++ www/php4/core/files/pear.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: pear.port.mk,v 1.2 2003/05/23 21:17:29 avsm Exp $
+# $OpenBSD: pear.port.mk,v 1.3 2003/07/04 17:35:57 avsm Exp $
 # PHP PEAR module
 
 RUN_DEPENDS+=    :php4-pear-4.3.*:www/php4/core,-pear
@@ -6,7 +6,6 @@ BUILD_DEPENDS+=  :php4-pear-4.3.*:www/php4/core,-pear
 
 NO_BUILD=       Yes
 NO_REGRESS=     Yes
-NO_CONFIGURE=	Yes
 
 MAKE_FILE=	${PORTSDIR}/www/php4/core/files/Makefile.pear
 FAKE_FLAGS+=	WRKINST=${WRKINST} WRKDIR=${WRKDIR}
