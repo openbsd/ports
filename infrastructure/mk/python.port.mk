706f3c88e2aeb90de382a00dfafd61f7986af7d8
sturm <sturm@openbsd.org>
2003-07-28 17:48:13 +0000

    python module for all standard python ports
    from Xavier Santolaria <xavier@santolaria.net>

---
+++ infrastructure/mk/python.port.mk
@@ -0,0 +1,2 @@
+# $OpenBSD: python.port.mk,v 1.1 2003/07/28 17:48:13 sturm Exp $
+.include "${PORTSDIR}/lang/python/python.port.mk"
---
+++ lang/python/python.port.mk
@@ -0,0 +1,33 @@
+# $OpenBSD: python.port.mk,v 1.1 2003/07/28 17:48:13 sturm Exp $
+
+MODPY_VERSION?=		2.2
+
+_MODPY_BUILD_DEPENDS=	:python-${MODPY_VERSION}*:lang/python/${MODPY_VERSION}
+
+BUILD_DEPENDS+=		${_MODPY_BUILD_DEPENDS}
+RUN_DEPENDS+=		${_MODPY_BUILD_DEPENDS}
+
+MODPY_BIN=		${LOCALBASE}/bin/python${MODPY_VERSION}
+
+# usually setup.py but Setup.py can be found too
+MODPY_SETUP?=		setup.py
+
+# build or build_ext are commonly used
+MODPY_DISTUTILS_BUILD?=		build --build-base=${WRKSRC}
+MODPY_DISTUTILS_INSTALL?=	install --prefix=${PREFIX}
+
+MODPY_CMD=	@cd ${WRKSRC} && ${MODPY_BIN} ./${MODPY_SETUP}
+
+SUBST_VARS+=	MODPY_VERSION
+
+# dirty way to do it with no modifications in bsd.port.mk
+.if !target(do-build)
+do-build:
+	${MODPY_CMD} ${MODPY_DISTUTILS_BUILD} ${MODPY_DISTUTILS_BUILDARGS}
+.endif
+
+# extra documentation or scripts should be installed via post-install
+.if !target(do-install)
+do-install:
+	${MODPY_CMD} ${MODPY_DISTUTILS_INSTALL} ${MODPY_DISTUTILS_INSTALLARGS}
+.endif
