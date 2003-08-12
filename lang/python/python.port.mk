8238657cc77cac8ebf7e7493325d19661c62a7f0
sturm <sturm@openbsd.org>
2003-08-12 20:23:20 +0000

    - add new variables MODPY_LIBDIR and MODPY_SITEPKG - make MODPY_CMD intern
    from Xavier Santolaria <xavier@santolaria.net>

--- lang/python/python.port.mk
+++ lang/python/python.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: python.port.mk,v 1.3 2003/07/31 07:01:21 jolan Exp $
+# $OpenBSD: python.port.mk,v 1.4 2003/08/12 20:23:20 sturm Exp $
 #
 #	python.port.mk - Xavier Santolaria <xavier@santolaria.net>
 #	This file is in the public domain.
@@ -11,6 +11,8 @@ BUILD_DEPENDS+=		${_MODPY_BUILD_DEPENDS}
 RUN_DEPENDS+=		${_MODPY_BUILD_DEPENDS}
 
 MODPY_BIN=		${LOCALBASE}/bin/python${MODPY_VERSION}
+MODPY_LIBDIR=		${LOCALBASE}/lib/python${MODPY_VERSION}
+MODPY_SITEPKG=		${PREFIX}/lib/python${MODPY_VERSION}/site-packages
 
 # usually setup.py but Setup.py can be found too
 MODPY_SETUP?=		setup.py
@@ -19,7 +21,7 @@ MODPY_SETUP?=		setup.py
 MODPY_DISTUTILS_BUILD?=		build --build-base=${WRKSRC}
 MODPY_DISTUTILS_INSTALL?=	install --prefix=${PREFIX}
 
-MODPY_CMD=	@cd ${WRKSRC} && ${SETENV} ${MAKE_ENV} \
+_MODPY_CMD=	@cd ${WRKSRC} && ${SETENV} ${MAKE_ENV} \
 			${MODPY_BIN} ./${MODPY_SETUP}
 
 SUBST_VARS+=	MODPY_VERSION
@@ -27,11 +29,11 @@ SUBST_VARS+=	MODPY_VERSION
 # dirty way to do it with no modifications in bsd.port.mk
 .if !target(do-build)
 do-build:
-	${MODPY_CMD} ${MODPY_DISTUTILS_BUILD} ${MODPY_DISTUTILS_BUILDARGS}
+	${_MODPY_CMD} ${MODPY_DISTUTILS_BUILD} ${MODPY_DISTUTILS_BUILDARGS}
 .endif
 
 # extra documentation or scripts should be installed via post-install
 .if !target(do-install)
 do-install:
-	${MODPY_CMD} ${MODPY_DISTUTILS_INSTALL} ${MODPY_DISTUTILS_INSTALLARGS}
+	${_MODPY_CMD} ${MODPY_DISTUTILS_INSTALL} ${MODPY_DISTUTILS_INSTALLARGS}
 .endif
