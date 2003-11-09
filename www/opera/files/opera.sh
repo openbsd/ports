943ace84ccbaf05fa87ccbce31253eb51247520e
sturm <sturm@openbsd.org>
2003-11-09 09:46:26 +0000

    update to opera 7.21

--- www/opera/files/opera.sh
+++ www/opera/files/opera.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 #
-# $OpenBSD: opera.sh,v 1.4 2002/04/23 15:44:43 naddy Exp $
+# $OpenBSD: opera.sh,v 1.5 2003/11/09 09:46:26 sturm Exp $
 
 PLUGIN_PATH=@PREFIX@/lib/opera/plugins
 PLUGIN_PATH=${PLUGIN_PATH}:@LOCALBASE@/lib/ns-plugins
 PLUGIN_PATH=${PLUGIN_PATH}:${HOME}/.netscape/plugins
 PLUGIN_PATH=${PLUGIN_PATH}:@LOCALBASE@/netscape/plugins
+LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/libexec
 
 export OPERA_DIR=@PREFIX@/share/opera
 export OPERA_PLUGIN_PATH=${OPERA_PLUGIN_PATH-${PLUGIN_PATH}}
+export LD_LIBRARY_PATH
 
 exec @PREFIX@/libexec/opera "$@"
