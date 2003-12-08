5dbadd83c27e307d3b1edc8f783e74319f53123e
sturm <sturm@openbsd.org>
2003-12-08 17:37:52 +0000

    split import of versions v3 and v4 of Berkeley DB

---
+++ databases/db/v4/pkg/PFRAG.tcl
@@ -0,0 +1,45 @@
+@comment $OpenBSD: PFRAG.tcl,v 1.1.1.1 2003/12/08 17:37:55 sturm Exp $
+lib/db4/libdb_tcl.a
+lib/db4/libdb_tcl.la
+lib/db4/libdb_tcl.so.4.1
+share/doc/db4/api_tcl/db_close.html
+share/doc/db4/api_tcl/db_count.html
+share/doc/db4/api_tcl/db_cursor.html
+share/doc/db4/api_tcl/db_del.html
+share/doc/db4/api_tcl/db_get.html
+share/doc/db4/api_tcl/db_get_join.html
+share/doc/db4/api_tcl/db_get_type.html
+share/doc/db4/api_tcl/db_is_byteswapped.html
+share/doc/db4/api_tcl/db_join.html
+share/doc/db4/api_tcl/db_open.html
+share/doc/db4/api_tcl/db_put.html
+share/doc/db4/api_tcl/db_remove.html
+share/doc/db4/api_tcl/db_rename.html
+share/doc/db4/api_tcl/db_stat.html
+share/doc/db4/api_tcl/db_sync.html
+share/doc/db4/api_tcl/db_truncate.html
+share/doc/db4/api_tcl/dbc_close.html
+share/doc/db4/api_tcl/dbc_del.html
+share/doc/db4/api_tcl/dbc_dup.html
+share/doc/db4/api_tcl/dbc_get.html
+share/doc/db4/api_tcl/dbc_put.html
+share/doc/db4/api_tcl/env_close.html
+share/doc/db4/api_tcl/env_dbremove.html
+share/doc/db4/api_tcl/env_dbrename.html
+share/doc/db4/api_tcl/env_open.html
+share/doc/db4/api_tcl/env_remove.html
+share/doc/db4/api_tcl/pindex.src
+share/doc/db4/api_tcl/tcl_index.html
+share/doc/db4/api_tcl/tcl_pindex.html
+share/doc/db4/api_tcl/txn.html
+share/doc/db4/api_tcl/txn_abort.html
+share/doc/db4/api_tcl/txn_commit.html
+share/doc/db4/api_tcl/version.html
+share/doc/db4/ref/tcl/error.html
+share/doc/db4/ref/tcl/faq.html
+share/doc/db4/ref/tcl/intro.html
+share/doc/db4/ref/tcl/program.html
+share/doc/db4/ref/tcl/using.html
+share/doc/db4/ref/upgrade.3.1/tcl.html
+@dirrm share/doc/db4/ref/tcl
+@dirrm share/doc/db4/api_tcl
