30a52ed69bb2fc27719caec943ae2cbe017be7f3
sturm <sturm@openbsd.org>
2003-12-30 22:21:24 +0000

    update to python 2.2.3, lots of bugfixes
    from Aleksander Piotrowski <aleksander dot piotrowski at nic dot com dot pl>

--- lang/python/2.2/pkg/PFRAG.mm
+++ lang/python/2.2/pkg/PFRAG.mm
@@ -1,4 +1,6 @@
-@comment $OpenBSD: PFRAG.mm,v 1.1 2002/02/15 19:42:19 matt Exp $
+@comment $OpenBSD: PFRAG.mm,v 1.2 2003/12/30 22:21:25 sturm Exp $
 lib/python2.2/lib-dynload/audioop.so
+lib/python2.2/lib-dynload/dlmodule.so
 lib/python2.2/lib-dynload/imageop.so
+lib/python2.2/lib-dynload/mpz.so
 lib/python2.2/lib-dynload/rgbimgmodule.so
