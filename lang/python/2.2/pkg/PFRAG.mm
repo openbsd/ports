0926d072255f067adaa0941b1e1d54fa45bb9e8c
sturm <sturm@openbsd.org>
2004-01-03 17:32:57 +0000

    fix tk84 warnings in 2.1 and 2.2, bump PKGNAME, from jose@ spelling fixes in CHANGES.OpenBSD remove mpz.so from PFRAG.mm, it has its own SUBPACKAGE from maintainer Aleksander Piotrowski

--- lang/python/2.2/pkg/PFRAG.mm
+++ lang/python/2.2/pkg/PFRAG.mm
@@ -1,6 +1,5 @@
-@comment $OpenBSD: PFRAG.mm,v 1.2 2003/12/30 22:21:25 sturm Exp $
+@comment $OpenBSD: PFRAG.mm,v 1.3 2004/01/03 17:32:57 sturm Exp $
 lib/python2.2/lib-dynload/audioop.so
 lib/python2.2/lib-dynload/dlmodule.so
 lib/python2.2/lib-dynload/imageop.so
-lib/python2.2/lib-dynload/mpz.so
 lib/python2.2/lib-dynload/rgbimgmodule.so
