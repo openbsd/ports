f9f57467b27de1e30851a280a13db2c15326050b
avsm <avsm@openbsd.org>
2004-01-08 20:33:23 +0000

    Add support for sparc ghc. Hard work by Donald Stewart <dons at cse.unsw.edu.au>

--- lang/ghc/files/ghc.port.mk
+++ lang/ghc/files/ghc.port.mk
@@ -1,9 +1,9 @@
-# $OpenBSD: ghc.port.mk,v 1.1 2003/07/23 18:26:35 avsm Exp $
+# $OpenBSD: ghc.port.mk,v 1.2 2004/01/08 20:33:23 avsm Exp $
 # Module for Glasgow Haskell Compiler
 
 # Not yet ported to other architectures
 # See comments in lang/ghc/Makefile for more information
-ONLY_FOR_ARCHS=	i386
+ONLY_FOR_ARCHS=	i386 sparc
 
 BUILD_DEPENDS+=	bin/ghc::lang/ghc
 RUN_DEPENDS+=	bin/ghc::lang/ghc
