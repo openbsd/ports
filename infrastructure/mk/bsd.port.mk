ec7a4914596ffe28bcd9cca1a204ff826558febf
espie <espie@openbsd.org>
2004-02-07 22:02:40 +0000

    let show be a list of variables

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-#	$OpenBSD: bsd.port.mk,v 1.606 2004/02/01 23:07:30 espie Exp $
+#	$OpenBSD: bsd.port.mk,v 1.607 2004/02/07 22:02:40 espie Exp $
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -140,7 +140,9 @@ PERMIT_DISTFILES_FTP = No
 .if defined(show)
 .MAIN: show
 show:
-	@echo ${${show}:Q}
+.  for _s in ${show}
+	@echo ${${_s}:Q}
+.  endfor
 .elif defined(clean)
 .MAIN: clean
 .else
