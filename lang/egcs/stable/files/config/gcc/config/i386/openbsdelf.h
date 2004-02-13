048c67fd421d128a5bf0e6a784e685a9a7826f81
espie <espie@openbsd.org>
2004-02-13 19:25:03 +0000

    fix i386 built-ins.

--- lang/egcs/stable/files/config/gcc/config/i386/openbsdelf.h
+++ lang/egcs/stable/files/config/gcc/config/i386/openbsdelf.h
@@ -39,8 +39,6 @@ Boston, MA 02111-1307, USA.  */
   do						\
     {						\
     	OPENBSD_OS_CPP_BUILTINS_ELF();		\
-	builtin_define ("__unix__");		\
-	builtin_assert ("system=bsd");		\
     }						\
   while (0)
 
@@ -111,9 +109,7 @@ Boston, MA 02111-1307, USA.  */
 
 /* Assembler format: exception region output.  */
 
-/* All configurations that don't use elf must be explicit about not using
-   dwarf unwind information. egcs doesn't try too hard to check internal
-   configuration files...  */
+/* our configuration still doesn't handle dwarf2 correctly */
 #define DWARF2_UNWIND_INFO 0
 
 /* Assembler format: alignment output.  */
