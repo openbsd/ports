af55a4777003cac2d8df4ed1915a9e08e8b2b7f8
sturm <sturm@openbsd.org>
2004-02-15 21:02:02 +0000

    rest

--- net/rrdtool/pkg/PFRAG.tcl
+++ net/rrdtool/pkg/PFRAG.tcl
@@ -1,4 +1,4 @@
-@comment $OpenBSD: PFRAG.tcl,v 1.3 2000/09/06 19:17:23 danh Exp $
-lib/tclrrd${VERSION}.so
-lib/tclrrd${VERSION}/pkgIndex.tcl
-@dirrm lib/tclrrd${VERSION}
+@comment $OpenBSD: PFRAG.tcl,v 1.4 2004/02/15 21:02:02 sturm Exp $
+lib/tclrrd.${VERSION}.so
+lib/tclrrd.${VERSION}/pkgIndex.tcl
+@dirrm lib/tclrrd.${VERSION}
