c809d508c5b9eae34709060e3208f02e4985a682
brad <brad@openbsd.org>
2004-02-25 02:35:04 +0000

    upgrade to EPIC4 2.0

--- net/epic4/patches/patch-include_config.h
+++ net/epic4/patches/patch-include_config.h
@@ -1,16 +1,7 @@
-$OpenBSD: patch-include_config.h,v 1.4 2002/06/30 18:23:31 brad Exp $
---- include/config.h.orig	Sun Jun 30 14:10:23 2002
-+++ include/config.h	Sun Jun 30 14:10:10 2002
-@@ -316,7 +316,7 @@
- #define DEFAULT_AUTO_NEW_NICK 1
- #define DEFAULT_AUTO_RECONNECT 1
- #define DEFAULT_AUTO_RECONNECT_DELAY 0
--#define DEFAULT_AUTO_REJOIN 1
-+#define DEFAULT_AUTO_REJOIN 0
- #define DEFAULT_AUTO_REJOIN_DELAY 0
- #define DEFAULT_AUTO_UNMARK_AWAY 0
- #define DEFAULT_AUTO_WHOWAS 1
-@@ -510,7 +510,7 @@
+$OpenBSD: patch-include_config.h,v 1.5 2004/02/25 02:35:04 brad Exp $
+--- include/config.h.orig	2003-12-26 14:14:49.000000000 -0500
++++ include/config.h	2004-02-15 22:15:13.000000000 -0500
+@@ -539,7 +539,7 @@
  #undef EPIC_DEBUG		/* force coredump on panic */
  #define EXEC_COMMAND		/* allow /exec comamnd */
  #undef HACKED_DCC_WARNING	/* warn if handshake != sender */
