d07f8e2cb2545123ae6d35a66dd77137b0c78158
jakob <jakob@openbsd.org>
2004-04-02 08:18:48 +0000

    sync c-client with pine v4.58

--- mail/c-client/patches/patch-src_c-client_auth_gss.c
+++
@@ -1,18 +0,0 @@
-$OpenBSD: patch-src_c-client_auth_gss.c,v 1.3 2002/12/01 21:15:38 jakob Exp $
---- src/c-client/auth_gss.c.orig	Thu Nov 22 05:07:04 2001
-+++ src/c-client/auth_gss.c	Sun Dec  1 14:47:24 2002
-@@ -19,8 +19,14 @@
-  */
- 
- #define PROTOTYPE(x) x
-+
-+#ifdef HEIMDAL
-+#include <gssapi.h>
-+#include <krb5.h>
-+#else
- #include <gssapi/gssapi_generic.h>
- #include <gssapi/gssapi_krb5.h>
-+#endif
- 
- long auth_gssapi_valid (void);
- long auth_gssapi_client (authchallenge_t challenger,authrespond_t responder,
