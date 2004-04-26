b3c56c6d98bc0af5da025a487feee6dd24d75be3
dons <dons@openbsd.org>
2004-04-26 23:48:50 +0000

    Update GHC to 6.2.1, and port to amd64
    ok avsm@ pvalchev@

--- lang/ghc/files/ghc.port.mk
+++ lang/ghc/files/ghc.port.mk
@@ -1,9 +1,9 @@
-# $OpenBSD: ghc.port.mk,v 1.2 2004/01/08 20:33:23 avsm Exp $
+# $OpenBSD: ghc.port.mk,v 1.3 2004/04/26 23:48:50 dons Exp $
 # Module for Glasgow Haskell Compiler
 
 # Not yet ported to other architectures
 # See comments in lang/ghc/Makefile for more information
-ONLY_FOR_ARCHS=	i386 sparc
+ONLY_FOR_ARCHS=	i386 sparc amd64
 
 BUILD_DEPENDS+=	bin/ghc::lang/ghc
 RUN_DEPENDS+=	bin/ghc::lang/ghc
