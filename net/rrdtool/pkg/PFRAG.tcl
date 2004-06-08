19b0e314b394b5ed2dd7814e84626fe6cfaf48fe
danh <danh@openbsd.org>
2004-06-08 20:21:24 +0000

    update to rrdtool 1.0.48

--- net/rrdtool/pkg/PFRAG.tcl
+++ net/rrdtool/pkg/PFRAG.tcl
@@ -1,4 +1,4 @@
-@comment $OpenBSD: PFRAG.tcl,v 1.4 2004/02/15 21:02:02 sturm Exp $
-lib/tclrrd.${VERSION}.so
-lib/tclrrd.${VERSION}/pkgIndex.tcl
-@dirrm lib/tclrrd.${VERSION}
+@comment $OpenBSD: PFRAG.tcl,v 1.5 2004/06/08 20:21:25 danh Exp $
+lib/tclrrd.${RRDVERSION}.so
+lib/tclrrd.${RRDVERSION}/pkgIndex.tcl
+@dirrm lib/tclrrd.${RRDVERSION}
