ee63e907a9d28650e047b9757563ec976ee40c95
espie <espie@openbsd.org>
2004-06-22 16:09:05 +0000

    missing dependency

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-#	$OpenBSD: bsd.port.mk,v 1.617 2004/06/06 11:49:08 espie Exp $
+#	$OpenBSD: bsd.port.mk,v 1.618 2004/06/22 16:09:05 espie Exp $
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -1133,7 +1133,7 @@ ${_PACKAGE_COOKIE${_s}}: ${_PACKAGE_COOKIE_DEPS}
 
 .PRECIOUS: ${_PACKAGE_COOKIES} ${_INSTALL_COOKIE}
 
-${_SYSTRACE_COOKIE}:
+${_SYSTRACE_COOKIE}: ${_WRKDIR_COOKIE}
 	@rm -f $@
 .for _i in ${_SYSTRACE_POLICIES}
 	@echo "Policy: ${_i}, Emulation: native" >> $@
