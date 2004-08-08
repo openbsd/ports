ed01c25be0e6d21218b7eca97d94db84fc564179
espie <espie@openbsd.org>
2004-08-08 23:14:03 +0000

    lock distfile as distfile.dist, since some ports have FULLPKGPATH=DISTFILE.

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-#	$OpenBSD: bsd.port.mk,v 1.636 2004/08/08 16:43:15 espie Exp $
+#	$OpenBSD: bsd.port.mk,v 1.637 2004/08/08 23:14:03 espie Exp $
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -1874,7 +1874,7 @@ ${_F}:
 .    endfor
 	@exit 1
 .  else
-	@lock=${_F:T}; ${_DO_LOCK}; mkdir -p ${_F:H}; \
+	@lock=${_F:T}.dist; ${_DO_LOCK}; mkdir -p ${_F:H}; \
 	cd ${_F:H}; \
 	select=${_EVERYTHING:M*${_F:S@^${FULLDISTDIR}/@@}\:[0-9]}; \
 	f=${_F:S@^${FULLDISTDIR}/@@}; \
