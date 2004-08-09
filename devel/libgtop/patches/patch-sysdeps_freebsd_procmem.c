2c055d84afa5fa8c97dbec9f364556560a1d512e
espie <espie@openbsd.org>
2004-08-09 15:00:56 +0000

    new plist, ditch INSTALL script.

--- devel/libgtop/patches/patch-sysdeps_freebsd_procmem.c
+++ devel/libgtop/patches/patch-sysdeps_freebsd_procmem.c
@@ -1,6 +1,6 @@
-$OpenBSD: patch-sysdeps_freebsd_procmem.c,v 1.4 2001/11/27 23:07:02 todd Exp $
+$OpenBSD: patch-sysdeps_freebsd_procmem.c,v 1.5 2004/08/09 15:00:56 espie Exp $
 --- sysdeps/freebsd/procmem.c.orig	Mon Nov 26 23:37:59 2001
-+++ sysdeps/freebsd/procmem.c	Tue Nov 27 21:30:45 2001
++++ sysdeps/freebsd/procmem.c	Mon Aug  9 16:47:10 2004
 @@ -31,8 +31,6 @@
  #include <sys/param.h>
  #include <sys/proc.h>
@@ -21,17 +21,18 @@ $OpenBSD: patch-sysdeps_freebsd_procmem.c,v 1.4 2001/11/27 23:07:02 todd Exp $
  /* Fixme ... */
  #undef _KERNEL
  #define _UVM_UVM_AMAP_I_H_ 1
-@@ -61,9 +58,7 @@ static const unsigned long _glibtop_sysd
+@@ -61,10 +58,8 @@ static const unsigned long _glibtop_sysd
  (1L << GLIBTOP_PROC_MEM_RSS_RLIM);
  
  static const unsigned long _glibtop_sysdeps_proc_mem_share =
 -#if defined(__NetBSD__) && (__NetBSD_Version__ >= 104000000)
--(1L << GLIBTOP_PROC_MEM_SHARE);
--#elif defined(__FreeBSD__)
 +#if (defined(__NetBSD__) && (__NetBSD_Version__ >= 104000000)) || defined(OpenBSD) || defined(__FreeBSD__)
  (1L << GLIBTOP_PROC_MEM_SHARE);
+-#elif defined(__FreeBSD__)
+-(1L << GLIBTOP_PROC_MEM_SHARE);
  #else
  0;
+ #endif
 @@ -111,7 +106,7 @@ glibtop_get_proc_mem_p (glibtop *server,
  	struct kinfo_proc *pinfo;
  	struct vm_map_entry entry, *first;
