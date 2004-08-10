9ab9eeaa460c899101f2037a65d8569c0a3d7cbd
espie <espie@openbsd.org>
2004-08-10 13:48:21 +0000

    prefer pkg_create -B

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-#	$OpenBSD: bsd.port.mk,v 1.637 2004/08/08 23:14:03 espie Exp $
+#	$OpenBSD: bsd.port.mk,v 1.638 2004/08/10 13:48:21 espie Exp $
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -684,7 +684,7 @@ _PKG_PREREQ+=${WRKPKG}/MESSAGE${SUBPACKAGE}
 .  endif
 .endif
 .if ${FAKE:L} == "yes"
-PKG_ARGS+=		-S ${WRKINST}
+PKG_ARGS+=		-B ${WRKINST}
 .endif
 PKG_ARGS+=-A'${PKG_ARCH}'
 .if ${LOCALBASE} != "/usr/local"
