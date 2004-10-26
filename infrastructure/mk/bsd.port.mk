68c791bb78f7f0cd74a8411e853da461346c407a
pvalchev <pvalchev@openbsd.org>
2004-10-26 06:16:53 +0000

    add mips64 to LP64_ARCHS

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-#	$OpenBSD: bsd.port.mk,v 1.653 2004/10/23 10:02:51 espie Exp $
+#	$OpenBSD: bsd.port.mk,v 1.654 2004/10/26 06:16:53 pvalchev Exp $
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -80,7 +80,7 @@ ARCH!=	uname -m
 OPSYS=	OpenBSD
 OPSYS_VER=	${OSREV}
 
-LP64_ARCHS=alpha amd64 sparc64
+LP64_ARCHS=alpha amd64 sparc64 mips64
 NO_SHARED_ARCHS=m88k vax
 
 # Set NO_SHARED_LIBS for those machines that don't support shared libraries.
