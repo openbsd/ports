fa73104d38e5921880268576dbe056674ac9e2e1
espie <espie@openbsd.org>
2004-10-30 09:12:35 +0000

    Reorganize stuff. - stop supporting !mt, since it has worked for a long time. - mt becomes a pseudo_flavor for compat purposes. - merge base back into the main module. - link mt names to not mt names. - finish recreating a full QTDIR structure for compatibility as well.

--- x11/qt3/qt3.port.mk
+++ x11/qt3/qt3.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: qt3.port.mk,v 1.1 2004/08/03 09:18:39 espie Exp $
+# $OpenBSD: qt3.port.mk,v 1.2 2004/10/30 09:12:35 espie Exp $
 
 MODULES+=	gcc3
 MODGCC3_ARCHES+=sparc64
@@ -9,7 +9,7 @@ MODGCC3_LANGS+=	c++
 MODQT_LIBDIR=	${LOCALBASE}/lib/qt3
 MODQT_INCDIR=	${LOCALBASE}/include/X11/qt3
 MODQT_OVERRIDE_UIC?=Yes
-MODQT_MT?=No
+MODQT_MT?=Yes
 MODQT_CONFIGURE_ARGS=	--with-qt-includes=${MODQT_INCDIR} \
 			--with-qt-libraries=${MODQT_LIBDIR}
 _MODQT_SETUP=	MOC=${MODQT_MOC} \
@@ -19,16 +19,14 @@ _MODQT_SETUP=	MOC=${MODQT_MOC} \
 _MODQT_SETUP+=	UIC=${MODQT_UIC}
 .endif
 
-.if ${MODQT_MT:L} == "yes"
-LIB_DEPENDS+=lib/qt3/qt-mt.3::x11/qt3,mt
-# includes are now handled by a subpackage
+LIB_DEPENDS+=lib/qt3/qt-mt.3::x11/qt3
 # may be needed to find plugins
 MODQT_MOC=	${LOCALBASE}/bin/moc3-mt
 MODQT_UIC=	${LOCALBASE}/bin/uic3-mt
-.else
-LIB_DEPENDS+=lib/qt3/qt.3::x11/qt3
-MODQT_MOC=	${LOCALBASE}/bin/moc3
-MODQT_UIC=	${LOCALBASE}/bin/uic3
+MODQT_QTDIR=	${LOCALBASE}/lib/qt3
+
+.if ${MODQT_MT:L} != "yes"
+ERRORS+="Fatal: support QTMT only"
 .endif
 
 CONFIGURE_ENV+=	${_MODQT_SETUP}
