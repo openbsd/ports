b48f786b0e2294e48b52806fc1efcadc8f59e7d9
jakob <jakob@openbsd.org>
2004-11-03 10:33:31 +0000

    update to Cyrus SASL 2.1.20; thanks to Ben Hooper for diffs

--- security/cyrus-sasl2/pkg/PFRAG.sql
+++ security/cyrus-sasl2/pkg/PFRAG.sql
@@ -1,2 +1,4 @@
-@comment $OpenBSD: PFRAG.sql,v 1.2 2004/09/15 18:35:59 espie Exp $
+@comment $OpenBSD: PFRAG.sql,v 1.3 2004/11/03 10:33:32 jakob Exp $
 %%SHARED%%
+lib/sasl2/libsql.a
+lib/sasl2/libsql.la
