48aa65b693e977526e219dd1f11fac4755d7b620
espie <espie@openbsd.org>
2004-11-21 15:59:25 +0000

    ready-to-go switch to @wantlib/@depend

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-#	$OpenBSD: bsd.port.mk,v 1.665 2004/11/21 11:26:22 espie Exp $
+#	$OpenBSD: bsd.port.mk,v 1.666 2004/11/21 15:59:25 espie Exp $
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -693,6 +693,7 @@ MTREE_FILE+=${PORTSDIR}/infrastructure/db/fake.mtree
 _PKG_PREREQ= ${WRKPKG}/PLIST${SUBPACKAGE} ${WRKPKG}/DESCR${SUBPACKAGE} ${WRKPKG}/COMMENT${SUBPACKAGE}
 PKG_ARGS+= -v -c '${WRKPKG}/COMMENT${SUBPACKAGE}' -d ${WRKPKG}/DESCR${SUBPACKAGE}
 PKG_ARGS+=-f ${WRKPKG}/PLIST${SUBPACKAGE} -f ${PLIST} -p ${PREFIX}
+#PKG_ARGS+=-f ${PLIST} -p ${PREFIX} `${MAKE} _print-package-args`
 .if exists(${PKGDIR}/INSTALL${SUBPACKAGE})
 PKG_ARGS+=		-i ${PKGDIR}/INSTALL${SUBPACKAGE}
 .endif
