a9268b2f4a5eec2d0ad91e12f87fded1d3c97a6b
espie <espie@openbsd.org>
2004-11-27 09:59:35 +0000

    switch to using @wantlib and @depends, say byebye to libdepends and newdepends. (this will break your machine if you don't have reasonably current pkg_add)

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-#	$OpenBSD: bsd.port.mk,v 1.666 2004/11/21 15:59:25 espie Exp $
+#	$OpenBSD: bsd.port.mk,v 1.667 2004/11/27 09:59:35 espie Exp $
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -690,10 +690,9 @@ MTREE_FILE?=
 MTREE_FILE+=${PORTSDIR}/infrastructure/db/fake.mtree
 
 # Fill out package command, and package dependencies
-_PKG_PREREQ= ${WRKPKG}/PLIST${SUBPACKAGE} ${WRKPKG}/DESCR${SUBPACKAGE} ${WRKPKG}/COMMENT${SUBPACKAGE}
+_PKG_PREREQ= ${WRKPKG}/DESCR${SUBPACKAGE} ${WRKPKG}/COMMENT${SUBPACKAGE}
 PKG_ARGS+= -v -c '${WRKPKG}/COMMENT${SUBPACKAGE}' -d ${WRKPKG}/DESCR${SUBPACKAGE}
-PKG_ARGS+=-f ${WRKPKG}/PLIST${SUBPACKAGE} -f ${PLIST} -p ${PREFIX}
-#PKG_ARGS+=-f ${PLIST} -p ${PREFIX} `${MAKE} _print-package-args`
+PKG_ARGS+=-f ${PLIST} -p ${PREFIX} `${MAKE} _print-package-args|sort -u`
 .if exists(${PKGDIR}/INSTALL${SUBPACKAGE})
 PKG_ARGS+=		-i ${PKGDIR}/INSTALL${SUBPACKAGE}
 .endif
