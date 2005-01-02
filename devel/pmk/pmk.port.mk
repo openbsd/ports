102044ac9e7745a90ac02c7e96fe27af08fa7318
couderc <couderc@openbsd.org>
2005-01-02 23:55:10 +0000

    changes to be used as a module.

--- devel/pmk/pmk.port.mk
+++ devel/pmk/pmk.port.mk
@@ -1,13 +1,8 @@
-# $OpenBSD: pmk.port.mk,v 1.1 2005/01/02 18:48:43 couderc Exp $
+# $OpenBSD: pmk.port.mk,v 1.2 2005/01/02 23:55:10 couderc Exp $
 # Pre Make Kit support
 # public domain
 
-.if ${CONFIGURE_STYLE:L:Mpmk}
 BUILD_DEPENDS+=		::devel/pmk
-CONFIGURE_SCRIPT=	pmk
-
-
-MODPMK_configure= cd ${WRKSRC} && \
-			${_SYSTRACE_CMD} ${SETENV} ${CONFIGURE_SCRIPT}
-.endif
-
+CONFIGURE_SCRIPT=	${LOCALBASE}/bin/pmk
+CONFIGURE_ARGS=		BIN_CC=${CC}
+MODPMK_configure=	${MODSIMPLE_configure}
