377731eb95a9439e1a392c56f55eae5d467daa9d
alek <alek@openbsd.org>
2005-02-04 21:23:39 +0000

    - Set NO_REGRESS only if do-regress target is not defined - Add PEAR_LIBDIR (path to pear lib directory) and PEAR_PHPBIN (path to php   interpreter) variables
    ok robert@

--- www/php5/pear/pear.port.mk
+++ www/php5/pear/pear.port.mk
@@ -1,13 +1,18 @@
-# $OpenBSD: pear.port.mk,v 1.1.1.1 2004/10/02 11:32:38 robert Exp $
+# $OpenBSD: pear.port.mk,v 1.2 2005/02/04 21:23:39 alek Exp $
 # PHP PEAR module
 
 RUN_DEPENDS+=    :php5-pear-5.0.*:www/php5/core,-pear
 BUILD_DEPENDS+=  :php5-pear-5.0.*:www/php5/core,-pear
 
 NO_BUILD=       Yes
-NO_REGRESS=     Yes
+.if !target(do-regress)
+NO_REGRESS=	Yes
+.endif
 
 MAKE_FILE=	${PORTSDIR}/www/php5/pear/Makefile.pear
 FAKE_FLAGS+=	WRKINST=${WRKINST} WRKDIR=${WRKDIR}
 
 PREFIX=		/var/www
+
+PEAR_LIBDIR=	${PREFIX}/pear/lib
+PEAR_PHPBIN=	${LOCALBASE}/bin/php
