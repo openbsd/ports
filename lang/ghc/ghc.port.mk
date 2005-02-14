cd5fda6e80e51d17844552e36cfa6f8674e0d2dd
dons <dons@openbsd.org>
2005-02-14 01:26:56 +0000

    Update ghc to 6.2.2. Adds support for ghci. More stable foreign function interface.
    Just i386 at the moment.
    
    ok sturm@ pvalchev@

--- lang/ghc/ghc.port.mk
+++ lang/ghc/ghc.port.mk
@@ -1,9 +1,10 @@
-# $OpenBSD: ghc.port.mk,v 1.1 2004/08/10 08:14:35 xsa Exp $
+# $OpenBSD: ghc.port.mk,v 1.2 2005/02/14 01:26:56 dons Exp $
 # Module for Glasgow Haskell Compiler
 
 # Not yet ported to other architectures
 # See comments in lang/ghc/Makefile for more information
-ONLY_FOR_ARCHS=	i386 sparc amd64
+ONLY_FOR_ARCHS=	i386 
+#                   sparc amd64
 
 BUILD_DEPENDS+=	bin/ghc::lang/ghc
 RUN_DEPENDS+=	bin/ghc::lang/ghc
