ff1e1a73f423c581d3d3c4fcfa69567ef0b518f9
espie <espie@openbsd.org>
2005-02-15 09:28:58 +0000

    get ready to switch to this port

---
+++ lang/gcc/3.3/gcc3.port.mk
@@ -0,0 +1,34 @@
+# $OpenBSD: gcc3.port.mk,v 1.1 2005/02/15 09:28:58 espie Exp $
+
+MODGCC3_ARCHES?=
+# Supported languages for now
+_MODGCC3CC=	cc
+_MODGCC3CXX=	c++
+_MODGCC3G77=	g77
+# Always include support for this
+MODGCC3_LANGS+=	${_MODGCC3CC}
+
+.if ${MODGCC3_ARCHES:L} != ""
+.  for _i in ${MODGCC3_ARCHES}
+.    if !empty(MACHINE_ARCH:M${_i})
+BUILD_DEPENDS+=	::lang/gcc/3.3
+.      for _j in ${MODGCC3_LANGS:L}
+.        if !empty(_MODGCC3CC:L:M${_j})
+MODGCC3_post-patch+= ln -s ${LOCALBASE}/bin/eg${_MODGCC3CC} ${WRKDIR}/bin/g${_MODGCC3CC};
+MODGCC3_post-patch+= ln -s ${LOCALBASE}/bin/eg${_MODGCC3CC} ${WRKDIR}/bin/${_MODGCC3CC};
+.        endif
+.        if !empty(_MODGCC3CXX:L:M${_j})
+BUILD_DEPENDS+=	::lang/gcc/3.3,-c++
+LIB_DEPENDS+=	estdc++.5::lang/egcs/stable,-estdc
+MODGCC3_post-patch+= ln -s ${LOCALBASE}/bin/e${_MODGCC3CXX} ${WRKDIR}/bin/g++;
+MODGCC3_post-patch+= ln -s ${LOCALBASE}/bin/e${_MODGCC3CXX} ${WRKDIR}/bin/${_MODGCC3CXX};
+.        endif
+.        if !empty(_MODGCC3G77:L:M${_j})
+BUILD_DEPENDS+=	::lang/gcc/3.3,-g77
+MODGCC3_post-patch+= ln -s ${LOCALBASE}/bin/e${_MODGCC3G77} ${WRKDIR}/bin/f77;
+MODGCC3_post-patch+= ln -s ${LOCALBASE}/bin/e${_MODGCC3G77} ${WRKDIR}/bin/${_MODGCC3G77};
+.	 endif
+.      endfor
+.    endif
+.  endfor
+.endif
