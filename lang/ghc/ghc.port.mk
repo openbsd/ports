d1837aebeae92b931a0e6b8a498883506437b5bc
dons <dons@openbsd.org>
2005-02-19 03:42:11 +0000

    Port ghc-6.2.2 to amd64.
    ok pvalchev@

--- lang/ghc/ghc.port.mk
+++ lang/ghc/ghc.port.mk
@@ -1,10 +1,10 @@
-# $OpenBSD: ghc.port.mk,v 1.2 2005/02/14 01:26:56 dons Exp $
+# $OpenBSD: ghc.port.mk,v 1.3 2005/02/19 03:42:11 dons Exp $
 # Module for Glasgow Haskell Compiler
 
 # Not yet ported to other architectures
 # See comments in lang/ghc/Makefile for more information
-ONLY_FOR_ARCHS=	i386 
-#                   sparc amd64
+ONLY_FOR_ARCHS=	i386 amd64
+#                   sparc
 
 BUILD_DEPENDS+=	bin/ghc::lang/ghc
 RUN_DEPENDS+=	bin/ghc::lang/ghc
