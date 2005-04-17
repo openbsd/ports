b84fda07591c81b9788ddfa7b61ba311ea71999f
espie <espie@openbsd.org>
2005-04-17 18:31:58 +0000

    use  both DEPBASE and LOCALBASE in PORTSPATH since we don't fake everything yet.

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-#	$OpenBSD: bsd.port.mk,v 1.689 2005/04/17 13:21:14 espie Exp $
+#	$OpenBSD: bsd.port.mk,v 1.690 2005/04/17 18:31:58 espie Exp $
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -451,7 +451,7 @@ _CIPHERS=		sha1 rmd160 md5
 # This is the one you can override
 PREFERRED_CIPHERS?= ${_CIPHERS}
 
-PORTPATH?= ${WRKDIR}/bin:/usr/bin:/bin:/usr/sbin:/sbin:${DEPBASE}/bin:${X11BASE}/bin
+PORTPATH?= ${WRKDIR}/bin:/usr/bin:/bin:/usr/sbin:/sbin:${DEPBASE}/bin:${LOCALBASE}/bin:${X11BASE}/bin
 
 # Add any COPTS to CFLAGS.  Note: programs that use imake do not
 # use CFLAGS!  Also, many (most?) ports hard code CFLAGS, ignoring
