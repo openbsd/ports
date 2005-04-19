fd850a1026b931ee47dee61b9c899da97eb86d88
pedro <pedro@openbsd.org>
2005-04-19 15:14:38 +0000

    upgrade to luasocket 2.0-beta3, okay jolan@

--- net/luasocket/files/init.lua
+++ net/luasocket/files/init.lua
@@ -1,4 +1,4 @@
--- $OpenBSD: init.lua,v 1.2 2005/04/19 14:31:27 pedro Exp $
+-- $OpenBSD: init.lua,v 1.3 2005/04/19 15:14:38 pedro Exp $
 -- luasocket's initialization steps
 lua_addpath("@socketpath@")
-require("lua")
+lua_addcpath("@socketcpath@")
