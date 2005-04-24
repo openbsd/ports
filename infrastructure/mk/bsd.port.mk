63ee44468c91540e5dd118377f656fc07e420f5a
espie <espie@openbsd.org>
2005-04-24 18:02:45 +0000

    missed test. found out (more or less) by alek@.

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-#	$OpenBSD: bsd.port.mk,v 1.696 2005/04/23 15:24:49 espie Exp $
+#	$OpenBSD: bsd.port.mk,v 1.697 2005/04/24 18:02:45 espie Exp $
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -1082,7 +1082,7 @@ _set_ld_library_path=:
 DEPBASE=${LOCALBASE}
 DEPDIR=
 .endif
-.if ${FAKE:L} == "all"
+.if ${FAKE:L} == "all" && ${USE_FAKE_LIB:L} == "yes"
 _build_depends_target=fake
 .endif
 
