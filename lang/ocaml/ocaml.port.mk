b679057ca9d767678dce920c1eee6663c4b26657
robert <robert@openbsd.org>
2005-08-09 15:22:54 +0000

    native code does not work on arm. this change unbreaks a couple of ports because they think that we have a native ocaml but we don't.

--- lang/ocaml/ocaml.port.mk
+++ lang/ocaml/ocaml.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: ocaml.port.mk,v 1.4 2005/05/28 20:55:45 avsm Exp $
+# $OpenBSD: ocaml.port.mk,v 1.5 2005/08/09 15:22:54 robert Exp $
 
 # regular file usage for bytecode:
 # PLIST               -- bytecode base files
@@ -11,7 +11,7 @@
 
 .if ${MACHINE_ARCH} == "alpha" || ${MACHINE_ARCH} == "i386" || \
 	${MACHINE_ARCH} == "sparc" || ${MACHINE_ARCH} == "amd64" || \
-	${MACHINE_ARCH} == "powerpc" || ${MACHINE_ARCH} == "arm"
+	${MACHINE_ARCH} == "powerpc"
 MODOCAML_NATIVE=Yes
 
 # include nativecode base files
