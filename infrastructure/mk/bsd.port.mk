0031945c6ebd0baecae7e65e21f341959b3462d8
espie <espie@openbsd.org>
2005-09-16 09:51:25 +0000

    Fix a buglet in bsd.port.mk where the PSEUDO_FLAVORS get encoded into the FULLPKGPATH, thus providing changes to packing-lists which shouldn't happen, and making update more difficult.
    Accordingly, bump all pkgnames with PSEUDO_FLAVORS, and provide an
    update @pkgpath for the bug for most of them (left out the ones with 3
    or 4 pseudo flavors for space constraints...)

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-#	$OpenBSD: bsd.port.mk,v 1.707 2005/09/10 18:07:47 naddy Exp $
+#	$OpenBSD: bsd.port.mk,v 1.708 2005/09/16 09:51:25 espie Exp $
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -583,9 +583,9 @@ PKGNAMES += ${FULLPKGNAME${_s}}
 .endfor
 
 .if empty(SUBPACKAGE)
-FULLPKGPATH=${PKGPATH}${_FLAVOR_EXT2:S/-/,/g}
+FULLPKGPATH=${PKGPATH}${_FLAVOR_EXT:S/-/,/g}
 .else
-FULLPKGPATH=${PKGPATH},${SUBPACKAGE}${_FLAVOR_EXT2:S/-/,/g}
+FULLPKGPATH=${PKGPATH},${SUBPACKAGE}${_FLAVOR_EXT:S/-/,/g}
 .endif
 
 # A few aliases for *-install targets
