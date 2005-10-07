21c89e0e8f3b98a585637a5f6a03d506a3e6938d
espie <espie@openbsd.org>
2005-10-07 21:08:16 +0000

    overeager code cloning. noticed by matthieu@/mbalmer@

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-#	$OpenBSD: bsd.port.mk,v 1.712 2005/09/25 09:43:09 espie Exp $
+#	$OpenBSD: bsd.port.mk,v 1.713 2005/10/07 21:08:16 espie Exp $
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -2450,7 +2450,7 @@ _print-package-args:
 	@d=${_i}; listlibs='ls $$shdir 2>/dev/null'; \
 	${_syslibresolve_fragment}; \
 	case "$$check" in \
-	*.a) continue;; \
+	*.a) ;; \
 	Missing\ library|Error:*) \
 		echo 1>&2 "Can't resolve libspec $$d"; \
 		exit 1;; \
