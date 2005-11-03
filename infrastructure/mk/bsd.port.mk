fea5203c2e4fdedc47f2183a1835e9b1a13d6a93
espie <espie@openbsd.org>
2005-11-03 17:23:30 +0000

    duh. found by naddy@

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-#	$OpenBSD: bsd.port.mk,v 1.725 2005/11/02 20:30:12 espie Exp $
+#	$OpenBSD: bsd.port.mk,v 1.726 2005/11/03 17:23:30 espie Exp $
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -1184,7 +1184,7 @@ _DEPlibs_COOKIES+=${WRKDIR}/.wantlib-$i
 .  endfor
 _DEPlibs_COOKIE=${WRKDIR}/.wantlibs
 .else
-_DEPlibs_COOKIE=${WRKDIR}/.wantlibs
+_DEPlibs_COOKIE=
 .endif
 
 
