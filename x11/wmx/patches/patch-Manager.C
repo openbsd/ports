c5695d9969dbf603c2e0fd28b6ede857667b6edc
naddy <naddy@openbsd.org>
2005-11-13 12:42:25 +0000

    match actual system prototype

--- x11/wmx/patches/patch-Manager.C
+++ x11/wmx/patches/patch-Manager.C
@@ -1,16 +1,19 @@
-$OpenBSD: patch-Manager.C,v 1.3 2002/09/30 14:31:50 naddy Exp $
+$OpenBSD: patch-Manager.C,v 1.4 2005/11/13 12:42:25 naddy Exp $
 --- Manager.C.orig	Mon Apr 23 11:10:42 2001
-+++ Manager.C	Mon Sep 30 16:34:44 2002
-@@ -916,7 +916,7 @@ Boolean WindowManager::raiseTransients(C
-     }
- }
- 
--#ifdef sgi
-+#if defined(sgi) || defined(__OpenBSD__)
- extern "C" {
++++ Manager.C	Sun Nov 13 13:35:32 2005
+@@ -921,6 +921,11 @@ extern "C" {
  extern int putenv(char *);	/* not POSIX */
  }
-@@ -1069,6 +1069,11 @@ void WindowManager::gnomeUpdateWindowLis
+ #endif
++#ifdef __OpenBSD__
++extern "C" {
++extern int putenv(const char *);
++}
++#endif
+ 
+ void WindowManager::spawn(char *name, char *file)
+ {
+@@ -1069,6 +1074,11 @@ void WindowManager::gnomeUpdateWindowLis
  
      delete windows;
  }
