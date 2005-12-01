fba46fb1037a5097c568a10e0ea373b99765221e
pedro <pedro@openbsd.org>
2005-12-01 20:17:05 +0000

    Install sajax.lua as a loadable module, prompted out and okay jolan@

--- www/xavante/files/init.lua
+++ www/xavante/files/init.lua
@@ -1,3 +1,4 @@
--- $OpenBSD: init.lua,v 1.1.1.1 2005/05/25 06:13:16 jolan Exp $
+-- $OpenBSD: init.lua,v 1.2 2005/12/01 20:17:05 pedro Exp $
 -- xavante's initialization steps
 lua_addpath("@xavantepath@")
+lua_addpath("@sajaxpath@")
