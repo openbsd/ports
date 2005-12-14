d0c6ecbe031e8475b4fb380a59ba63b1ea21fa4f
brad <brad@openbsd.org>
2005-12-14 06:57:18 +0000

    fix bogus pkgspec for pear dependency entries.
    From: Ian McWilliam <ian at dodo dot com dot au>

--- www/php4/pear/pear.port.mk
+++ www/php4/pear/pear.port.mk
@@ -1,8 +1,8 @@
-# $OpenBSD: pear.port.mk,v 1.1 2004/08/03 12:54:40 espie Exp $
+# $OpenBSD: pear.port.mk,v 1.2 2005/12/14 06:57:18 brad Exp $
 # PHP PEAR module
 
-RUN_DEPENDS+=    :php4-pear-4.3.*:www/php4/core,-pear
-BUILD_DEPENDS+=  :php4-pear-4.3.*:www/php4/core,-pear
+RUN_DEPENDS+=    :php4-pear-4.4.*:www/php4/core,-pear
+BUILD_DEPENDS+=  :php4-pear-4.4.*:www/php4/core,-pear
 
 NO_BUILD=       Yes
 NO_REGRESS=     Yes
