13f14aa6a8ed4e4c36ba17b1d85c041eaabcdbd6
pedro <pedro@openbsd.org>
2006-05-04 22:53:57 +0000

    LuaGD, a binding of the GD library for Lua, prod and okay jolan@

---
+++ graphics/luagd/files/gd.lua
@@ -0,0 +1,2 @@
+-- $OpenBSD: gd.lua,v 1.1.1.1 2006/05/04 22:53:57 pedro Exp $
+require("gd.core")
---
+++ graphics/luagd/files/init.lua
@@ -0,0 +1,4 @@
+-- $OpenBSD: init.lua,v 1.1.1.1 2006/05/04 22:53:57 pedro Exp $
+-- luagd's initialization steps
+lua_addpath("@gdpath@")
+lua_addcpath("@gdcpath@")
