151101789ebdfada4a3fbe4ed01fe0e094dbbcb0
robert <robert@openbsd.org>
2006-05-09 14:15:40 +0000

    Update to version 5.1.4;
    Add a hardened flavor for both core and extensions (inspired by niallo@);
    Use our own way to install pear because the bundled installer is totally
    broken and upstream refuses to fix it.
    
    Add a mysqli subpackage which can be used to access the functionality
    provided by MySQL 4.1 and above.
    
    Other minor changes and fixes are also included.
    
    ok sturm@; tested by many

--- www/php5/pear/pear.port.mk
+++ www/php5/pear/pear.port.mk
@@ -1,10 +1,10 @@
-# $OpenBSD: pear.port.mk,v 1.2 2005/02/04 21:23:39 alek Exp $
+# $OpenBSD: pear.port.mk,v 1.3 2006/05/09 14:15:40 robert Exp $
 # PHP PEAR module
 
-RUN_DEPENDS+=    :php5-pear-5.0.*:www/php5/core,-pear
-BUILD_DEPENDS+=  :php5-pear-5.0.*:www/php5/core,-pear
+RUN_DEPENDS+=	:php5-pear-*:www/php5/core,-pear
+BUILD_DEPENDS+=	${RUN_DEPENDS}
 
-NO_BUILD=       Yes
+NO_BUILD=	Yes
 .if !target(do-regress)
 NO_REGRESS=	Yes
 .endif
