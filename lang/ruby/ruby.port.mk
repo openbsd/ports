c48321f2f58412d6061176c011e9f15481b59431
sturm <sturm@openbsd.org>
2006-06-11 18:57:11 +0000

    add CONFIGURE_STYLE "ruby setup" and related targets move LIB_DEPENDS to relevant ports, no PKGNAME bump needed this is in preparation of a soon to come ruby-gems port
    with msf's consent

--- lang/ruby/ruby.port.mk
+++ lang/ruby/ruby.port.mk
@@ -1,9 +1,11 @@
-# $OpenBSD: ruby.port.mk,v 1.2 2006/03/23 17:15:25 msf Exp $
+# $OpenBSD: ruby.port.mk,v 1.3 2006/06/11 18:57:11 sturm Exp $
 
 # ruby module
 
-RUN_DEPENDS+=::lang/ruby
-LIB_DEPENDS+=ruby.1.84::lang/ruby
+RUBY=			${LOCALBASE}/bin/ruby
+
+BUILD_DEPENDS+=		::lang/ruby
+RUN_DEPENDS+=		::lang/ruby
 
 # location of ruby libraries
 MODRUBY_LIBDIR=		${LOCALBASE}/lib/ruby
@@ -20,3 +22,20 @@ REV=1.8
 SUB=${MACHINE_ARCH:S/amd64/x86_64/}-openbsd${OSREV}
 SUBST_VARS=SUB REV
 
+.if ${CONFIGURE_STYLE:L:Msetup}
+MODRUBY_configure= \
+	cd ${WRKSRC}; ${SETENV} ${CONFIGURE_ENV} ${RUBY} setup.rb config \
+		--prefix=${PREFIX} ${CONFIGURE_ARGS};
+.  if !target(do-build)
+do-build:
+	@cd ${WRKSRC} && ${SETENV} ${MAKE_ENV} ${RUBY} setup.rb setup
+.  endif
+.  if !target(do-regress)
+NO_REGRESS=Yes
+.  endif
+.  if !target(do-install)
+do-install:
+	@cd ${WRKSRC} && ${SETENV} ${MAKE_ENV} ${RUBY} setup.rb install \
+		--prefix=${DESTDIR}
+.  endif
+.endif
