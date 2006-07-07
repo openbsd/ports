8a60986c7d171325486753d5d63255174f708085
kurt <kurt@openbsd.org>
2006-07-07 01:48:54 +0000

    - update eclipse to 3.2 - add amd64 support Thanks to Henrik Kramshøj for providing access to amd64. Tested by ian@.

--- devel/eclipse/sdk/scripts/prepatch.sh
+++ devel/eclipse/sdk/scripts/prepatch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $OpenBSD: prepatch.sh,v 1.2 2005/08/09 14:49:09 kurt Exp $
+# $OpenBSD: prepatch.sh,v 1.3 2006/07/07 01:48:55 kurt Exp $
 # $FreeBSD: ports/java/eclipse/scripts/configure,v 1.2 2004/07/25 08:01:09 nork Exp $
 
 copy_file()
@@ -44,25 +44,44 @@ prepatch()
 	echo Copying $src into $dst
 	copy_file "${WRKSRC}/$src" "${WRKSRC}/$dst"
 
+	# Copy missing mpl about_files dir so PLIST is consistant
+	src="features/org.eclipse.platform/about_files/openbsd.gtk.x86_64/about_files"
+	dst="features/org.eclipse.platform/about_files/openbsd.gtk.x86"
+	cp -r ${WRKSRC}/$src ${WRKSRC}/$dst
+
 	find ${WRKSRC} -type f -and \
 		\( -name \*.so -or -name \*.so.\* -or -name eclipse \) | xargs rm
 }
 
 COPY_LIST="
 assemble.org.eclipse.sdk.linux.gtk.x86.xml
+assemble.org.eclipse.sdk.linux.gtk.x86_64.xml
 features/org.eclipse.platform.launchers/bin/gtk/linux/x86
+features/org.eclipse.platform.launchers/bin/gtk/linux/x86_64
 features/org.eclipse.platform.launchers/library/gtk/make_linux.mak
 features/org.eclipse.platform/about_files/linux.gtk.x86
-plugins/org.eclipse.core.resources.linux
-plugins/org.eclipse.core.resources.linux/os/linux
-plugins/org.eclipse.core.resources.linux/os/linux/x86
+features/org.eclipse.platform/about_files/linux.gtk.x86_64
+package.org.eclipse.sdk.linux.gtk.x86.xml
+package.org.eclipse.sdk.linux.gtk.x86_64.xml
+plugins/org.eclipse.core.filesystem.linux.x86
+plugins/org.eclipse.core.filesystem.linux.x86/os/linux
+plugins/org.eclipse.core.filesystem.linux.x86_64
+plugins/org.eclipse.core.filesystem.linux.x86_64/os/linux
+plugins/org.eclipse.core.filesystem/natives/unix/linux
+plugins/org.eclipse.jdt.core/scripts/export-ejavac2_linux.xml
+plugins/org.eclipse.jdt.core/scripts/export-ejavac_linux.xml
 plugins/org.eclipse.jdt.source.linux.gtk.x86
+plugins/org.eclipse.jdt.source.linux.gtk.x86_64
 plugins/org.eclipse.jface/src/org/eclipse/jface/resource/jfacefonts_linux.properties
 plugins/org.eclipse.jface/src/org/eclipse/jface/resource/jfacefonts_linux_gtk.properties
 plugins/org.eclipse.pde.source.linux.gtk.x86
+plugins/org.eclipse.pde.source.linux.gtk.x86
 plugins/org.eclipse.platform.source.linux.gtk.x86
+plugins/org.eclipse.platform.source.linux.gtk.x86_64
 plugins/org.eclipse.rcp.source.linux.gtk.x86
+plugins/org.eclipse.rcp.source.linux.gtk.x86_64
 plugins/org.eclipse.swt.gtk.linux.x86
+plugins/org.eclipse.swt.gtk.linux.x86_64
 plugins/org.eclipse.update.core.linux
 plugins/org.eclipse.update.core.linux/os/linux
 "
