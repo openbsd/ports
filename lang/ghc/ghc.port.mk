da0914ecfab830c29672e5d94cb01d04cb33bd2b
alek <alek@openbsd.org>
2006-07-30 13:21:38 +0000

    Add MODGHC_RUNTIME to add/remove runtime dependency on lang/ghc.
    From Jon Olsson <jon@vexed.se>

--- lang/ghc/ghc.port.mk
+++ lang/ghc/ghc.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: ghc.port.mk,v 1.3 2005/02/19 03:42:11 dons Exp $
+# $OpenBSD: ghc.port.mk,v 1.4 2006/07/30 13:21:38 alek Exp $
 # Module for Glasgow Haskell Compiler
 
 # Not yet ported to other architectures
@@ -7,4 +7,9 @@ ONLY_FOR_ARCHS=	i386 amd64
 #                   sparc
 
 BUILD_DEPENDS+=	bin/ghc::lang/ghc
+
+# Only add runtime when it is actually needed (by default yes)
+MODGHC_RUNTIME?=Yes
+.if ${MODGHC_RUNTIME:L} == "yes"
 RUN_DEPENDS+=	bin/ghc::lang/ghc
+.endif
