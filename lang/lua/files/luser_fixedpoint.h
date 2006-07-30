b5b8b158bcc1f866ef614094f6936ea2cc352dab
pedro <pedro@openbsd.org>
2006-07-30 04:25:41 +0000

    Update to Lua 5.1, okay jolan@

--- lang/lua/files/luser_fixedpoint.h
+++
@@ -1,6 +0,0 @@
-/*	$OpenBSD: luser_fixedpoint.h,v 1.1 2006/01/21 01:29:40 jolan Exp $	*/
-
-#define LUA_NUMBER              int
-#define LUA_NUMBER_SCAN         "%d"
-#define LUA_NUMBER_FMT          "%d"
-#define lua_str2number(s,p)     ((int) strtol((s), (p), 10))
--- lang/lua/files/packages.lua
+++
@@ -1,2 +0,0 @@
-installed_packages = {
-}
