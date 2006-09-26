df845c732224b57d2d71b9c3c1f30a7ad622d683
djm <djm@openbsd.org>
2006-09-26 15:27:02 +0000

    support bits for setuptools-style ports, activated using:
    MODPY_SETUPTOOLS = Yes
    
    feedback and ok xsa@

--- lang/python/python.port.mk
+++ lang/python/python.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: python.port.mk,v 1.12 2006/06/03 09:14:04 alek Exp $
+# $OpenBSD: python.port.mk,v 1.13 2006/09/26 15:27:02 djm Exp $
 #
 #	python.port.mk - Xavier Santolaria <xavier@santolaria.net>
 #	This file is in the public domain.
@@ -11,6 +11,14 @@ _MODPY_BUILD_DEPENDS=	:python-${MODPY_VERSION}*:lang/python/${MODPY_VERSION}
 BUILD_DEPENDS+=		${_MODPY_BUILD_DEPENDS}
 .endif
 RUN_DEPENDS+=		${_MODPY_BUILD_DEPENDS}
+
+# The setuptools module provides a package locator (site.py) that is
+# required at runtime for the pkg_resources stuff to work
+.if defined(MODPY_SETUPTOOLS) && ${MODPY_SETUPTOOLS:U} == YES
+MODPY_SETUPUTILS_DEPEND?=:py-setuptools-*:devel/py-setuptools
+RUN_DEPENDS+=		${MODPY_SETUPUTILS_DEPEND}
+BUILD_DEPENDS+=		${MODPY_SETUPUTILS_DEPEND}
+.endif
 
 .if !defined(NO_SHARED_LIBS) || ${NO_SHARED_LIBS:U} != YES
 MODPY_EXPAT_DEPENDS=	:python-expat-${MODPY_VERSION}*:lang/python/${MODPY_VERSION},-expat	
@@ -27,7 +35,14 @@ MODPY_SETUP?=		setup.py
 
 # build or build_ext are commonly used
 MODPY_DISTUTILS_BUILD?=		build --build-base=${WRKSRC}
+
+.if defined(MODPY_SETUPTOOLS) && ${MODPY_SETUPTOOLS:U} == YES
+MODPY_DISTUTILS_INSTALL?=	install --prefix=${LOCALBASE} \
+				--root=${DESTDIR} \
+				--single-version-externally-managed
+.else
 MODPY_DISTUTILS_INSTALL?=	install --prefix=${PREFIX}
+.endif
 
 MAKE_ENV+=	CC=${CC}
 
@@ -49,4 +64,12 @@ do-install:
 	${_MODPY_CMD} ${MODPY_DISTUTILS_BUILD} ${MODPY_DISTUTILS_BUILDARGS} \
 		${MODPY_DISTUTILS_INSTALL} ${MODPY_DISTUTILS_INSTALLARGS}
 .  endif
+
+# setuptools supports regress testing from setup.py using a standard target
+.  if !target(do-regress) && \
+      defined(MODPY_SETUPTOOLS) && ${MODPY_SETUPTOOLS:U} == YES
+do-regress:
+	${_MODPY_CMD} test
+.  endif
+
 .endif
