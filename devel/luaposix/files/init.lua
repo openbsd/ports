b80c45521af4a8b6717c660b7cfc03b4330d7594
pedro <pedro@openbsd.org>
2006-10-11 21:03:24 +0000

    luaposix-20060408, okay jolan@

--- devel/luaposix/files/init.lua
+++
@@ -1,3 +0,0 @@
--- $OpenBSD: init.lua,v 1.2 2005/04/19 14:31:27 pedro Exp $
--- luaposix's initialization steps
-lua_addpath("@posixpath@")
--- devel/luaposix/files/posix.lua
+++
@@ -1,2 +0,0 @@
--- $OpenBSD: posix.lua,v 1.1.1.1 2004/12/16 21:21:14 pedro Exp $
-assert(loadlib("luaposix.so","luaopen_posix"))()
