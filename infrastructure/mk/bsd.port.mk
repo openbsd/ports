748da30a4f311353ac56e92548e0fd7f9f7861dd
espie <espie@openbsd.org>
2006-10-15 09:20:53 +0000

    also mention -n along -q on `print-plist' commands. I've started documenting it, and having -q do things on its own is quirky...

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-#	$OpenBSD: bsd.port.mk,v 1.777 2006/10/12 08:45:56 espie Exp $
+#	$OpenBSD: bsd.port.mk,v 1.778 2006/10/15 09:20:53 espie Exp $
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -2098,10 +2098,10 @@ _register_plist=perl ${PORTSDIR}/infrastructure/package/register-plist ${PLIST_D
 .endif
 
 print-plist:
-	@${PKG_CMD} -q ${PKG_ARGS} ${_PACKAGE_COOKIE}
+	@${PKG_CMD} -n -q ${PKG_ARGS} ${_PACKAGE_COOKIE}
 
 print-plist-contents:
-	@${PKG_CMD} -q -Q ${PKG_ARGS} ${_PACKAGE_COOKIE}
+	@${PKG_CMD} -n -q -Q ${PKG_ARGS} ${_PACKAGE_COOKIE}
 
 _internal-subpackage: ${_PACKAGE_DEPS}
 
