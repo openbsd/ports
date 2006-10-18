1f312996acaefb60b868c26ff21c556a7330ba5b
ian <ian@openbsd.org>
2006-10-18 17:05:57 +0000

    pass filenames to program from front-end script, ok robert@

--- editors/openoffice/files/soffice.sh
+++ editors/openoffice/files/soffice.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 #
-# $OpenBSD: soffice.sh,v 1.1.1.1 2006/08/17 14:56:21 robert Exp $
+# $OpenBSD: soffice.sh,v 1.2 2006/10/18 17:05:57 ian Exp $
 #
 
 case "$0"
 in
 	*swriter)
-		%%LOCALBASE%%/openoffice/program/soffice -writer
+		%%LOCALBASE%%/openoffice/program/soffice -writer $*
 		;;
 	*scalc)
-		%%LOCALBASE%%/openoffice/program/soffice -calc
+		%%LOCALBASE%%/openoffice/program/soffice -calc $*
 		;;
 	*sdraw)
-		%%LOCALBASE%%/openoffice/program/soffice -draw
+		%%LOCALBASE%%/openoffice/program/soffice -draw $*
 		;;
 	*simpress)
-		%%LOCALBASE%%/openoffice/program/soffice -impress
+		%%LOCALBASE%%/openoffice/program/soffice -impress $*
 		;;
 	*)
-		%%LOCALBASE%%/openoffice/program/soffice
+		%%LOCALBASE%%/openoffice/program/soffice $*
 esac
