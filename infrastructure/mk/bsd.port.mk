1b169a7d4bfdf80e945df9142a00fb7508d1b817
espie <espie@openbsd.org>
2006-10-21 12:10:35 +0000

    can not depend directly on PACKAGE_COOKIES. Found out by nikolay, fun to debug...

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-#	$OpenBSD: bsd.port.mk,v 1.787 2006/10/21 11:50:48 espie Exp $
+#	$OpenBSD: bsd.port.mk,v 1.788 2006/10/21 12:10:35 espie Exp $
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -1758,7 +1758,8 @@ ${_t}:
 .  endif
 .endfor
 
-${_BULK_COOKIE}: ${_PACKAGE_COOKIES}
+${_BULK_COOKIE}: 
+	@cd ${.CURDIR} && exec ${MAKE} _internal-package
 	@mkdir -p ${BULK_COOKIES_DIR}
 .for _i in ${BULK_TARGETS_${PKGPATH}}
 	@${ECHO_MSG} "===> Running ${_i}"
