cca855dc21921699e86ffd34dfb7603a1ac58da7
alek <alek@openbsd.org>
2006-11-01 20:02:06 +0000

    Switch build to setup.py/distutils instead of Setup files; redo PLISTs; add bz2 subpackage

---
+++ lang/python/2.3/pkg/PFRAG.mm
@@ -0,0 +1,5 @@
+@comment $OpenBSD: PFRAG.mm,v 1.3 2006/11/01 20:02:06 alek Exp $
+lib/python2.3/lib-dynload/audioop.so
+lib/python2.3/lib-dynload/dl.so
+lib/python2.3/lib-dynload/imageop.so
+lib/python2.3/lib-dynload/rgbimg.so
