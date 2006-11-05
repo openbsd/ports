adbf2219c0693f19d2359f536e5d0a55dde10426
espie <espie@openbsd.org>
2006-11-05 20:20:28 +0000

    have _internal-regress depend on the _BUILD_COOKIE, so that build is performed before regress dependencies.

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-#	$OpenBSD: bsd.port.mk,v 1.794 2006/11/05 15:46:51 espie Exp $
+#	$OpenBSD: bsd.port.mk,v 1.795 2006/11/05 20:20:28 espie Exp $
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -1648,7 +1648,7 @@ _internal-regress:
 	@${ECHO_MSG} "===>  ${FULLPKGNAME${SUBPACKAGE}}${_MASTER} ${_IGNORE_REGRESS}."
 .    endif
 .  else
-_internal-regress: ${_DEPregress_COOKIES} ${_REGRESS_COOKIE}
+_internal-regress: ${_BUILD_COOKIE} ${_DEPregress_COOKIES} ${_REGRESS_COOKIE}
 .  endif
 
 # packing list utilities.  This generates a packing list from a recently
