47a8fcf5fc67ca578a35ecfb827e760fb4cb6bf4
espie <espie@openbsd.org>
2006-11-12 10:45:40 +0000

    tweaks so it works

--- infrastructure/mk/cpan.port.mk
+++ infrastructure/mk/cpan.port.mk
@@ -1,11 +1,18 @@
-# $OpenBSD: cpan.port.mk,v 1.1 2006/11/12 10:29:15 espie Exp $
+# $OpenBSD: cpan.port.mk,v 1.2 2006/11/12 10:45:40 espie Exp $
 
 PKGNAME?=	p5-${DISTNAME}
-MASTER_SITES?=	${MASTER_SITE_PERL_CPAN:=${DISTNAME:C/-*//}}
+MASTER_SITES?=	${MASTER_SITE_PERL_CPAN:=${DISTNAME:C/-.*$//}/}
 CATEGORIES+=	perl5
 CONFIGURE_STYLE+=perl
 MODULES+=	perl
 
+REGRESS_DEPENDS+=${RUN_DEPENDS}
+
 .if !defined(SHARED_ONLY)
 PKG_ARCH?=	* 
+.endif
+
+.if ${MAKE_ENV:MTEST_POD=*}
+REGRESS_DEPENDS+=::devel/p5-Test-Pod \
+		 ::devel/p5-Test-Pod-Coverage
 .endif
