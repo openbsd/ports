85b2fe8d8ac0e12f94414fcfc607364f9d616e83
bernd <bernd@openbsd.org>
2006-11-20 12:41:40 +0000

    Provide MODLIBICONV_{LIB,RUN}_DEPENDS to help MULTI_PACKAGES.
    ok espie@

--- converters/libiconv/libiconv.port.mk
+++ converters/libiconv/libiconv.port.mk
@@ -1,7 +1,10 @@
-# $OpenBSD: libiconv.port.mk,v 1.2 2006/08/01 10:50:19 espie Exp $
+# $OpenBSD: libiconv.port.mk,v 1.3 2006/11/20 12:41:40 bernd Exp $
 
 # The RUN_DEPENDS entry is to ensure libiconv is installed. This is
 # necessary so that we have charset.alias installed on static archs.
 # Typically installed in PREFIX/lib.
-LIB_DEPENDS+=	iconv.>=2::converters/libiconv
-RUN_DEPENDS+=	:libiconv-*:converters/libiconv
+MODLIBICONV_LIB_DEPENDS=	iconv.>=2::converters/libiconv
+MODLIBICONV_RUN_DEPENDS=	:libiconv-*:converters/libiconv
+
+LIB_DEPENDS+=			${MODLIBICONV_LIB_DEPENDS}
+RUN_DEPENDS+=			${MODLIBICONV_RUN_DEPENDS}
