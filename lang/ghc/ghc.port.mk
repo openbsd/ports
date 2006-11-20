b75dec414cd03b8bca78bba2ac01ac2165a5ddd7
espie <espie@openbsd.org>
2006-11-20 13:07:19 +0000

    fix a few obviously wrong dependencies, and remove extra shells in ghc.

--- lang/ghc/ghc.port.mk
+++ lang/ghc/ghc.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: ghc.port.mk,v 1.4 2006/07/30 13:21:38 alek Exp $
+# $OpenBSD: ghc.port.mk,v 1.5 2006/11/20 13:07:20 espie Exp $
 # Module for Glasgow Haskell Compiler
 
 # Not yet ported to other architectures
@@ -6,10 +6,10 @@
 ONLY_FOR_ARCHS=	i386 amd64
 #                   sparc
 
-BUILD_DEPENDS+=	bin/ghc::lang/ghc
+BUILD_DEPENDS+=	::lang/ghc
 
 # Only add runtime when it is actually needed (by default yes)
 MODGHC_RUNTIME?=Yes
 .if ${MODGHC_RUNTIME:L} == "yes"
-RUN_DEPENDS+=	bin/ghc::lang/ghc
+RUN_DEPENDS+=	::lang/ghc
 .endif
