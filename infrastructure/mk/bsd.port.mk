15375bededb1637b5939eaaa1a5aefd885c4d05b
espie <espie@openbsd.org>
2006-11-20 11:15:39 +0000

    fix NO_SHARED_LIBS case, found out by bernd@

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-#	$OpenBSD: bsd.port.mk,v 1.823 2006/11/20 11:11:38 espie Exp $
+#	$OpenBSD: bsd.port.mk,v 1.824 2006/11/20 11:15:39 espie Exp $
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -1163,6 +1163,7 @@ _RUN_DEP2+= 	${LIB_DEPENDS${SUBPACKAGE}:C/^[^:]*:([^:]*:[^:]*).*$/\1/}
 _LIB_DEP3=		${LIB_DEPENDS${SUBPACKAGE}:C/^[^:]*:([^:]*:[^:]*).*$/\1/}
 _DEPRUNLIBS=	${LIB_DEPENDS${SUBPACKAGE}:C/:.*//:S/,/ /g}
 .else
+_LIB_DEP3=
 _DEPRUNLIBS=
 .endif
 
