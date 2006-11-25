598ff7edac8ab1f14469c740e2808a21b88fd23b
espie <espie@openbsd.org>
2006-11-25 19:47:53 +0000

    add BASE_PKGPATH to SUBST_VARS

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-#	$OpenBSD: bsd.port.mk,v 1.834 2006/11/25 18:31:23 espie Exp $
+#	$OpenBSD: bsd.port.mk,v 1.835 2006/11/25 19:47:53 espie Exp $
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -685,7 +685,8 @@ _lt_libs+=lib${_n:S/+/_/g:S/-/_/g:S/./_/g}_ltversion=${_v}
 .endfor
 
 # Create the generic variable substitution list, from subst vars
-SUBST_VARS+=MACHINE_ARCH ARCH HOMEPAGE PREFIX SYSCONFDIR FLAVOR_EXT MAINTAINER
+SUBST_VARS+=MACHINE_ARCH ARCH HOMEPAGE PREFIX SYSCONFDIR FLAVOR_EXT \
+			MAINTAINER BASE_PKGPATH
 _tmpvars=
 _SED_SUBST=sed
 
