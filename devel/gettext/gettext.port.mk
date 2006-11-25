61a8e0278e1c876b0d86763ecc016083a7a20acf
steven <steven@openbsd.org>
2006-11-25 12:58:18 +0000

    tighten build dependency to current version
    ok bernd@ espie@

--- devel/gettext/gettext.port.mk
+++ devel/gettext/gettext.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: gettext.port.mk,v 1.5 2006/11/20 13:37:00 bernd Exp $
+# $OpenBSD: gettext.port.mk,v 1.6 2006/11/25 12:58:18 steven Exp $
 
 MODGETTEXT_LIB_DEPENDS=	intl.>=3:gettext->=0.10.38:devel/gettext \
 			iconv.>=4::converters/libiconv
@@ -9,5 +9,5 @@ MODGETTEXT_RUN_DEPENDS=	:gettext->=0.10.38:devel/gettext
 # necessary so that we have locale.alias installed on static archs.
 # Typically installed in PREFIX/share/locale.
 LIB_DEPENDS+=	${MODGETTEXT_LIB_DEPENDS}
-BUILD_DEPENDS+=	${MODGETTEXT_RUN_DEPENDS}
+BUILD_DEPENDS+=	:gettext->=0.14.6:devel/gettext
 RUN_DEPENDS+=	${MODGETTEXT_RUN_DEPENDS}
