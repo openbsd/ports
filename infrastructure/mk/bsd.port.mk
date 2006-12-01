5f5c120220f0843ec4c68d765b0c530526aa8eb9
espie <espie@openbsd.org>
2006-12-01 17:37:15 +0000

    use the pkg-config in /usr/local preferably, until we get a decent pkg-config in /usr/bin...

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-#	$OpenBSD: bsd.port.mk,v 1.861 2006/12/01 17:33:16 espie Exp $
+#	$OpenBSD: bsd.port.mk,v 1.862 2006/12/01 17:37:15 espie Exp $
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -1865,6 +1865,7 @@ ${_BULK_COOKIE}:
 ${_WRKDIR_COOKIE}:
 	@rm -rf ${WRKDIR}
 	@mkdir -p ${WRKDIR} ${WRKDIR}/bin ${DEPDIR}
+	@ln -s ${LOCALBASE}/bin/pkg-config ${WRKDIR}/bin
 	@${_MAKE_COOKIE} $@
 
 ${_EXTRACT_COOKIE}: ${_WRKDIR_COOKIE} ${_SYSTRACE_COOKIE}
