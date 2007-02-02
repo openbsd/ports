1c6866fbf46b567014e1102728399b6c509e1b4b
steven <steven@openbsd.org>
2007-02-02 16:28:27 +0000

    oops

--- security/tcltls/blah.tcl
+++
@@ -1,7 +0,0 @@
-package require http
-package require tls
-
-http::register https 443 [list ::tls::socket -require 1 -cafile ./server.pem]
-
-set tok [http::geturl https://securewww.esat.kuleuven.be/]
-
