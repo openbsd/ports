a68b1de1076929c837f73a9fa81362162c1dc8ee
aanriot <aanriot@openbsd.org>
2007-05-26 01:16:13 +0000

    update to 0.9.11 (0.9.10 was broken and has been fixed upstream).
    tested by rui@

---
+++ security/prelude/prewikka/files/siteconfig.py
@@ -0,0 +1,5 @@
+htdocs_dir = '%%PREFIX%%/share/prewikka/htdocs'
+database_dir = '%%PREFIX%%/share/prewikka/database'
+locale_dir = '%%PREFIX%%/share/locale/'
+conf_dir = '%%SYSCONFDIR%%/prewikka'
+version = '%%VERSION%%'
