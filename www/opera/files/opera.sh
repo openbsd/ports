91c2a0d17a0cfee54c8ed511763963a3b06576ad
sturm <sturm@openbsd.org>
2007-05-29 10:46:38 +0000

    remove workaround for broken linux compat regarding shmat() this needs a current kernel to work

--- www/opera/files/opera.sh
+++ www/opera/files/opera.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $OpenBSD: opera.sh,v 1.7 2007/05/21 20:15:11 sturm Exp $
+# $OpenBSD: opera.sh,v 1.8 2007/05/29 10:46:38 sturm Exp $
 
 PLUGIN_PATH=@PREFIX@/lib/opera/plugins
 PLUGIN_PATH=${PLUGIN_PATH}:@LOCALBASE@/lib/ns-plugins
@@ -10,7 +10,6 @@ LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/libexec
 PATH=/bin:/usr/bin
 
 export OPERA_DIR=@PREFIX@/share/opera
-export OPERA_NUM_XSHM=0
 export OPERA_PLUGIN_PATH=${OPERA_PLUGIN_PATH-${PLUGIN_PATH}}
 export LD_LIBRARY_PATH PATH
 
