dc92ed5abf91e41c4b261da1cf8459350e4c69d2
jasper <jasper@openbsd.org>
2007-05-31 11:30:35 +0000

    construct MASTER_SITES based on GNOME_PROJECT and GNOME_VERSION Both should be set in a port's Makefile.

--- x11/gnome/gnome.port.mk
+++ x11/gnome/gnome.port.mk
@@ -1,7 +1,10 @@
-# $OpenBSD: gnome.port.mk,v 1.1 2007/05/30 09:19:02 jasper Exp $
+# $OpenBSD: gnome.port.mk,v 1.2 2007/05/31 11:30:35 jasper Exp $
 # Module for GNOME related ports
 
 CATEGORIES+=		x11/gnome
+
+DISTNAME=		${GNOME_PROJECT}-${GNOME_VERSION}
+V=			${GNOME_VERSION:C/:*.[0-9]$//}
 
 USE_LIBTOOL?=		Yes
 USE_X11=		Yes
@@ -12,6 +15,7 @@ BUILD_DEPENDS+=	 	:intltool-*:textproc/intltool \
 			:p5-XML-Parser-*:textproc/p5-XML-Parser	
 RUN_DEPENDS+=		${MODGNOME_RUN_DEPENDS}
 
+MASTER_SITES=		${MASTER_SITE_GNOME:=sources/${GNOME_PROJECT}/${V}/}
 EXTRACT_SUFX?=		.tar.bz2
 
 MODGNOME_CONFIGURE_ENV=	CPPFLAGS="-I${LOCALBASE}/include -I${X11BASE}/include" \
