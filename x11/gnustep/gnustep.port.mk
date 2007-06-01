7b55a8c59cceec7904eccbf0a9833b8859de1275
ajacoutot <ajacoutot@openbsd.org>
2007-06-01 00:27:19 +0000

    - don't use RUN_DEPENDS+=${BUILD_DEPENDS} so people stop bugging me and start testing instead

--- x11/gnustep/gnustep.port.mk
+++ x11/gnustep/gnustep.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: gnustep.port.mk,v 1.1.1.1 2007/05/29 02:59:11 ajacoutot Exp $
+# $OpenBSD: gnustep.port.mk,v 1.2 2007/06/01 00:27:19 ajacoutot Exp $
 
 SHARED_ONLY=	Yes
 
@@ -6,7 +6,7 @@ USE_X11=	Yes
 USE_GMAKE=	Yes
 
 BUILD_DEPENDS+=	:gnustep-make-*:x11/gnustep/make
-RUN_DEPENDS+=	${BUILD_DEPENDS}
+RUN_DEPENDS+=	:gnustep-make-*:x11/gnustep/make
 
 MAKE_FLAGS+=	CC="${CC}" CPP="${CC} -E" OPTFLAG="${CFLAGS}"
 
