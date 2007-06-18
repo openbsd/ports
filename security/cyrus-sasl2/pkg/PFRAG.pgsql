610c24112b1627fec9d7ecde24a0a27d63d0d896
ajacoutot <ajacoutot@openbsd.org>
2007-06-18 12:31:20 +0000

    - long due update to 2.1.22 - merge the 3 PLISTs for sql related FLAVORS (sqlite, pgsql and mysql) into one - use a VARIABLE for the "incompatible FLAVORS" message - post-extract -> post-configure, to ease patches updates - fmt pkg/DESCR
    "go ahead" robert@

--- security/cyrus-sasl2/pkg/PFRAG.pgsql
+++
@@ -1,4 +0,0 @@
-@comment $OpenBSD: PFRAG.pgsql,v 1.1 2005/01/05 10:03:14 jakob Exp $
-%%SHARED%%
-lib/sasl2/libsql.a
-lib/sasl2/libsql.la
---
+++ security/cyrus-sasl2/pkg/PFRAG.sql
@@ -0,0 +1,4 @@
+@comment $OpenBSD: PFRAG.sql,v 1.5 2007/06/18 12:31:20 ajacoutot Exp $
+%%SHARED%%
+lib/sasl2/libsql.a
+lib/sasl2/libsql.la
