cf1f0a4d30f5db57741d3fff3350ab806b8d25b4
jasper <jasper@openbsd.org>
2007-07-03 08:50:36 +0000

    only use desktop-file-utils if DESKTOP_FILES is set.

--- x11/gnome/gnome.port.mk
+++ x11/gnome/gnome.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: gnome.port.mk,v 1.13 2007/06/30 12:14:14 jasper Exp $
+# $OpenBSD: gnome.port.mk,v 1.14 2007/07/03 08:50:36 jasper Exp $
 # Module for GNOME related ports
 
 CATEGORIES+=		x11/gnome
@@ -12,9 +12,11 @@ BUILD_DEPENDS+=	 	:intltool-*:textproc/intltool \
 			:p5-XML-Parser-*:textproc/p5-XML-Parser
 .endif
 
+.if defined(DESKTOP_FILES)
 MODGNOME_RUN_DEPENDS=	:desktop-file-utils-*:devel/desktop-file-utils
 
 RUN_DEPENDS+=		${MODGNOME_RUN_DEPENDS}
+.endif
 
 MASTER_SITES?=		${MASTER_SITE_GNOME:=sources/${GNOME_PROJECT}/${GNOME_VERSION:R}/}
 EXTRACT_SUFX?=		.tar.bz2
