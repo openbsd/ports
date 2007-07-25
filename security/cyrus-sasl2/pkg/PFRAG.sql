79b1a483cd914d62d9f095ad1a7a285f050b01c0
ajacoutot <ajacoutot@openbsd.org>
2007-07-25 16:25:57 +0000

    - add a sample Sendmail.conf-sql for Sendmail authentication (*sql FLAVORS only)
    based on a bigger diff by Mike Erdely <mike at erdelynet dot com>

--- security/cyrus-sasl2/pkg/PFRAG.sql
+++ security/cyrus-sasl2/pkg/PFRAG.sql
@@ -1,4 +1,6 @@
-@comment $OpenBSD: PFRAG.sql,v 1.5 2007/06/18 12:31:20 ajacoutot Exp $
+@comment $OpenBSD: PFRAG.sql,v 1.6 2007/07/25 16:25:57 ajacoutot Exp $
 %%SHARED%%
 lib/sasl2/libsql.a
 lib/sasl2/libsql.la
+share/examples/sasl2/
+share/examples/sasl2/Sendmail.conf-sql
