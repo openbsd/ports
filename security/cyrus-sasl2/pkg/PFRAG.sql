6fb3bea15a884b0c4118b17c2415507b3ab77286
ajacoutot <ajacoutot@openbsd.org>
2008-03-15 12:50:46 +0000

    - provide @sample saslauthd.conf for saslauthd LDAP auth - also add LDAP_SASLAUTHD doc

--- security/cyrus-sasl2/pkg/PFRAG.sql
+++ security/cyrus-sasl2/pkg/PFRAG.sql
@@ -1,6 +1,5 @@
-@comment $OpenBSD: PFRAG.sql,v 1.6 2007/07/25 16:25:57 ajacoutot Exp $
+@comment $OpenBSD: PFRAG.sql,v 1.7 2008/03/15 12:50:46 ajacoutot Exp $
 %%SHARED%%
 lib/sasl2/libsql.a
 lib/sasl2/libsql.la
-share/examples/sasl2/
 share/examples/sasl2/Sendmail.conf-sql
