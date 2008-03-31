feb531b75c362899b4870dcbc8a55829406c8f46
jakemsr <jakemsr@openbsd.org>
2008-03-31 01:05:54 +0000

    bring back esound-0.2.38.
    fix the issues with audio(4) handling that caused this to get
    reverted previously.
    
    add an arts flavor.

--- audio/esound/files/audio_sun.c
+++ audio/esound/files/audio_sun.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: audio_sun.c,v 1.3 2004/01/25 22:07:28 fgsch Exp $	*/
+/*	$OpenBSD: audio_sun.c,v 1.4 2008/03/31 01:05:54 jakemsr Exp $	*/
 
 /*
  * Copyright (c) 2002 CubeSoft Communications, Inc.
@@ -27,7 +27,10 @@
 
 #include "config.h"
 
+#include <sys/types.h>
+#include <sys/ioctl.h>
 #include <sys/audioio.h>
+#include <string.h>
 
 static void sun_panic(int fd, char *s)
 {
@@ -48,9 +51,8 @@ int esd_audio_open()
 {
     const char *device;
     int afd = -1;
-    int fmt = 0, channels = 0;
+    int fmt = 0, channels = 0, bits = 0;
     int mode = O_WRONLY;
-    audio_encoding_t enc;
     audio_info_t info;
 
     AUDIO_INITINFO(&info);
@@ -58,12 +60,10 @@ int esd_audio_open()
     /* set the appropriate mode */
     if((esd_audio_format & ESD_MASK_FUNC) == ESD_RECORD) {
         mode = O_RDWR;
-	info.mode = AUMODE_PLAY | AUMODE_RECORD;
+	info.mode = AUMODE_PLAY | AUMODE_PLAY_ALL | AUMODE_RECORD;
     } else {
-	info.mode = AUMODE_PLAY;
+	info.mode = AUMODE_PLAY | AUMODE_PLAY_ALL;
     }
-
-    mode |= O_NONBLOCK;
 
     /* open the sound device */
     device = esd_audio_device ? esd_audio_device : "/dev/audio";
@@ -71,10 +71,6 @@ int esd_audio_open()
         perror(device);
         return( -2 );
     }
-
-    mode = fcntl(afd, F_GETFL);
-    mode &= ~O_NONBLOCK;
-    fcntl(afd, F_SETFL, mode);
 
     /* set the requested mode */
     if(ioctl(afd, AUDIO_SETINFO, &info) < 0) {
@@ -91,22 +87,24 @@ int esd_audio_open()
     }
 
     /* pick a supported encoding */
-    
-#if defined(WORDS_BIGENDIAN)
-    fmt = ( (esd_audio_format & ESD_MASK_BITS) == ESD_BITS16 )
-        ? /* 16 bit */  AUDIO_ENCODING_SLINEAR_BE
-	: /* 8 bit */	AUDIO_ENCODING_PCM8;
-#else
-    fmt = ( (esd_audio_format & ESD_MASK_BITS) == ESD_BITS16 )
-        ? /* 16 bit */	AUDIO_ENCODING_SLINEAR_LE
-	: /* 8 bit */	AUDIO_ENCODING_PCM8;
-#endif
-
-    enc.index = 0;
-    while(!ioctl(afd, AUDIO_GETENC, &enc) && enc.encoding != fmt) {
-	enc.index++;
+    if ((esd_audio_format & ESD_MASK_BITS) == ESD_BITS16) {
+        bits = 16;
+        fmt = (BYTE_ORDER == 4321) ?
+          AUDIO_ENCODING_SLINEAR_BE : AUDIO_ENCODING_SLINEAR_LE;
+    } else {
+        bits = 8;
+        fmt = (BYTE_ORDER == 4321) ?
+          AUDIO_ENCODING_ULINEAR_BE : AUDIO_ENCODING_ULINEAR_LE;
     }
-    
+    info.play.encoding = fmt;
+    info.play.precision = bits;
+    if(ioctl(afd, AUDIO_SETINFO, &info) == -1) {
+	fprintf(stderr, "Unsupported encoding: %i-bit (0x%x)\n",
+	    bits, fmt);
+	sun_panic(afd, "SETINFO");
+	return(-1);
+    }
+
     /* number of channels */
     channels = (((esd_audio_format & ESD_MASK_CHAN) == ESD_STEREO)
         ? /* stereo */	2
@@ -116,13 +114,19 @@ int esd_audio_open()
     if((esd_audio_format & ESD_MASK_FUNC) == ESD_RECORD) {
 	info.record.channels = channels;
     }
-
-    info.play.encoding = fmt;
-    info.play.precision = enc.precision;
-
     if(ioctl(afd, AUDIO_SETINFO, &info) == -1) {
-	fprintf(stderr, "Unsupported encoding: %i-bit \"%s\" (0x%x)\n",
-	    enc.precision, enc.name, fmt);
+	fprintf(stderr, "Unsupported channel count: %d\n",
+	    channels);
+	sun_panic(afd, "SETINFO");
+	return(-1);
+    }
+
+    /* blocksize, sync to internal esd buffer size */
+    info.blocksize = ESD_BUF_SIZE;
+    info.hiwat = 4;
+    if(ioctl(afd, AUDIO_SETINFO, &info) < 0) {
+	fprintf(stderr, "Unsupported blocksize: %d\n",
+	    info.blocksize);
 	sun_panic(afd, "SETINFO");
 	return(-1);
     }
