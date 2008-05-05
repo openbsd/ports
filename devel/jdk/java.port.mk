92ac5f7392545c77eff7fed3aec58bbe0234c523
kurt <kurt@openbsd.org>
2008-05-05 02:53:15 +0000

    Add support for 1.6 and 1.7 level ports. okay nikolay@ ian@

--- devel/jdk/java.port.mk
+++ devel/jdk/java.port.mk
@@ -1,9 +1,9 @@
-# $OpenBSD: java.port.mk,v 1.14 2008/03/18 19:40:03 sturm Exp $
+# $OpenBSD: java.port.mk,v 1.15 2008/05/05 02:53:15 kurt Exp $
 
 # Set MODJAVA_VER to x.y or x.y+ based on the version
 # of the jdk needed for the port. x.y  means any x.y jdk.
 # x.y+ means any x.y jdk or higher version. Valid values
-# for x.y are 1.3, 1.4 or 1.5.
+# for x.y are 1.3, 1.4, 1.5, 1.6 or 1.7
 
 MODJAVA_VER?=
 
@@ -111,6 +111,38 @@ MODJAVA_JRERUN?=no
      MODJAVA_RUN_DEPENDS= :${_MODJAVA_RUNDEP:S/-/->=/g}:devel/jdk/1.5
 .  else
      MODJAVA_RUN_DEPENDS= :${_MODJAVA_RUNDEP}:devel/jdk/1.5
+.  endif
+.elif ${MODJAVA_VER:S/+//} == "1.6"
+   ONLY_FOR_ARCHS?= i386 amd64
+.  if ${NO_BUILD:L} != "yes"
+     JAVA_HOME= ${LOCALBASE}/jdk-1.6.0
+     BUILD_DEPENDS+= :jdk-1.6.0*:devel/jdk/1.6
+.  endif
+.  if ${MODJAVA_JRERUN:L} == "yes"
+     _MODJAVA_RUNDEP= jdk-1.6.0.*|jre-1.6.0.*
+.  else
+     _MODJAVA_RUNDEP= jdk-1.6.0.*
+.  endif
+.  if ${MODJAVA_VER} == "1.6+"
+     MODJAVA_RUN_DEPENDS= :${_MODJAVA_RUNDEP:S/-/->=/g}:devel/jdk/1.6
+.  else
+     MODJAVA_RUN_DEPENDS= :${_MODJAVA_RUNDEP}:devel/jdk/1.6
+.  endif
+.elif ${MODJAVA_VER:S/+//} == "1.7"
+   ONLY_FOR_ARCHS?= i386 amd64
+.  if ${NO_BUILD:L} != "yes"
+     JAVA_HOME= ${LOCALBASE}/jdk-1.7.0
+     BUILD_DEPENDS+= :jdk-1.7.0*:devel/jdk/1.7
+.  endif
+.  if ${MODJAVA_JRERUN:L} == "yes"
+     _MODJAVA_RUNDEP= jdk-1.7.0.*|jre-1.7.0.*
+.  else
+     _MODJAVA_RUNDEP= jdk-1.7.0.*
+.  endif
+.  if ${MODJAVA_VER} == "1.7+"
+     MODJAVA_RUN_DEPENDS= :${_MODJAVA_RUNDEP:S/-/->=/g}:devel/jdk/1.7
+.  else
+     MODJAVA_RUN_DEPENDS= :${_MODJAVA_RUNDEP}:devel/jdk/1.7
 .  endif
 .else
    ERRORS+="Fatal: MODJAVA_VER must be set to a valid value."
