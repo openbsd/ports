30848b140619cd61d71122a087dd52c50b882e72
ajacoutot <ajacoutot@openbsd.org>
2008-05-20 07:16:15 +0000

    - update to version 2007a1
    from Daniel Dickman <didickman at gmail dot com>, thanks!

--- mail/imap-uw/patches/patch-src_osdep_unix_env_unix.c
+++ mail/imap-uw/patches/patch-src_osdep_unix_env_unix.c
@@ -1,8 +1,8 @@
-$OpenBSD: patch-src_osdep_unix_env_unix.c,v 1.10 2008/01/03 10:52:25 ajacoutot Exp $
+$OpenBSD: patch-src_osdep_unix_env_unix.c,v 1.11 2008/05/20 07:16:16 ajacoutot Exp $
 
---- src/osdep/unix/env_unix.c.orig	Mon Dec 17 22:56:08 2007
-+++ src/osdep/unix/env_unix.c	Thu Jan  3 10:58:07 2008
-@@ -942,15 +942,12 @@ char *myhomedir ()
+--- src/osdep/unix/env_unix.c.orig	Fri Feb 15 17:26:44 2008
++++ src/osdep/unix/env_unix.c	Sun May 18 16:23:38 2008
+@@ -957,15 +957,12 @@ char *myhomedir ()
  static char *mymailboxdir ()
  {
    char *home = myhomedir ();
