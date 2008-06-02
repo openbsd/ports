e37b0475cc0c5d18383183d5ba1aaec80abd5c46
martynas <martynas@openbsd.org>
2008-06-02 17:29:56 +0000

    - remove origin - remove redundant substitution - better pattern for watch_fullscreen - bump package name "sounds good" jolan@;  tested by Wiktor Izdebski

--- net/yt/files/yt.lua
+++ net/yt/files/yt.lua
@@ -1,5 +1,5 @@
 #!/usr/local/bin/lua
--- $OpenBSD: yt.lua,v 1.10 2008/05/06 20:49:51 martynas Exp $
+-- $OpenBSD: yt.lua,v 1.11 2008/06/02 17:29:56 martynas Exp $
 -- Fetch videos from YouTube.com and convert them to MPEG.
 -- Written by Pedro Martelletto in August 2006. Public domain.
 -- Example: lua yt.lua http://www.youtube.com/watch?v=c5uoo1Kl_uA
@@ -16,7 +16,7 @@ fetch = "ftp -o <file> <url>"
 convert = "ffmpeg -y -i <flv> -b 1000k -f mp4 -vcodec mpeg4 -acodec libfaac -ab 128k <mp4> 1>/dev/null 2>&1"
 
 -- Set this to the base location where to fetch YouTube videos from.
-base_url = "http://www.youtube.com/get_video?origin=youtube.com"
+base_url = "http://www.youtube.com/get_video"
 
 -- Fetch the page holding the embedded video.
 print(string.format("Getting %s ...", url))
@@ -27,7 +27,6 @@ pattern = "<meta name=\"title\" content=\"(.-)\""
 title = assert(string.match(body, pattern))
 
 -- Build a name for the files the video will be stored in.
-file = string.gsub(title, " ", "_")
 file = string.gsub(title, "[^%w-]", "_")
 file = "youtube-" .. string.lower(file)
 flv = file .. ".flv"
@@ -38,15 +37,15 @@ e_flv = string.format("%q", flv)
 e_mp4 = string.format("%q", mp4)
 
 -- Look for the video ID.
-pattern = "/watch_fullscreen.*video_id=(.-)&"
+pattern = "/watch_fullscreen%?.*video_id=(.-)&"
 video_id = assert(string.match(body, pattern))
 
 -- Look for the additional video ID.
-pattern = "/watch_fullscreen.*&t=(.-)&"
+pattern = "/watch_fullscreen%?.*&t=(.-)&"
 t = assert(string.match(body, pattern))
 
 -- Fetch the video.
-url = string.format("%q", base_url .. "&video_id=" .. video_id .. "&t=" .. t)
+url = string.format("%q", base_url .. "?video_id=" .. video_id .. "&t=" .. t)
 cmd = string.gsub(fetch, "<(%w+)>", { url = url, file = e_flv })
 assert(os.execute(cmd) == 0, "Failed")
 
