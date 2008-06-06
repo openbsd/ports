13bf7688dd7dbc9b45377f325b18384d56db3e13
fgsch <fgsch@openbsd.org>
2008-06-06 01:17:13 +0000

    fix fetching files from MASTER_SITE[0-9]. found earlier by me during a full port-lib-depends-check and later reported on ports by Tim Donahue (tdonahue at vonsystems dot com). espie@ ok.

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-#	$OpenBSD: bsd.port.mk,v 1.941 2008/06/05 08:14:00 fgsch Exp $
+#	$OpenBSD: bsd.port.mk,v 1.942 2008/06/06 01:17:13 fgsch Exp $
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -2309,7 +2309,7 @@ ${_F}:
 	@lock=${_F:T}.dist; ${_SIMPLE_LOCK}; mkdir -p ${_F:H}; \
 	cd ${_F:H}; \
 	test -f ${_F:T} && exit 0; \
-	select=${_EVERYTHING:M*${_F:S@^${FULLDISTDIR}/@@}\:[0-9]}; \
+	select='${_EVERYTHING:M*${_F:S@^${FULLDISTDIR}/@@}\:[0-9]}'; \
 	f=${_F:S@^${FULLDISTDIR}/@@}; \
 	${ECHO_MSG} ">> $$f doesn't seem to exist on this system."; \
 	${_CDROM_OVERRIDE}; \
