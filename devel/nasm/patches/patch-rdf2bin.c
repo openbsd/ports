596cd2c0cd6deab0397885d46fa78b1d2c44fdd3
okan <okan@openbsd.org>
2008-06-11 19:35:46 +0000

    update to 2.03 from Gallon Sylvestre - thanks!
    ok merdely@

--- devel/nasm/patches/patch-rdf2bin.c
+++ devel/nasm/patches/patch-rdf2bin.c
@@ -1,15 +1,1 @@
---- rdoff/rdf2bin.c.orig	Tue Feb 19 14:08:57 2008
-+++ rdoff/rdf2bin.c	Mon Apr 21 15:18:26 2008
-@@ -119,9 +119,9 @@ int main(int argc, char **argv)
-         return 1;
-     }
- 
--    if (fwrite(m->t, 1, m->f.seg[0].length, of) != m->f.seg[0].length ||
--        fwrite(padding, 1, codepad, of) != codepad ||
--        fwrite(m->d, 1, m->f.seg[1].length, of) != m->f.seg[1].length) {
-+    if (fwrite(m->t, 1, m->f.seg[0].length, of) != (unsigned int)m->f.seg[0].length ||
-+        fwrite(padding, 1, codepad, of) != (unsigned int)codepad ||
-+        fwrite(m->d, 1, m->f.seg[1].length, of) != (unsigned int)m->f.seg[1].length) {
-         fprintf(stderr, "rdf2bin: error writing to %s\n", *argv);
-         return 1;
-     }
+
