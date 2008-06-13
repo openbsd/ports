382931c0ad68d341a7386ca193ef8b7968c00019
naddy <naddy@openbsd.org>
2008-06-13 04:56:33 +0000

    Update to 0.17. The major new functionality in this release is colorization support in the message catalog tools. ok brad@

--- devel/gettext/gettext.port.mk
+++ devel/gettext/gettext.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: gettext.port.mk,v 1.7 2007/08/25 13:48:23 naddy Exp $
+# $OpenBSD: gettext.port.mk,v 1.8 2008/06/13 04:56:33 naddy Exp $
 
 MODGETTEXT_LIB_DEPENDS=	intl.>=4:gettext->=0.10.38:devel/gettext \
 			iconv.>=4::converters/libiconv
@@ -9,5 +9,5 @@ MODGETTEXT_RUN_DEPENDS=	:gettext->=0.10.38:devel/gettext
 # necessary so that we have locale.alias installed on static archs.
 # Typically installed in PREFIX/share/locale.
 LIB_DEPENDS+=	${MODGETTEXT_LIB_DEPENDS}
-BUILD_DEPENDS+=	:gettext->=0.16.1:devel/gettext
+BUILD_DEPENDS+=	:gettext->=0.17:devel/gettext
 RUN_DEPENDS+=	${MODGETTEXT_RUN_DEPENDS}
