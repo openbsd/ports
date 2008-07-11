3874e6995b59b8752a28d6dc693c207b068bd214
brad <brad@openbsd.org>
2008-07-11 11:40:33 +0000

    Update to Samba 3.0.30; bug fixes and a security issue fixed, CVE-2008-1105.
    Specifically crafted SMB responses can result in a heap overflow
    in the Samba client code. Because the server process, smbd, can
    itself act as a client during operations such as printer
    notification and domain authentication, this issue affects both
    Samba client and server installations.
    
    Feedback from sthen@
    ok mbalmer@ sthen@

--- net/samba/patches/patch-lib_iconv.c
+++ net/samba/patches/patch-lib_iconv.c
@@ -1,7 +1,7 @@
-$OpenBSD: patch-lib_iconv.c,v 1.1 2007/12/14 08:53:27 mbalmer Exp $
---- lib/iconv.c.orig	Tue Jun  5 01:45:05 2007
-+++ lib/iconv.c	Tue Jun  5 01:46:09 2007
-@@ -137,7 +137,7 @@
+$OpenBSD: patch-lib_iconv.c,v 1.2 2008/07/11 11:40:33 brad Exp $
+--- lib/iconv.c.orig	Wed May 28 08:41:11 2008
++++ lib/iconv.c	Thu Jul  3 04:19:09 2008
+@@ -137,7 +137,7 @@ static size_t sys_iconv(void *cd, 
  			char **outbuf, size_t *outbytesleft)
  {
  	size_t ret = iconv((iconv_t)cd, 
