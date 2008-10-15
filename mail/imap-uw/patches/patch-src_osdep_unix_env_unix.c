aff161c8cbbc42c5cf98278faaace6c230f13d4b
ajacoutot <ajacoutot@openbsd.org>
2008-10-15 14:29:04 +0000

    - update to imap-uw-2007b - use SUBST_CMD - simplify comment about certificate in README.OpenBSD (suggested by Sam Smith)

--- mail/imap-uw/patches/patch-src_osdep_unix_env_unix.c
+++ mail/imap-uw/patches/patch-src_osdep_unix_env_unix.c
@@ -1,8 +1,8 @@
-$OpenBSD: patch-src_osdep_unix_env_unix.c,v 1.11 2008/05/20 07:16:16 ajacoutot Exp $
+$OpenBSD: patch-src_osdep_unix_env_unix.c,v 1.12 2008/10/15 14:29:04 ajacoutot Exp $
 
---- src/osdep/unix/env_unix.c.orig	Fri Feb 15 17:26:44 2008
-+++ src/osdep/unix/env_unix.c	Sun May 18 16:23:38 2008
-@@ -957,15 +957,12 @@ char *myhomedir ()
+--- src/osdep/unix/env_unix.c.orig	Tue May 13 03:17:54 2008
++++ src/osdep/unix/env_unix.c	Sat Oct 11 18:33:06 2008
+@@ -963,15 +963,12 @@ char *myhomedir ()
  static char *mymailboxdir ()
  {
    char *home = myhomedir ();
