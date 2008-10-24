b2f25c8ba0e9339ebaaf4a55b6440dc72f65d039
jasper <jasper@openbsd.org>
2008-10-24 15:07:13 +0000

    - use ${TMPDIR} instead of "/tmp"
    ok robert@

--- lang/mono/mono.port.mk
+++ lang/mono/mono.port.mk
@@ -1,9 +1,9 @@
-# $OpenBSD: mono.port.mk,v 1.4 2008/10/24 15:05:03 robert Exp $
+# $OpenBSD: mono.port.mk,v 1.5 2008/10/24 15:07:13 jasper Exp $
 
 CATEGORIES+=		lang/mono
 
-CONFIGURE_ENV+=		MONO_SHARED_DIR=/tmp
-MAKE_FLAGS+=		MONO_SHARED_DIR=/tmp
+CONFIGURE_ENV+=		MONO_SHARED_DIR=${TMPDIR}
+MAKE_FLAGS+=		MONO_SHARED_DIR=${TMPDIR}
 
 # A list of files where we have to remove the stupid hardcoded .[0-9] major
 # version from library names. 
