62d1eb4a9c4d0a2e1e202d4c8e6f1b85ac76390a
robert <robert@openbsd.org>
2008-10-24 14:49:44 +0000

    Add MONO_SHARED_DIR=/tmp to the MAKE_FLAGS so the ports using this module won't try to write to /root/.wapi

--- lang/mono/mono.port.mk
+++ lang/mono/mono.port.mk
@@ -1,6 +1,8 @@
-# $OpenBSD: mono.port.mk,v 1.2 2008/10/16 21:47:10 jasper Exp $
+# $OpenBSD: mono.port.mk,v 1.3 2008/10/24 14:49:44 robert Exp $
 
 CATEGORIES+=		lang/mono
+
+MAKE_FLAGS+=		MONO_SHARED_DIR=/tmp
 
 # A list of files where we have to remove the stupid hardcoded .[0-9] major
 # version from library names. 
