ce7b688eb4d0747cc0d5cd822d4846ab43b10355
landry <landry@openbsd.org>
2008-10-26 12:21:47 +0000

    Fix waf MODULE, honour CONFIGURE_ENV and CONFIGURE_ARGS, clean up some root-owned directories in ${WRKSRC} after fake. Nothing in tree used it, hence no PKGNAME bump.
    ok wcmaier@, discussed a bit with ajacoutot@ too

--- devel/waf/waf.port.mk
+++ devel/waf/waf.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: waf.port.mk,v 1.1.1.1 2008/09/23 19:55:02 landry Exp $
+# $OpenBSD: waf.port.mk,v 1.2 2008/10/26 12:21:47 landry Exp $
 
 MODULES +=		lang/python
 MODPY_RUNDEP =		no
@@ -6,9 +6,10 @@ BUILD_DEPENDS +=	::devel/waf
 MODWAF_BIN =		${LOCALBASE}/bin/waf
 MAKE_ENV +=		PYTHON=${MODPY_BIN} PYTHON_VERSION=${MODPY_VERSION}
 _MODWAF_CMD =		@cd ${WRKSRC} && ${SETENV} ${MAKE_ENV} ${MODWAF_BIN}
+_MODWAF_CONFIGURE_CMD =	@cd ${WRKSRC} && ${SETENV} ${MAKE_ENV} ${CONFIGURE_ENV} ${MODWAF_BIN}
 
 do-configure:
-	${_MODWAF_CMD} configure
+	${_MODWAF_CONFIGURE_CMD} configure ${CONFIGURE_ARGS}
 
 .if !target(do-build)
 do-build:
@@ -18,4 +19,9 @@ do-build:
 .if !target(do-install)
 do-install:
 	${_MODWAF_CMD} install --destdir=${WRKINST}
+.endif
+
+.if !target(post-install)
+post-install:
+	@rm -Rf ${WRKSRC}/_build_
 .endif
