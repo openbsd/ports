c78568ec50f8927a1a6a0265795b066d6e6adb58
ajacoutot <ajacoutot@openbsd.org>
2008-10-28 12:26:39 +0000

    - reverse logic so that gcc3/4 is actually used when USE_GCC* is set - set USE_GCC* to Yes by default when using a gcc* MODULE which makes much more sense
    discussed with and ok robert@

--- infrastructure/mk/gcc3.port.mk
+++ infrastructure/mk/gcc3.port.mk
@@ -1,5 +1,5 @@
-USE_GCC3 ?= No
+USE_GCC3 ?= Yes
 
-.if ${USE_GCC3:L} == "no"
+.if ${USE_GCC3:L} == "yes"
 .include "${PORTSDIR}/lang/gcc/3.3/gcc3.port.mk"
 .endif
--- infrastructure/mk/gcc4.port.mk
+++ infrastructure/mk/gcc4.port.mk
@@ -1,5 +1,5 @@
-USE_GCC4 ?= No
+USE_GCC4 ?= Yes
 
-.if ${USE_GCC4:L} == "no"
+.if ${USE_GCC4:L} == "yes"
 .include "${PORTSDIR}/lang/gcc/4.2/gcc4.port.mk"
 .endif
