66af83992e3030fb85b5261a7c106df3ea6f74aa
ajacoutot <ajacoutot@openbsd.org>
2008-11-29 12:15:57 +0000

    - add MODQT_PKG_CONFIG_PATH so that ports depending on the qt4 MODULE don't need to be tweaked to be able to find qt4 pkgconfig files
    input from and ok espie@

--- x11/qt4/qt4.port.mk
+++ x11/qt4/qt4.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: qt4.port.mk,v 1.3 2008/09/03 21:58:17 brad Exp $
+# $OpenBSD: qt4.port.mk,v 1.4 2008/11/29 12:15:57 ajacoutot Exp $
 
 # This fragment defines MODQT_* variables to make it easier to substitute
 # qt1/qt2/qt3 in a port.
@@ -9,12 +9,14 @@ MODQT4_LIBDIR =	${LOCALBASE}/lib/qt4
 MODQT_LIBDIR ?= ${MODQT4_LIBDIR}
 MODQT4_INCDIR =	${LOCALBASE}/include/X11/qt4
 MODQT_INCDIR ?= ${MODQT4_INCDIR}
+MODQT_PKG_CONFIG_PATH ?= ${LOCALBASE}/lib/qt4/pkgconfig
 MODQT4_CONFIGURE_ARGS =	--with-qt-includes=${MODQT4_INCDIR} \
 			--with-qt-libraries=${MODQT4_LIBDIR}
 MODQT_CONFIGURE_ARGS ?= ${MODQT4_CONFIGURE_ARGS}
 _MODQT4_SETUP =	MOC=${MODQT4_MOC} \
 		MODQT_INCDIR=${MODQT4_INCDIR} \
-		MODQT_LIBDIR=${MODQT4_LIBDIR}
+		MODQT_LIBDIR=${MODQT4_LIBDIR} \
+		PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${MODQT_PKG_CONFIG_PATH}
 .if ${MODQT4_OVERRIDE_UIC:L} == "yes"
 _MODQT4_SETUP +=UIC=${MODQT4_UIC}
 .endif
