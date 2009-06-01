9cf638ecd00e436dcc5bc673c645d6c34085e05a
wcmaier <wcmaier@openbsd.org>
2009-06-01 18:39:51 +0000

    Update to 1.0.1, kill cruft in files/setup.py.

--- net/cjc/files/setup.py
+++ net/cjc/files/setup.py
@@ -1,11 +1,10 @@
-# $OpenBSD: setup.py,v 1.1.1.1 2006/11/11 04:28:17 alek Exp $
+# $OpenBSD: setup.py,v 1.2 2009/06/01 18:39:52 wcmaier Exp $
 
 from distutils.core import setup
-import sys, os
 
 setup(
         name = "Console Jabber Client",
-        version = "1.0.0",
+        version = "1.0.1",
         description = "Jabber/XMPP client for text terminals",
         author = "Jacek Konieczny",
         author_email = "jajcus@bnet.pl",
