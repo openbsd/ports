fc772b4a3b2c76cdda3c5db15291a27cac906155
ajacoutot <ajacoutot@openbsd.org>
2009-06-11 16:42:09 +0000

    All `configure' do not behave the same when --disable-schemas-install is given so let's come up with something different: remove the configure switch and create a link from /usr/bin/true to ${WRKDIR}/bin/gconftool-2

--- devel/gconf2/gconf2.port.mk
+++ devel/gconf2/gconf2.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: gconf2.port.mk,v 1.2 2009/06/08 16:42:45 ajacoutot Exp $
+# $OpenBSD: gconf2.port.mk,v 1.3 2009/06/11 16:42:09 ajacoutot Exp $
 
 MODGCONF2_LIB_DEPENDS=	gconf-2::devel/gconf2
 MODGCONF2_BUILD_DEPENDS=:gconf2-*:devel/gconf2
@@ -18,6 +18,7 @@ BUILD_DEPENDS+=	${MODGCONF2_BUILD_DEPENDS}
 .if defined(MODGCONF2_SCHEMAS_DIR)
 SCHEMAS_INSTDIR=share/schemas/${MODGCONF2_SCHEMAS_DIR:L}
 SUBST_VARS+=	SCHEMAS_INSTDIR
-CONFIGURE_ARGS+=--disable-schemas-install \
-		--with-gconf-schema-file-dir=${LOCALBASE}/${SCHEMAS_INSTDIR}
+CONFIGURE_ARGS+=--with-gconf-schema-file-dir=${LOCALBASE}/${SCHEMAS_INSTDIR}
 .endif
+
+MODGCONF2_post-patch+=	ln -s /usr/bin/true ${WRKDIR}/bin/gconftool-2
