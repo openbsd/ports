78188160777988ae4fedd991b525ef9bcf42c2a5
espie <espie@openbsd.org>
2009-07-26 12:20:07 +0000

    tweak SUBST_VARS so that nodeaccess won't match DRUPAL_LOCALE (oops) a few PKGNAME bumps needed after regenerating plists, because of DESCR changes or .orig files from patches.

--- www/drupal6/drupal6.port.mk
+++ www/drupal6/drupal6.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: drupal6.port.mk,v 1.2 2009/07/15 09:42:31 espie Exp $
+# $OpenBSD: drupal6.port.mk,v 1.3 2009/07/26 12:20:07 espie Exp $
 
 
 # three types of things we can install, by default plugin
@@ -25,10 +25,9 @@ WRKDIST =	${WRKDIR}/${DISTNAME:C/-6.x.*$//}
 PREFIX ?=	/var/www
 DRUPAL ?=	drupal6
 DRUPAL_ROOT ?=	htdocs/${DRUPAL}
-DRUPAL_MODS ?=	${DRUPAL_ROOT}/sites/all/modules
-DRUPAL_THEMES ?=${DRUPAL_ROOT}/sites/all/themes
-DRUPAL_LOCALE ?=${DRUPAL_MODS}/node
-DRUPAL_LOCALE ?=${DRUPAL_ROOT}/modules/node
+DRUPAL_MODS ?=	${DRUPAL_ROOT}/sites/all/modules/
+DRUPAL_THEMES ?=${DRUPAL_ROOT}/sites/all/themes/
+DRUPAL_LOCALE ?=${DRUPAL_ROOT}/modules/node/
 DRUPAL_OWNER =	www
 DRUPAL_GROUP =	www
 SUBST_VARS += 	DRUPAL_LOCALE DRUPAL_MODS DRUPAL_THEMES DRUPAL_ROOT \
