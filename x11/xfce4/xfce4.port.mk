3a5f2457b5e9bd1ebf9ba094dc237932f58f4c8f
landry <landry@openbsd.org>
2009-08-11 19:51:42 +0000

    When switching to git for development, upstream adopted a new archive layout for distfiles at http://archive.xfce.org/src/. Adapt xfce4.port.mk to new paths for MASTER_SITES. Now xfce4-related ports are divided in 5 categories : core, goodies, artwork, panel plugins and thunar plugins.

--- x11/xfce4/xfce4.port.mk
+++ x11/xfce4/xfce4.port.mk
@@ -1,7 +1,7 @@
-# $OpenBSD: xfce4.port.mk,v 1.6 2009/04/28 19:37:29 landry Exp $
+# $OpenBSD: xfce4.port.mk,v 1.7 2009/08/11 19:51:42 landry Exp $
 
-# Module for Xfce related ports, divided into three categories:
-# core, goodies, plugins.
+# Module for Xfce related ports, divided into five categories:
+# core, goodie, artwork, thunar plugins, panel plugins.
 
 XFCE_DESKTOP_VERSION=	4.6.1
 CATEGORIES+=	x11/xfce4
@@ -20,6 +20,8 @@ MODULES+=	devel/gettext textproc/intltool
 XFCE_VERSION=	${XFCE_DESKTOP_VERSION}
 .endif
 
+XFCE_BRANCH=	${XFCE_VERSION:C/^([0-9]+\.[0-9]+).*/\1/}
+
 # Set to 'yes' if there are .desktop files in the package list.
 .if defined(DESKTOP_FILES) && ${DESKTOP_FILES:L} == "yes"
 MODXFCE_RUN_DEPENDS+=	:desktop-file-utils-*:devel/desktop-file-utils
@@ -28,7 +30,7 @@ MODXFCE_RUN_DEPENDS+=	:desktop-file-utils-*:devel/desktop-file-utils
 .if defined(XFCE_PLUGIN)
 HOMEPAGE?=	http://goodies.xfce.org/projects/panel-plugins/xfce4-${XFCE_PLUGIN}-plugin
 
-MASTER_SITES?=	http://goodies.xfce.org/releases/xfce4-${XFCE_PLUGIN}-plugin/
+MASTER_SITES?=	http://archive.xfce.org/src/panel-plugins/xfce4-${XFCE_PLUGIN}-plugin/${XFCE_BRANCH}/
 DISTNAME?=	xfce4-${XFCE_PLUGIN}-plugin-${XFCE_VERSION}
 PKGNAME?=	${DISTNAME:S/-plugin//}
 
@@ -36,12 +38,23 @@ MODXFCE_LIB_DEPENDS=	xfce4panel.>=2::x11/xfce4/xfce4-panel
 .elif defined(XFCE_GOODIE)
 HOMEPAGE?=	http://goodies.xfce.org/projects/applications/${XFCE_GOODIE}
 
-MASTER_SITES?=	http://goodies.xfce.org/releases/${XFCE_GOODIE}/
+MASTER_SITES?=	http://archive.xfce.org/src/apps/${XFCE_GOODIE:L}/${XFCE_BRANCH}/
 DISTNAME=	${XFCE_GOODIE}-${XFCE_VERSION}
+.elif defined(XFCE_ARTWORK)
+HOMEPAGE?=	http://www.xfce.org/projects/
+
+MASTER_SITES?=	http://archive.xfce.org/src/art/${XFCE_ARTWORK}/${XFCE_BRANCH}/
+DISTNAME=	${XFCE_ARTWORK}-${XFCE_VERSION}
+.elif defined(THUNAR_PLUGIN)
+HOMEPAGE?=	http://goodies.xfce.org/projects/thunar-plugins/${THUNAR_PLUGIN}
+
+MASTER_SITES?=	http://archive.xfce.org/src/thunar-plugins/${THUNAR_PLUGIN}/${XFCE_BRANCH}/
+DISTNAME?=	${THUNAR_PLUGIN}-${XFCE_VERSION}
+PKGNAME?=	${DISTNAME:S/-plugin//}
 .elif defined(XFCE_PROJECT)
 HOMEPAGE?=	http://www.xfce.org/projects/${XFCE_PROJECT}
 
-MASTER_SITES?=	http://www.xfce.org/archive/xfce-${XFCE_DESKTOP_VERSION}/src/
+MASTER_SITES?=	http://archive.xfce.org/src/xfce/${XFCE_PROJECT:L}/${XFCE_BRANCH}/
 DISTNAME=	${XFCE_PROJECT}-${XFCE_VERSION}
 .endif
 
