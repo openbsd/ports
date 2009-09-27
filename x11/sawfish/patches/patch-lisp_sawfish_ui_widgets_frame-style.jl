615e873e9e6bddb5562ef5fd47f39ec243253ecc
jasper <jasper@openbsd.org>
2009-09-27 16:56:40 +0000

    and the rest of the update to 1.3.3

--- x11/sawfish/patches/patch-lisp_sawfish_ui_widgets_frame-style.jl
+++ x11/sawfish/patches/patch-lisp_sawfish_ui_widgets_frame-style.jl
@@ -1,6 +1,6 @@
---- lisp/sawfish/ui/widgets/frame-style.jl.orig	Tue Nov 28 05:13:51 2000
-+++ lisp/sawfish/ui/widgets/frame-style.jl	Mon Feb  5 19:16:00 2001
-@@ -99,8 +99,7 @@
+--- lisp/sawfish/ui/widgets/frame-style.jl.orig	Sun Jul 29 11:45:04 2007
++++ lisp/sawfish/ui/widgets/frame-style.jl	Sun Sep 27 18:54:31 2009
+@@ -112,8 +112,7 @@
  					  (setq full dir)
  					  (throw 'out t))
  				      (error))))
