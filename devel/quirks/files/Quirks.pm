1692a7f774a9ed557b629a92fb62a2741ac8f10a
espie <espie@openbsd.org>
2009-12-05 14:42:52 +0000

    less confusing version number (okay, we still have always-update, but let's try harder not to go back). put more packages we know to be gone in the base system list. display a more helpful message in case we don't find base packages (say we're not removing them since we can't find the file in the base system)

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.5 2009/12/05 12:28:26 espie Exp $
+# $OpenBSD: Quirks.pm,v 1.6 2009/12/05 14:42:52 espie Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -54,13 +54,40 @@ sub tweak_list
 # stem => existing file   hash table
 #	if file exists, then it's now in base and we can remove it.
 
-my $perllib = $Config{archlib};
+my $p5a = $Config{archlib};
+my $p5 = "/usr/libdata/perl5";
 my $base_exceptions = {
-	'p5-version' => "/usr/libdata/perl5/version.pm",
+# very old
+	'pkgconfig' => "/usr/bin/pkg-config",
+	'expat' => "/usr/lib/libexpat.a",
+	'cwm' => "/usr/X11R6/bin/cwm",
+# 4.5 stuff
+	'p5-version' => "$p5/version.pm",
+	'p5-Archive-Tar' => "$p5/Archive/Tar.pm",
+	'p5-Compress-Zlib' => "$p5a/Compress/Zlib.pm",
+	'p5-Compress-Raw-Zlib' => "$p5a/Compress/Raw/Zlib.pm",
+	'p5-IO-Compress-Base' => "$p5a/IO/Compress/Base.pm",
+	'p5-IO-Compress-Zlib' => "$p5a/IO/Compress/Zlib.pm",
+	'p5-IO-Zlib' => "$p5/IO/Zlib.pm",
+	'p5-ExtUtils-CBuilder' => "$p5/ExtUtils/CBuilder.pm",
+	'p5-ExtUtils-ParseXS' => "$p5/ExtUtils/ParseXS.pm",
+	'p5-Locale-Maketext-Simple' => "$p5/Locale/Maketext/Simple.pm",
+	'p5-Module-Build' =>  "$p5/Module/Build.pm",
+	'p5-Module-CoreList' => "$p5/Module/CoreList.pm",
+	'p5-Module-Load' => "$p5/Module/Load.pm",
+	'p5-Module-Loaded' => "$p5/Module/Loaded.pm",
+	'p5-Module-Pluggable' => "$p5/Module/Pluggable.pm",
+	'p5-Time-Piece' => "$p5a/Time/Piece.pm",
+	'p5-Digest-SHA' => "$p5a/Digest/SHA.pm",
+	'p5-Pod-Escapes' => "$p5/Escapes.pm",
+	'p5-Pod-Simple' => "$p5/Pod/Simple.pm",
+	'xcompmgr' => "/usr/X11R6/bin/xcompmgr",
+# 4.6 stuff
 	'tmux' => "/usr/bin/tmux",
 };
 
 my $stem_extensions = {
+# 4.6snap
 	'thunar-vcs-plugin' => 'thunar-vcs',
 };
 
@@ -73,10 +100,15 @@ sub is_base_system
 	my ($self, $handle, $state) = @_;
 	my $stem = OpenBSD::PackageName::splitstem($handle->pkgname);
 	my $test = $base_exceptions->{$stem};
-	if (defined $test && -e $test) {
-		$state->say("Removing ", $handle->pkgname, 
-		    " (part of base system now)");
-		return 1;
+	if (defined $test) {
+		if (-e $test) {
+			$state->say("Removing ", $handle->pkgname, 
+			    " (part of base system now)");
+			return 1;
+		} else {
+			$state->say("Not removing ", $handle->pkgname,
+			 ", ", $test, " not found");
+		}
 	} else {
 		return 0;
 	}
