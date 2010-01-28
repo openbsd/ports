2a6cec57245700da4715dad9bd3567e8cee0d21a
espie <espie@openbsd.org>
2010-01-28 10:09:06 +0000

    duplicate RUN_DEPENDS, it should be indexed only.

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-#	$OpenBSD: bsd.port.mk,v 1.972 2009/10/14 13:01:03 steven Exp $
+#	$OpenBSD: bsd.port.mk,v 1.973 2010/01/28 10:09:06 espie Exp $
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -82,7 +82,7 @@ BULK_DO ?=
 CHECK_LIB_DEPENDS ?= No
 FORCE_UPDATE ?= No
 # All variables relevant to the port's description
-_ALL_VARIABLES ?= HOMEPAGE DISTNAME BUILD_DEPENDS RUN_DEPENDS \
+_ALL_VARIABLES ?= HOMEPAGE DISTNAME BUILD_DEPENDS \
 	REGRESS_DEPENDS USE_GMAKE MODULES FLAVORS \
 	NO_BUILD NO_REGRESS SHARED_ONLY ONLY_FOR_ARCHS NOT_FOR_ARCHS IS_INTERACTIVE \
 	BROKEN MULTI_PACKAGES PSEUDO_FLAVORS \
