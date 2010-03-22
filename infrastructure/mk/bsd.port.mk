abbf59d6e39c6146d6f89df8e6cb320c2556d342
espie <espie@openbsd.org>
2010-03-22 20:19:12 +0000

    typo, noticed by Andreas Kähäri <ak@ebi.ac.uk>

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-#	$OpenBSD: bsd.port.mk,v 1.985 2010/03/21 17:12:10 espie Exp $
+#	$OpenBSD: bsd.port.mk,v 1.986 2010/03/22 20:19:12 espie Exp $
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -1302,7 +1302,7 @@ _force_update_fragment = { \
 			exit 1; \
 		fi; \
 	}
-_PKG_ADD_FORCE = -D update -D updatedepends -D
+_PKG_ADD_FORCE = -D update -D updatedepends
 .  if ${FORCE_UPDATE:L} == "hard"
 _PKG_ADD_FORCE += -D installed
 .  endif
