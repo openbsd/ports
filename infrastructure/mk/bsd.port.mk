39f6cafc40cca6841d2fe3c638a401ee2c2564e6
espie <espie@openbsd.org>
2010-04-05 14:03:26 +0000

    fix buglet

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-#	$OpenBSD: bsd.port.mk,v 1.987 2010/04/05 14:02:50 espie Exp $
+#	$OpenBSD: bsd.port.mk,v 1.988 2010/04/05 14:03:26 espie Exp $
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -1547,8 +1547,9 @@ ${_PACKAGE_COOKIE${_S}}:
 
 ${_INSTALL_COOKIE${_S}}:
 .  if ${FETCH_PACKAGES:L} == "yes"
-	@cd ${.CURDIR} && exec ${MAKE} subpackage
+	@cd ${.CURDIR} && SUBPACKAGE=${_S} exec ${MAKE} subpackage
 .  else
+
 	@cd ${.CURDIR} && exec ${MAKE} package
 .  endif
 	@cd ${.CURDIR} && SUBPACKAGE=${_S} DEPENDS_TARGET=install \
