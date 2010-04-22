a2d22041b46e794f9122536cc4f66a455c1b52db
kili <kili@openbsd.org>
2010-04-22 21:49:37 +0000

    Update to 6.12.2.
    Many thanks to Darrin Chandler and dcoppa@ for testing, reporting about
    broken stuff, missing dependencies here and in ports depending on ghc.
    
    Notes and rants:
    
    - Bootstrapping is done using precompiled binaries, since .hc
      bootstrapping still doesn't work. I really hate this.
    
      THIS MEANS THAT GHC IS NOW AND WILL STAY LEGACY-ONLY (i386 and amd64)
    
      At least until someone fixes it. I tried for more than two year
      (well, only in my spare time and during my vacations) and failed.
    
    - libgmp is currently disabled, because I didn't yet hack the GHC build
      system to use the system libgmp instead of the patched one included
      in GHC.
    
    - The haddock ncluded in the ghc distfile is replaced by the version
      of haddock found in devel/haddock. Haddock itself is @commented
      in the ghc PLIST. Unfortunately, this needs an ugly hack that
      introduces an otherwise useless pseudo flavor `no_deps' in
      devel/haddock.
    
    - CLDouble has been removed from GHC some time ago, because it was
      an alias for double (AFAIK there's now support for long double
      in GHC). As this isn't a really big problem, it currently breaks
      c2hs, which I'll mark broken temporarily before committing the
      ghc update.
    
    - The external codeset defaults to latin1 (suggested by Simon Marlow)
      and can be overridden by setting the HS_ENCODING to any codeset
      supported by libiconv.
    
    - ghc.port.mk still needs some love, especially for letting a port add
      additional parameters to certain invocations of ${MODGHC_SETUP_PROG}.

--- lang/ghc/ghc.port.mk
+++ lang/ghc/ghc.port.mk
@@ -1,11 +1,13 @@
-# $OpenBSD: ghc.port.mk,v 1.6 2007/07/21 17:14:57 kili Exp $
+# $OpenBSD: ghc.port.mk,v 1.7 2010/04/22 21:49:37 kili Exp $
 # Module for Glasgow Haskell Compiler
 
 # Not yet ported to other architectures
-# See comments in lang/ghc/Makefile for more information
-ONLY_FOR_ARCHS=	i386 amd64
+ONLY_FOR_ARCHS =	i386 amd64
 
-MODGHC_VER=	6.6.1
+MODGHC_VER =		6.12.2
+SUBST_VARS +=		MODGHC_VER
+
+MODGHC_BIN =		${LOCALBASE}/bin/ghc
 
 # The following is for depending ports only.  GHC itself just needs
 # MODGHC_VER and ONLY_FOR_ARCHS. This is a little bit ugly, but
@@ -15,11 +17,88 @@ MODGHC_VER=	6.6.1
 # i.e. RUN_DEPENDS = :ghc-${MODGHC_VERSION}:lang/ghc, and not
 # just ::lang/ghc.
 .if ${PKGPATH} != "lang/ghc"
-BUILD_DEPENDS+=	::lang/ghc
+BUILD_DEPENDS +=	::lang/ghc
 
-# Only add runtime when it is actually needed (by default yes)
-MODGHC_RUNTIME?=Yes
-. if ${MODGHC_RUNTIME:L} == "yes"
-RUN_DEPENDS+=	:ghc-${MODGHC_VER}:lang/ghc
+# Set to "cabal" to get the typical Cabal targets defined. Add "haddock"
+# to generate API documentation using Haddock. Add "register" to create
+# and include register/unregister scripts (you'll still have to add the
+# necessary tags to your PLIST by hand).
+# Add "nort" if the port doesn't depend on the GHC runtime. This will
+# also turn off the default "hs-" prefix for PKGNAME.
+# Finally, set it to or add "hackage" if the distfiles are avialable on
+# hackage.org.
+
+MODGHC_BUILD ?=
+
+. if !${MODGHC_BUILD:L:Mnort}
+PKGNAME ?=		hs-${DISTNAME}
+RUN_DEPENDS +=		:ghc-${MODGHC_VER}:lang/ghc
+. endif
+
+. if ${MODGHC_BUILD:L:Mhackage}
+MODGHC_HACKAGE_NAME =		${DISTNAME:C,-[0-9.]*$,,}
+MODGHC_HACKAGE_VERSION =	${DISTNAME:C,.*-([0-9.]*)$,\1,}
+HOMEPAGE ?=			http://hackage.haskell.org/package/${MODGHC_HACKAGE_NAME}
+MASTER_SITES =			http://hackage.haskell.org/packages/archive/${MODGHC_HACKAGE_NAME}/${MODGHC_HACKAGE_VERSION}/
+SUBST_VARS +=			DISTNAME MODGHC_HACKAGE_VERSION
+. endif
+
+. if ${MODGHC_BUILD:L:Mcabal}
+MODGHC_SETUP_SCRIPT ?=		Setup.lhs Setup.hs
+MODGHC_SETUP_PROG ?=		${WRKSRC}/Setup
+MODGHC_SETUP_CONF_ARGS ?=	configure -g -O --prefix=${PREFIX}
+MODGHC_SETUP_CONF_ENV ?=	HOME=${PORTHOME} PATH=${PATH}
+
+.  if ${MODGHC_BUILD:L:Mhaddock}
+BUILD_DEPENDS +=		::devel/haddock
+.  endif
+
+# Little hack to let ports still add CONFIGURE_STYLE = autoconf and go
+# without a do-configure: target (some Haskell ports are built with
+# Cabal but use autohell for the documentation):
+MODCABAL_configure = \
+	cd ${WRKSRC} && \
+	for s in ${MODGHC_SETUP_SCRIPT}; do \
+		test -f "$$s" && \
+		${MODGHC_BIN} --make \
+			-o ${MODGHC_SETUP_PROG} "$$s" && \
+		break; \
+	done && \
+	cd ${WRKBUILD} && exec ${SETENV} ${MODGHC_SETUP_CONF_ENV} \
+		${MODGHC_SETUP_PROG} ${MODGHC_SETUP_CONF_ARGS}
+
+CONFIGURE_STYLE +=		CABAL
+
+.  if !target(do-build)
+do-build:
+	@cd ${WRKBUILD} && exec ${SETENV} ${MODGHC_SETUP_CONF_ENV} \
+		${MODGHC_SETUP_PROG} build
+.   if ${MODGHC_BUILD:L:Mhaddock}
+	@cd ${WRKBUILD} && exec ${SETENV} ${MAKE_ENV} \
+		${MODGHC_SETUP_PROG} haddock
+.   endif
+.   if ${MODGHC_BUILD:L:Mregister}
+	@cd ${WRKBUILD} && exec ${SETENV} ${MAKE_ENV} \
+		${MODGHC_SETUP_PROG} register --gen-script
+	@cd ${WRKBUILD} && exec ${SETENV} ${MAKE_ENV} \
+		${MODGHC_SETUP_PROG} unregister --gen-script
+.   endif
+.  endif
+
+.  if !target(do-install)
+do-install:
+	@cd ${WRKBUILD} && exec ${SETENV} ${MODGHC_SETUP_CONF_ENV} \
+		${MODGHC_SETUP_PROG} copy --destdir=${DESTDIR}
+.   if ${MODGHC_BUILD:L:Mregister}
+	@${INSTALL_SCRIPT} ${WRKBUILD}/register.sh ${PREFIX}/lib/${DISTNAME}
+	@${INSTALL_SCRIPT} ${WRKBUILD}/unregister.sh ${PREFIX}/lib/${DISTNAME}
+.   endif
+.  endif
+
+.  if !target(do-regress)
+do-regress:
+	@cd ${WRKBUILD} && exec ${SETENV} ${MODGHC_SETUP_CONF_ENV} \
+		${MODGHC_SETUP_PROG} test
+.  endif
 . endif
 .endif
