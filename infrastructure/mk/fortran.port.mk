90ca9c763db58688493765b5aca38dda39dadb42
espie <espie@openbsd.org>
2010-06-05 16:38:47 +0000

    BREAK the tree in a very obvious way for people who did not remove /usr/bin/f77 and /usr/bin/g77

--- infrastructure/mk/fortran.port.mk
+++ infrastructure/mk/fortran.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: fortran.port.mk,v 1.5 2010/05/25 10:10:43 espie Exp $
+# $OpenBSD: fortran.port.mk,v 1.6 2010/06/05 16:38:47 espie Exp $
 
 .if ${COMPILER_VERSION:L:Mgcc[34]*}
 MODFORTRAN_LIB_DEPENDS77 = g2c::devel/libf2c
@@ -14,4 +14,9 @@ MODFORTRAN_BUILD_DEPENDS77 = ::lang/g77-old ::devel/libf2c-old
 MODFORTRAN_LIB_DEPENDS += ${MODFORTRAN_LIB_DEPENDS77}
 MODFORTRAN_WANTLIB += ${MODFORTRAN_WANTLIB77}
 MODFORTRAN_BUILD_DEPENDS += ${MODFORTRAN_BUILD_DEPENDS77}
+MODFORTRAN_post-patch = \
+if test -e /usr/bin/g77 -o -e /usr/bin/f77; then \
+    echo "Error: remove old fortran compiler /usr/bin/f77 /usr/bin/g77"; \
+    exit 1; \
+fi
 .endif
