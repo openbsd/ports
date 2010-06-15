38d11011eb0a1babeda2c855c02f92b7bc10fe86
jasper <jasper@openbsd.org>
2010-06-15 21:17:46 +0000

    - introduce LUA_VERSION, LUA_LIBDIR and LUA_DATADIR. the latter two are duplicated in (almost) every lua port, so they should move to lua.port.mk * lua modules will be switched over soon
    ok landry@ jolan@

--- lang/lua/lua.port.mk
+++ lang/lua/lua.port.mk
@@ -1,6 +1,15 @@
-# $OpenBSD: lua.port.mk,v 1.1 2008/06/11 05:00:01 landry Exp $
+# $OpenBSD: lua.port.mk,v 1.2 2010/06/15 21:17:46 jasper Exp $
 
 CATEGORIES+=	lang/lua
+
+# Major.Minor of current lua version provided by lang/lua
+LUA_VERSION=	5.1
+
+# Where the lua libraries will be installed to
+LUA_LIBDIR=	${PREFIX}/lib/lua/${LUA_VERSION}/
+
+# Where the lua modules will be installed to
+LUA_DATADIR=	${PREFIX}/share/lua/${LUA_VERSION}/
 
 RUN_DEPENDS+=	::lang/lua
 
