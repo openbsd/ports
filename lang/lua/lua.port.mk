aaadc7ef3bb07255dc88c183067a403f01137cac
jasper <jasper@openbsd.org>
2010-06-15 21:20:39 +0000

    - tweak names to have the "MOD" prefix.

--- lang/lua/lua.port.mk
+++ lang/lua/lua.port.mk
@@ -1,15 +1,15 @@
-# $OpenBSD: lua.port.mk,v 1.2 2010/06/15 21:17:46 jasper Exp $
+# $OpenBSD: lua.port.mk,v 1.3 2010/06/15 21:20:39 jasper Exp $
 
 CATEGORIES+=	lang/lua
 
 # Major.Minor of current lua version provided by lang/lua
-LUA_VERSION=	5.1
+MODLUA_VERSION=	5.1
 
 # Where the lua libraries will be installed to
-LUA_LIBDIR=	${PREFIX}/lib/lua/${LUA_VERSION}/
+MODLUA_LIBDIR=	${PREFIX}/lib/lua/${LUA_VERSION}/
 
 # Where the lua modules will be installed to
-LUA_DATADIR=	${PREFIX}/share/lua/${LUA_VERSION}/
+MODLUA_DATADIR=	${PREFIX}/share/lua/${LUA_VERSION}/
 
 RUN_DEPENDS+=	::lang/lua
 
