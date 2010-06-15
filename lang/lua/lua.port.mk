ac07b715a22685ee1b50484f0c8bc8e14ae0d66b
jasper <jasper@openbsd.org>
2010-06-15 22:05:24 +0000

    tyop

--- lang/lua/lua.port.mk
+++ lang/lua/lua.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: lua.port.mk,v 1.3 2010/06/15 21:20:39 jasper Exp $
+# $OpenBSD: lua.port.mk,v 1.4 2010/06/15 22:05:24 jasper Exp $
 
 CATEGORIES+=	lang/lua
 
@@ -6,10 +6,10 @@ CATEGORIES+=	lang/lua
 MODLUA_VERSION=	5.1
 
 # Where the lua libraries will be installed to
-MODLUA_LIBDIR=	${PREFIX}/lib/lua/${LUA_VERSION}/
+MODLUA_LIBDIR=	${PREFIX}/lib/lua/${MODLUA_VERSION}/
 
 # Where the lua modules will be installed to
-MODLUA_DATADIR=	${PREFIX}/share/lua/${LUA_VERSION}/
+MODLUA_DATADIR=	${PREFIX}/share/lua/${MODLUA_VERSION}/
 
 RUN_DEPENDS+=	::lang/lua
 
