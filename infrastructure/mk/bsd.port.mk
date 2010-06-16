5d22cfc900d454dc48c3b76563eedf170b4e6548
espie <espie@openbsd.org>
2010-06-16 12:22:20 +0000

    zap old stuff (can't work, since -m is used for something else now)

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-#	$OpenBSD: bsd.port.mk,v 1.1004 2010/06/16 12:06:46 espie Exp $
+#	$OpenBSD: bsd.port.mk,v 1.1005 2010/06/16 12:22:20 espie Exp $
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -885,9 +885,6 @@ ERRORS += "Fatal: DEINSTALL script support is obsolete"
 .  endif
 .  if exists(${PKGDIR}/REQ)
 ERRORS += "Fatal: REQ script support is obsolete"
-.  endif
-.  if exists(${PKGDIR}/MODULE.pm)
-PKG_ARGS- += -m ${PKGDIR}/MODULE.pm
 .  endif
 DESCR ?= ${PKGDIR}/DESCR
 
