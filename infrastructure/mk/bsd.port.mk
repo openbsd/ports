d9dd312bbff1067eca83893fde6599995e44362f
espie <espie@openbsd.org>
2010-07-08 20:48:39 +0000

    synch with src

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-#	$OpenBSD: bsd.port.mk,v 1.1016 2010/07/06 12:38:26 espie Exp $
+#	$OpenBSD: bsd.port.mk,v 1.1017 2010/07/08 20:48:39 espie Exp $
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -133,10 +133,9 @@ ALL_ARCHS = alpha amd64 arm armish arm hppa hppa64 i386 landisk \
 APM_ARCHS = amd64 arm i386 loongson macppc sparc sparc64
 LP64_ARCHS = alpha amd64 hppa64 sparc64 mips64 mips64el
 NO_SHARED_ARCHS = m88k vax
-GCC4_ARCHS = amd64 sparc64
-GCC3_ARCHS = alpha arm armish beagle gumstix hppa hppa64 i386 \
-	landisk loongson macppc mips64 mips64el mvmeppc palm powerpc sgi sh \
-	socppc zaurus
+GCC4_ARCHS = amd64 i386 hppa macppc mvmeppc powerpc socppc sparc64
+GCC3_ARCHS = alpha arm armish beagle gumstix hppa64 \
+	landisk loongson mips64 mips64el palm sgi sh zaurus
 GCC2_ARCHS = aviion luna88k m68k m88k mac68k mvme68k mvme88k sparc vax
 
 # Set NO_SHARED_LIBS for those machines that don't support shared libraries.
