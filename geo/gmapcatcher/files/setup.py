18c93d6cc0ddda88450e7a5dcf42e99436fe236c
kevlo <kevlo@openbsd.org>
2010-07-16 10:14:00 +0000

    Update to 0.7.1.0
    ok sthen@

--- geo/gmapcatcher/files/setup.py
+++ geo/gmapcatcher/files/setup.py
@@ -1,15 +1,17 @@
-# $OpenBSD: setup.py,v 1.1.1.1 2010/03/25 10:13:26 kevlo Exp $
+# $OpenBSD: setup.py,v 1.2 2010/07/16 10:14:01 kevlo Exp $
 
 from distutils.core import setup
 import sys, os
 
 setup(
         name = "gmapcatcher",
-        version = "0.6.3.0",
+        version = "0.7.1.0",
         description = "offline maps viewer",
         license = "GPLv2",
         url = "http://code.google.com/p/gmapcatcher/",
-        packages = ('gmapcatcher','gmapcatcher.mapServers',
-	    'gmapcatcher.pyGPSD','gmapcatcher.pyGPSD.nmea',
-	    'gmapcatcher.pyGPSD.nmea.serial'),
+        packages = ('gmapcatcher','gmapcatcher.src.mapServers',
+	    'gmapcatcher.src.pyGPSD','gmapcatcher.src.pyGPSD.nmea',
+	    'gmapcatcher.src.pyGPSD.nmea.serial'),
+	package_dir = {'': 'lib'},
+	package_data  = {'gmapcatcher': ['images/*']},
 )
