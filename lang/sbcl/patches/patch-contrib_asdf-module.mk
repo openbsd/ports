2ed4a8f6f138ae141c83161c95621bd6fdaf69f9
jasper <jasper@openbsd.org>
2010-08-24 12:20:30 +0000

    - update sbcl to 1.0.41 - add support for powerpc
    from josh elasser (MAINTAINER), thanks!

--- lang/sbcl/patches/patch-contrib_asdf-module.mk
+++ lang/sbcl/patches/patch-contrib_asdf-module.mk
@@ -1,12 +1,12 @@
-$OpenBSD: patch-contrib_asdf-module.mk,v 1.2 2009/11/17 10:45:00 pirofti Exp $
+$OpenBSD: patch-contrib_asdf-module.mk,v 1.3 2010/08/24 12:20:30 jasper Exp $
 
 Fix 'all' target to allow building without running tests.
 
 Don't copy every single file when installing the contribs, only the
 ones that are actually needed to load the system.
 
---- contrib/asdf-module.mk.orig	Tue Apr 28 09:02:13 2009
-+++ contrib/asdf-module.mk	Tue Jul  7 17:57:02 2009
+--- contrib/asdf-module.mk.orig	Thu Jan 28 11:46:39 2010
++++ contrib/asdf-module.mk	Fri Jul 30 12:24:33 2010
 @@ -22,7 +22,8 @@ endif
  
  export CC SBCL EXTRA_CFLAGS EXTRA_LDFLAGS
@@ -22,5 +22,5 @@ ones that are actually needed to load the system.
  # preserve owner, so chown after installing for the current user.
  install: $(EXTRA_INSTALL_TARGETS)
 -	tar cf - . | ( cd "$(BUILD_ROOT)$(INSTALL_DIR)" && tar xpvf - )
--	find "$(BUILD_ROOT)$(INSTALL_DIR)" -type f -exec chown `id -u`:`id -g` {} \;
+-	find "$(BUILD_ROOT)$(INSTALL_DIR)" -exec chown `id -u`:`id -g` {} \;
 +	cp -p $(SYSTEM).asd *.lisp *.fasl "$(BUILD_ROOT)$(INSTALL_DIR)"
