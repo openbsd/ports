e65e5ec009fe3d2dce8a2e1cb810b19ceb648872
sthen <sthen@openbsd.org>
2010-09-05 22:12:22 +0000

    missing p5- on Audio-MPD

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.24 2010/09/01 22:33:25 sthen Exp $
+# $OpenBSD: Quirks.pm,v 1.25 2010/09/05 22:12:22 sthen Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -148,6 +148,7 @@ my $stem_extensions = {
 	'wordpress-mu' => 'wordpress',
 # 4.9
 	'sybperl' => 'p5-sybperl',
+	'Audio-MPD' => 'p5-Audio-MPD',
 };
 
 # ->is_base_system($handle, $state):
