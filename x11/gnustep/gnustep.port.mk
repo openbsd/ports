76167b459b5381b36becdeebf40d3708d5c9800f
sebastia <sebastia@openbsd.org>
2010-10-26 12:39:10 +0000

    * Update the gnustep.port.mk file, to have some standard WANTLIBS and LIB/RUN depends added for the GUI based apps. * Bump package versions for ports depending on gnustep-back. * Fix PLIST for gnumail, after library bump.

--- x11/gnustep/gnustep.port.mk
+++ x11/gnustep/gnustep.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: gnustep.port.mk,v 1.8 2010/10/15 10:37:51 sebastia Exp $
+# $OpenBSD: gnustep.port.mk,v 1.9 2010/10/26 12:39:10 sebastia Exp $
 
 # until tested on others
 ONLY_FOR_ARCHS=	i386 amd64
@@ -19,6 +19,21 @@ MAKE_ENV+=	GNUSTEP_MAKEFILES=`gnustep-config --variable=GNUSTEP_MAKEFILES`
 MAKE_ENV+=	INSTALL_AS_USER=${BINOWN}
 MAKE_ENV+=	INSTALL_AS_GROUP=${BINGRP}
 MAKE_ENV+=	GS_DEFAULTS_LOCKDIR=${WRKDIR}
+
+MODGNUSTEP_NEEDS_BASE?=	Yes
+MODGNUSTEP_NEEDS_GUI?=	Yes
+MODGNUSTEP_NEEDS_BACK?=	Yes
+.if ${MODGNUSTEP_NEEDS_GUI} == Yes 
+WANTLIB+=	gnustep-gui
+LIB_DEPENDS+=	::x11/gnustep/gui
+.if ${MODGNUSTEP_NEEDS_BACK} == Yes
+RUN_DEPENDS+=	::x11/gnustep/back
+.endif
+.endif
+.if ${MODGNUSTEP_NEEDS_BASE} == Yes
+WANTLIB+=	objc gnustep-base
+LIB_DEPENDS+=	::x11/gnustep/base
+.endif
 
 MAKE_ENV+=	messages=yes
 
