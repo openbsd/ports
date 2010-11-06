4ca14c8052c6c1af33169696db0a69bc3dd77f93
espie <espie@openbsd.org>
2010-11-06 10:03:31 +0000

    add the missing pieces to handle MULTI_PACKAGES port: LIB/RUN/WANT should always go thru an indirection, so that a multi-packages port can turn them on/off piecewise (required for libiconv cleanup of sope).
    style nits: makefiles with lots of ?= +=   do *REQUIRE* spaces.
    also indent tests, and do the :L dance to have cleaner stuff.

--- x11/gnustep/gnustep.port.mk
+++ x11/gnustep/gnustep.port.mk
@@ -1,46 +1,51 @@
-# $OpenBSD: gnustep.port.mk,v 1.9 2010/10/26 12:39:10 sebastia Exp $
+# $OpenBSD: gnustep.port.mk,v 1.10 2010/11/06 10:03:31 espie Exp $
 
 # until tested on others
-ONLY_FOR_ARCHS=	i386 amd64
+ONLY_FOR_ARCHS =	i386 amd64
 
-SHARED_ONLY=	Yes
+SHARED_ONLY =	Yes
 
-CATEGORIES+=	x11/gnustep
+CATEGORIES +=	x11/gnustep
 
-USE_GMAKE?=	Yes
-MAKE_FILE?=	GNUmakefile
+USE_GMAKE ?=	Yes
+MAKE_FILE ?=	GNUmakefile
 
-BUILD_DEPENDS+=	:gnustep-make-*:x11/gnustep/make
-RUN_DEPENDS+=	:gnustep-make-*:x11/gnustep/make
+BUILD_DEPENDS +=		:gnustep-make-*:x11/gnustep/make
+MODGNUSTEP_RUN_DEPENDS +=	:gnustep-make-*:x11/gnustep/make
 
-MAKE_FLAGS+=	CC="${CC}" CPP="${CC} -E" OPTFLAG="${CFLAGS}"
+MAKE_FLAGS +=	CC="${CC}" CPP="${CC} -E" OPTFLAG="${CFLAGS}"
 
-MAKE_ENV+=	GNUSTEP_MAKEFILES=`gnustep-config --variable=GNUSTEP_MAKEFILES`
-MAKE_ENV+=	INSTALL_AS_USER=${BINOWN}
-MAKE_ENV+=	INSTALL_AS_GROUP=${BINGRP}
-MAKE_ENV+=	GS_DEFAULTS_LOCKDIR=${WRKDIR}
+MAKE_ENV +=	GNUSTEP_MAKEFILES=`gnustep-config --variable=GNUSTEP_MAKEFILES`
+MAKE_ENV +=	INSTALL_AS_USER=${BINOWN}
+MAKE_ENV +=	INSTALL_AS_GROUP=${BINGRP}
+MAKE_ENV +=	GS_DEFAULTS_LOCKDIR=${WRKDIR}
 
-MODGNUSTEP_NEEDS_BASE?=	Yes
-MODGNUSTEP_NEEDS_GUI?=	Yes
-MODGNUSTEP_NEEDS_BACK?=	Yes
-.if ${MODGNUSTEP_NEEDS_GUI} == Yes 
-WANTLIB+=	gnustep-gui
-LIB_DEPENDS+=	::x11/gnustep/gui
-.if ${MODGNUSTEP_NEEDS_BACK} == Yes
-RUN_DEPENDS+=	::x11/gnustep/back
+MODGNUSTEP_NEEDS_BASE ?=	Yes
+MODGNUSTEP_NEEDS_GUI ?=		Yes
+MODGNUSTEP_NEEDS_BACK ?=	Yes
+
+.if ${MODGNUSTEP_NEEDS_GUI:L} == yes 
+MODGNUSTEP_WANTLIB +=		gnustep-gui
+MODGNUSTEP_LIB_DEPENDS +=	::x11/gnustep/gui
+.  if ${MODGNUSTEP_NEEDS_BACK:L} == yes
+MODGNUSTEP_RUN_DEPENDS +=	::x11/gnustep/back
+.  endif
 .endif
-.endif
-.if ${MODGNUSTEP_NEEDS_BASE} == Yes
-WANTLIB+=	objc gnustep-base
-LIB_DEPENDS+=	::x11/gnustep/base
+.if ${MODGNUSTEP_NEEDS_BASE:L} == yes
+MODGNUSTEP_WANTLIB +=		objc gnustep-base
+MODGNUSTEP_LIB_DEPENDS +=	::x11/gnustep/base
 .endif
 
-MAKE_ENV+=	messages=yes
+WANTLIB += ${MODGNUSTEP_WANTLIB}
+LIB_DEPENDS += ${MODGNUSTEP_LIB_DEPENDS}
+RUN_DEPENDS += ${MODGNUSTEP_RUN_DEPENDS}
+
+MAKE_ENV +=	messages=yes
 
 .ifdef DEBUG
-MAKE_ENV+=	debug=yes strip=no
+MAKE_ENV +=	debug=yes strip=no
 .else
-MAKE_ENV+=	debug=no
+MAKE_ENV +=	debug=no
 .endif
 
-MASTER_SITE_GNUSTEP= ftp://ftp.gnustep.org/pub/gnustep/
+MASTER_SITE_GNUSTEP = ftp://ftp.gnustep.org/pub/gnustep/
