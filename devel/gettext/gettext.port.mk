632a3518ab4698ccc4ae9037cbe2229351e55ce7
espie <espie@openbsd.org>
2010-11-06 16:07:00 +0000

    move gettext to new WANTLIB

--- devel/gettext/gettext.port.mk
+++ devel/gettext/gettext.port.mk
@@ -1,13 +1,16 @@
-# $OpenBSD: gettext.port.mk,v 1.9 2010/07/03 03:23:22 naddy Exp $
+# $OpenBSD: gettext.port.mk,v 1.10 2010/11/06 16:07:01 espie Exp $
 
-MODGETTEXT_LIB_DEPENDS=	intl.>=5:gettext->=0.10.38:devel/gettext \
-			iconv.>=6::converters/libiconv
+_MODGETTEXT_SPEC =		:gettext->=0.10.38:devel/gettext
 
-MODGETTEXT_RUN_DEPENDS=	:gettext->=0.10.38:devel/gettext
+MODGETTEXT_LIB_DEPENDS =	${_MODGETTEXT_SPEC} ::converters/libiconv
+MODGETTEXT_WANTLIB =		intl.>=5 iconv.>=6
+
+MODGETTEXT_RUN_DEPENDS =	${_MODGETTEXT_SPEC}
 
 # The RUN_DEPENDS entry is to ensure gettext is installed. This is
 # necessary so that we have locale.alias installed on static archs.
 # Typically installed in PREFIX/share/locale.
-LIB_DEPENDS+=	${MODGETTEXT_LIB_DEPENDS}
-BUILD_DEPENDS+=	:gettext->=0.18.1:devel/gettext
-RUN_DEPENDS+=	${MODGETTEXT_RUN_DEPENDS}
+LIB_DEPENDS +=		${MODGETTEXT_LIB_DEPENDS}
+BUILD_DEPENDS +=	${_MODGETTEXT_SPEC}
+RUN_DEPENDS +=		${MODGETTEXT_RUN_DEPENDS}
+WANTLIB +=		${MODGETTEXT_WANTLIB}
