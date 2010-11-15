2314c85f45d2708fa4b1e5b4ed85fc62d455e54a
espie <espie@openbsd.org>
2010-11-15 00:22:45 +0000

    convert to new style depends zap default spec that are not needed convert libspecs as well convert p* to REVISION.
    No package changes

--- converters/libiconv/libiconv.port.mk
+++ converters/libiconv/libiconv.port.mk
@@ -1,12 +1,12 @@
-# $OpenBSD: libiconv.port.mk,v 1.5 2010/11/06 10:18:39 espie Exp $
+# $OpenBSD: libiconv.port.mk,v 1.6 2010/11/15 00:22:50 espie Exp $
 
 # The RUN_DEPENDS entry is to ensure libiconv is installed. This is
 # necessary so that we have charset.alias installed on static archs.
 # Typically installed in PREFIX/lib.
-MODLIBICONV_LIB_DEPENDS =	::converters/libiconv
-MODLIBICONV_RUN_DEPENDS =	:libiconv-*:converters/libiconv
+MODLIBICONV_LIB_DEPENDS =	converters/libiconv
+MODLIBICONV_RUN_DEPENDS =	converters/libiconv
 
-MODLIBICONV_WANTLIB =		iconv.>=2
+MODLIBICONV_WANTLIB =		iconv>=2
 
 LIB_DEPENDS +=			${MODLIBICONV_LIB_DEPENDS}
 RUN_DEPENDS +=			${MODLIBICONV_RUN_DEPENDS}
