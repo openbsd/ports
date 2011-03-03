4969d3057afdf4b9cbade64411cc36c8e9332e86
ajacoutot <ajacoutot@openbsd.org>
2011-03-03 14:27:56 +0000

    Only log client connection timeout at DEBUG, not NOTICE, to prevent filling up the logs with useless info.
    MAINTAINER timeout
    ok sthen@

--- www/transproxy/patches/patch-tproxy.c
+++ www/transproxy/patches/patch-tproxy.c
@@ -1,5 +1,5 @@
---- tproxy.c.orig	Sun Feb  4 05:13:48 2001
-+++ tproxy.c	Fri Nov 30 13:39:20 2001
+--- tproxy.c.orig	Sun Feb  4 14:13:48 2001
++++ tproxy.c	Mon Feb 21 09:34:18 2011
 @@ -49,6 +49,15 @@
  # include <netinet/ip_nat.h>
  #endif
@@ -48,7 +48,7 @@
  #ifdef LOG_TO_FILE
  	/*
  	 * Open the log file for the first time.
-@@ -1002,6 +1029,9 @@ static void trans_proxy(int sock, struct
+@@ -1002,6 +1029,9 @@ static void trans_proxy(int sock, struct sockaddr_in *
  #ifdef IPFILTER
  	natlookup_t			natlook;
  #endif
@@ -58,11 +58,10 @@
  
  	/*
  	 * Initialise the connection structure.
-@@ -1078,6 +1108,34 @@ static void trans_proxy(int sock, struct
- 	conn.dest_addr.sin_addr = natlook.nl_realip;
+@@ -1079,6 +1109,34 @@ static void trans_proxy(int sock, struct sockaddr_in *
  	conn.dest_addr.sin_port = natlook.nl_realport;
  #endif
-+
+ 
 +#ifdef OPENBSD_PF
 +	/*
 +	 * Build up the PF natlookup structure.
@@ -90,6 +89,16 @@
 +	conn.dest_addr.sin_addr.s_addr = natlook.rdaddr.addr32[0];
 +	conn.dest_addr.sin_port = natlook.rdport;
 +#endif /* OPENBSD_PF */
- 
++
  #endif/*!IPTABLES*/
  
+ 	/*
+@@ -2034,7 +2092,7 @@ static void write_pid(char *prog)
+ static void alarm_signal(int sig)
+ {
+ #if defined(LOG_TO_SYSLOG) || defined(LOG_FAULTS_TO_SYSLOG)
+-	syslog(LOG_NOTICE, "Alarm signal caught - connection timeout");
++	syslog(LOG_DEBUG, "Alarm signal caught - connection timeout");
+ #endif
+ 	exit(1);
+ }
