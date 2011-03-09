c89dcfb35f129030ad42b6de7858ad04e76fbf17
landry <landry@openbsd.org>
2011-03-09 13:16:47 +0000

    Add quirk for libxfce4menu -> garcon migration.

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.42 2011/03/09 10:08:52 robert Exp $
+# $OpenBSD: Quirks.pm,v 1.43 2011/03/09 13:16:47 landry Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -192,6 +192,7 @@ my $stem_extensions = {
 	'openoffice-i18n-ru' => 'libreoffice-i18n-ru',
 	'openoffice-i18n-sl' => 'libreoffice-i18n-sl',
 	'openoffice-i18n-sv' => 'libreoffice-i18n-sv',
+	'libxfce4menu' => 'garcon',
 };
 
 # ->is_base_system($handle, $state):
