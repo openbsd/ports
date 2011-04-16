bdca4a9a30c13d1a7d2c7e46243bff6db29229d5
espie <espie@openbsd.org>
2011-04-16 12:21:44 +0000

    prefer the V- version for non-MULTI_PACKAGES vars. fixes E on libf2c-old on i386.

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-#	$OpenBSD: bsd.port.mk,v 1.1077 2011/04/16 10:31:20 espie Exp $
+#	$OpenBSD: bsd.port.mk,v 1.1078 2011/04/16 12:21:44 espie Exp $
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -3349,7 +3349,9 @@ verbose-show:
 dump-vars:
 .if ${MULTI_PACKAGES} == "-"
 .  for _v in ${_ALL_VARIABLES} ${_ALL_VARIABLES_INDEXED}
-.   if defined(${_v})
+.   if defined(${_v}-)
+	@echo ${FULLPKGPATH}.${_v}=${${_v}-:Q}
+.   elif defined(${_v})
 	@echo ${FULLPKGPATH}.${_v}=${${_v}:Q}
 .   endif
 .  endfor
