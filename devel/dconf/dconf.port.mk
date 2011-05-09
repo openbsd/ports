6736ec938c19446b0168aec2c69f19a30b7eae4b
ajacoutot <ajacoutot@openbsd.org>
2011-05-09 08:43:45 +0000

    Bugfix update to dconf-0.7.5. This should be the latest release before the new stable 0.8.

--- devel/dconf/dconf.port.mk
+++ devel/dconf/dconf.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: dconf.port.mk,v 1.1 2011/03/25 08:59:48 ajacoutot Exp $
+# $OpenBSD: dconf.port.mk,v 1.2 2011/05/09 08:43:45 ajacoutot Exp $
 
 # This module is used by ports installing gsettings schemas under
 #     PREFIX/share/glib-2.0/schemas/
@@ -7,7 +7,7 @@
 # @exec %D/bin/glib-compile-schemas %D/share/glib-2.0/schemas
 # @unexec-delete %D/bin/glib-compile-schemas %D/share/glib-2.0/schemas
 
-# use the "no_editor" FLAVOR to prevent a cyclic dependency with x11/gtk+3
+# use the "no_editor" FLAVOR to prevent a cyclic dependency
 MODDCONF_BUILD_DEPENDS=	devel/glib2
 MODDCONF_RUN_DEPENDS=	devel/glib2 \
 			devel/dconf,no_editor
