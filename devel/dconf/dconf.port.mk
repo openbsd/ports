81c428a12d73afe3d826a6e8294633c46512c680
ajacoutot <ajacoutot@openbsd.org>
2011-05-28 08:15:33 +0000

    Redirect the glib-compile-schemas goo stdout to /dev/null, no need to be chatty here.

--- devel/dconf/dconf.port.mk
+++ devel/dconf/dconf.port.mk
@@ -1,11 +1,11 @@
-# $OpenBSD: dconf.port.mk,v 1.2 2011/05/09 08:43:45 ajacoutot Exp $
+# $OpenBSD: dconf.port.mk,v 1.3 2011/05/28 08:15:33 ajacoutot Exp $
 
 # This module is used by ports installing gsettings schemas under
 #     PREFIX/share/glib-2.0/schemas/
 
 # It requires the following goo in PLIST:
-# @exec %D/bin/glib-compile-schemas %D/share/glib-2.0/schemas
-# @unexec-delete %D/bin/glib-compile-schemas %D/share/glib-2.0/schemas
+# @exec %D/bin/glib-compile-schemas %D/share/glib-2.0/schemas >/dev/null
+# @unexec-delete %D/bin/glib-compile-schemas %D/share/glib-2.0/schemas >/dev/null
 
 # use the "no_editor" FLAVOR to prevent a cyclic dependency
 MODDCONF_BUILD_DEPENDS=	devel/glib2
