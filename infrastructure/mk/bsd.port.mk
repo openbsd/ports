fb83b9e1318f462a359ec4ea2978ccb8ee8bf7cd
espie <espie@openbsd.org>
2011-06-21 17:11:45 +0000

    oops

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-#	$OpenBSD: bsd.port.mk,v 1.1085 2011/06/21 17:04:32 espie Exp $
+#	$OpenBSD: bsd.port.mk,v 1.1086 2011/06/21 17:11:45 espie Exp $
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -3101,7 +3101,6 @@ _lib-depends-args:
 _wantlib-args:
 	@${_MAKE} _port-wantlib-args >${_P_WANTLIB_COOKIE}
 	@${_MAKE} _fake-wantlib-args >${_I_WANTLIB_COOKIE}
-	@echo zoinx >>${_I_WANTLIB_COOKIE}
 	@if cmp -s ${_P_WANTLIB_COOKIE} ${_I_WANTLIB_COOKIE}; \
 	then \
 		cat ${_P_WANTLIB_COOKIE}; \
