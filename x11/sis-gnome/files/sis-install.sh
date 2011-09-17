776393952e00a8226cee117a2f1a561837b14107
pirofti <pirofti@openbsd.org>
2011-09-17 10:55:47 +0000

    Make little sisters all around the world happy.
    Just pkg_add sis-gnome, and your little sister will stop calling you
    for the whole week as she'll have a full Destop Manager to work with
    on her computer!
    
    Okay robert@

---
+++ x11/sis-gnome/files/sis-install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+pkg_add		gnome-session		\
+		eog			\
+		file-roller		\
+		gedit			\
+		gnome-control-center	\
+		gnome-keyring		\
+		gnome-media-main	\
+		gnome-system-monitor	\
+		gnome-terminal		\
+		gnome-utils		\
+		evince			\
+		evolution		\
+		gnome-games		\
+		rhythmbox		\
+		totem			\
+		gnome-nettool
