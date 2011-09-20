326d7a7e09b80b5b45c329b2af164215ef27bc18
ajacoutot <ajacoutot@openbsd.org>
2011-09-20 23:01:29 +0000

    Update to cyrus-sasl-2.1.25. sqlite FLAVOR replaced with sqlite3.

--- security/cyrus-sasl2/pkg/PFRAG.sql
+++ security/cyrus-sasl2/pkg/PFRAG.sql
@@ -1,5 +1,5 @@
-@comment $OpenBSD: PFRAG.sql,v 1.7 2008/03/15 12:50:46 ajacoutot Exp $
+@comment $OpenBSD: PFRAG.sql,v 1.8 2011/09/20 23:01:29 ajacoutot Exp $
 %%SHARED%%
-lib/sasl2/libsql.a
-lib/sasl2/libsql.la
+@comment lib/sasl2/libsql.a
+@comment lib/sasl2/libsql.la
 share/examples/sasl2/Sendmail.conf-sql
