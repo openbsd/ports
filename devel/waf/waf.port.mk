8edc2d858dfec17a327c8ab3b3d079b1a2fe30e0
landry <landry@openbsd.org>
2011-09-22 13:05:57 +0000

    Add MODWAF_VERBOSE defaulting to Yes, and if set pass -v to the waf commands through _MODWAF_VERBOSE_FLAG. Allows to actually show build lines like in any "sane" build system. something i should have done ages ago.

--- devel/waf/waf.port.mk
+++ devel/waf/waf.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: waf.port.mk,v 1.3 2010/11/15 19:46:10 espie Exp $
+# $OpenBSD: waf.port.mk,v 1.4 2011/09/22 13:05:57 landry Exp $
 
 MODULES +=		lang/python
 MODPY_RUNDEP =		no
@@ -7,18 +7,23 @@ MODWAF_BIN =		${LOCALBASE}/bin/waf
 MAKE_ENV +=		PYTHON=${MODPY_BIN} PYTHON_VERSION=${MODPY_VERSION}
 _MODWAF_CMD =		@cd ${WRKSRC} && ${SETENV} ${MAKE_ENV} ${MODWAF_BIN}
 _MODWAF_CONFIGURE_CMD =	@cd ${WRKSRC} && ${SETENV} ${MAKE_ENV} ${CONFIGURE_ENV} ${MODWAF_BIN}
+MODWAF_VERBOSE ?=	Yes
+
+.if ${MODWAF_VERBOSE:L} == "yes"
+_MODWAF_VERBOSE_FLAG =	-v
+.endif
 
 do-configure:
-	${_MODWAF_CONFIGURE_CMD} configure ${CONFIGURE_ARGS}
+	${_MODWAF_CONFIGURE_CMD} configure ${_MODWAF_VERBOSE_FLAG} ${CONFIGURE_ARGS}
 
 .if !target(do-build)
 do-build:
-	${_MODWAF_CMD} build
+	${_MODWAF_CMD} build ${_MODWAF_VERBOSE_FLAG}
 .endif
 
 .if !target(do-install)
 do-install:
-	${_MODWAF_CMD} install --destdir=${WRKINST}
+	${_MODWAF_CMD} install ${_MODWAF_VERBOSE_FLAG} --destdir=${WRKINST}
 .endif
 
 .if !target(post-install)
