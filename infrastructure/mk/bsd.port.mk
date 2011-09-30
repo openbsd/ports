00d241e48d1f5a5895bd0d54ff1f97c7a850748a
espie <espie@openbsd.org>
2011-09-30 05:35:52 +0000

    oops

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-#	$OpenBSD: bsd.port.mk,v 1.1112 2011/09/28 10:20:19 espie Exp $
+#	$OpenBSD: bsd.port.mk,v 1.1113 2011/09/30 05:35:52 espie Exp $
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -445,7 +445,7 @@ _PKG_ARGS += -D${_i}=1
 .    endif
 .  endfor
 .endif
-.if !${BUILD_PKGPATH:M,}
+.if !${BUILD_PKGPATH:M*,*}
 BUILD_PKGPATH := ${BUILD_PKGPATH},
 .endif
 
