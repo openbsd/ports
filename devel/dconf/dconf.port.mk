367d281f8c15050a85c1aeb1678d3974d2dc4349
espie <espie@openbsd.org>
2011-11-11 11:39:13 +0000

    convert dconf to bsd.port.arch.mk recognize small build needed for boostrapping

--- devel/dconf/dconf.port.mk
+++ devel/dconf/dconf.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: dconf.port.mk,v 1.3 2011/05/28 08:15:33 ajacoutot Exp $
+# $OpenBSD: dconf.port.mk,v 1.4 2011/11/11 11:39:13 espie Exp $
 
 # This module is used by ports installing gsettings schemas under
 #     PREFIX/share/glib-2.0/schemas/
@@ -10,7 +10,7 @@
 # use the "no_editor" FLAVOR to prevent a cyclic dependency
 MODDCONF_BUILD_DEPENDS=	devel/glib2
 MODDCONF_RUN_DEPENDS=	devel/glib2 \
-			devel/dconf,no_editor
+			devel/dconf,no_editor,bootstrap
 
 BUILD_DEPENDS +=	${MODDCONF_BUILD_DEPENDS}
 RUN_DEPENDS +=		${MODDCONF_RUN_DEPENDS}
