e5a4a9f138ca645f0b5776682fb5f12df146d3cb
robert <robert@openbsd.org>
2011-11-17 12:53:49 +0000

    replace pecl-pcntl with php-pcntl

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.62 2011/11/13 13:54:42 ajacoutot Exp $
+# $OpenBSD: Quirks.pm,v 1.63 2011/11/17 12:53:49 robert Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -252,7 +252,8 @@ my $stem_extensions = {
 	'py-zeya' => 'zeya',
 # 5.1
 	'php5-mapscript' => 'php-mapscript',
-	'libusb' => 'libusb-compat'
+	'libusb' => 'libusb-compat',
+	'pecl-pcntl' => 'php-pcntl'
 };
 
 # ->is_base_system($handle, $state):
