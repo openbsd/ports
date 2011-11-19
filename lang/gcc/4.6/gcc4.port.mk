76c4d2cea0a910586ca9891b2219e6938668c175
pascal <pascal@openbsd.org>
2011-11-19 22:19:53 +0000

    - add Go support (only tested on amd64, but I'd be surprised if it broke   anywhere else.  The only thing not supported yet is tracing using ptrace(2). - clean up Makefile and restructure a bit - VMEM_WARNING when -java is built - some minor PLIST tweaks
    ok espie@

--- lang/gcc/4.6/gcc4.port.mk
+++ lang/gcc/4.6/gcc4.port.mk
@@ -1,16 +1,16 @@
-# $OpenBSD: gcc4.port.mk,v 1.2 2011/11/17 18:03:43 sthen Exp $
+# $OpenBSD: gcc4.port.mk,v 1.3 2011/11/19 22:19:53 pascal Exp $
 
-MODGCC4_ARCHS?=
-MODGCC4_LANGS?=
+MODGCC4_ARCHS ?=
+MODGCC4_LANGS ?=
 
 
 .if ${MODGCC4_LANGS:L} != "java" && !${MODGCC4_LANGS:L:Mc}
 # Always include support for this
 # unless only java is needed
-MODGCC4_LANGS+=	c
+MODGCC4_LANGS +=	c
 .endif
 
-_MODGCC4_OKAY = c c++ java fortran
+_MODGCC4_OKAY = c c++ java fortran go
 .for _l in ${MODGCC4_LANGS:L}
 .  if !${_MODGCC4_OKAY:M${_l}}
 ERRORS += "Fatal: unknown language ${_l}"
@@ -33,7 +33,7 @@ _MODGCC4_LINKS =
 .if ${_MODGCC4_ARCH_USES:L} == "yes"
 
 .  if ${MODGCC4_LANGS:L:Mc} && ${COMPILER_VERSION:L:Ngcc4*}
-BUILD_DEPENDS += gcc->=4.6,<4.7:lang/gcc/4.6
+BUILD_DEPENDS += lang/gcc/4.6>=4.6,<4.7
 _MODGCC4_LINKS += egcc gcc egcc cc
 .  endif
 
@@ -42,28 +42,33 @@ _MODGCC4_LINKS += egcc gcc egcc cc
 MODGCC4STDCPP = stdc++
 WANTLIB += stdc++>=53.0
 .    else
-BUILD_DEPENDS += g++->=4.6,<4.7:lang/gcc/4.6,-c++
+BUILD_DEPENDS += lang/gcc/4.6,-c++>=4.6,<4.7
 MODGCC4STDCPP = estdc++
-LIB_DEPENDS += libstdc++->=4.6,<4.7:lang/gcc/4.6,-estdc
-WANTLIB += estdc++>=7
+LIB_DEPENDS += lang/gcc/4.6,-estdc>=4.6,<4.7
+WANTLIB += estdc++>=12
 _MODGCC4_LINKS += eg++ g++ eg++ c++
 .    endif
 .  endif
 
 .  if ${MODGCC4_LANGS:L:Mfortran}
-BUILD_DEPENDS += g95->=4.6,<4.7:lang/gcc/4.6,-f95
+BUILD_DEPENDS += lang/gcc/4.6,-f95>=4.6,<4.7
 WANTLIB += gfortran>=2
-LIB_DEPENDS += g95->=4.6,<4.7:lang/gcc/4.6,-f95
+LIB_DEPENDS += lang/gcc/4.6,-f95>=4.6,<4.7
 _MODGCC4_LINKS += egfortran gfortran
 .  endif
 
 .  if ${MODGCC4_LANGS:L:Mjava}
-BUILD_DEPENDS += gcj->=4.6,<4.7:lang/gcc/4.6,-java
+BUILD_DEPENDS += lang/gcc/4.6,-java>=4.6,<4.7
 MODGCC4_GCJWANTLIB = gcj
-MODGCC4_GCJLIBDEP = gcj->=4.6,<4.7:lang/gcc/4.6,-java
+MODGCC4_GCJLIBDEP = lang/gcc/4.6,-java>=4.6,<4.7
 _MODGCC4_LINKS += egcj gcj egcjh gcjh ejar gjar egij gij
 .  endif
 
+.  if ${MODGCC4_LANGS:L:Mgo}
+BUILD_DEPENDS += lang/gcc/4.6,-go>=4.6,<4.7
+WANTLIB += go
+LIB_DEPENDS += lang/gcc/4.6,-go>=4.6,<4.7
+_MODGCC4_LINKS += egccgo gccgo
 .endif
 
 .if !empty(_MODGCC4_LINKS)
