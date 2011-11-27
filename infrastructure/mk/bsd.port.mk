51892d063112005a306e51e9cbb88dca7a02596a
espie <espie@openbsd.org>
2011-11-27 16:52:04 +0000

    explicitly ask for stdin

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-#	$OpenBSD: bsd.port.mk,v 1.1145 2011/11/26 13:52:52 espie Exp $
+#	$OpenBSD: bsd.port.mk,v 1.1146 2011/11/27 16:52:04 espie Exp $
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -2011,7 +2011,7 @@ port-lib-depends-check: ${WRKINST}/.saved_libs
 		SUBPACKAGE=$$s ${MAKE} print-plist-with-depends \
 		lib_depends_args=all-lib-depends-args \
 		wantlib_args=fake-wantlib-args| \
-			${_CHECK_LIB_DEPENDS} -s ${WRKINST}/.saved_libs; \
+			${_CHECK_LIB_DEPENDS} -i -s ${WRKINST}/.saved_libs; \
 	done
 
 _internal-manpages-check: ${_FAKE_COOKIE}
