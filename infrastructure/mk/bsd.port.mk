b411a2917d398940e17f218e5fbf2281d6d37264
espie <espie@openbsd.org>
2012-04-16 09:21:39 +0000

    strip PORTSDIR (or equivalent) from DESCR the same way we compute PKGPATH.
    avoids random changes of INDEX depending on where the ports tree is installed
    and configured.

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-#	$OpenBSD: bsd.port.mk,v 1.1161 2012/03/22 14:08:50 espie Exp $
+#	$OpenBSD: bsd.port.mk,v 1.1162 2012/04/16 09:21:39 espie Exp $
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -2758,7 +2758,7 @@ describe:
 .  endif
 	@echo -n ${_COMMENT${_S}:S/^"//:S/"$//:S/^'//:S/'$//:Q}"|"; \
 	if [ -f ${DESCR${_S}} ]; then \
-		echo -n "${DESCR${_S}:S,^${PORTSDIR}/,,}|"; \
+		echo -n `PORTSDIR_PATH=${PORTSDIR_PATH} ${_PERLSCRIPT}/getpkgpath ${DESCR${_S}}`'|';  \
 	else \
 		echo -n "/dev/null|"; \
 	fi; \
