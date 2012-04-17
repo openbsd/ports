390cba80dfbccce29415db267aff98a91f7555c5
espie <espie@openbsd.org>
2012-04-17 09:31:47 +0000

    handle modbuild (perl module) stuff similarly to what gnu does. there's no actual reason not to do it.

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-#	$OpenBSD: bsd.port.mk,v 1.1162 2012/04/16 09:21:39 espie Exp $
+#	$OpenBSD: bsd.port.mk,v 1.1163 2012/04/17 09:31:47 espie Exp $
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -255,6 +255,12 @@ INSTALL_TARGET ?= install
 	${CONFIGURE_STYLE:L:Mautoupdate}
 .  if !${CONFIGURE_STYLE:L:Mgnu}
 CONFIGURE_STYLE += gnu
+.  endif
+.endif
+
+.if ${CONFIGURE_STYLE:L:Mmodbuild}
+.  if !${CONFIGURE_STYLE:L:Mperl}
+CONFIGURE_STYLE += perl
 .  endif
 .endif
 
