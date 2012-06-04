769ccfc457aeb3a02218be31c65070a5a7526df7
pascal <pascal@openbsd.org>
2012-06-04 18:41:46 +0000

    Add BE_ARCHS and LE_ARCHS vars to be able to determine endianness.
    ok espie@

--- infrastructure/mk/arch-defines.mk
+++ infrastructure/mk/arch-defines.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: arch-defines.mk,v 1.3 2012/04/12 15:06:05 landry Exp $
+# $OpenBSD: arch-defines.mk,v 1.4 2012/06/04 18:41:46 pascal Exp $
 #
 # ex:ts=4 sw=4 filetype=make:
 #
@@ -16,6 +16,8 @@ ALL_ARCHS = alpha amd64 arm armish arm hppa hppa64 i386 landisk \
 	mvme68k mvme88k palm sgi socppc sparc sparc64 vax zaurus
 # not all powerpc have apm(4), hence the use of macppc
 APM_ARCHS = amd64 arm i386 loongson macppc sparc sparc64
+BE_ARCHS = hppa hppa64 m68k m88k mips64 powerpc sparc sparc64
+LE_ARCHS = alpha amd64 arm i386 mips64el sh vax
 LP64_ARCHS = alpha amd64 hppa64 sparc64 mips64 mips64el
 NO_SHARED_ARCHS = m88k vax
 GCC4_ARCHS = alpha amd64 arm armish beagle gumstix i386 hppa hppa64 \
@@ -26,7 +28,7 @@ GCC2_ARCHS = aviion luna88k m68k m88k mac68k mvme68k mvme88k vax
 # XXX easier for ports that depend on mono
 MONO_ARCHS = amd64 i386
 
-.for PROP in ALL APM LP64 NO_SHARED GCC4 GCC3 GCC2 MONO
+.for PROP in ALL APM BE LE LP64 NO_SHARED GCC4 GCC3 GCC2 MONO
 .  for A B in ${MACHINE_ARCH} ${ARCH}
 .    if !empty(${PROP}_ARCHS:M$A) || !empty(${PROP}_ARCHS:M$B)
 PROPERTIES += ${PROP:L}
