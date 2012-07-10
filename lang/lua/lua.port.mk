1ab04af85e2e2419b69d39eb9f5e2190d48f71fe
jasper <jasper@openbsd.org>
2012-07-10 15:09:25 +0000

    - rewrite lua.port.mk to support multiple lua versions.
    written and rewritten above and in budapest, with some help from sthen@ (and his cider)

--- lang/lua/lua.port.mk
+++ lang/lua/lua.port.mk
@@ -1,41 +1,125 @@
-# $OpenBSD: lua.port.mk,v 1.13 2012/05/15 19:24:21 jasper Exp $
+# $OpenBSD: lua.port.mk,v 1.14 2012/07/10 15:09:25 jasper Exp $
 
 CATEGORIES+=	lang/lua
 
-# Major.Minor of current lua version provided by lang/lua
-MODLUA_VERSION=	5.1
+#
+# Handle multiple versions/flavors.
+# Defaults to MODLUA_DEFAULT_VERSION if no FLAVOR is set.
+#
 
-MODLUA_WANTLIB=	lua
+# Define the default version and use that if MODLUA_VERSION is not set.
+MODLUA_DEFAULT_VERSION=	5.1
 
-MODLUA_RUN_DEPENDS=	lang/lua
+# The default version is not a flavor, but any other version is.
+.if !defined(FLAVORS)
+FLAVORS?=		lua52
+.endif
+
+FLAVOR?=		# empty
+
+.if empty(FLAVOR)
+MODLUA_VERSION=		${MODLUA_DEFAULT_VERSION}
+.elif ${FLAVOR:L:Mlua52}
+MODLUA_VERSION=		5.2
+.else
+ERRORS +=		"Fatal: Invalid FLAVOR set: ${FLAVOR}"
+.endif
+
+.if ${MODLUA_VERSION} == "5.1"
+_MODLUA_PKG_PREFIX=	lua
+MODLUA_FLAVOR=		# empty
+.elif ${MODLUA_VERSION} == "5.2"
+_MODLUA_PKG_PREFIX=	lua52
+MODLUA_FLAVOR=		lua52
+.else
+ERRORS +=		"Fatal: Invalid MODLUA_VERSION"
+.endif
+
+# Based on lua version, adjust the prefix. But don't change the prefix
+# of default (5.1) packages. Unless it's just a standalone application
+# that happens to embed lua (in which case MODLUA_SA needs to be set
+# and we'll just append the flavor suffix.
+MODLUA_SA?=		No
+
+.if ${MODLUA_VERSION} != "5.1"
+.if !${MODLUA_SA:L:Myes}
+FULLPKGNAME?=${PKGNAME:S/^lua/${_MODLUA_PKG_PREFIX}/}
+.endif
+.endif
+
+#
+# Shorthand variables used for common tasks, e.g. pkg-config use ${MODLUA_DEP}.
+#
+MODLUA_DEP_VERSION?=	${MODLUA_VERSION:S/.//g}
+MODLUA_DEP=		lua${MODLUA_DEP_VERSION}
+
+MODLUA_WANTLIB=		lua${MODLUA_VERSION}
+MODLUA_LIB=		-l${MODLUA_WANTLIB}
+
+.if ${MODLUA_VERSION} == "5.1"
+_MODLUA_RUN_DEPENDS=	lang/lua/5.1
+.elif ${MODLUA_VERSION} == "5.2"
+_MODLUA_RUN_DEPENDS=	lang/lua/5.2
+.endif
+
 MODLUA_LIB_DEPENDS=	${MODLUA_RUN_DEPENDS}
-_MODLUA_BUILD_DEPENDS=	${MODLUA_RUN_DEPENDS}
+
+MODLUA_BIN=		${LOCALBASE}/bin/lua${MODLUA_DEP_VERSION}
+
+# Propagate the flavor to all dependencies
+.for r in ${MODLUA_RUN_DEPENDS}
+_MODLUA_RUN_DEPS+=	${r},${MODLUA_FLAVOR}
+.endfor
+
+.for b in ${MODLUA_BUILD_DEPENDS}
+_MODLUA_BUILD_DEPENDS+=	${b},${MODLUA_FLAVOR}
+.endfor
+
+.for x in ${MODLUA_REGRESS_DEPENDS}
+_MODLUA_REGRESS_DEPENDS+=${x},${MODLUA_FLAVOR}
+.endfor
+
+#
+# Default directories
+#
+
+# Where the lua distribution headers have been installed to.
+MODLUA_INCL_DIR=	${LOCALBASE}/include/lua-${MODLUA_VERSION}/
 
 # Where the example will be installed
 MODLUA_EXAMPLEDIR=	${PREFIX}/share/examples/${FULLPKGNAME}/
 
 # Where the docs will be installed
-MODLUA_DOCDIR=	${PREFIX}/share/doc/${FULLPKGNAME}/
+MODLUA_DOCDIR=		${PREFIX}/share/doc/${FULLPKGNAME}/
 
 # Where the lua libraries will be installed to
-MODLUA_LIBDIR=	${PREFIX}/lib/lua/${MODLUA_VERSION}/
+MODLUA_LIBDIR=		${PREFIX}/lib/lua/${MODLUA_VERSION}/
 
 # Where the lua modules will be installed to
-MODLUA_DATADIR=	${PREFIX}/share/lua/${MODLUA_VERSION}/
+MODLUA_DATADIR=		${PREFIX}/share/lua/${MODLUA_VERSION}/
 
+#
+# Fixup run/build dependencies if needed
+#
 MODLUA_RUNDEP?=		Yes
 MODLUA_BUILDDEP?=	Yes
 
 .if ${MODLUA_RUNDEP:L} == yes
-RUN_DEPENDS+=	${MODLUA_RUN_DEPENDS}
+RUN_DEPENDS+=		${_MODLUA_RUN_DEPENDS} \
+			${_MODLUA_RUN_DEPS}
 .endif
 
 .if ${NO_BUILD:L} == "no" && ${MODLUA_BUILDDEP:L} == "yes"
-BUILD_DEPENDS+=	${_MODLUA_BUILD_DEPENDS}
+BUILD_DEPENDS+=		${_MODLUA_BUILD_DEPENDS} \
+			${_MODLUA_RUN_DEPENDS}
 .endif
+
+REGRESS_DEPENDS+=	${_MODLUA_REGRESS_DEPENDS}
 
 .if !defined(SHARED_ONLY) || ${SHARED_ONLY:L} == "no"
-PKG_ARCH=*
+PKG_ARCH=		*
 .endif
 
-SUBST_VARS+=	MODLUA_VERSION
+SUBST_VARS+=	MODLUA_VERSION MODLUA_LIB MODLUA_INCL_DIR \
+		MODLUA_EXAMPLEDIR MODLUA_DOCDIR MODLUA_LIBDIR \
+		MODLUA_DATADIR MODLUA_DEP
