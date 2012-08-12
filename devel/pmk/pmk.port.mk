5f483ddb1019f379ffb70602423fc0723fab371e
pascal <pascal@openbsd.org>
2012-08-12 22:21:27 +0000

    tedu devel/pmk.  Unused and broken with PIE, so now is a good time to get rid of it.
    ok naddy@

--- devel/pmk/pmk.port.mk
+++
@@ -1,8 +0,0 @@
-# $OpenBSD: pmk.port.mk,v 1.3 2010/11/15 19:46:07 espie Exp $
-# Pre Make Kit support
-# public domain
-
-BUILD_DEPENDS+=		devel/pmk
-CONFIGURE_SCRIPT=	${LOCALBASE}/bin/pmk
-CONFIGURE_ARGS=		BIN_CC=${CC}
-MODPMK_configure=	${MODSIMPLE_configure}
