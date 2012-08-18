0ffcd882eb4a54d267c8ecfc69746714b12a99c8
avsm <avsm@openbsd.org>
2012-08-18 21:49:16 +0000

    update to ocaml-4.00.0
    - uses new ocaml_native and ocaml_native_dynlink arch properties
    - changelog: http://caml.inria.fr/pub/distrib/ocaml-4.00/notes/Changes
    
    From Christopher Zimmermann <madroach@gmerlin.de> with fixes from me

--- lang/ocaml/ocaml.port.mk
+++ lang/ocaml/ocaml.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: ocaml.port.mk,v 1.17 2012/06/25 11:43:35 espie Exp $
+# $OpenBSD: ocaml.port.mk,v 1.18 2012/08/18 21:49:16 avsm Exp $
 
 # regular file usage for bytecode:
 # PLIST               -- bytecode base files
@@ -9,17 +9,17 @@
 # PFRAG.foo-native    -- nativecode files for FLAVOR == foo
 # PFRAG.no-foo-native -- nativecode files for FLAVOR != foo
 
-OCAML_VERSION=3.12.1
+OCAML_VERSION=4.00.0
 
-.if ${MACHINE_ARCH} == "alpha" || ${MACHINE_ARCH} == "i386" || \
-	${MACHINE_ARCH} == "sparc" || ${MACHINE_ARCH} == "amd64" || \
-	${MACHINE_ARCH} == "powerpc"
+.include <bsd.port.arch.mk>
+
+.if ${PROPERTIES:Mocaml_native}
 MODOCAML_NATIVE=Yes
 
 # include nativecode base files
 PKG_ARGS+=-Dnative=1
 
-.if ${MACHINE_ARCH} == "i386" || ${MACHINE_ARCH} == "amd64"
+.if ${PROPERTIES:Mocaml_native_dynlink}
 MODOCAML_NATDYNLINK=Yes
 
 # include native dynlink base files
@@ -43,5 +43,79 @@ PKG_ARGS+=-Dnative=0
 .endif
 
 BUILD_DEPENDS+=	lang/ocaml=${OCAML_VERSION}
-MAKE_ENV+= OCAMLFIND_DESTDIR=${DESTDIR}${TRUEPREFIX}/lib/ocaml/site-lib
+MAKE_ENV+= OCAMLFIND_DESTDIR=${DESTDIR}${TRUEPREFIX}/lib/ocaml
 
+NAME ?= ${PKGNAME:C/-[0-9].*//}
+
+MODOCAML_pre-fake = \
+  	${SUDO} ${INSTALL_DATA_DIR} ${WRKINST}${LOCALBASE}/lib/ocaml/stublibs
+
+
+######################################################################
+#
+# CONFIGURE_STYLE for oasis.
+#
+# Also overrides do-{build,install,test}
+#
+.if ${CONFIGURE_STYLE:L:Moasis}
+
+ALL_TARGET ?= -build -doc
+# XXX can't do ?= here, because INSTALL_TARGET is already initialized
+# with default value
+INSTALL_TARGET = -install
+REGRESS_TARGET ?= -test
+_MODOASIS_SETUP = ${WRKDIR}/oasis_setup.byte
+
+######################################################################
+# CONFIGURE
+.if ${PROPERTIES:Mocaml_native}
+_MODOASIS_OCAMLC = ocamlc.opt
+.else
+_MODOASIS_OCAMLC = ocamlc
+.endif
+MODOASIS_configure = \
+	${_MODOASIS_OCAMLC} -o ${_MODOASIS_SETUP} ${WRKSRC}/setup.ml && \
+	cd ${WRKSRC} && \
+	rm setup.cm[io] && \
+	${SETENV} ${CONFIGURE_ENV} ${_MODOASIS_SETUP} -configure \
+	--destdir ${WRKINST} \
+	--override pkg_name ${NAME} \
+	${CONFIGURE_ARGS}
+
+######################################################################
+# BUILD
+MODOASIS_BUILD_TARGET = cd ${WRKSRC}
+. for TARGET in ${ALL_TARGET}
+MODOASIS_BUILD_TARGET += \
+	&& ${SETENV} ${MAKE_ENV} ${_MODOASIS_SETUP} ${TARGET}
+. endfor
+. if !target(do-build)
+do-build: 
+	${MODOASIS_BUILD_TARGET}
+. endif
+
+######################################################################
+# INSTALL
+MODOASIS_INSTALL_TARGET = cd ${WRKSRC}
+. for TARGET in ${INSTALL_TARGET}
+MODOASIS_INSTALL_TARGET += \
+	&& ${SETENV} ${MAKE_ENV} ${_MODOASIS_SETUP} ${TARGET}
+. endfor
+. if !target(do-install)
+do-install: 
+	${MODOASIS_INSTALL_TARGET}
+. endif
+
+######################################################################
+# REGRESS
+MODOASIS_REGRESS_TARGET = cd ${WRKSRC}
+. for TARGET in ${REGRESS_TARGET}
+MODOASIS_REGRESS_TARGET += \
+	&& ${SETENV} ${MAKE_ENV} ${_MODOASIS_SETUP} ${TARGET}
+. endfor
+. if !target(do-regress)
+do-regress: 
+	${MODOASIS_REGRESS_TARGET}
+. endif
+
+.endif
