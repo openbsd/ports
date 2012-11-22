1bacafccbabcc66894d94526c92b1464d1418eb6
ajacoutot <ajacoutot@openbsd.org>
2012-11-22 16:01:38 +0000

    Update to cyrus-sasl-2.1.26.

--- security/cyrus-sasl2/pkg/PFRAG.sql
+++ security/cyrus-sasl2/pkg/PFRAG.sql
@@ -1,5 +1,3 @@
-@comment $OpenBSD: PFRAG.sql,v 1.8 2011/09/20 23:01:29 ajacoutot Exp $
+@comment $OpenBSD: PFRAG.sql,v 1.9 2012/11/22 16:01:38 ajacoutot Exp $
 %%SHARED%%
-@comment lib/sasl2/libsql.a
-@comment lib/sasl2/libsql.la
-share/examples/sasl2/Sendmail.conf-sql
+share/examples/cyrus-sasl/Sendmail.conf-sql
