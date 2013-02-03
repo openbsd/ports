634056d9900053981cd6e287b92df3d20813e9b0
rpe <rpe@openbsd.org>
2013-02-03 12:09:27 +0000

    - add a e17.port.mk module handling RUN_DEPENDS for icon cache,   desktop db, mime info db updates and removal of .la files in   modules. - add exec/unexec-delete goo for update-desktop-database in e17/e - bump REVISIONs accordingly due to changed PLISTs
    with help/feedback sthen@, ajacoutot@
    ok ajacoutot@

---
+++ x11/e17/e17.port.mk
@@ -0,0 +1,27 @@
+# $OpenBSD: e17.port.mk,v 1.1 2013/02/03 12:09:27 rpe Exp $
+
+# Module for e17 related ports
+
+# Set to 'yes' if there are .desktop files under share/applications/.
+.if defined(MODE17_DESKTOP_FILE) && ${MODE17_DESKTOP_FILE:L} == "yes"
+MODE17_RUN_DEPENDS+=	devel/desktop-file-utils
+.endif
+
+# Set to 'yes' if there are icon files under share/icons/.
+.if defined(MODE17_ICON_CACHE) && ${MODE17_ICON_CACHE:L} == "yes"
+MODE17_RUN_DEPENDS+= x11/gtk+2,-guic
+.endif
+
+# Set to 'yes' if there are mime xml files under share/mime/.
+.if defined(MODE17_MIME_DB) && ${MODE17_MIME_DB:L} == "yes"
+MODE17_RUN_DEPENDS+=	misc/shared-mime-info>=0.21
+.endif
+
+# remove useless .la file
+MODE17_PURGE_LA ?=
+.if !empty(MODE17_PURGE_LA)
+MODE17_post-install = for f in ${MODE17_PURGE_LA} ; do \
+		find ${PREFIX}/$${f} -name '*.la' -exec rm {} \; ; done
+.endif
+
+RUN_DEPENDS+=	${MODE17_RUN_DEPENDS}
