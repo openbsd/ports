fcf6c1486901b3ffb3f2f55e5793c46b430e2fac
jasper <jasper@openbsd.org>
2013-03-17 19:38:04 +0000

    directly write the completions file into ${FILESDIR}

--- lang/clojure/files/clj_completions.clj
+++ lang/clojure/files/clj_completions.clj
@@ -1,5 +1,5 @@
-;;; $OpenBSD: clj_completions.clj,v 1.1 2013/03/08 10:24:00 jasper Exp $
+;;; $OpenBSD: clj_completions.clj,v 1.2 2013/03/17 19:38:04 jasper Exp $
 
 (def completions (mapcat (comp keys ns-publics) (all-ns)))
-(with-open [f (java.io.BufferedWriter. (java.io.FileWriter. "clj_completions"))]
+(with-open [f (java.io.BufferedWriter. (java.io.FileWriter. "files/clj_completions"))]
 	   (.write f (apply str (interpose \newline completions))))
