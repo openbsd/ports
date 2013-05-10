e83d9fcc8954de473bc2359cae8a3f9f9e97a8c3
sthen <sthen@openbsd.org>
2013-05-10 09:56:45 +0000

    normalise patches (context diffs -> unidiffs, and use standard filenames). no pkg change.

--- net/wide-dhcp/patches/patch-server_common.h
+++
@@ -1,9 +0,0 @@
-*** server/common.h.orig	Mon Aug 17 18:13:35 1998
---- server/common.h	Mon Aug 17 18:14:02 1998
-***************
-*** 339,341 ****
---- 339,342 ----
-  char    *haddrtos();
-  char    *cidtos();
-  char    *_ctime();
-+ char    *chaddrtos();
