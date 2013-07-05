c38dcacc50e5c318839545a32907b4df0df1f24b
jasper <jasper@openbsd.org>
2013-07-05 09:34:35 +0000

    use ${VARBASE} instead of hardcoding /var

--- www/pear/pear.port.mk
+++ www/pear/pear.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: pear.port.mk,v 1.6 2013/03/11 11:44:47 espie Exp $
+# $OpenBSD: pear.port.mk,v 1.7 2013/07/05 09:34:35 jasper Exp $
 # PHP PEAR module
 
 PKGNAME?=	pear-${DISTNAME:S/_/-/}
@@ -13,7 +13,7 @@ NO_TEST=	Yes
 MAKE_FILE=	${PORTSDIR}/www/pear/Makefile.pear
 FAKE_FLAGS+=	WRKINST=${WRKINST} WRKDIR=${WRKDIR}
 
-PREFIX=		/var/www
+PREFIX=		${VARBASE}/www
 
 PEAR_LIBDIR=	${PREFIX}/pear/lib
 PEAR_PHPBIN=	${LOCALBASE}/bin/php
