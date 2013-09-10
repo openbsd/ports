4bc2b17ea00214c476a3b01d439324b0ca973a01
ajacoutot <ajacoutot@openbsd.org>
2013-09-10 13:26:12 +0000

    'dbus-python' => 'py-dbus'

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.97 2013/08/12 20:26:36 brad Exp $
+# $OpenBSD: Quirks.pm,v 1.98 2013/09/10 13:26:12 ajacoutot Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -325,6 +325,7 @@ my $stem_extensions = {
 	'thttpd' => 'sthttpd',
 	'pecl-fileinfo' => 'php',
 	'thttpd' => 'sthttpd',
+	'dbus-python' => 'py-dbus',
 };
 
 # ->is_base_system($handle, $state):
