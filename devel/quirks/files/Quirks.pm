6f7fadfd09b11b0393a85f6a3ade9f9abe98cb07
sthen <sthen@openbsd.org>
2013-09-25 07:57:37 +0000

    libungif->giflib

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.98 2013/09/10 13:26:12 ajacoutot Exp $
+# $OpenBSD: Quirks.pm,v 1.99 2013/09/25 07:57:38 sthen Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -326,6 +326,7 @@ my $stem_extensions = {
 	'pecl-fileinfo' => 'php',
 	'thttpd' => 'sthttpd',
 	'dbus-python' => 'py-dbus',
+	'libungif' => 'giflib',
 };
 
 # ->is_base_system($handle, $state):
