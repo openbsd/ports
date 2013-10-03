8aeef831123932e4b606d59f193a8a682390a233
espie <espie@openbsd.org>
2013-10-03 18:16:53 +0000

    ouch, remove "tainted" property only if junk REALLY occurred

--- infrastructure/lib/DPB/Job/Port.pm
+++ infrastructure/lib/DPB/Job/Port.pm
@@ -1,5 +1,5 @@
 # ex:ts=8 sw=4:
-# $OpenBSD: Port.pm,v 1.119 2013/10/03 17:42:58 espie Exp $
+# $OpenBSD: Port.pm,v 1.120 2013/10/03 18:16:53 espie Exp $
 #
 # Copyright (c) 2010 Marc Espie <espie@openbsd.org>
 #
@@ -498,16 +498,17 @@ sub finalize
 		$core->prop->{junk_count} = 0;
 		$core->prop->{ports_count} = 0;
 		$core->prop->{depends_count} = 0;
-	}
-	my $still_tainted = 0;
-	for my $job ($core->same_host_jobs) {
-		if ($job->{v}{info}->has_property('tag')) {
-			$still_tainted = 1;
-			last;
+
+		my $still_tainted = 0;
+		for my $job ($core->same_host_jobs) {
+			if ($job->{v}{info}->has_property('tag')) {
+				$still_tainted = 1;
+				last;
+			}
 		}
-	}
-	if (!$still_tainted) {
-		delete $core->prop->{tainted};
+		if (!$still_tainted) {
+			delete $core->prop->{tainted};
+		}
 	}
 
 	$core->{status} = 0;
