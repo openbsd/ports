4cb98fbf25c541003486e754979e728082f522ae
espie <espie@openbsd.org>
2013-10-25 13:40:42 +0000

    split up the fetch code for readability. don't bother passing logger around, just keep it in the fetch object, the distfile objects, and use it directly, that's simpler.

---
+++ infrastructure/lib/DPB/Distfile.pm
@@ -0,0 +1,363 @@
+# ex:ts=8 sw=4:
+# $OpenBSD: Distfile.pm,v 1.1 2013/10/25 13:40:42 espie Exp $
+#
+# Copyright (c) 2010-2013 Marc Espie <espie@openbsd.org>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+use strict;
+use warnings;
+
+use OpenBSD::md5;
+package DPB::Distfile;
+
+# same distfile may exist in several ports.
+
+my $cache = {};
+
+sub create
+{
+	my ($class, $file, $short, $site, $distinfo, $v, $repo) = @_;
+
+	my $sz = $distinfo->{size}{$file};
+	my $sha = $distinfo->{sha}{$file};
+	if (!defined $sz || !defined $sha) {
+		$v->break("Incomplete info for $file");
+		return;
+	}
+	$repo->known_file($sha, $file);
+	bless {
+		name => $file,
+		short => $short,
+		sz => $sz,
+		sha => $sha,
+		site => $site,
+		path => $v,
+		repo => $repo,
+	}, $class;
+}
+
+sub distdir
+{
+	my ($self, @rest) = @_;
+	return join('/', $self->{repo}->distdir, @rest);
+}
+
+sub logger
+{
+	my $self = shift;
+	return $self->{repo}{logger};
+}
+
+sub debug_dump
+{
+	my $self = shift;
+	my $msg = $self->logname;
+	if ($self->{okay}) {
+		$msg .= "(okay)";
+	}
+}
+
+sub cached
+{
+	my $self = shift;
+	return $self->{repo}{sha};
+}
+
+sub new
+{
+	my ($class, $file, $url, $dir, @r) = @_;
+	my $full = (defined $dir) ? join('/', $dir->string, $file) : $file;
+	if (!defined $url) {
+		$url = $file;
+	}
+	$cache->{$full} //= $class->create($full, $url, @r);
+}
+
+sub logname
+{
+	my $self = shift;
+	return $self->{path}->fullpkgpath.":".$self->{name};
+}
+
+sub lockname
+{
+	return shift->{name}.".dist";
+}
+
+sub simple_lockname
+{
+	&lockname;
+}
+
+# should be used for rebuild_info and logging only
+
+sub fullpkgpath
+{
+	return shift->{path}->fullpkgpath;
+}
+
+sub print_parent
+{
+	my ($self, $fh) = @_;
+	$self->{path}->print_parent($fh);
+}
+
+sub pkgpath_and_flavors
+{
+	return shift->{path}->pkgpath_and_flavors;
+}
+
+sub tempfilename
+{
+	my $self = shift;
+	return $self->filename.".part";
+}
+
+sub filename
+{
+	my $self = shift;
+	return $self->distdir($self->{name});
+}
+
+# this is the entry point from the Engine, this is run as soon as the path
+# has been scanned. For performance reasons, we cannot run a sha at that point.
+sub check
+{
+	my $self = shift;
+	# XXX in fetch_only mode, we won't build anything, so this is
+	# the only place we can check the file is okay
+	if ($self->{repo}->{fetch_only}) {
+		return $self->checksum_and_cache($self->filename);
+	} else {
+		return $self->checkcache_or_size($self->filename);
+	}
+}
+
+sub make_link
+{
+	my $self = shift;
+	my $sha = $self->{sha}->stringize;
+	if ($sha =~ m/^(..)/) {
+		my $result = $self->distdir('by_cipher', 'sha256', $1, $sha);
+		File::Path::make_path($result);
+		my $dest = $self->{name};
+		$dest =~ s/^.*\///;
+		link $self->filename, "$result/$dest";
+	}
+}
+
+sub find_copy
+{
+	my ($self, $name) = @_;
+
+	# sha256 must match AND size as well
+	my $alternate = $self->{repo}{reverse}{$self->{sha}->stringize};
+	if (defined $alternate) {
+		my $full = $self->distdir($alternate);
+		if ((stat $full)[7] == $self->{sz}) {
+			unlink($name);
+			if (link($full, $name)) {
+				$self->do_cache;
+				$self->{okay} = 1;
+				return 1;
+			}
+		}
+	}
+	return 0;
+}
+
+sub checkcache_or_size
+{
+	my ($self, $name) = @_;
+	# XXX if we matched once, then we match "forever"
+	return 1 if $self->{okay};
+	if (defined $self->cached->{$self->{name}}) {
+		return $self->checkcached($name);
+	}
+	return $self->checksize($name);
+}
+
+sub checksize
+{
+	my ($self, $name) = @_;
+	# XXX if we matched once, then we match "forever"
+	return 1 if $self->{okay};
+	if ($self->{sz} == 0) {
+		my $fh = $self->logger->open('dist/'.$self->{name});
+		print $fh "incomplete distinfo: no size\n";
+	}
+		
+	if (!stat $name) {
+		return $self->find_copy($name);
+	}
+	if ((stat _)[7] != $self->{sz}) {
+		my $fh = $self->logger->open('dist/'.$self->{name});
+		print $fh "size does not match\n";
+		return 0;
+	}
+	return 1;
+}
+
+sub checkcached
+{
+	my ($self, $name) = @_;
+	if (!defined $self->{sha}) {
+		my $fh = $self->logger->open('dist/'.$self->{name});
+		print $fh "incomplete distinfo: no sha\n";
+		return 0;
+	}
+	if ($self->cached->{$self->{name}}->equals($self->{sha})) {
+		$self->{okay} = 1;
+		return 1;
+	} else {
+		delete $self->cached->{$self->{name}};
+		my $fh = $self->logger->open('dist/'.$self->{name});
+		print $fh "sha cache info does not match,";
+		if ($self->caches_okay($name)) {
+			print $fh "but actual file had the right sha\n";
+			return 1;
+		} else {
+			print $fh "and actual file was wrong, deleted\n";
+			return 0;
+		}
+	}
+}
+
+sub do_cache
+{
+	my $self = shift;
+
+	eval {
+	$self->make_link;
+	print {$self->{repo}->{log}} "SHA256 ($self->{name}) = ",
+	    $self->{sha}->stringize, "\n";
+	};
+	# also enter ourselves into the internal repository
+	$self->cached->{$self->{name}} = $self->{sha};
+}
+
+# this is where we actually enter new files in the cache, when they do match.
+sub caches_okay
+{
+	my ($self, $name) = @_;
+	if (-f -r $name) {
+		if (OpenBSD::sha->new($name)->equals($self->{sha})) {
+			$self->{okay} = 1;
+			$self->do_cache;
+			return 1;
+		} else {
+			unlink($name);
+		}
+	}
+	return 0;
+}
+
+sub checksum_and_cache
+{
+	my ($self, $name) = @_;
+	# XXX if we matched once, then we match "forever"
+	return 1 if $self->{okay};
+	if (!defined $self->{sha}) {
+		return 0;
+	}
+	if (defined $self->cached->{$self->{name}}) {
+		return $self->checkcached($name);
+	}
+	if ($self->caches_okay($name)) {
+		return 1;
+	}
+	return $self->find_copy($name);
+}
+
+sub cache
+{
+	my $self = shift;
+	# XXX if we matched once, then we match "forever"
+	return 1 if $self->{okay};
+	$self->{okay} = 1;
+	# already done
+	if (defined $self->cached->{$self->{name}}) {
+		if ($self->cached->{$self->{name}}->equals($self->{sha})) {
+			return;
+		}
+	}
+	$self->do_cache;
+}
+
+sub checksum
+{
+	my ($self, $name) = @_;
+	# XXX if we matched once, then we match "forever"
+	return 1 if $self->{okay};
+	print "checksum for $name: ";
+	if (!defined $self->{sha}) {
+		print "NONE\n";
+		return 0;
+	}
+	if (defined $self->cached->{$self->{name}}) {
+		if ($self->cached->{$self->{name}}->equals($self->{sha})) {
+			print "OK (cached)\n";
+			$self->{okay} = 1;
+			return 1;
+		}
+	}
+	if ($self->caches_okay($name)) {
+		print "OK\n";
+		return 1;
+	}
+	print "BAD\n";
+	return 0;
+}
+
+sub cached_checksum
+{
+	my ($self, $fh, $name) = @_;
+	# XXX if we matched once, then we match "forever"
+	return 1 if $self->{okay};
+	print $fh "checksum for $name: ";
+	if (!defined $self->{sha}) {
+		print $fh "NONE\n";
+		return 0;
+	}
+	if (defined $self->cached->{$self->{name}}) {
+		if ($self->cached->{$self->{name}}->equals($self->{sha})) {
+			print $fh "OK (cached)\n";
+			$self->{okay} = 1;
+			return 1;
+		}
+	}
+	print $fh "UNKNOWN (uncached)\n";
+	return 0;
+}
+
+sub unlock_conditions
+{
+	my ($self, $engine) = @_;
+	return $self->check;
+}
+
+sub requeue
+{
+	my ($v, $engine) = @_;
+	$engine->requeue_dist($v);
+}
+
+sub forget
+{
+	my $self = shift;
+	delete $self->{size};
+	delete $self->{sha};
+	delete $self->{okay};
+}
+
+1;
---
+++ infrastructure/lib/DPB/Job/Fetch.pm
@@ -0,0 +1,234 @@
+# ex:ts=8 sw=4:
+# $OpenBSD: Fetch.pm,v 1.1 2013/10/25 13:40:42 espie Exp $
+#
+# Copyright (c) 2010-2013 Marc Espie <espie@openbsd.org>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+use strict;
+use warnings;
+
+package DPB::Task::Checksum;
+our @ISA = qw(DPB::Task::Fork);
+
+sub new
+{
+	my ($class, $fetcher, $status) = @_;
+	bless {fetcher => $fetcher, fetch_status => $status}, $class;
+}
+
+sub run
+{
+	my ($self, $core) = @_;
+	my $job = $core->job;
+	$self->redirect_fh($job->{logfh}, $job->{log});
+	exit(!$job->{file}->checksum($job->{file}->tempfilename));
+}
+
+sub finalize
+{
+	my ($self, $core) = @_;
+	$self->SUPER::finalize($core);
+	my $job = $core->job;
+	if ($core->{status} != 0) {
+		# XXX if we continued, and it failed, then maybe we
+		# got a stupid error message instead, so retry for
+		# full size.
+		if (defined $self->{fetcher}->{initial_sz}) {
+			unlink($job->{file}->tempfilename);
+		} else {
+			shift @{$job->{sites}};
+		}
+		return $job->bad_file($self->{fetcher}, $core);
+	}
+	rename($job->{file}->tempfilename, $job->{file}->filename);
+	print {$job->{logfh}} "Renamed to ", $job->{file}->filename, "\n";
+	$job->{file}->cache;
+	my $sz = $job->{file}->{sz};
+	if (defined $self->{fetcher}->{initial_sz}) {
+		$sz -= $self->{fetcher}->{initial_sz};
+	}
+	my $fh = $job->{file}->logger->open("fetch/good");
+	my $elapsed = $self->{fetcher}->elapsed;
+	print $fh $self->{fetcher}{site}.$job->{file}->{short}, " in ",
+	    $elapsed, "s ";
+	if ($elapsed != 0) {
+		print $fh "(", sprintf("%.2f", $sz / $elapsed / 1024), "KB/s)";
+	}
+	print $fh "\n";
+	close $fh;
+	return 1;
+}
+
+# Fetching stuff is almost a normal job
+package DPB::Task::Fetch;
+our @ISA = qw(DPB::Task::Clocked);
+
+sub stopped_clock
+{
+	my ($self, $gap) = @_;
+	# note that we're missing time
+	$self->{got_suspended}++;
+	$self->SUPER::stopped_clock($gap);
+}
+
+sub new
+{
+	my ($class, $job) = @_;
+	if (@{$job->{sites}}) {
+		my $o = bless { site => $job->{sites}[0]}, $class;
+		my $sz = (stat $job->{file}->tempfilename)[7];
+		if (defined $sz) {
+			$o->{initial_sz} = $sz;
+		}
+		return $o;
+	} else {
+		undef;
+	}
+}
+
+sub run
+{
+	my ($self, $core) = @_;
+	my $job = $core->job;
+	my $shell = $core->shell;
+	my $site = $self->{site};
+	$self->redirect($job->{log});
+	if ($job->{file}{sz} == 0) {
+		print STDERR "No size in distinfo\n";
+		exit(1);
+	}
+	my $ftp = OpenBSD::Paths->ftp;
+	$self->redirect($job->{log});
+	my @cmd = ('-C', '-o', $job->{file}->tempfilename, '-v',
+	    $site.$job->{file}->{short});
+	if ($ftp =~ /\s/) {
+		unshift @cmd, split(/\s+/, $ftp);
+	} else {
+		unshift @cmd, $ftp;
+	}
+	print STDERR "===> Trying $site\n";
+	print STDERR join(' ', @cmd), "\n";
+	# run ftp;
+	$core->shell->nochroot->exec(@cmd);
+}
+
+sub finalize
+{
+	my ($self, $core) = @_;
+	$self->SUPER::finalize($core);
+	my $job = $core->job;
+	if ($job->{file}->checksize($job->{file}->tempfilename)) {
+	    	$job->new_checksum_task($self, $core->{status});
+	} else {
+		if ($job->{file}->{sz} == 0) {
+			$job->{sites} = [];
+			return $job->bad_file($self, $core);
+		}
+		# Fetch exited okay, but the file is not the right size
+		if ($core->{status} == 0 ||
+		# definite error also if file is too large
+		    stat($job->{file}->tempfilename) &&
+		    (stat _)[7] > $job->{file}->{sz}) {
+			unlink($job->{file}->tempfilename);
+		}
+		# if we got suspended, well, might have to retry same site
+		if (!$self->{got_suspended}) {
+			shift @{$job->{sites}};
+		}
+		return $job->bad_file($self, $core);
+	}
+}
+
+package DPB::Job::Fetch;
+our @ISA = qw(DPB::Job::Normal);
+
+use File::Path;
+use File::Basename;
+
+sub new_fetch_task
+{
+	my $self = shift;
+	my $task = DPB::Task::Fetch->new($self);
+	if ($task) {
+		push(@{$self->{tasks}}, $task);
+		$self->{tries}++;
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+sub bad_file
+{
+	my ($job, $task, $core) = @_;
+	my $fh = $job->{file}->logger->open("fetch/bad");
+	print $fh $task->{site}.$job->{file}->{short}, "\n";
+	if ($job->new_fetch_task) {
+		$core->{status} = 0;
+		return 1;
+	} else {
+		$core->{status} = 1;
+		return 0;
+	}
+}
+
+sub new_checksum_task
+{
+	my ($self, $fetcher, $status) = @_;
+	push(@{$self->{tasks}}, DPB::Task::Checksum->new($fetcher, $status));
+}
+
+sub new
+{
+	my ($class, $file, $e) = @_;
+	my $job = bless {
+		sites => [@{$file->{site}}],
+		file => $file,
+		tasks => [],
+		endcode => $e,
+		log => $file->logger->make_distlogs($file),
+	}, $class;
+	open $job->{logfh}, '>>', $job->{log};
+	print {$job->{logfh}} ">>> From ", $file->fullpkgpath, "\n";
+	File::Path::mkpath(File::Basename::dirname($file->filename));
+	$job->{watched} = DPB::Watch->new($file->tempfilename,
+		$file->{sz}, undef, $job->{started});
+	$job->new_fetch_task;
+	return $job;
+}
+
+sub name
+{
+	my $self = shift;
+	return '<'.$self->{file}->{name}."(#".$self->{tries}.")";
+}
+
+sub watched
+{
+	my ($self, $current, $core) = @_;
+	my $w = $self->{watched};
+	my $diff = $w->check_change($current);
+	my $msg = $w->percent_message . $w->frozen_message($diff);
+	my $to = $core->fetch_timeout;
+	if (defined $to) {
+		if ($diff > $to) {
+			$self->{stuck} =
+			    "KILLED: $self->{current} stuck at $msg";
+			kill 9, $core->{pid};
+			return $self->{stuck};
+		}
+	}
+	return $msg;
+}
+
+1;
