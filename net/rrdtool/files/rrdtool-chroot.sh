596c7f24b2c40d943cc916fe8cb9f3bd4e92f2a1
sthen <sthen@openbsd.org>
2013-11-05 17:49:01 +0000

    Update to rrdtool 1.4.8. Merge p5-RRD into the main package.
    Note that this now uses cairo for graphics generation and pango/fontconfig
    for text rendering; if you are using this in a chroot jail (for cgi/php
    scripts, etc) you will need to take additional steps to install the relevant
    files. A script is provided to copy the relevant libraries and support files;
    see /usr/local/share/doc/pkg-readmes/rrdtool-1.4.8 for more details.

---
+++ net/rrdtool/files/rrdtool-chroot.sh
@@ -0,0 +1,59 @@
+#!/bin/ksh
+#
+# make rrdtool(1) available in Apache chroot(8)
+
+# WARNING: /var/www/usr/libexec/ld.so and /var/www/lib/* may be shared
+# by other executables in the chroot(8)
+
+if [ "$(id -u)" -ne 0 ]; then
+	echo " *** Error: need root privileges to run this script"
+	exit 1
+fi
+
+do_enable() {
+	mkdir -p /var/www{/etc/{pango,fonts/TTF},/usr/lib,/usr/libexec,${LOCALBASE}/bin,${X11BASE}/lib,/cache/fontconfig}
+	cp -p ${LOCALBASE}/bin/rrdtool /var/www${LOCALBASE}/bin
+	for i in $(ldd ${LOCALBASE}/bin/rrdtool | grep 'lib/' | awk '{ print $7 }') ; do \
+		cp -p $i /var/www/usr/lib/
+	done
+	cp -p ${LOCALBASE}/lib/pango/*/modules/pango-basic-fc.so /var/www/usr/lib/
+	cp -p /usr/libexec/ld.so /var/www/usr/libexec/
+	cp -p /usr/X11R6/lib/X11/fonts/TTF/DejaVuSans.ttf /var/www/etc/fonts/TTF
+	cat << EOF > /var/www/etc/fonts/fonts.conf
+<?xml version="1.0"?>
+<!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+<fontconfig>
+	<dir>/etc/fonts/TTF</dir>
+	<cachedir>/cache/fontconfig</cachedir>
+</fontconfig>
+EOF
+	cat << EOF > /var/www/etc/pango/pango.modules
+/usr/lib/pango-basic-fc.so BasicScriptEngineFc PangoEngineShape PangoRenderFc common:
+EOF
+}
+
+do_disable() {
+	rm -rf /var/www/usr/local/bin/rrdtool \
+		/var/www/usr/libexec/ld.so \
+		/var/www/etc/fonts \
+		/var/www/etc/pango \
+		/var/www/cache/fontconfig
+	for i in $(ldd ${LOCALBASE}/bin/rrdtool | grep 'lib/' | awk '{ print $7 }') ; do \
+		rm -f /var/www/usr/lib/$(basename $i | sed -e 's,\.so.*,,')*
+	done
+	rm -f /var/www/usr/lib/pango-basic-fc.so
+	rmdir /var/www{/etc/fonts,${LOCALBASE}/bin,${LOCALBASE},/usr/lib,/usr/libexec,/usr} 2>/dev/null
+}
+
+case $1 in
+enable)
+	do_disable
+	do_enable
+	;;
+disable)
+	do_disable
+	;;
+*)
+	echo "usage: ${0##*/} {enable|disable}"
+	;;
+esac
