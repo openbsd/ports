df232ae16163e128b527fb8889826b3104b0ff3f
landry <landry@openbsd.org>
2013-12-12 19:49:51 +0000

    Winter cleanup of mozilla.port.mk:
    - remove all the cruft that was here for 'old' mozillas now that they're
      in the attic
    - sanitize CONFIGURE_ARGS, remove all the unneeded stuff.
    - cleanup CONFIGURE/MAKE_ENV
    - reorganize *DEPENDS, and finally fix WANTLIB
    
    mozilla.port.mk |  144 ++++++++++----------------------------------------------
    1 file changed, 28 insertions(+), 116 deletions(-)

--- www/mozilla/mozilla.port.mk
+++ www/mozilla/mozilla.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: mozilla.port.mk,v 1.64 2013/11/30 12:03:44 landry Exp $
+# $OpenBSD: mozilla.port.mk,v 1.65 2013/12/12 19:49:51 landry Exp $
 
 SHARED_ONLY =	Yes
 ONLY_FOR_ARCHS=	amd64 arm i386 powerpc sparc64
@@ -23,6 +23,8 @@ MOZILLA_DIST_VERSION ?=	${MOZILLA_VERSION}
 
 HOMEPAGE ?=	http://www.mozilla.org/projects/${MOZILLA_DIST}
 
+# first is the CDN and only has last releases
+# ftp.m.o has all the betas/candidate builds but should only be used as fallback
 MASTER_SITES ?=	http://releases.mozilla.org/pub/mozilla.org/${MOZILLA_DIST}/releases/${MOZILLA_DIST_VERSION}/source/ \
 		https://ftp.mozilla.org/pub/mozilla.org/${MOZILLA_DIST}/releases/${MOZILLA_DIST_VERSION}/source/ \
 		ftp://ftp.mozilla.org/pub/mozilla.org/${MOZILLA_DIST}/releases/${MOZILLA_DIST_VERSION}/source/
@@ -33,19 +35,12 @@ DIST_SUBDIR ?=	mozilla
 MODMOZ_RUN_DEPENDS =	devel/desktop-file-utils
 MODMOZ_BUILD_DEPENDS =	devel/libIDL \
 			archivers/gtar \
+			archivers/unzip \
 			archivers/zip>=2.3
 
-MODMOZ_LIB_DEPENDS =	x11/gtk+2
-
-# special case the long-term maintained mozillas
-.if ${MOZILLA_PROJECT} == "firefox" || \
-	${MOZILLA_PROJECT} == "thunderbird" || \
-	${MOZILLA_PROJECT} == "xulrunner" || \
-	${MOZILLA_PROJECT} == "seamonkey"
-MODMOZ_LIB_DEPENDS +=	devel/nspr>=4.10 \
-			security/nss>=3.15
-# needed during install
-MODMOZ_BUILD_DEPENDS +=	archivers/unzip
+MODMOZ_LIB_DEPENDS =	x11/gtk+2 \
+			devel/nspr>=4.10.2 \
+			security/nss>=3.15.3.1
 
 # bug #736961
 SEPARATE_BUILD =	Yes
@@ -55,45 +50,15 @@ SEPARATE_BUILD =	Yes
 MODMOZ_BUILD_DEPENDS +=	devel/yasm
 .endif
 
-CONFIGURE_ARGS +=	--enable-official-branding
-CONFIGURE_ARGS +=	--disable-gconf
-CONFIGURE_ARGS +=	--enable-gio
-CONFIGURE_ARGS +=	--with-system-libevent=/usr/
-CONFIGURE_ARGS +=	--with-system-bz2=${LOCALBASE}
-MODMOZ_WANTLIB +=	event
-.else
-# for old mozillas : fennec, sunbird, firefox36, xulrunner
-MODMOZ_LIB_DEPENDS +=	devel/nspr \
-			security/nss
-
-# no --with-system-jpeg starting with fx 18, requires libjpeg-turbo because of bug 791305
-CONFIGURE_ARGS +=	--with-system-jpeg=${LOCALBASE}
-MODMOZ_WANTLIB += jpeg
-
-# for mozilla branches 1.9.2 and esr10 (fennec), build against systemwide cairo
-CONFIGURE_ARGS +=	--enable-system-cairo
-
-# gecko doesnt link anymore with krb5 since 22 (bug 648730)
-MODMOZ_WANTLIB +=	crypto krb5 asn1 com_err heimbase roken wind
-
-# sydneyaudio was removed in gecko 22
-post-extract:
-# syndeyaudio sndio file comes from ffx FILESDIR
-	cp -f ${PORTSDIR}/www/mozilla-firefox/files/sydney_audio_sndio.c \
-		${WRKSRC}/${_MOZDIR}/media/libsydneyaudio/src/
-.endif
-
 MODMOZ_WANTLIB +=	X11 Xcomposite Xcursor Xdamage Xext Xfixes Xi \
 		Xinerama Xrandr Xrender Xt atk-1.0 c cairo \
 		fontconfig freetype gdk-x11-2.0 gdk_pixbuf-2.0 gio-2.0 glib-2.0 \
 		gobject-2.0 gthread-2.0 gtk-x11-2.0 m \
 		nspr4 nss3 pango-1.0 pangocairo-1.0 pangoft2-1.0 \
-		pixman-1 plc4 plds4 pthread pthread-stubs \
+		plc4 plds4 pthread event kvm sqlite3>=23 \
 		smime3 sndio nssutil3 ssl3 stdc++ z
 
-# for all mozilla ports, build against systemwide sqlite3
-MODMOZ_WANTLIB +=	sqlite3>=23
-CONFIGURE_ARGS +=	--enable-system-sqlite
+# hack to build against systemwide sqlite3 (# 546162)
 CONFIGURE_ENV +=	ac_cv_sqlite_secure_delete=yes
 
 # --no-keep-memory avoids OOM when linking libxul
@@ -111,37 +76,27 @@ RUN_DEPENDS +=	${MODMOZ_RUN_DEPENDS}
 
 USE_GMAKE ?=	Yes
 
+# no --with-system-jpeg starting with fx 18, requires libjpeg-turbo because of bug 791305
+# no --with-system-cairo, too much gfx problems because of version mismatch
+# gecko doesnt link anymore with krb5 since 22 (bug 648730)
+# no --with-system-ffi, needs 3.0.10 when not using gcc
+# no --with-system-png, apng support not bundled in
+
 AUTOCONF_VERSION =	2.13
 CONFIGURE_ARGS +=	--with-system-zlib=/usr	\
+		--with-system-libevent=/usr/	\
+		--with-system-bz2=${LOCALBASE}	\
 		--with-system-nspr		\
 		--with-system-nss		\
-		--with-pthreads			\
+		--enable-system-sqlite		\
+		--enable-official-branding	\
+		--enable-gio			\
+		--disable-gconf			\
+		--disable-necko-wifi		\
 		--disable-optimize		\
 		--disable-tests			\
-		--disable-pedantic		\
-		--disable-installer		\
 		--disable-updater		\
-		--disable-gnomeui		\
-		--disable-gnomevfs		\
-		--disable-dbus			\
-		--enable-default-toolkit=cairo-gtk2 \
-		--enable-xinerama		\
-		--enable-svg			\
-		--enable-svg-renderer=cairo	\
-		--enable-canvas
-
-
-# those ones only apply to mozilla branch 1.9.2
-# crashreporter uses google breakpad, osx/win/lin/sol only
-CONFIGURE_ARGS +=--disable-freetypetest		\
-		--disable-mochitest		\
-		--disable-libIDLtest		\
-		--disable-glibtest		\
-		--disable-necko-wifi		\
-		--disable-crashreporter		\
-		--disable-libnotify		\
-		--enable-xft			\
-		--disable-ipc
+		--disable-dbus
 
 FLAVORS +=	debug
 FLAVOR ?=
@@ -158,47 +113,27 @@ INSTALL_STRIP =
 CONFIGURE_ARGS +=--enable-application=${MOZILLA_CODENAME}
 
 .if ${MOZILLA_PROJECT} == "firefox" || \
-	${MOZILLA_PROJECT} == "xulrunner" || \
-	${MOZILLA_PROJECT} == "fennec" || \
-	${MOZILLA_PROJECT} == "xulrunner1.9"
+	${MOZILLA_PROJECT} == "xulrunner"
 WRKDIST ?=	${WRKDIR}/mozilla-${MOZILLA_BRANCH}
 .else
 WRKDIST ?=	${WRKDIR}/comm-${MOZILLA_BRANCH}
 _MOZDIR =	mozilla
 .endif
 
-# target directory for installation
-MOZ =		${PREFIX}/${MOZILLA_PROJECT}
-# source for installation
-MOB =		${WRKSRC}/${_MOZDIR}/dist/bin
-
 # needed for PLIST
-MOZILLA_VER =	${MOZILLA_VERSION:C/b.$//:C/esr//}
-SUBST_VARS +=	MOZILLA_PROJECT MOZILLA_VER MOZILLA_VERSION 
+MOZILLA_VER =	${MOZILLA_VERSION:C/b.$//:C/esr$//}
+SUBST_VARS +=	MOZILLA_PROJECT MOZILLA_VER MOZILLA_VERSION
 
-MAKE_ENV +=	MOZ_CO_PROJECT=${MOZILLA_CODENAME} \
-		LD_LIBRARY_PATH=${MOB} \
-		BUILD_OFFICIAL=1 \
-		MOZILLA_OFFICIAL=1 \
+MAKE_ENV +=	MOZILLA_OFFICIAL=1 \
 		SO_VERSION="${SO_VERSION}"
 
-CONFIGURE_ENV +=${MAKE_ENV} \
-		PKG_CONFIG_PATH="${LOCALBASE}/lib/pkgconfig:${X11BASE}/lib/pkgconfig" \
-		MOZ_ENABLE_COREXFONTS=1 \
-		topsrcdir=${WRKSRC}
+CONFIGURE_ENV +=	${MAKE_ENV}
 
 MODGNU_CONFIG_GUESS_DIRS +=	${WRKSRC}/${_MOZDIR}/build/autoconf \
 				${WRKSRC}/${_MOZDIR}/js/src/build/autoconf
 
 # files to run SUBST_CMD on
-MOZILLA_SUBST_FILES +=	${_MOZDIR}/xpcom/io/nsAppFileLocationProvider.cpp \
-			${_MOZDIR}/build/unix/mozilla.in \
-			${_MOZDIR}/extensions/spellcheck/hunspell/src/mozHunspell.cpp \
-			${_MOZDIR}/toolkit/xre/nsXREDirProvider.cpp
-
-.if ${MOZILLA_BRANCH} == 1.9.2
-MOZILLA_SUBST_FILES +=	${_MOZDIR}/js/src/xpconnect/shell/Makefile.in
-.endif
+MOZILLA_SUBST_FILES +=	${_MOZDIR}/extensions/spellcheck/hunspell/src/mozHunspell.cpp \
 
 pre-configure:
 .for d in ${MOZILLA_AUTOCONF_DIRS}
@@ -207,26 +142,3 @@ pre-configure:
 .for f in ${MOZILLA_SUBST_FILES}
 	${SUBST_CMD} ${WRKSRC}/${f}
 .endfor
-
-# common install target - ports can use post-install for specific stuff
-.if ${MOZILLA_PROJECT} == "xulrunner1.9" || \
-	${MOZILLA_PROJECT} == "firefox36" || \
-	${MOZILLA_PROJECT} == "sunbird"
-do-install:
-	cd ${MOB} && \
-		find ${MOZILLA_DATADIRS} -type d \
-			-exec ${INSTALL_DATA_DIR} ${MOZ}/{} \; && \
-		find ${MOZILLA_DATADIRS} ! -type d \
-			-exec ${INSTALL_DATA} -m 644 {} ${MOZ}/{} \;
-	${INSTALL_DATA} ${MOB}/*.so.${SO_VERSION} ${MOB}/*.ini ${MOZ}
-	# install shell wrapper to ${PREFIX}/bin
-	${INSTALL_SCRIPT} ${MOB}/${MOZILLA_PROJECT} ${PREFIX}/bin
-	${INSTALL_SCRIPT} ${MOB}/run-mozilla.sh ${MOZ}
-	${INSTALL_PROGRAM} ${MOB}/${MOZILLA_PROJECT}-bin ${MOB}/mozilla-xremote-client ${MOZ}
-	${INSTALL_PROGRAM} ${MOB}/regxpcom ${MOZ}
-	if [ -f ${FILESDIR}/${MOZILLA_PROJECT}.desktop ] ; then \
-		${INSTALL_DATA_DIR} ${PREFIX}/share/applications/ ; \
-		${SUBST_CMD} -o ${SHAREOWN} -g ${SHAREGRP} -c ${FILESDIR}/${MOZILLA_PROJECT}.desktop \
-			${PREFIX}/share/applications/${MOZILLA_PROJECT}.desktop ; \
-	fi ;
-.endif
