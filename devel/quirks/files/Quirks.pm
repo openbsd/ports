1e539a5c3ef5ce96f0bcd8d5dc3964d65dabcfa6
sthen <sthen@openbsd.org>
2014-01-07 15:06:38 +0000

    deletion annotation for net/ez-ipupdate

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.115 2013/12/19 19:25:04 ajacoutot Exp $
+# $OpenBSD: Quirks.pm,v 1.116 2014/01/07 15:06:38 sthen Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -355,6 +355,7 @@ my $obsolete_reason = {
 	'ruby-postgres' => 3,
 	'metasploit' => 2,
 	'childsplay-plugins' => 3,
+	'ez-ipupdate' => 3,
 };
 
 # ->is_base_system($handle, $state):
