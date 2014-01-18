9d2c132861f4dd828a9eb63dff04c6cc2c271c33
ajacoutot <ajacoutot@openbsd.org>
2014-01-18 13:41:22 +0000

    tesseract-dan-frak is going away.

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.116 2014/01/07 15:06:38 sthen Exp $
+# $OpenBSD: Quirks.pm,v 1.117 2014/01/18 13:41:22 ajacoutot Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -356,6 +356,7 @@ my $obsolete_reason = {
 	'metasploit' => 2,
 	'childsplay-plugins' => 3,
 	'ez-ipupdate' => 3,
+	'tesseract-dan-frak' => 3,
 };
 
 # ->is_base_system($handle, $state):
