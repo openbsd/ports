467d26b5d9ec74a5defded797aac7bfc69c4da78
landry <landry@openbsd.org>
2014-03-10 21:35:04 +0000

    Remove trailing spaces (long line iz looooooong!) that snuck in when i failed at copy&paste.

--- infrastructure/mk/gcc4.port.mk
+++ infrastructure/mk/gcc4.port.mk
@@ -1,6 +1,6 @@
-.if ${MACHINE_ARCH} == "hppa" || ${MACHINE_ARCH} == "alpha"                                                                                                   
-MODGCC4_VERSION?=4.6                                                                                                                                          
-.else                                                                                                                                                         
+.if ${MACHINE_ARCH} == "hppa" || ${MACHINE_ARCH} == "alpha"
+MODGCC4_VERSION?=4.6
+.else
 MODGCC4_VERSION?=4.8
 .endif
 .include "${PORTSDIR}/lang/gcc/${MODGCC4_VERSION}/gcc4.port.mk"
