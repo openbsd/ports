243a20d69970a8d7562f7fd2a025b5a5d7f2007d
landry <landry@openbsd.org>
2014-03-10 20:26:00 +0000

    Default to gcc 4.8 for ports using gcc4 module. Stay at 4.6 for hppa & alpha for now, since lang/gcc/4.8 doesnt build there yet. Discussed with espie@ pascal@ sthen@ tobiasu@ ajacoutot@

--- infrastructure/mk/gcc4.port.mk
+++ infrastructure/mk/gcc4.port.mk
@@ -1,2 +1,6 @@
-MODGCC4_VERSION?=4.6
+.if ${MACHINE_ARCH} == "hppa" || ${MACHINE_ARCH} == "alpha"                                                                                                   
+MODGCC4_VERSION?=4.6                                                                                                                                          
+.else                                                                                                                                                         
+MODGCC4_VERSION?=4.8
+.endif
 .include "${PORTSDIR}/lang/gcc/${MODGCC4_VERSION}/gcc4.port.mk"
