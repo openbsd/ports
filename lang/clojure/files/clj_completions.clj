ec922a63c20e1b91a0f656d5a4a89b8503fa029e
jasper <jasper@openbsd.org>
2014-03-30 21:08:24 +0000

    write out the sorted list of completions, makes it easier to diff the list

--- lang/clojure/files/clj_completions.clj
+++ lang/clojure/files/clj_completions.clj
@@ -1,5 +1,5 @@
-;;; $OpenBSD: clj_completions.clj,v 1.2 2013/03/17 19:38:04 jasper Exp $
+;;; $OpenBSD: clj_completions.clj,v 1.3 2014/03/30 21:08:24 jasper Exp $
 
 (def completions (mapcat (comp keys ns-publics) (all-ns)))
 (with-open [f (java.io.BufferedWriter. (java.io.FileWriter. "files/clj_completions"))]
-	   (.write f (apply str (interpose \newline completions))))
+	   (.write f (apply str (interpose \newline (sort completions)))))
