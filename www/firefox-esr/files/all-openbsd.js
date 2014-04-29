3867da5b180f82d67d27ffe6b333c33cce4bc516
landry <landry@openbsd.org>
2014-04-29 21:30:57 +0000

    Update to firefox-esr 24.5.0.
    - See http://www.mozilla.org/en-US/firefox/24.5.0/releasenotes/
    - Fixes MFSA-2014-34,35,37,38,42,43,44 & 46
    - add a default prefs all-openbsd.js file, pointing
      spellchecker.dictionary_path to ${LOCALBASE}/share/mozilla-dicts/ so
    that systemwide dictionaries are looked for by default.
    - while here the same file disables safebrowsing feature, as is already
      done in www/mozilla-firefox.

---
+++ www/firefox-esr/files/all-openbsd.js
@@ -0,0 +1,4 @@
+// OpenBSD-specific defaults overrides
+pref("browser.safebrowsing.enabled", false);
+pref("browser.safebrowsing.malware.enabled", false);
+pref("spellchecker.dictionary_path", "${LOCALBASE}/share/mozilla-dicts/");
