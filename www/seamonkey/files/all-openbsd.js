8006227429d034c91491338606ac187eada987c7
landry <landry@openbsd.org>
2014-04-29 21:47:00 +0000

    Update to seamonkey 2.26rc1 (to ride the mozilla.port.mk changes) and lightning 3.1.
    - See http://www.seamonkey-project.org/releases/seamonkey2.26/
    - Fixes MFSA 2014-34,36->39,41->47
    - remove now useless config.mk patch, was only here for enigmail
    - add patches to fix lightning display (#1001985)
    - set spellchecker.dictionary_path to look for systemwide dicts
    
    (note that seamonkey-i18n is not update yet on purpose, as 2.26 final is
    not yet available, langpacks are not there *yet* - will be fixed tmrw)

--- www/seamonkey/files/all-openbsd.js
+++ www/seamonkey/files/all-openbsd.js
@@ -1,3 +1,4 @@
 // OpenBSD-specific defaults overrides
 // enable systemwide extensions by default
 pref("extensions.autoDisableScopes", 3);
+pref("spellchecker.dictionary_path", "${LOCALBASE}/share/mozilla-dicts/");
