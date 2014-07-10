284f465ddcda7f81dabee59ec92a9a39517c4423
zhuk <zhuk@openbsd.org>
2014-07-10 19:50:25 +0000

    Another meaningful update from KDE land: Phonon 4.7.2 and friends. Main change is that VLC backend becames the default (and really performs better than GStreamer one).
    (now please get your children out of display)
    
    Another change is that due the fact that Phonon now supports library
    renaming upstream, libphononexperimental.so becomes
    libphonon_sexperimental.so. This doesn't affect any ports imported as of now.
    
    REVISION bumps for the dependant ports to follow.

--- multimedia/phonon/phonon.port.mk
+++ multimedia/phonon/phonon.port.mk
@@ -1,12 +1,12 @@
-# $OpenBSD: phonon.port.mk,v 1.2 2013/04/25 11:54:01 zhuk Exp $
+# $OpenBSD: phonon.port.mk,v 1.3 2014/07/10 19:50:25 zhuk Exp $
 MODPHONON_WANTLIB =	${MODKDE4_LIB_DIR}/phonon_s
-MODPHONON_LIB_DEPENDS =	phonon->=4.6.0:multimedia/phonon
+MODPHONON_LIB_DEPENDS =	phonon->=4.7.0:multimedia/phonon
 
 # If enabled (default), make sure at least one Phonon backend is
 # installed prior installing affected port.
 MODPHONON_PLUGIN_DEPS ?=	Yes
 .if ${MODPHONON_PLUGIN_DEPS:L} == "yes"
-MODPHONON_RUN_DEPENDS =	phonon-gstreamer-*|phonon-vlc-*:multimedia/phonon-backend/gstreamer
+MODPHONON_RUN_DEPENDS =	phonon-vlc-*|phonon-vlc-*:multimedia/phonon-backend/vlc
 .endif
 
 WANTLIB +=	${MODPHONON_WANTLIB}
