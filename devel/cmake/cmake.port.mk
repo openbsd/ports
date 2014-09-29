d2034e3daa09beb9744927a82bbea49bff0cf62b
dcoppa <dcoppa@openbsd.org>
2014-09-29 08:25:39 +0000

    Merge OpenBSD-related fixes from upstream git kudos to Rolf Eike Beer

--- devel/cmake/cmake.port.mk
+++ devel/cmake/cmake.port.mk
@@ -1,6 +1,6 @@
-# $OpenBSD: cmake.port.mk,v 1.25 2014/09/17 11:57:17 sthen Exp $
+# $OpenBSD: cmake.port.mk,v 1.26 2014/09/29 08:25:39 dcoppa Exp $
 
-BUILD_DEPENDS+=	devel/cmake>=3.0.2
+BUILD_DEPENDS+=	devel/cmake>=3.0.2p2
 
 .for _n _v in ${SHARED_LIBS}
 CONFIGURE_ENV+=LIB${_n}_VERSION=${_v}
