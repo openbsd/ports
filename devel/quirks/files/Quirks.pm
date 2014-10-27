3f1ecf0217da5fa9291c0abaa6a7bae4ceb8617b
jasper <jasper@openbsd.org>
2014-10-27 15:28:21 +0000

    register smarty removal

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.185 2014/10/13 12:34:42 ajacoutot Exp $
+# $OpenBSD: Quirks.pm,v 1.186 2014/10/27 15:28:21 jasper Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -446,6 +446,8 @@ my $obsolete_reason = {
 	'ruby-fastthread' => 3,
 	'dellflash' => 0,
 	'libgee06' => 3,
+	'smarty' => 2,
+	'smarty-docs' => 2,
 };
 
 # ->is_base_system($handle, $state):
