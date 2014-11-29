1d65080449d0f8aa28f9f60f7c6fdfae93eadf23
sthen <sthen@openbsd.org>
2014-11-29 01:07:32 +0000

    add a little shell trickery from afresh1 to fixup the command name displayed in usage text

--- net/unifi/files/unifi.sh
+++ net/unifi/files/unifi.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
-# $OpenBSD: unifi.sh,v 1.1 2014/11/29 00:26:58 sthen Exp $
+# $OpenBSD: unifi.sh,v 1.2 2014/11/29 01:07:32 sthen Exp $
 
 # Optionally symlink to things like unifi-discover
 cmd=${0##*unifi-}
 [ "$cmd" = "$0" ] && cmd=
+name=${0##*/}
  
 daemon="${TRUEPREFIX}/share/unifi/lib/ace.jar"
 java="$(${LOCALBASE}/bin/javaPathHelper -c unifi)"
  
-${java} -jar ${daemon} $cmd "$@" 
+# with some filehandle trickery to do substition on stderr
+# 3>&1 - fh3 -> fh1 (stdout)
+# 1>&2 - fh1 -> fh2 (stderr)
+# 2>&3 - fh2 -> fh3 (stdout)
+# 3>&- - fh3 -> close
+(${java} -jar ${daemon} $cmd "$@" 3>&1 1>&2 2>&3 |
+	sed -e "s,java -jar lib/ace.jar,$name,") 3>&1 1>&2 2>&3 3>&-
