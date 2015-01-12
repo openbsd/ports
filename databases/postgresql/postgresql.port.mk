2ae5b98b961b7bc7fc9e9f866aaa8eec2ab4a12b
zhuk <zhuk@openbsd.org>
2015-01-12 20:54:09 +0000

    Add infrastructure for testing PostgreSQL-based apps. It sets up database cluster, creates a database (optionally), starts server and then starts actual test code. The server gets correctly shut down after testing. Also, only Unix sockets are used for connections, minimizing side effects on running system.
    Man page to follow.
    
    okay espie@

---
+++ databases/postgresql/postgresql.port.mk
@@ -0,0 +1,43 @@
+# $OpenBSD: postgresql.port.mk,v 1.1 2015/01/12 20:54:09 zhuk Exp $ 
+#
+# Helps testing PostgreSQL-based software, no B/L/R-DEPS here.
+
+MODPOSTGRESQL_TEST_CMD ?= \
+	${MAKE_PROGRAM} ${ALL_TEST_FLAGS} -f ${MAKE_FILE} ${TEST_TARGET}
+
+MODPOSTGRESQL_TEST_PGHOST ?=	${WRKDIR}
+_MODPOSTGRESQL_TEST_PGDATA =	${WRKDIR}/testdb
+
+TEST_DEPENDS +=		databases/postgresql,-server
+TEST_ENV +=		PGDATA=${_MODPOSTGRESQL_TEST_PGDATA} \
+			PGHOST=${MODPOSTGRESQL_TEST_PGHOST}
+.ifdef MODPOSTGRESQL_TEST_DBNAME
+TEST_ENV +=		PGDATABASE=${MODPOSTGRESQL_TEST_DBNAME}
+.endif
+
+MODPOSTGRESQL_TEST_TARGET = \
+	rm -Rf ${_MODPOSTGRESQL_TEST_PGDATA}; \
+	export ${ALL_TEST_ENV}; \
+	${LOCALBASE}/bin/initdb -D ${_MODPOSTGRESQL_TEST_PGDATA} \
+	    -A trust --locale=C; \
+	${LOCALBASE}/bin/pg_ctl start -w -D ${_MODPOSTGRESQL_TEST_PGDATA} \
+	    -l ${WRKDIR}/pg-test.log \
+	    -o "-F -h '' -k ${MODPOSTGRESQL_TEST_PGHOST}";
+.ifdef MODPOSTGRESQL_TEST_DBNAME
+MODPOSTGRESQL_TEST_TARGET += \
+	${LOCALBASE}/bin/createdb ${MODPOSTGRESQL_TEST_DBNAME} || \
+	    (${LOCALBASE}/bin/pg_ctl stop -D ${_MODPOSTGRESQL_TEST_PGDATA} \
+	     -m i && exit 1);
+.endif
+MODPOSTGRESQL_TEST_TARGET += \
+	set +e; \
+	cd ${WRKBUILD}; \
+	${MODPOSTGRESQL_TEST_CMD}; \
+	Q=$$?; \
+	${LOCALBASE}/bin/pg_ctl stop -D ${_MODPOSTGRESQL_TEST_PGDATA} -m i; \
+	exit $$Q
+
+.if !target(do-test)
+do-test:
+	${MODPOSTGRESQL_TEST_TARGET}
+.endif
