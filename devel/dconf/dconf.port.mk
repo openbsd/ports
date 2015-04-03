89453e59e8c49ecbce9209f61efb5c2aace1dc78
ajacoutot <ajacoutot@openbsd.org>
2015-04-03 08:43:30 +0000

    Update to dconf-0.24.0. The -editor subpackage is dropped and a new dconf-editor will be imported instead.

--- devel/dconf/dconf.port.mk
+++ devel/dconf/dconf.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: dconf.port.mk,v 1.5 2014/07/09 16:49:41 ajacoutot Exp $
+# $OpenBSD: dconf.port.mk,v 1.6 2015/04/03 08:43:30 ajacoutot Exp $
 
 # This module is used by ports installing gsettings schemas under
 #     PREFIX/share/glib-2.0/schemas/
@@ -7,10 +7,9 @@
 # @exec %D/bin/glib-compile-schemas %D/share/glib-2.0/schemas >/dev/null
 # @unexec-delete %D/bin/glib-compile-schemas %D/share/glib-2.0/schemas >/dev/null
 
-# use the "no_editor" FLAVOR to prevent a cyclic dependency
 MODDCONF_BUILD_DEPENDS=	devel/glib2
 MODDCONF_RUN_DEPENDS=	devel/glib2 \
-			devel/dconf,no_editor,bootstrap
+			devel/dconf
 
 BUILD_DEPENDS +=	${MODDCONF_BUILD_DEPENDS}
 RUN_DEPENDS +=		${MODDCONF_RUN_DEPENDS}
