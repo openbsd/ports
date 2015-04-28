ae4caa77df6c6fd661dcc7e09e799d1edb132ee0
ajacoutot <ajacoutot@openbsd.org>
2015-04-28 07:32:24 +0000

    Register schroedinger removal.

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.232 2015/04/23 14:37:33 schwarze Exp $
+# $OpenBSD: Quirks.pm,v 1.233 2015/04/28 07:32:25 ajacoutot Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -521,6 +521,7 @@ my $obsolete_reason = {
 	'onyx' => 3,
 	'libunicode' => 5,
 	'cook' => 3,
+	'schroedinger' => 3,
 };
 
 # ->is_base_system($handle, $state):
