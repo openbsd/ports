11506574f9bea46689a123556b1b543308a0a1ea
czarkoff <czarkoff@openbsd.org>
2015-06-08 09:17:04 +0000

    Refactor lang/go module
    From port-modules(5):
    
         lang/go       Adds Go toolchain support.  Requires ALL_TARGET to be set
                       to canonical Go import path of port.  (Module sets it
                       automatically for ports that use GH_ACCOUNT and GH_PROJECT
                       macros.)
    
                       During execution of post-patch target module moves source
                       code from ${MODGO_SUBDIR} to ${WRKSRC}, subdirectory of
                       ${MODGO_WORKSPACE} - specially-crafted Go workspace located
                       at ${WRKDIR}/go.  During do-build module calls ``go
                       install'' with GOPATH set to ${MODGO_WORKSPACE}, runs its
                       output through sed to prevent writes outside WRKDIR sandbox
                       and sends output to sh(1).  During do-install it copies
                       executables from ${MODGO_WORKSPACE}/bin to ${PREFIX}/bin,
                       and/or directories ${MODGO_WORKSPACE}/pkg and
                       ${MODGO_WORKSPACE}/src to ${PREFIX}/go, depending on
                       MODGO_TYPE contents.
    
                       Sets BUILD_DEPENDS, RUN_DEPENDS, ALL_TARGET, TEST_TARGET,
                       ONLY_FOR_ARCHS, SEPARATE_BUILD, and WRKSRC.
    
                       Defines:
    
                       MODGO_TYPE       Type of port.  May be any combination of:
    
                                        bin  ordinary binary, which should be
                                             installed to ${PREFIX}/bin,
    
                                        lib  library, which should come with
                                             source code.
    
                                        Defaults to bin.
    
                       MODGO_WORKSPACE  Path to Go workspace set up for port build
                                        process.  Defaults to ${WRKDIR}/go.  See
                                        Go documentation for details.
    
                       MODGO_SUBDIR     Path to Go source code within port's
                                        sources tarball.  Defaults to ${WRKDIST}.
    
                       MODGO_SETUP_WORKSPACE
                                        Commands setting up Go workspace for
                                        building ports.  By default, happens
                                        during execution of post-patch target.
    
                       MODGO_BUILDDEP   Controls whether contents of
                                        MODGO_BUILD_DEPENDS are appended to port's
                                        BUILD_DEPENDS.  Defaults to Yes.
    
                       Additionally defines MODGO_PACKAGES, MODGO_SOURCES and
                       MODGO_TOOLS (paths for installed Go packages, sources and
                       tools respectively), MODGO_CMD and MODGO_FLAGS (source code
                       build command and flags passed as its arguments),
                       MODGO_BUILD_CMD and MODGO_TEST_CMD (commands for building
                       and testing go packages; normally called with canonical Go
                       package names as arguments),
                       MODGO_{BUILD,INSTALL,TEST}_TARGET and
                       MODGO_{BUILD,RUN}_DEPENDS.
    
    OK sthen@

--- lang/go/go.port.mk
+++ lang/go/go.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: go.port.mk,v 1.1 2015/05/15 07:30:41 jasper Exp $
+# $OpenBSD: go.port.mk,v 1.2 2015/06/08 09:17:04 czarkoff Exp $
 
 ONLY_FOR_ARCHS ?=	${GO_ARCHS}
 
@@ -11,8 +11,76 @@ MODGO_BUILD_DEPENDS =	lang/go
 BUILD_DEPENDS +=	${MODGO_BUILD_DEPENDS}
 .endif
 
-GO_PKG ?=		pkg/tool/openbsd_${MACHINE_ARCH:S/i386/386/}
+MODGO_PACKAGES =	go/pkg/openbsd_${MACHINE_ARCH:S/i386/386/}
+MODGO_SOURCES =		go/src
+MODGO_TOOLS =		go/pkg/tool/openbsd_${MACHINE_ARCH:S/i386/386/}
 
-SUBST_VARS +=		GO_PKG
+SUBST_VARS +=		MODGO_TOOLS MODGO_PACKAGES MODGO_SOURCES
 
-GOPATH ?=		"${WRKSRC}:${LOCALBASE}/go"
+MODGO_SUBDIR ?=		${WRKDIST}
+MODGO_TYPE ?=		bin
+MODGO_WORKSPACE ?=	${WRKDIR}/go
+MODGO_CMD ?=		unset GOPATH; export GOPATH="${MODGO_WORKSPACE}"; go
+MODGO_FLAGS +=		-a -x -work
+MODGO_BUILD_CMD =	${MODGO_CMD} install ${MODGO_FLAGS}
+MODGO_TEST_CMD =	${MODGO_CMD} test ${MODGO_FLAGS}
+
+.if ${MODGO_TYPE:L:Mlib}
+RUN_DEPENDS +=		${MODGO_RUN_DEPENDS}
+.endif
+
+.if defined(GH_ACCOUNT) && defined(GH_PROJECT)
+ALL_TARGET ?=		github.com/${GH_ACCOUNT}/${GH_PROJECT}
+.endif
+TEST_TARGET ?=		${ALL_TARGET}
+
+SEPARATE_BUILD ?=	Yes
+WRKSRC ?=		${MODGO_WORKSPACE}/src/${ALL_TARGET}
+
+MODGO_SETUP_WORKSPACE =	mkdir -p ${WRKSRC:H}; mv ${MODGO_SUBDIR} ${WRKSRC};
+
+# Go tends to ignore environment and place some files to system-wide
+# directories.  To prevent such behavior, this modules fixes paths in
+# auto-generated build instructions, and then feeds fixed script to shell
+# The "operation not permitted" filter is needed because Go outputs permission
+# error if USE_SYSTRACE=Yes option is set.
+MODGO_BUILD_TARGET =	${MODGO_BUILD_CMD} ${ALL_TARGET} 2>&1 | sed -E \
+				-e 's, ${LOCALBASE}/go, ${MODGO_WORKSPACE},' \
+				-e '/operation not permitted/d' \
+				-e 's,\$$WORK,${WRKBUILD},g' | sh -v
+
+# Go source files serve the purpose of libraries, so sources should be included
+# with library ports.
+.if ${MODGO_TYPE:L:Mlib}
+MODGO_INSTALL_TARGET =	${INSTALL_DATA_DIR} ${PREFIX}/go; \
+			cp -R ${MODGO_WORKSPACE}/pkg \
+			      ${MODGO_WORKSPACE}/src \
+					${PREFIX}/go;
+.endif
+.if ${MODGO_TYPE:L:Mbin}
+MODGO_INSTALL_TARGET += cp ${MODGO_WORKSPACE}/bin/* ${PREFIX}/bin
+.endif
+
+MODGO_TEST_TARGET =	${MODGO_TEST_CMD} ${TEST_TARGET}
+
+.if empty(CONFIGURE_STYLE)
+.  if !target(post-patch)
+post-patch:
+	${MODGO_SETUP_WORKSPACE}
+.  endif
+
+.  if !target(do-build)
+do-build:
+	${MODGO_BUILD_TARGET}
+.  endif
+
+.  if !target(do-install)
+do-install:
+	${MODGO_INSTALL_TARGET}
+.  endif
+
+.  if !target(do-test)
+do-test:
+	${MODGO_TEST_TARGET}
+.  endif
+.endif
