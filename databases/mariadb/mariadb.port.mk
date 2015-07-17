10683474c23274a663415a49975a4d468dd6c190
zhuk <zhuk@openbsd.org>
2015-07-17 14:41:58 +0000

    Add mariadb.port.mk, a test-support module for MySQL/MariaDB-based ports.
    This is an analog of postgresql.port.mk, but for MariaDB. p5-DBD-mysql
    is on its way to use this module soon, others to follow.
    
    Documentation bits will go in soon as well.
    
    okay giovanni@

---
+++ databases/mariadb/mariadb.port.mk
@@ -0,0 +1,71 @@
+# $OpenBSD: mariadb.port.mk,v 1.1 2015/07/17 14:41:58 zhuk Exp $
+#
+# Helps testing MySQL/MariaDB-based software, no B/L/R-DEPS here.
+
+MODMARIADB_TEST_CMD ?= \
+	${MAKE_PROGRAM} ${ALL_TEST_FLAGS} -f ${MAKE_FILE} ${TEST_TARGET}
+
+MODMARIADB_TEST_DBNAME ?=
+MODMARIADB_TEST_SOCKET =	${WRKDIR}/mariadb.sock
+_MODMARIADB_TEST_DATA_DIR =	${WRKDIR}/testdb
+
+MODMARIADB_SERVER_ARGS = \
+	--no-defaults \
+	--datadir=${_MODMARIADB_TEST_DATA_DIR} \
+	--skip-grant-tables \
+	--skip-networking \
+	--socket=${MODMARIADB_TEST_SOCKET}
+
+MODMARIADB_ADMIN_ARGS = \
+	--no-defaults \
+	--host=localhost \
+	--protocol=socket \
+	--socket=${MODMARIADB_TEST_SOCKET}
+
+MODMARIADB_CLIENT_ARGS =	${MODMARIADB_ADMIN_ARGS}
+
+.if !empty(MODMARIADB_TEST_DBNAME)
+MODMARIADB_CLIENT_ARGS +=	--database=${MODMARIADB_TEST_DBNAME}
+.endif
+
+TEST_DEPENDS +=		databases/mariadb,-server
+TEST_ENV += \
+	MYSQL_HOME=${WRKDIR} \
+	MYSQL_HOST=localhost \
+	MYSQL_UNIX_PORT=${MODMARIADB_TEST_SOCKET}
+
+MODMARIADB_TEST_TARGET = \
+	rm -Rf ${_MODMARIADB_TEST_DATA_DIR}; \
+	export ${ALL_TEST_ENV}; \
+	${LOCALBASE}/bin/mysql_install_db \
+		--skip-name-resolve \
+		${MODMARIADB_SERVER_ARGS}; \
+	${LOCALBASE}/libexec/mysqld \
+		${MODMARIADB_SERVER_ARGS} & \
+	started=false; \
+	for i in $$(jot 10); do \
+		if mysqladmin ${MODMARIADB_ADMIN_ARGS} ping; then \
+			started=true; \
+			break; \
+		fi; \
+		sleep 1; \
+	done; \
+	$$started || { kill $$!; exit 1; };
+.if !empty(MODMARIADB_TEST_DBNAME)
+MODMARIADB_TEST_TARGET += \
+	${LOCALBASE}/bin/mysqladmin ${MODMARIADB_ADMIN_ARGS} \
+	    create ${MODMARIADB_TEST_DBNAME} || \
+	    { kill $$!; exit 1; };
+.endif
+MODMARIADB_TEST_TARGET += \
+	set +e; \
+	cd ${WRKBUILD}; \
+	${MODMARIADB_TEST_CMD}; \
+	Q=$$?; \
+	kill $$!; \
+	exit $$Q
+
+.if !target(do-test)
+do-test:
+	${MODMARIADB_TEST_TARGET}
+.endif
