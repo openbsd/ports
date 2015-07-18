6860bba494eca49a4b676c055f410a602cb04da4
zhuk <zhuk@openbsd.org>
2015-07-18 22:02:05 +0000

    Remove lmbench. Highly outdated (1999 year benchmark, really?), non-free (the license marker in port was a lie) and ugly.
    okay aja@; also there were okays from sthen@ and kirby on earlier patch.

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.266 2015/07/18 20:35:45 kili Exp $
+# $OpenBSD: Quirks.pm,v 1.267 2015/07/18 22:02:05 zhuk Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -573,6 +573,7 @@ my $obsolete_reason = {
 	'hs-snap-loader-dynamic' => 5,
 	'hs-snap-loader-static' => 5,
 	'hs-snap-server' => 5,
+	'lmbench' => 5,
 };
 
 # ->is_base_system($handle, $state):
