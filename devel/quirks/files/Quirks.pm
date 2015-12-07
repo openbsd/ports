41a0a39c8c879445356f4cc64beabb74062811b8
jasper <jasper@openbsd.org>
2015-12-07 20:51:24 +0000

    register antisniff removal

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.331 2015/12/07 18:42:53 jca Exp $
+# $OpenBSD: Quirks.pm,v 1.332 2015/12/07 20:51:24 jasper Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -687,6 +687,7 @@ my $obsolete_reason = {
 	'openobex' => 6,
 	'obexftp' => 6,
 	'py-openbsd' => 3,
+	'antisniff' => 3,
 };
 
 my @msg = (
