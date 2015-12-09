10e8b4e05c95995390e3360f3a7c92840d89de50
jasper <jasper@openbsd.org>
2015-12-09 08:51:45 +0000

    register WebGUI removal

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.332 2015/12/07 20:51:24 jasper Exp $
+# $OpenBSD: Quirks.pm,v 1.333 2015/12/09 08:51:45 jasper Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -688,6 +688,7 @@ my $obsolete_reason = {
 	'obexftp' => 6,
 	'py-openbsd' => 3,
 	'antisniff' => 3,
+	'WebGUI' => 2,
 };
 
 my @msg = (
