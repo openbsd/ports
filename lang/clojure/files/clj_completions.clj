c6df09664b47ea50ba7d6632a759569ffce3b09e
jasper <jasper@openbsd.org>
2016-01-30 09:31:59 +0000

    remove duplicates from the completions list

--- lang/clojure/files/clj_completions.clj
+++ lang/clojure/files/clj_completions.clj
@@ -1,5 +1,5 @@
-;;; $OpenBSD: clj_completions.clj,v 1.3 2014/03/30 21:08:24 jasper Exp $
+;;; $OpenBSD: clj_completions.clj,v 1.4 2016/01/30 09:31:59 jasper Exp $
 
 (def completions (mapcat (comp keys ns-publics) (all-ns)))
 (with-open [f (java.io.BufferedWriter. (java.io.FileWriter. "files/clj_completions"))]
-	   (.write f (apply str (interpose \newline (sort completions)))))
+	   (.write f (apply str (interpose \newline (sort (distinct completions))))))
