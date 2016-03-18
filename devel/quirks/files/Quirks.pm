a01c656c860244d5c280067cb3dca51df7102696
sebastia <sebastia@openbsd.org>
2016-03-18 14:35:15 +0000

    removal of libretto-config
    Then you may do the honors and delete the port. naddy@

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.371 2016/03/16 19:32:32 naddy Exp $
+# $OpenBSD: Quirks.pm,v 1.372 2016/03/18 14:35:15 sebastia Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -742,6 +742,7 @@ my $obsolete_reason = {
 	'xcept' => 0,
 	'cqcam' => 6,
 	'uisp' => 6,
+        'libretto-config' => 6,
 };
 
 my @msg = (
