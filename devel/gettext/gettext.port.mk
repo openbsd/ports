8f97216ead334a9a7d5a68698e0308f1cf0b9f7a
naddy <naddy@openbsd.org>
2016-04-05 19:18:53 +0000

    remove unused variable MODGETTEXT_TOOLS

--- devel/gettext/gettext.port.mk
+++ devel/gettext/gettext.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: gettext.port.mk,v 1.13 2015/07/19 23:46:08 naddy Exp $
+# $OpenBSD: gettext.port.mk,v 1.14 2016/04/05 19:18:53 naddy Exp $
 
 _MODGETTEXT_SPEC =		devel/gettext>=0.10.38
 
@@ -16,8 +16,4 @@ RUN_DEPENDS +=		${MODGETTEXT_RUN_DEPENDS}
 WANTLIB +=		${MODGETTEXT_WANTLIB}
 
 # Always provide the development tools: msgfmt(1) etc.
-MODGETTEXT_TOOLS ?=	Yes
-
-.if ${MODGETTEXT_TOOLS:L} == yes
 BUILD_DEPENDS +=	devel/gettext-tools
-.endif
