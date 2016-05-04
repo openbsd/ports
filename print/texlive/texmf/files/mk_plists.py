1587311f6bd750f5e283343b33ee34244ff0681d
edd <edd@openbsd.org>
2016-05-04 22:52:19 +0000

    TeX Live: Update PLIST generation code in light of new py-texscythe.
    OK abieber@, thanks.

--- print/texlive/texmf/files/mk_plists.py
+++ print/texlive/texmf/files/mk_plists.py
@@ -3,17 +3,25 @@
 # This is how we generate the OpenBSD packing lists for TeX Live.
 # It is hooked in to the plist target in the port makefile.
 
+import re
+import sys
+from texscythe import config, subset, orm
+
+
 PLIST_BUILDSET_OUT = "../pkg/PLIST-buildset"
 PLIST_MAIN_OUT = "../pkg/PLIST-main"
 PLIST_FULL_OUT = "../pkg/PLIST-full"
 PLIST_DOCS_OUT = "../pkg/PLIST-docs"
 PLIST_CONTEXT_OUT = "../pkg/PLIST-context"
 
-import re
-from texscythe import config, subset
-
 YEAR = 2014
 MAN_INFO_REGEX = "texmf-dist\/doc\/(man\/man[0-9]\/.*[0-9]|info\/.*\.info)$"
+
+if len(sys.argv) != 2:
+    print("Please specify a tlpdb file")
+    sys.exit(1)
+
+TLPDB = sys.argv[1]
 
 
 class NastyError(Exception):
@@ -190,13 +198,16 @@ def filter_junk(filelist):
 
 def collect_files(specs, regex=None):
     cfg = config.Config(
+        TLPDB,
         inc_pkgspecs=specs,
         plist=None,  # return file list
         prefix_filenames="share/",
         dirs=False,  # Do this manually as we will filter the list
         regex=regex,
     )
-    files = subset.compute_subset(cfg)
+    sess = orm.init_orm(cfg)
+    files = subset.compute_subset(cfg, sess)
+    sess.close()
     files = relocate_mans_and_infos(files)
     files = filter_junk(files)
     return sorted(files)
@@ -279,7 +290,7 @@ buildset_pkgs = [
 print(">>> texlive_texmf-buildset")
 buildset_specs = runspecs(buildset_pkgs)  # note, no manuals
 buildset_top_matter = [
-    "@comment $OpenBSD: mk_plists.py,v 1.2 2015/09/25 12:13:47 dcoppa Exp $",
+    "@comment $OpenBSD: mk_plists.py,v 1.3 2016/05/04 22:52:19 edd Exp $",
     "@conflict teTeX_texmf-*",
     "@conflict texlive_base-<%s" % YEAR,
     "@conflict texlive_texmf-docs-<%s" % YEAR,
@@ -346,7 +357,7 @@ context_pkgs = [
 
 print(">>> PLIST-context")
 context_top_matter = [
-    "@comment $OpenBSD: mk_plists.py,v 1.2 2015/09/25 12:13:47 dcoppa Exp $",
+    "@comment $OpenBSD: mk_plists.py,v 1.3 2016/05/04 22:52:19 edd Exp $",
     "@conflict teTeX_texmf-*",
     "@conflict texlive_base-<%s" % YEAR,
     "@conflict texlive_texmf-docs-<%s" % YEAR,
@@ -376,7 +387,7 @@ print("\n\n")
 print(">>> texlive_texmf-minimal")
 minimal_pkgs = ["scheme-tetex"]
 minimal_top_matter = [
-    "@comment $OpenBSD: mk_plists.py,v 1.2 2015/09/25 12:13:47 dcoppa Exp $",
+    "@comment $OpenBSD: mk_plists.py,v 1.3 2016/05/04 22:52:19 edd Exp $",
     "@conflict teTeX_texmf-*",
     "@conflict texlive_base-<%s" % YEAR,
     "@conflict texlive_texmf-docs-<%s" % YEAR,
@@ -407,7 +418,7 @@ print("\n\n")
 print(">>> texlive_texmf-full")
 full_pkgs = ["scheme-full"]
 full_top_matter = [
-    "@comment $OpenBSD: mk_plists.py,v 1.2 2015/09/25 12:13:47 dcoppa Exp $",
+    "@comment $OpenBSD: mk_plists.py,v 1.3 2016/05/04 22:52:19 edd Exp $",
     "@conflict teTeX_texmf-*",
     "@conflict texlive_base-<%s" % YEAR,
     "@conflict texlive_texmf-docs-<%s" % YEAR,
@@ -442,7 +453,7 @@ NO_MAN_INFO_PDFMAN_REGEX = \
 print(">>> texlive_texmf-docs")
 doc_specs = ["scheme-tetex:doc"]
 doc_top_matter = [
-    "@comment $OpenBSD: mk_plists.py,v 1.2 2015/09/25 12:13:47 dcoppa Exp $",
+    "@comment $OpenBSD: mk_plists.py,v 1.3 2016/05/04 22:52:19 edd Exp $",
     "@conflict teTeX_texmf-doc-*",
     "@conflict texlive_base-<%s" % YEAR,
     "@conflict texlive_texmf-minimal-<%s" % YEAR,
