95e98391744359c84606d603191b733f40f417c2
zhuk <zhuk@openbsd.org>
2016-05-27 02:50:05 +0000

    Fix plugin dependencies for Qt5 case.

--- multimedia/phonon/phonon.port.mk
+++ multimedia/phonon/phonon.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: phonon.port.mk,v 1.6 2016/05/27 00:00:37 zhuk Exp $
+# $OpenBSD: phonon.port.mk,v 1.7 2016/05/27 02:50:05 zhuk Exp $
 .if ${MODULES:Mx11/qt5} || ${MODULES:Mdevel/kf5}
 MODPHONON_WANTLIB =	phonon4qt5
 MODPHONON_LIB_DEPENDS =	phonon-qt5->=4.8.0:multimedia/phonon,qt5
@@ -11,7 +11,11 @@ MODPHONON_LIB_DEPENDS =	phonon->=4.8.0:multimedia/phonon
 # installed prior installing affected port.
 MODPHONON_PLUGIN_DEPS ?=	Yes
 .if ${MODPHONON_PLUGIN_DEPS:L} == "yes"
+. if ${MODULES:Mx11/qt5} || ${MODULES:Mdevel/kf5}
+MODPHONON_RUN_DEPENDS =	phonon-qt5-gstreamer-*:multimedia/phonon-backend/gstreamer,qt5
+. else
 MODPHONON_RUN_DEPENDS =	phonon-vlc-*|phonon-gstreamer-*:multimedia/phonon-backend/vlc
+. endif
 .endif
 
 WANTLIB +=	${MODPHONON_WANTLIB}
