5a92bd34e5e5ea423027ac98f56ea2dc744ae0de
sebastia <sebastia@openbsd.org>
2016-06-03 11:43:41 +0000

    cleanup after gnustep-make update to 2.6.8

--- x11/gnustep/gnustep.port.mk
+++ x11/gnustep/gnustep.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: gnustep.port.mk,v 1.27 2016/03/19 23:50:53 naddy Exp $
+# $OpenBSD: gnustep.port.mk,v 1.28 2016/06/03 11:43:41 sebastia Exp $
 
 # until tested on others
 ONLY_FOR_ARCHS ?=	alpha i386 amd64 macppc
@@ -17,10 +17,10 @@ MODCLANG_LANGS =		c++
 
 .if ${MACHINE_ARCH} == "amd64" || ${MACHINE_ARCH} == "i386"
 CONFIGURE_ENV +=	CC="clang" CXX="clang++" CPP="clang -E"
-CONFIGURE_ENV +=	OBJCFLAGS=-fobjc-runtime=gnustep-1.7
 CONFIGURE_ENV +=	OPTFLAG="${CFLAGS}"
+# Not yet GS_WITH_ARC
+#MAKE_FLAGS +=		GS_WITH_ARC=1
 MAKE_FLAGS +=		CC="clang" CXX="clang++" CPP="clang -E"
-MAKE_FLAGS +=		OBJCFLAGS=-fobjc-runtime=gnustep-1.7
 MAKE_FLAGS +=		OPTFLAG="${CFLAGS}"
 .else
 MAKE_FLAGS +=  		CC="${CC}" CPP="${CC} -E" OPTFLAG="${CFLAGS}"
