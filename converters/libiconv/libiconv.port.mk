669415bd148731385a4e802d38414f47fe09a14a
naddy <naddy@openbsd.org>
2016-09-13 19:44:35 +0000

    Remove the converters/libiconv module.  No longer useful after the removal of static architectures.  Ports now use a normal LIB_DEPENDS.

--- converters/libiconv/libiconv.port.mk
+++
@@ -1,8 +0,0 @@
-# $OpenBSD: libiconv.port.mk,v 1.7 2016/09/12 11:52:42 naddy Exp $
-
-# The RUN_DEPENDS entry is to ensure libiconv is installed. This is
-# necessary so that we have charset.alias installed on static archs.
-# Typically installed in PREFIX/lib.
-LIB_DEPENDS +=			converters/libiconv
-RUN_DEPENDS +=			converters/libiconv
-WANTLIB +=			iconv>=2
