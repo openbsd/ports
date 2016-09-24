04c35def2c4902c848e73665a5f7f546d9998665
landry <landry@openbsd.org>
2016-09-24 14:46:45 +0000

    Update to firefox 49.0.1.
    See https://www.mozilla.org/en-US/firefox/49.0/releasenotes/ (.1 is for
    a windows-only fix) - not that 'loop' (ex-firefox hello) was removed
    from the browser.
    - Fixes MFSA 2016-85
    - Remove media/ffvpx/config.h patch, merged upstream/fixed differently in
    bug #1239550
    - Set media.autoplay.enabled to false by default, i'm usually wary of
      changing upstream defaults, but this one is unfuriating with "the
    modern web and all this funky video ads".. this one got support from
    many.
    - Add a blurb to README explaining how to enable/test e10s/multi-process
      support.

--- www/mozilla-firefox/files/all-openbsd.js
+++ www/mozilla-firefox/files/all-openbsd.js
@@ -1,4 +1,5 @@
 // OpenBSD-specific defaults overrides
+pref("media.autoplay.enabled", false);
 pref("browser.safebrowsing.enabled", false);
 pref("browser.safebrowsing.malware.enabled", false);
 pref("spellchecker.dictionary_path", "${LOCALBASE}/share/mozilla-dicts/");
