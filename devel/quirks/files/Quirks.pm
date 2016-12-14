c3f62f40707d33dd8fd5930faee357da7ecf8906
jca <jca@openbsd.org>
2016-12-14 14:21:44 +0000

    Remove security/l0phtcrack
    Reasons include:
    - i386 only
    - full of scary patches and build warnings, embeds old libcrypto code,
      readsmb is a pcap listener that needs to run as root...
    - no licensing information for l0phtcrack itself
    - LANMAN and NTLMv1 auth are so old and braindead it's not even funny.
      Yes, they are still used out there, no, they really shouldn't be.
    
    ok mestre@ sthen@ ajacoutot@

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.439 2016/12/11 14:10:52 patrick Exp $
+# $OpenBSD: Quirks.pm,v 1.440 2016/12/14 14:21:44 jca Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -896,6 +896,7 @@ my $obsolete_reason = {
 	'eclipse-plugin-uml2-sdk' => 2,
 	'eclipse-plugin-wtp-sdk' => 2,
 	'lsof' => 0,
+	'l0phtcrack' => 0,
 };
 
 # reasons for obsolete packages
