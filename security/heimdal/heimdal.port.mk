f2973904805f25c5f3b8340ddf9715b48baef2bf
ajacoutot <ajacoutot@openbsd.org>
2016-12-17 14:58:31 +0000

    Update to heimdal-7.0.3. This is still an RC but the final version is supposed to come out in a couple of days. Committing early to catch regressions and all. Not much depends on this in ports anyway...

--- security/heimdal/heimdal.port.mk
+++ security/heimdal/heimdal.port.mk
@@ -1,15 +1,16 @@
-# $OpenBSD: heimdal.port.mk,v 1.3 2014/12/09 15:55:10 ajacoutot Exp $
+# $OpenBSD: heimdal.port.mk,v 1.4 2016/12/17 14:58:31 ajacoutot Exp $
 
+MODHEIMDAL_WANTLIB +=	com_err pthread util
 MODHEIMDAL_WANTLIB +=	heimdal/lib/asn1
+MODHEIMDAL_WANTLIB +=	heimdal/lib/hcrypto
 MODHEIMDAL_WANTLIB +=	heimdal/lib/heimbase
 MODHEIMDAL_WANTLIB +=	heimdal/lib/hx509
 MODHEIMDAL_WANTLIB +=	heimdal/lib/krb5
 MODHEIMDAL_WANTLIB +=	heimdal/lib/roken
 MODHEIMDAL_WANTLIB +=	heimdal/lib/wind
+WANTLIB +=		${MODHEIMDAL_WANTLIB}
 
 MODHEIMDAL_LIB_DEPENDS=	security/heimdal,-libs
-
 LIB_DEPENDS +=		${MODHEIMDAL_LIB_DEPENDS}
-WANTLIB +=		${MODHEIMDAL_WANTLIB}
 
 MODHEIMDAL_post-patch=	ln -sf ${LOCALBASE}/heimdal/bin/krb5-config ${WRKDIR}/bin/krb5-config
