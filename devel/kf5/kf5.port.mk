c2e48b909208b389a6f4abd48d7ca053003d6372
zhuk <zhuk@openbsd.org>
2016-12-25 14:54:47 +0000

    Update KDE Frameworks to 5.29.0 and sync with Qt5.
    A few tweaks are coming, but things should build at least for now.
    
    Most work done from Rafael Sadowski, thanks!

--- devel/kf5/kf5.port.mk
+++ devel/kf5/kf5.port.mk
@@ -1,6 +1,6 @@
-# $OpenBSD: kf5.port.mk,v 1.1.1.1 2016/05/26 23:58:57 zhuk Exp $
+# $OpenBSD: kf5.port.mk,v 1.2 2016/12/25 14:54:47 zhuk Exp $
 
-MODKF5_VERSION =	5.21.0
+MODKF5_VERSION =	5.29.0
 
 MAINTAINER ?=		KDE porting team <openbsd-kde@googlegroups.com>
 
@@ -24,13 +24,10 @@ CONFIGURE_ARGS += \
 	-DKDE_INSTALL_QTPLUGINDIR=${MODQT_LIBDIR}/plugins \
 	-DKDE_INSTALL_SHAREDSTATEDIR=/var \
 	-DKDE_INSTALL_SYSCONFDIR=/etc \
-	-DMAN_INSTALL_DIR=${PREFIX}/man
+	-DKDE_INSTALL_MANDIR=${PREFIX}/man
 
 # XXX it's very strange this is off by default
 CONFIGURE_ARGS +=	-DALLOW_UNDEFINED_LIB_SYMBOLS=Yes
-
-# shut up CMake
-CONFIGURE_ARGS +=	-DCMAKE_POLICY_DEFAULT_CMP0063=OLD
 .endif
 
 # make sure cmake module preceeds qt5, unless we really want qmake
@@ -39,19 +36,19 @@ MODULES +=		x11/qt5
 # fix {/usr/local,}/etc/{dbus-1,xdg} and friends
 MODKF5_EXAMPLES_DIR =	${PREFIX}/share/examples/kde
 MODKF5_post-install += \
- 	if [ -d ${PREFIX}/etc ]; then \
+	if [ -d ${PREFIX}/etc ]; then \
 		cd ${PREFIX}/etc; \
 		${INSTALL_DATA_DIR} ${MODKF5_EXAMPLES_DIR}; \
 		pax -rw * ${MODKF5_EXAMPLES_DIR}; \
 		rm -Rf ${PREFIX}/etc; \
 	fi; \
- 	if [ -d ${WRKINST}/etc/dbus-1 ]; then \
+	if [ -d ${WRKINST}/etc/dbus-1 ]; then \
 		cd ${WRKINST}/etc; \
 		${INSTALL_DATA_DIR} ${MODKF5_EXAMPLES_DIR}; \
 		pax -rw dbus-1 ${MODKF5_EXAMPLES_DIR}; \
 		rm -Rf ${WRKINST}/etc/dbus-1; \
 	fi; \
- 	if [ -d ${WRKINST}/etc/xdg ]; then \
+	if [ -d ${WRKINST}/etc/xdg ]; then \
 		cd ${WRKINST}/etc; \
 		pax -rw xdg ${PREFIX}/share/examples; \
 		rm -Rf ${WRKINST}/etc/xdg; \
@@ -66,12 +63,12 @@ ALL_LANGS +=	zh_CN zh_TW
 # if needed, mark conflicts with kde-l10n-* packages from KDE 4
 MODKF5_L10N_CONFLICT ?=	no
 .if ${MODKF5_L10N_CONFLICT:L} != "no"
-PKG_ARGS +=		-f ${MAKEFILE_LIST:M*/kf5.port.mk:C,/[^/]+$,,}/PFRAG.l10n
+PKG_ARGS +=	-f ${MAKEFILE_LIST:M*/kf5.port.mk:C,/[^/]+$,,}/PFRAG.l10n
 .endif
 .for _s in ${MULTI_PACKAGES}
 MODKF5_L10N_CONFLICT${_s} ?=	no
 . if ${MODKF5_L10N_CONFLICT${_s}:L} != "no"
-PKG_ARGS${_s} +=	-f ${MAKEFILE_LIST:M*/kf5.port.mk:C,/[^/]+$,,}/PFRAG.l10n
+PKG_ARGS${_s} +=-f ${MAKEFILE_LIST:M*/kf5.port.mk:C,/[^/]+$,,}/PFRAG.l10n
 . endif
 .endfor
 
@@ -82,10 +79,13 @@ MODKF5_post-install += \
 # could not use this in devel/kf5/Makefile.inc because MODKF5_VERSION
 # is not set there yet
 .if ${PKGPATH:Mdevel/kf5/*}
-BUILD_DEPENDS :=	${BUILD_DEPENDS:Mdevel/kf5/*:C,(>=.*)?$,>=${MODKF5_VERSION:R},} \
+BUILD_DEPENDS := \
+	${BUILD_DEPENDS:Mdevel/kf5/*:C,(>=.*)?$,>=${MODKF5_VERSION:R},} \
 			${BUILD_DEPENDS:Ndevel/kf5/*}
-RUN_DEPENDS :=		${RUN_DEPENDS:Mdevel/kf5/*:C,(>=.*)?$,>=${MODKF5_VERSION:R},} \
+RUN_DEPENDS := \
+	${RUN_DEPENDS:Mdevel/kf5/*:C,(>=.*)?$,>=${MODKF5_VERSION:R},} \
 			${RUN_DEPENDS:Ndevel/kf5/*}
-LIB_DEPENDS :=		${LIB_DEPENDS:Mdevel/kf5/*:C,(>=.*)?$,>=${MODKF5_VERSION:R},} \
+LIB_DEPENDS := \
+	${LIB_DEPENDS:Mdevel/kf5/*:C,(>=.*)?$,>=${MODKF5_VERSION:R},} \
 			${LIB_DEPENDS:Ndevel/kf5/*}
 .endif
