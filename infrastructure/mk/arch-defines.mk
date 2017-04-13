e732ccd914c37add84840caaf814fcd60f8a4b33
fcambus <fcambus@openbsd.org>
2017-04-13 11:15:15 +0000

    Remove aviion from architecture constants, the OpenBSD/aviion port has been discontinued after the 5.7 release.
    OK sthen@

--- infrastructure/mk/arch-defines.mk
+++ infrastructure/mk/arch-defines.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: arch-defines.mk,v 1.33 2017/04/10 11:18:28 sthen Exp $
+# $OpenBSD: arch-defines.mk,v 1.34 2017/04/13 11:15:15 fcambus Exp $
 #
 # ex:ts=4 sw=4 filetype=make:
 #
@@ -11,9 +11,8 @@
 
 ARCH ?!= uname -m
 
-ALL_ARCHS = alpha amd64 arm aviion hppa i386 landisk loongson \
-	luna88k m88k macppc mips64 mips64el octeon sgi socppc \
-	sparc64
+ALL_ARCHS = alpha amd64 arm hppa i386 landisk loongson luna88k \
+	m88k macppc mips64 mips64el octeon sgi socppc sparc64
 # not all powerpc have apm(4), hence the use of macppc
 APM_ARCHS = amd64 i386 loongson macppc sparc64
 BE_ARCHS = hppa m88k mips64 powerpc sparc64
@@ -22,7 +21,7 @@ LP64_ARCHS = aarch64 alpha amd64 sparc64 mips64 mips64el
 CLANG_ARCHS = aarch64
 GCC4_ARCHS = alpha amd64 arm armv7 i386 hppa landisk loongson \
 	macppc mips64 mips64el octeon powerpc sgi sh socppc sparc64
-GCC3_ARCHS = aviion luna88k m88k
+GCC3_ARCHS = luna88k m88k
 # XXX easier for ports that depend on mono
 MONO_ARCHS = amd64 i386
 LLVM_ARCHS = aarch64 amd64 arm i386 powerpc mips64 mips64el sparc64
