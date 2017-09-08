2951bba89ececfd9dfe813641179e72f914dfd52
ajacoutot <ajacoutot@openbsd.org>
2017-09-08 10:07:47 +0000

    Register toad's removal.

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.528 2017/09/06 07:22:46 ajacoutot Exp $
+# $OpenBSD: Quirks.pm,v 1.529 2017/09/08 10:07:47 ajacoutot Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -1116,6 +1116,7 @@ my $obsolete_reason = {
 	'banshee' => 3,
 	'mono-zeroconf' => 5,
 	'mono-avahi' => 5,
+	'toad' => 3,
 };
 
 # reasons for obsolete packages
