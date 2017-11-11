f4bd8d057ffa96b971793d534d381713c9b1dc37
steven <steven@openbsd.org>
2017-11-11 10:48:25 +0000

    register removal of gnuvd

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.554 2017/11/09 18:16:58 espie Exp $
+# $OpenBSD: Quirks.pm,v 1.555 2017/11/11 10:48:25 steven Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -1154,6 +1154,7 @@ my $obsolete_reason = {
 	'hs-GLUT' => 6,
 	'hs-OpenGL' => 6,
 	'hs-OpenGLRaw' => 6,
+	'gnuvd' => 6,
 };
 
 # reasons for obsolete packages
