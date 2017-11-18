adfe0960ab61e1d72f29c56eae51391c673c6f58
naddy <naddy@openbsd.org>
2017-11-18 22:23:59 +0000

    Move BUILD_DEPENDS+=devel/gettext-tools out of the gettext module and into those ports that actually require it.

--- devel/gettext/gettext.port.mk
+++ devel/gettext/gettext.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: gettext.port.mk,v 1.15 2017/11/06 20:43:22 naddy Exp $
+# $OpenBSD: gettext.port.mk,v 1.16 2017/11/18 22:23:59 naddy Exp $
 
 _MODGETTEXT_SPEC =		devel/gettext>=0.10.38
 
@@ -9,6 +9,3 @@ LIB_DEPENDS +=		${_MODGETTEXT_SPEC} converters/libiconv
 BUILD_DEPENDS +=	${_MODGETTEXT_SPEC}
 RUN_DEPENDS +=		${_MODGETTEXT_SPEC}
 WANTLIB +=		intl>=5 iconv>=6
-
-# Always provide the development tools: msgfmt(1) etc.
-BUILD_DEPENDS +=	devel/gettext-tools
