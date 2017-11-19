ecda180b889915cbfb1f0a4c7d52050ab693b2ff
naddy <naddy@openbsd.org>
2017-11-19 16:17:16 +0000

    drop redundant BUILD_DEPENDS

--- devel/gettext/gettext.port.mk
+++ devel/gettext/gettext.port.mk
@@ -1,11 +1,8 @@
-# $OpenBSD: gettext.port.mk,v 1.16 2017/11/18 22:23:59 naddy Exp $
-
-_MODGETTEXT_SPEC =		devel/gettext>=0.10.38
+# $OpenBSD: gettext.port.mk,v 1.17 2017/11/19 16:17:16 naddy Exp $
 
 # The RUN_DEPENDS entry is to ensure gettext is installed. This is
 # necessary so that we have locale.alias installed on static archs.
 # Typically installed in PREFIX/share/locale.
-LIB_DEPENDS +=		${_MODGETTEXT_SPEC} converters/libiconv
-BUILD_DEPENDS +=	${_MODGETTEXT_SPEC}
-RUN_DEPENDS +=		${_MODGETTEXT_SPEC}
+LIB_DEPENDS +=		devel/gettext>=0.10.38 converters/libiconv
+RUN_DEPENDS +=		devel/gettext>=0.10.38
 WANTLIB +=		intl>=5 iconv>=6
