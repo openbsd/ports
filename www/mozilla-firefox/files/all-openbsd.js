6c4c50522cb90a533efaa2d97122d3da1e13fc99
landry <landry@openbsd.org>
2017-12-15 21:03:58 +0000

    Out-out from shield/studies by default in new profiles.
    For existing profiles, visit about:preferences#privacy to disable it (cf
    https://support.mozilla.org/en-US/kb/shield)
    
    Users just want to browse the web, not be opted-in by default to random UI
    experiments. See https://wiki.mozilla.org/Firefox/Shield/Shield_Studies
    for details.
    
    While i usually don't like diverting from upstream defaults, something
    that automagically loads unwanted add-ons isn't right, so i'm making an
    exception here.

--- www/mozilla-firefox/files/all-openbsd.js
+++ www/mozilla-firefox/files/all-openbsd.js
@@ -1,5 +1,6 @@
 // OpenBSD-specific defaults overrides
 pref("media.autoplay.enabled", false);
+pref("app.shield.optoutstudies.enabled", false);
 pref("browser.safebrowsing.enabled", false);
 pref("browser.safebrowsing.malware.enabled", false);
 pref("spellchecker.dictionary_path", "${LOCALBASE}/share/mozilla-dicts/");
