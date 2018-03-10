af7fc5853f742ec28d6b9efca578ea642c6507e7
jasper <jasper@openbsd.org>
2018-03-10 12:50:18 +0000

    register xerces removal

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.580 2018/03/07 13:34:09 juanfra Exp $
+# $OpenBSD: Quirks.pm,v 1.581 2018/03/10 12:50:18 jasper Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -1237,6 +1237,7 @@ my $obsolete_reason = {
 	'p5-Net-LDNS' => 6,
 	'sirc' => 0,
 	'hgview' => 6,
+	'xerces' => 5,
 };
 
 # reasons for obsolete packages
