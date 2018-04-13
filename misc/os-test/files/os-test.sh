3b7d53380918cefc00b67bd5150c29c1392fe550
bluhm <bluhm@openbsd.org>
2018-04-13 15:38:15 +0000

    import os-test 0.000041 OK sthen@
    Prepare to run os-test on a regular basis as a OpenBSD regression
    test by providing the software as a port.  As it was not designed
    to be installed as a package, implement the runtime environment in
    do-install.  The port specific tool os-test uses the installed files
    from /usr/local to run the tests and creates the html output in the
    current directory.
    
    Comment:
    test suites for POSIX operating systems
    
    Description:
    os-test is a set of test suites for POSIX operating systems designed
    to make it easy to compare differences between operating systems
    and to find operating system bugs.  It consists of test suites that
    focus on different operating system areas.

---
+++ misc/os-test/files/os-test.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -e
+
+libexec_dir=${LOCALBASE}/libexec/os-test
+libdata_dir=${LOCALBASE}/libdata/os-test
+
+uname -srm > uname.out
+
+for suite in `cat $libdata_dir/suite.list`; do
+	rm -rf -- $suite $suite.expect
+	mkdir $suite $suite.expect
+	cp $libdata_dir/$suite/README $suite/
+	for test in `cat $libdata_dir/$suite-test.list`; do
+		echo -n . >&2
+		set +e
+		$libexec_dir/$suite/$test > $suite/$test.out 2>&1
+		code=$?
+		set -e
+		if [ ! -s $suite/$test.out ] || [ 2 -le $code ]; then
+			echo "exit: $code" >> $suite/$test.out
+		fi
+		cp $libdata_dir/$suite/$test.c $suite/
+		cp $libdata_dir/$suite.expect/$test* $suite.expect/
+	done
+	echo >&2
+done
+
+${LOCALBASE}/bin/os-test-html --enable-legend --enable-suites-overview \
+    --suite-list "`cat $libdata_dir/suite.list`" > os-test.html
