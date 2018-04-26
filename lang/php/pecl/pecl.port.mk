384af9db117d1ead176cc87f57ff52f88d1f9b3e
sthen <sthen@openbsd.org>
2018-04-26 15:08:56 +0000

    move pecl to multi-version; packages are now named pecl56-foo and pecl70-foo for the relevant PHP versions.
    ports which are 5.6-only remain in pecl-foo port directories, set to only
    build 5.6 packages
    
    ports which have separate versions for 5.6 and 7.0 are split; the port
    directories are named pecl56-foo for the 5.6-compatible version and
    pecl-foo for the 7+-version
    
    normal ports are in pecl-foo port directories with flavours for 5.6 and 7.0

--- lang/php/pecl/pecl.port.mk
+++ lang/php/pecl/pecl.port.mk
@@ -1,16 +1,43 @@
-# $OpenBSD: pecl.port.mk,v 1.7 2018/04/24 17:36:51 sthen Exp $
+# $OpenBSD: pecl.port.mk,v 1.8 2018/04/26 15:08:56 sthen Exp $
 # PHP PECL module
 
 MODULES +=	lang/php
 
+.if defined(MODPECL_V)
+.  if ${MODPECL_V} == 5.6
+FLAVORS = php56
+FLAVOR = php56
+.  elif ${MODPECL_V} == 7
+FLAVORS = php70
+FLAVOR = php70
+.  endif
+.else
+FLAVORS ?= php56 php70
+FLAVOR ?= php56
+.endif
+
+.if ${FLAVOR} == php56
+MODPHP_VERSION = 5.6
+MODPHP_FLAVOR = ,php56
+MODPECL_56ONLY =
+.elif ${FLAVOR} == php70
+MODPHP_VERSION = 7.0
+MODPHP_FLAVOR = ,php70
+MODPECL_56ONLY = "@comment "
+.endif
+
 CATEGORIES +=	www
 
-PKGNAME ?=	pecl-${DISTNAME:S/pecl-//:S/_/-/:L}
+_PECL_PREFIX =	pecl${MODPHP_VERSION:S/.//}
+PKGNAME ?=	${_PECL_PREFIX}-${DISTNAME:S/pecl-//:L}
+FULLPKGNAME ?=	${PKGNAME}
 _PECLMOD ?=	${DISTNAME:S/pecl-//:C/-[0-9].*//:L}
+
+SUBST_VARS +=	MODPECL_56ONLY
 
 .if !defined(MASTER_SITES)
 MASTER_SITES ?=	https://pecl.php.net/get/
-HOMEPAGE ?=	http://pecl.php.net/package/${_PECLMOD}
+HOMEPAGE ?=	https://pecl.php.net/package/${_PECLMOD}
 EXTRACT_SUFX ?=	.tgz
 .endif
 
