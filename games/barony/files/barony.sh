c94e3983e5cb782f42ebd4409c33268811be1e9d
ajacoutot <ajacoutot@openbsd.org>
2018-05-05 08:23:17 +0000

    Unbreak error handling in the wrapper script (it was exiting the subshell instead of the current one).
    ok solene@

--- games/barony/files/barony.sh
+++ games/barony/files/barony.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mkdir -p ~/.barony
-cd ~/.barony || (echo "Can't cd into ~/.barony" >&2; exit 1)
+cd ~/.barony || { echo "Can't cd into ~/.barony" >&2; exit 1; }
 exec ${TRUEPREFIX}/bin/barony-bin $@
