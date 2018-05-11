c71758479f71e25fe5cf2254043d7abfc71681ec
landry <landry@openbsd.org>
2018-05-11 20:00:57 +0000

    Update to firefox 60.
    See https://www.mozilla.org/en-US/firefox/60.0/releasenotes/
    Fixes https://www.mozilla.org/en-US/security/advisories/mfsa2018-11/
    
    - Add initial/wip pledge() support, with distinct subsets for main &
    content processes. The promises are runtime-configurable for now via
    about:config knobs to ease debugging/testing. They're printed on stderr
    when applied by the sandboxing codepaths (cf
    patch-toolkit_xre_nsAppRunner_cpp and patch-dom_ipc_ContentChild_cpp).
    
    - Add a bunch of patches (patch-widget_nsShmImage_cpp,
      patch-xpcom_base_nsDebugImpl_cpp, patch-mozglue_misc_TimeStamp_posix_cpp and
    patch-xpcom_base_nsMemoryReporterManager_cpp) to neuter non-critical
    codepaths that calls pledge-forbidden syscalls.
    
    - All this pledge() stuff being currently discussed upstream in
    https://bugzilla.mozilla.org/show_bug.cgi?id=1457092 and done at p2k18.
    Been running with it since then, and i'm fairly confident the pledge
    subsets i come up with are now enough for most usages.
    
    If you encounter crashes due to pledge, look into your kernel log, and
    try to figure out what missing pledge is needed or what firefox codepath
    hits it.
    
    So far i know 'getpw' might be needed when uploading files but i havent
    hit it, and 'proc' might be needed by the content process when there's
    no dbus daemon running, but they're not needed in the 'common case', and
    too broad.
    
    - While here, tweak defaults to disable the pocket extension, and try to
    disable the activity-stream/sponsored content that is being shown by
    default on the new tab page in new profiles. Stop the madness, plz.

--- www/mozilla-firefox/files/all-openbsd.js
+++ www/mozilla-firefox/files/all-openbsd.js
@@ -1,6 +1,14 @@
 // OpenBSD-specific defaults overrides
 pref("media.autoplay.enabled", false);
 pref("app.shield.optoutstudies.enabled", false);
+pref("app.normandy.enabled",false);
 pref("browser.safebrowsing.enabled", false);
 pref("browser.safebrowsing.malware.enabled", false);
 pref("spellchecker.dictionary_path", "${LOCALBASE}/share/mozilla-dicts/");
+// enable pledging the content process
+pref("security.sandbox.content.level", 1);
+pref("security.sandbox.pledge.main","stdio rpath wpath cpath inet proc exec prot_exec flock ps sendfd recvfd dns vminfo tty drm unix fattr");
+pref("security.sandbox.pledge.content","stdio rpath wpath cpath inet recvfd sendfd prot_exec unix drm ps");
+pref("extensions.pocket.enabled", false);
+pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+pref("browser.newtabpage.activity-stream.feeds.snippets", false);
