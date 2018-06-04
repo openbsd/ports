c4114c4a655be5e264505f0a8f5ad3d6819f5bb7
rsadowski <rsadowski@openbsd.org>
2018-06-04 20:25:31 +0000

    Update KDE Releases Frameworks to 5.46.0
    Changelogs;
    
    https://www.kde.org/announcements/kde-frameworks-5.44.0.php
    https://www.kde.org/announcements/kde-frameworks-5.45.0.php
    https://www.kde.org/announcements/kde-frameworks-5.46.0.php

--- devel/kf5/kf5.port.mk
+++ devel/kf5/kf5.port.mk
@@ -1,6 +1,6 @@
-# $OpenBSD: kf5.port.mk,v 1.8 2018/02/26 20:33:06 rsadowski Exp $
+# $OpenBSD: kf5.port.mk,v 1.9 2018/06/04 20:25:31 rsadowski Exp $
 
-MODKF5_VERSION =	5.43.0
+MODKF5_VERSION =	5.46.0
 
 MAINTAINER ?=		KDE porting team <openbsd-kde@googlegroups.com>
 
