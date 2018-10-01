2cfdd718b25abbf9190547c390f9d3834730ab9f
danj <danj@openbsd.org>
2018-10-01 01:06:43 +0000

    Register py-pcs removal
    I doubt anyone still has it installed, but a quirk is cheap.

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.629 2018/09/23 17:09:18 awolk Exp $
+# $OpenBSD: Quirks.pm,v 1.630 2018/10/01 01:06:43 danj Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -1056,6 +1056,7 @@ my $obsolete_reason = {
 	'dkim-milter' => 10,
 	'aircontrol' => 11,
 	'corebird' => 3,
+	'py-pcs' => 5,
 };
 
 # reasons for obsolete packages
