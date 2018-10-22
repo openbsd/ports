e6b8da349b81ad938465862f8b20141b96391909
naddy <naddy@openbsd.org>
2018-10-22 20:27:44 +0000

    Remove sysutils/memtest86+. It would require work to adapt to lld, but it doesn't even work on many recent-ish systems and "being able to build this from OpenBSD doesn't seem particularly useful". ok kn@ sthen@

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.640 2018/10/22 15:18:56 jeremy Exp $
+# $OpenBSD: Quirks.pm,v 1.641 2018/10/22 20:27:44 naddy Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -1061,6 +1061,7 @@ my $obsolete_reason = {
 # 6.5
 	'py-dtopt' => 5,
 	'py3-dtopt' => 5,
+	'memtest86+' => 0,
 };
 
 # reasons for obsolete packages
