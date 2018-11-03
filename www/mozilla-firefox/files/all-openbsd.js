0fb7ae19a02ff43c8ee409e3f770ba63f0843c60
landry <landry@openbsd.org>
2018-11-03 21:45:52 +0000

    Bugfix update to firefox 63.0.1.
    See https://www.mozilla.org/en-US/firefox/63.0.1/releasenotes/
    - fix build with cbindgen 0.6.7
    - really disable media autoplay by default, the knob changed (cf #1470082)
    - use about:blank as default homepage/new tab page in new profiles.
    
    Rationale: some parts of Activity Stream (the new Firefox Home) are nice
    (ie searchbox, highlights, topsites from browsing history, etc) but
    'snippets' is invasive (them being broken is a driver for this version,
    cf #1503047, where comments are.. worrying), as is 'Recommended by
    pocket' content shown to some countries (DE, CA, US..) - sadly, those
    two sections cant easily be disabled (cf #1504279) on about:newtab, so
    use the about:blank big hammer for privacy. Existing profiles still
    using the default 'Firefox Home' are advised to visit
    about:preferences#home and uncheck 'snippets' and 'pages saved to
    pocket'... and read https://www.mozilla.org/en-US/privacy/firefox/.
    
    While here add a section in pkg/README advising users using NIS or with
    profiles on NFS to add getpw to the content process pledge, as it's
    apparently needed in those conditions.

--- www/mozilla-firefox/files/all-openbsd.js
+++ www/mozilla-firefox/files/all-openbsd.js
@@ -1,8 +1,5 @@
 // OpenBSD-specific defaults overrides
-// Remove in 63 as its configurable in the UI
-// and the preference key changes
-// cf https://bugzilla.mozilla.org/show_bug.cgi?id=1470082
-pref("media.autoplay.enabled", false);
+pref("media.autoplay.default", 1);
 pref("app.shield.optoutstudies.enabled", false);
 pref("app.normandy.enabled",false);
 pref("browser.safebrowsing.enabled", false);
@@ -13,5 +10,5 @@ pref("security.sandbox.content.level", 1);
 pref("security.sandbox.pledge.main","stdio rpath wpath cpath inet proc exec prot_exec flock ps sendfd recvfd dns vminfo tty drm unix fattr getpw mcast");
 pref("security.sandbox.pledge.content","stdio rpath wpath cpath inet recvfd sendfd prot_exec unix drm ps");
 pref("extensions.pocket.enabled", false);
-pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
-pref("browser.newtabpage.activity-stream.feeds.snippets", false);
+pref("browser.newtabpage.enabled", false);
+pref("browser.startup.homepage", "about:blank");
