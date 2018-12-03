432418389e7e2f121a5ef3b73278f8f015ad3283
sthen <sthen@openbsd.org>
2018-12-03 18:40:07 +0000

    update to mupdf-1.14.0, some notable changes:
    - mupdf-gl now requires building with javascript support, so is no longer
    available in the default flavour (which has js disabled). If you use mupdf-gl
    you will need to switch to the js flavour (pkg_delete mupdf; pkg_add mupdf--js).
    
    - drop the patch to convert to shared libraries, the build process of
    mupdf is a headache and changes rather often, so this patch is getting in
    the way too much. unfortunately this bloats the packages somewhat...

---
+++ textproc/mupdf/pkg/PFRAG.js
@@ -0,0 +1,12 @@
+@comment $OpenBSD: PFRAG.js,v 1.1 2018/12/03 18:40:07 sthen Exp $
+@bin bin/mupdf-gl
+share/examples/mupdf/bbox-device.js
+share/examples/mupdf/create-thumbnail.js
+share/examples/mupdf/draw-device.js
+share/examples/mupdf/draw-document.js
+share/examples/mupdf/fix-s22pdf.js
+share/examples/mupdf/jpx-to-pdf.js
+share/examples/mupdf/pdf-create-lowlevel.js
+share/examples/mupdf/pdf-create.js
+share/examples/mupdf/pdf-merge.js
+share/examples/mupdf/trace-device.js
