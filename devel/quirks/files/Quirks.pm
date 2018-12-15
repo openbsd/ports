453877888bd36d426fc82b50a913787d16ef1724
danj <danj@openbsd.org>
2018-12-15 20:25:54 +0000

    Update to haproxy-1.8.15
    2 CVEs are fixed with this update

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.680 2018/12/15 15:37:28 danj Exp $
+# $OpenBSD: Quirks.pm,v 1.681 2018/12/15 20:25:54 danj Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -1248,6 +1248,7 @@ my $cve = {
 	'mail/p5-Mail-SpamAssassin' => 'p5-Mail-SpamAssassin-<3.4.2',
 	'mail/roundcubemail' => 'roundcubemail-<1.3.8',
 	'net/curl' => 'curl-<7.62.0',
+	'net/haproxy' => 'haproxy-<1.8.15',
 	'net/icecast' => 'icecast-<2.4.4',
 	'net/isc-bind' => 'isc-bind-<9.11.4pl2',
 	'net/lldpd' => 'lldpd-<0.7.18p0',
