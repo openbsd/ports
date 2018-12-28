e26a1161d1128016bf0fea2d4c98131acebb7586
feinerer <feinerer@openbsd.org>
2018-12-28 18:41:34 +0000

    Add math/hdf5 to CVE table

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.691 2018/12/27 21:31:16 danj Exp $
+# $OpenBSD: Quirks.pm,v 1.692 2018/12/28 18:41:34 feinerer Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -1257,6 +1257,7 @@ my $cve = {
 	'mail/exim' => 'exim-<4.83',
 	'mail/p5-Mail-SpamAssassin' => 'p5-Mail-SpamAssassin-<3.4.2',
 	'mail/roundcubemail' => 'roundcubemail-<1.3.8',
+	'math/hdf5' => 'hdf5-<1.8.21',
 	'net/curl' => 'curl-<7.62.0',
 	'net/haproxy' => 'haproxy-<1.8.16',
 	'net/icecast' => 'icecast-<2.4.4',
