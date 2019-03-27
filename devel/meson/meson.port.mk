71b1244b2fe41581964cd9a1d574edac49b7cabe
ajacoutot <ajacoutot@openbsd.org>
2019-03-27 23:26:01 +0000

    Refactor the devel/meson MODULE. - Add MODMESON_CONFIGURE_ENV and MODMESON_CONFIGURE_ARGS and append them to their CONFIGURE_* counterparts if CONFIGURE_STYLE == meson - set --auto-features=enabled and --wrap-mode=nodownload by default - set --buildtype=debug when DEBUG is set

--- devel/meson/meson.port.mk
+++ devel/meson/meson.port.mk
@@ -1,23 +1,10 @@
-# $OpenBSD: meson.port.mk,v 1.35 2019/03/11 16:08:26 ajacoutot Exp $
+# $OpenBSD: meson.port.mk,v 1.36 2019/03/27 23:26:01 ajacoutot Exp $
 
 BUILD_DEPENDS +=	devel/meson>=0.50.0v0
 SEPARATE_BUILD ?=	Yes
 
-MODMESON_WANTCOLOR ?=	No
-.if ${MODMESON_WANTCOLOR:L} == "no"
-CONFIGURE_ENV += TERM="dumb"
-.endif
-
-.for solib sover in ${SHARED_LIBS}
-CONFIGURE_ENV += LIB${solib}_VERSION=${sover}
-.endfor
-
 .if empty(CONFIGURE_STYLE)
 CONFIGURE_STYLE=	meson
-.endif
-
-.if ! empty(INSTALL_STRIP)
-CONFIGURE_ARGS +=	--strip
 .endif
 
 # from ${LOCALBASE}/bin/meson:
@@ -25,21 +12,47 @@ CONFIGURE_ARGS +=	--strip
 # such as os.stat not being able to decode filenames with unicode in them.
 # There is no way to reset both the preferred encoding and the filesystem
 # encoding, so we can just warn about it.
+CONFIGURE_ENV +=	LC_CTYPE="en_US.UTF-8"
 MAKE_ENV +=		LC_CTYPE="en_US.UTF-8"
 
 # don't pick up llvm-ar(1)
 CONFIGURE_ENV +=	AR="ar"
 
-MODMESON_configure=	${SETENV} CC="${CC}" CFLAGS="${CFLAGS}" CXX="${CXX}" \
-				CXXFLAGS="${CXXFLAGS}" LDFLAGS="${LDFLAGS}" \
-				LC_CTYPE="en_US.UTF-8" ${CONFIGURE_ENV} \
-				${LOCALBASE}/bin/meson --buildtype=plain \
-				--prefix "${PREFIX}" --mandir="${PREFIX}/man" \
+MODMESON_CONFIGURE_ENV +=	CC="${CC}" CFLAGS="${CFLAGS}" \
+				CXX="${CXX}" CXXFLAGS="${CXXFLAGS}"
+
+MODMESON_CONFIGURE_ARGS +=	--prefix "${PREFIX}" \
+				--mandir="${PREFIX}/man" \
 				--sysconfdir="${SYSCONFDIR}" \
 				--localstatedir="${LOCALSTATEDIR}" \
 				--sharedstatedir="/var/db" \
-				${CONFIGURE_ARGS} ${WRKSRC} ${WRKBUILD}
+				--auto-features=enabled \
+				--wrap-mode=nodownload
 
+MODMESON_WANTCOLOR ?=	No
+.if ${MODMESON_WANTCOLOR:L} == "no"
+MODMESON_CONFIGURE_ENV +=	TERM="dumb"
+.endif
+
+.for solib sover in ${SHARED_LIBS}
+MODMESON_CONFIGURE_ENV +=	LIB${solib}_VERSION=${sover}
+.endfor
+
+.if empty(DEBUG)
+MODMESON_CONFIGURE_ARGS +=	--buildtype=plain
+.else
+MODMESON_CONFIGURE_ARGS +=	--buildtype=debug
+.endif
+
+.if ! empty(INSTALL_STRIP)
+MODMESON_CONFIGURE_ARGS +=	--strip
+.endif
+
+.if ${CONFIGURE_STYLE} == "meson"
+CONFIGURE_ARGS +=	${MODMESON_CONFIGURE_ARGS}
+CONFIGURE_ENV +=	${MODMESON_CONFIGURE_ENV}
+MODMESON_configure=	${SETENV} ${CONFIGURE_ENV} ${LOCALBASE}/bin/meson \
+				${CONFIGURE_ARGS} ${WRKSRC} ${WRKBUILD}
 .if !target(do-build)
 do-build:
 	exec ${SETENV} ${MAKE_ENV} \
@@ -56,4 +69,5 @@ do-install:
 do-test:
 	exec ${SETENV} ${ALL_TEST_ENV} \
 		${LOCALBASE}/bin/ninja -C ${WRKBUILD} ${TEST_TARGET}
+.endif
 .endif
