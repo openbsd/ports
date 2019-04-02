365539f3ddf60a1da228d9e347d5f5681515b626
sthen <sthen@openbsd.org>
2019-04-02 09:45:23 +0000

    PREFIX->TRUEPREFIX, problem reported by Robert Klein

--- net/freeradius3/files/freeradius-enable.sh
+++ net/freeradius3/files/freeradius-enable.sh
@@ -18,9 +18,9 @@ echo '===>  Copying initial configuration'
 ln -s mods-config/preprocess/hints ${FREERADIUS_ETC}/hints
 ln -s mods-config/preprocess/huntgroups ${FREERADIUS_ETC}/huntgroups
 ln -s mods-config/files/authorize ${FREERADIUS_ETC}/users
-cp -R ${PREFIX}/share/examples/freeradius/mods-enabled ${FREERADIUS_ETC}/
-cp -R ${PREFIX}/share/examples/freeradius/sites-enabled ${FREERADIUS_ETC}/
+cp -R ${TRUEPREFIX}/share/examples/freeradius/mods-enabled ${FREERADIUS_ETC}/
+cp -R ${TRUEPREFIX}/share/examples/freeradius/sites-enabled ${FREERADIUS_ETC}/
 echo '===>  Generating self-signed keys, this will take a few minutes'
 su -l -s /bin/sh _freeradius -c 'umask 007; ${FREERADIUS_ETC}/certs/bootstrap > /dev/null'
 echo '===>  Please note: to avoid conflicting with radiusd in base,'
-echo "      for debug mode use the full path: ${PREFIX}/sbin/radiusd -X"
+echo "      for debug mode use the full path: ${TRUEPREFIX}/sbin/radiusd -X"
