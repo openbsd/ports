0dd964804f1dca0222072b1a5e809cd80c503bd1
jasper <jasper@openbsd.org>
2019-04-29 17:48:38 +0000

    register {gnome,gedit}-code-assistance removal

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.757 2019/04/29 15:10:15 naddy Exp $
+# $OpenBSD: Quirks.pm,v 1.758 2019/04/29 17:48:38 jasper Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -1219,6 +1219,8 @@ my $obsolete_reason = {
 	'gcj' => 5,
 	'classpath' => 5,
 	'jamvm' => 5,
+	'gnome-code-assistance' => 3,
+	'gedit-code-assistance' => 3,
 };
 
 # reasons for obsolete packages
