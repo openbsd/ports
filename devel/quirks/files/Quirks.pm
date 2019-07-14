29bc9ca41dcd0e0e57cbed11297adcc7f35b0059
kmos <kmos@openbsd.org>
2019-07-14 20:02:02 +0000

    FreeBSD removed this port in June with the reasoning that the distfile was only available in the directory of one of their committers who's been inactive since 2001. It's not licensed such that it can be packaged.
    OK rsadowski@ jca@

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.789 2019/07/09 17:29:39 gsoares Exp $
+# $OpenBSD: Quirks.pm,v 1.790 2019/07/14 20:02:02 kmos Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -1258,6 +1258,7 @@ my $obsolete_reason = {
 	'py-amf' => 6,
 	'lam' => 3,
 	'gxml' => 6,
+	'nulib' => 3,
 };
 
 # reasons for obsolete packages
