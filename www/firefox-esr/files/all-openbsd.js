562ce4721ad6d89a147502b6151946f0640a15c4
landry <landry@openbsd.org>
2019-09-03 16:31:12 +0000

    Update firefox-esr to 68.1.0.
    See https://www.mozilla.org/en-US/firefox/68.1.0/releasenotes/
    Fixes https://www.mozilla.org/en-US/security/advisories/mfsa2019-26/
    
    Basically a copy of what was under www/mozilla-firefox, as 60esr will
    soon be EOL, and we want to track 68esr in 6.6-stable.
    
    As a side-effect, esr now uses pledge like mainline firefox.
    
    The bn-BD & bn-IN lackpacks were merged into bn.

--- www/firefox-esr/files/all-openbsd.js
+++ www/firefox-esr/files/all-openbsd.js
@@ -1,4 +1,14 @@
 // OpenBSD-specific defaults overrides
+pref("media.autoplay.default", 1);
+pref("app.shield.optoutstudies.enabled", false);
+pref("app.normandy.enabled",false);
 pref("browser.safebrowsing.enabled", false);
 pref("browser.safebrowsing.malware.enabled", false);
 pref("spellchecker.dictionary_path", "${LOCALBASE}/share/mozilla-dicts/");
+// enable pledging the content process
+pref("security.sandbox.content.level", 1);
+pref("security.sandbox.pledge.main","stdio rpath wpath cpath inet proc exec prot_exec flock ps sendfd recvfd dns vminfo tty drm unix fattr getpw mcast video");
+pref("security.sandbox.pledge.content","stdio rpath wpath cpath inet recvfd sendfd prot_exec unix drm ps");
+pref("extensions.pocket.enabled", false);
+pref("browser.newtabpage.enabled", false);
+pref("browser.startup.homepage", "about:blank");
