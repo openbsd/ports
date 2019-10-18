a47efc02149be7a063e40acc81a7af1a1e570821
kmos <kmos@openbsd.org>
2019-10-18 13:08:05 +0000

    Retire Python 3.6
    ok sthen

--- lang/python/python.port.mk
+++ lang/python/python.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: python.port.mk,v 1.115 2019/09/25 14:34:13 kmos Exp $
+# $OpenBSD: python.port.mk,v 1.116 2019/10/18 13:08:05 kmos Exp $
 #
 #	python.port.mk - Xavier Santolaria <xavier@santolaria.net>
 #	This file is in the public domain.
@@ -24,7 +24,6 @@ MODPY_VERSION ?=	${MODPY_DEFAULT_VERSION_2}
 # verify if MODPY_VERSION forced is correct
 .else
 .  if ${MODPY_VERSION} != "2.7" && \
-      ${MODPY_VERSION} != "3.6" && \
       ${MODPY_VERSION} != "3.7"
 ERRORS += "Fatal: unknown or unsupported MODPY_VERSION: ${MODPY_VERSION}"
 .  endif
