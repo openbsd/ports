81d404bd6cc14eb8c47a94492198a3db872495f8
semarie <semarie@openbsd.org>
2019-10-29 10:22:25 +0000

    mail/thunderbird: unbreak sandbox by using invalid pledge(2) promise instead of valid empty string
    jcs@ noted that sandbox should be only enabled with --enable-sandbox (what
    thunderbird doesn't use).
    
    sets the promises for now to permit quick fix, to permit users to figure what
    pledge promises are needed without rebuilding the whole package.
    
    ok landry@ sthen@

--- mail/mozilla-thunderbird/files/all-openbsd.js
+++ mail/mozilla-thunderbird/files/all-openbsd.js
@@ -2,3 +2,6 @@
 // enable systemwide extensions by default
 pref("extensions.autoDisableScopes", 3);
 pref("spellchecker.dictionary_path", "${LOCALBASE}/share/mozilla-dicts/");
+// quick fix to effectively disable sandbox for now
+pref("security.sandbox.pledge.main", "notyet");
+pref("security.sandbox.pledge.content", "notyet");
