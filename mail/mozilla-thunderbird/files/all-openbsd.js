65d87a5a658932019d1cc18f05bd5997e0e45203
landry <landry@openbsd.org>
2019-11-01 08:38:06 +0000

    Update to thunderbird 68.2.1.
    See https://www.thunderbird.net/en-US/thunderbird/68.2.1/releasenotes/
    
    Use --disable-sandbox in CONFIGURE_ARGS instead of setting invalid
    pledge strings, thunderbird is a monolithic process so far anyway so its
    pledge would be super wide.. to be revisited by someone someday ?

--- mail/mozilla-thunderbird/files/all-openbsd.js
+++ mail/mozilla-thunderbird/files/all-openbsd.js
@@ -2,6 +2,3 @@
 // enable systemwide extensions by default
 pref("extensions.autoDisableScopes", 3);
 pref("spellchecker.dictionary_path", "${LOCALBASE}/share/mozilla-dicts/");
-// quick fix to effectively disable sandbox for now
-pref("security.sandbox.pledge.main", "notyet");
-pref("security.sandbox.pledge.content", "notyet");
