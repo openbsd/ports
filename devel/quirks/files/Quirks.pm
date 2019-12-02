98ccc023dd960d544d9a115a1c620e0f2576373c
kmos <kmos@openbsd.org>
2019-12-02 03:38:56 +0000

    Remove net/py-oauth2
    This port is python2-only and has no consumers.
    
    ok bcallah

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.834 2019/11/27 14:16:20 kmos Exp $
+# $OpenBSD: Quirks.pm,v 1.835 2019/12/02 03:38:56 kmos Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -1346,6 +1346,7 @@ my $obsolete_reason = {
 	'terraform-provider-softlayer' => 3,
 	'py-elementtidy' => 5,
 	'py-gd' => 5,
+	'py-oauth2' => 5,
 };
 
 # reasons for obsolete packages
