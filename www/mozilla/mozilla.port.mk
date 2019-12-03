0a8dfddd741dd7a644c1f3ad165231c136506fd4
landry <landry@openbsd.org>
2019-12-03 16:49:45 +0000

    Bump dependency to nss 3.47.1 for gecko 71.

--- www/mozilla/mozilla.port.mk
+++ www/mozilla/mozilla.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: mozilla.port.mk,v 1.127 2019/09/22 17:19:06 sthen Exp $
+# $OpenBSD: mozilla.port.mk,v 1.128 2019/12/03 16:49:45 landry Exp $
 
 # ppc: firefox-esr/thunderbird xpcshell segfaults during startup compilation
 # ppc: seamonkey/firefox - failure to link for atomic ops on 64 bits
@@ -61,7 +61,7 @@ MODMOZ_BUILD_DEPENDS =	devel/autoconf/2.13 \
 			archivers/zip>=2.3
 
 .if !defined(MOZILLA_USE_BUNDLED_NSS)
-MODMOZ_LIB_DEPENDS +=	security/nss>=3.45
+MODMOZ_LIB_DEPENDS +=	security/nss>=3.47.1
 MODMOZ_WANTLIB +=	nss3 nssutil3 smime3 ssl3
 CONFIGURE_ARGS +=	--with-system-nss
 .endif
