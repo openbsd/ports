37dd5b140e2e79b6c30a5770e24e7c874e247b79
rpointel <rpointel@openbsd.org>
2020-03-20 07:47:24 +0000

    +       'py-bencode' => 'py3-bencode', +       'py-dfwinreg' => 'py3-dfwinreg', +       'py-dfvfs' => 'py3-dfvfs', +       'py-dfdatetime' => 'py3-dfdatetime', +       'py-artifacts' => 'py3-artifacts',
    ok landry@.

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.907 2020/03/19 18:44:55 kn Exp $
+# $OpenBSD: Quirks.pm,v 1.908 2020/03/20 07:47:24 rpointel Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -390,6 +390,11 @@ my $stem_extensions = {
 	'py-nbconvert' => 'py3-nbconvert',
 	'py-smbc' => 'py3-smbc',
 	'py-git2' => 'py3-git2',
+	'py-bencode' => 'py3-bencode',
+	'py-dfwinreg' => 'py3-dfwinreg',
+	'py-dfvfs' => 'py3-dfvfs',
+	'py-dfdatetime' => 'py3-dfdatetime',
+	'py-artifacts' => 'py3-artifacts',
 };
 
 my $obsolete_reason = {
