834f9c0e241fbc63d1ceb942001add758add8245
jasper <jasper@openbsd.org>
2020-03-23 17:15:11 +0000

    register radiotray removal

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.913 2020/03/23 14:35:06 jasper Exp $
+# $OpenBSD: Quirks.pm,v 1.914 2020/03/23 17:15:11 jasper Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -1449,6 +1449,7 @@ my $obsolete_reason = {
 	'mirage' => 0,
 	'rpm' => 0,
 	'oggconvert' => 3,
+	'radiotray' => 3,
 };
 
 # reasons for obsolete packages
