53ba201daf5057d3583d0d0cbd2fa2b56836aadc
solene <solene@openbsd.org>
2020-05-21 09:21:46 +0000

    net/pop3gwd is abandoned upstream

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.963 2020/05/18 16:06:20 kn Exp $
+# $OpenBSD: Quirks.pm,v 1.964 2020/05/21 09:21:46 solene Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -1572,6 +1572,7 @@ my $obsolete_reason = {
 	'nipper' => 23,
 	'bg5pdf' => 13,
 	'pdflib' => 23,
+	'pop3gwd' => 3,
 };
 
 # reasons for obsolete packages
