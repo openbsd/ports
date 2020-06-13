3e75aed1ec7d3e915968180009915b4e706fd9f2
rsadowski <rsadowski@openbsd.org>
2020-06-13 16:22:52 +0000

    Unhook KDE4
    It's time to get rid of KDE4. x11/kde4 contained all applications and the
    desktop environment. All applications and the framework have been ported to
    KDE5 and are available under x11/kde-applications and devel/kf5.
    
    The desktop environment has not yet been ported and is NO LONGER available!
    You can't have everything.
    
    What happened here:
    
    - Remove multimedia/phonon kde4 bits and set set phonon FLAVOR=qt5 for now.
    - Unhook x11/kde4
    - Unhook meta/kde4
    - Mark kmymoney as broken, update is coming.
    - Unhook all kde4 module consumers:
     - klogic
     - soprano
     - strigi
     - automoc
     - prison
     - qimageblitz
     - cagibi
     - libkvkontakte
     - akonadi
     - attica
     - libalkimia
     - libkfbapi
     - libkgapi
     - qoauth
     - kwebkitpart
     - grantlee
     - libkscreen
     - polkit-qt

--- multimedia/phonon/phonon.port.mk
+++ multimedia/phonon/phonon.port.mk
@@ -1,10 +1,7 @@
-# $OpenBSD: phonon.port.mk,v 1.9 2018/10/19 20:53:27 rsadowski Exp $
+# $OpenBSD: phonon.port.mk,v 1.10 2020/06/13 16:22:52 rsadowski Exp $
 .if ${MODULES:Mx11/qt5} || ${MODULES:Mdevel/kf5}
 MODPHONON_WANTLIB =	phonon4qt5
 MODPHONON_LIB_DEPENDS =	phonon-qt5->=4.10.1:multimedia/phonon-qt5
-.else
-MODPHONON_WANTLIB =	${MODKDE4_LIB_DIR}/phonon_s
-MODPHONON_LIB_DEPENDS =	phonon->=4.10.1:multimedia/phonon
 .endif
 
 # If enabled (default), make sure at least one Phonon backend is
@@ -13,8 +10,6 @@ MODPHONON_PLUGIN_DEPS ?=	Yes
 .if ${MODPHONON_PLUGIN_DEPS:L} == "yes"
 . if ${MODULES:Mx11/qt5} || ${MODULES:Mdevel/kf5}
 MODPHONON_RUN_DEPENDS =	phonon-qt5-gstreamer-*:multimedia/phonon-backend/gstreamer,qt5
-. else
-MODPHONON_RUN_DEPENDS =	phonon-vlc-*|phonon-gstreamer-*:multimedia/phonon-backend/vlc
 . endif
 .endif
 
