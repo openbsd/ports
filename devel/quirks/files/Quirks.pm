e923766787f13f3c5a89faa7caa706f703343a00
jasper <jasper@openbsd.org>
2020-06-27 08:52:16 +0000

    add quirks entry for wireguard-go after it was effectively removed (through IGNORE)

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.996 2020/06/24 08:57:03 jasper Exp $
+# $OpenBSD: Quirks.pm,v 1.997 2020/06/27 08:52:16 jasper Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -1749,6 +1749,7 @@ my $obsolete_reason = {
 	'smokeqt' => 3,
 	'superkaramba' => 3,
 	'gbirthday' => 24,
+	'wireguard-go' => 4,
 };
 
 # reasons for obsolete packages
