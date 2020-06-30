e7d53cc84856f90f6625470701424c29d81edabc
kn <kn@openbsd.org>
2020-06-30 12:32:27 +0000

    Remove wiresep
    wg(4) offers all that wiresep can do and more.
    
    With jasper and Tim Kuijsten (Author/Maintainer)

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.997 2020/06/27 08:52:16 jasper Exp $
+# $OpenBSD: Quirks.pm,v 1.998 2020/06/30 12:32:27 kn Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -1750,6 +1750,7 @@ my $obsolete_reason = {
 	'superkaramba' => 3,
 	'gbirthday' => 24,
 	'wireguard-go' => 4,
+	'wiresep' => 4,
 };
 
 # reasons for obsolete packages
