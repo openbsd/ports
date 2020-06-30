a565b6eaea5161dd2169d4a19dc3c1a28c64f6e7
kn <kn@openbsd.org>
2020-06-30 13:10:40 +0000

    Remove wireless
    This port came to be before ifconfig(8)'s "join" existed, which should now
    be used.
    
    OK solene

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.998 2020/06/30 12:32:27 kn Exp $
+# $OpenBSD: Quirks.pm,v 1.999 2020/06/30 13:10:40 kn Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -1751,6 +1751,7 @@ my $obsolete_reason = {
 	'gbirthday' => 24,
 	'wireguard-go' => 4,
 	'wiresep' => 4,
+	'wireless' => 4,
 };
 
 # reasons for obsolete packages
