96fc245e6123a373963f722f0ea38afa2751a652
pvk <pvk@openbsd.org>
2020-09-22 17:59:25 +0000

    Update beats 7.8.1 -> 7.9.1 Release notes: https://www.elastic.co/guide/en/beats/libbeat/current/release-notes-7.9.1.html

---
+++ sysutils/beats/packetbeat/files/pcap_poll_openbsd.go
@@ -0,0 +1,18 @@
+// +build openbsd
+
+package pcap
+
+/*
+#include <pcap.h>
+*/
+import "C"
+
+type packetPoll struct{}
+
+func NewPacketPoll(_ *C.pcap_t, _ C.int) *packetPoll {
+	return nil
+}
+
+func (t *packetPoll) AwaitForPackets() bool {
+	return true
+}
