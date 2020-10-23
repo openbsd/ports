b5f760b09b6825ed87a2aeb10c505951049bf1c3
landry <landry@openbsd.org>
2020-10-23 07:04:09 +0000

    Update to tor-browser 10.0.2, from MAINTAINER Caspar Schutijser.
    This version is based off firefox 78esr, and support for unveil() was
    added.
    
    See https://blog.torproject.org/new-release-tor-browser-100 and
    https://blog.torproject.org/new-release-tor-browser-1002

--- www/tor-browser/browser/files/all-openbsd.js
+++ www/tor-browser/browser/files/all-openbsd.js
@@ -3,7 +3,3 @@
 pref("spellchecker.dictionary_path", "${LOCALBASE}/share/mozilla-dicts/");
 pref("general.config.filename", "tor-browser.cfg");
 pref("general.config.obscure_value", 0);
-// enable pledging the content process
-pref("security.sandbox.content.level", 1);
-pref("security.sandbox.pledge.main","stdio rpath wpath cpath inet proc exec prot_exec flock ps sendfd recvfd dns vminfo tty drm unix fattr getpw mcast video");
-pref("security.sandbox.pledge.content","stdio rpath wpath cpath inet recvfd sendfd prot_exec unix drm ps");
