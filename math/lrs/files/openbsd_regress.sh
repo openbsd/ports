34a1d6de32c518557a20b79b7fff6106f5371cd9
edd <edd@openbsd.org>
2020-11-20 19:16:18 +0000

    Update math/lrs to version 071a.
    Diff sent in by Dimitri Karamazov and then tweaks from sthen@ and myself.
    
    Dimitri is also taking MAINTAINER since I don't use this any more.
    
    Thanks!

--- math/lrs/files/openbsd_regress.sh
+++ math/lrs/files/openbsd_regress.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-# $OpenBSD: openbsd_regress.sh,v 1.1.1.1 2011/10/07 08:58:42 edd Exp $
+# $OpenBSD: openbsd_regress.sh,v 1.2 2020/11/20 19:16:18 edd Exp $
 
 IN=test.ine
 OUT=actual_outcome.ine
 EXPECT=expected_outcome.ine
 
-./glrs ${IN} | grep -e '^ 1' 2>&1 | tee ${OUT}
+./lrsgmp ${IN} | grep -e '^ 1' 2>&1 | tee ${OUT}
 
 DIFF=`diff -u ${OUT} ${EXPECT}`
 if [ -n "${DIFF}" ]; then
