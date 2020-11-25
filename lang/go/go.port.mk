e43bc025781342fb33d5fa728543403082200c95
sthen <sthen@openbsd.org>
2020-11-25 15:23:49 +0000

    For MODGO_MODULES ports, don't point the port to fetch files directly from /usr/ports/distfiles when it wants to unpack modules during the build, instead copy the files into the WRKDIR and point it there.
    If there was a mistake with setting up MODGO_MODULES/MODGO_MODFILES
    in a port then this change will cause it to show up in build, otherwise
    it may succeed or fail randomly depending on what files are present in
    distfiles (fetched by other ports).

--- lang/go/go.port.mk
+++ lang/go/go.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: go.port.mk,v 1.35 2020/10/09 13:23:51 paco Exp $
+# $OpenBSD: go.port.mk,v 1.36 2020/11/25 15:23:49 sthen Exp $
 
 ONLY_FOR_ARCHS ?=	${GO_ARCHS}
 
@@ -71,7 +71,7 @@ DISTFILES +=	${MODGO_DIST_SUBDIR}/${_modpath}/@v/${_modver}.mod{${_modpath}/@v/$
 .  for _modpath _modver in ${MODGO_MODFILES}
 DISTFILES +=	${MODGO_DIST_SUBDIR}/${_modpath}/@v/${_modver}.mod{${_modpath}/@v/${_modver}.mod}:${MODGO_MASTER_SITESN}
 .  endfor
-MAKE_ENV +=		GOPROXY=file://${DISTDIR}/${MODGO_DIST_SUBDIR}
+MAKE_ENV +=		GOPROXY=file://${WRKDIR}/go_modules
 MAKE_ENV +=		GO111MODULE=on GOPATH="${MODGO_GOPATH}"
 .else
 # ports are not allowed to fetch from the network at build time; point
@@ -105,7 +105,10 @@ WRKSRC ?=		${MODGO_WORKSPACE}/src/${ALL_TARGET}
 MODGO_SETUP_WORKSPACE =	mkdir -p ${WRKSRC:H}; mv ${MODGO_SUBDIR} ${WRKSRC};
 .else
 WRKSRC ?=		${WRKDIR}/${MODGO_MODNAME}@${MODGO_VERSION}
-MODGO_SETUP_WORKSPACE =	ln -sf ${WRKSRC} ${WRKDIR}/${MODGO_MODNAME}
+MODGO_SETUP_WORKSPACE =	ln -sf ${WRKSRC} ${WRKDIR}/${MODGO_MODNAME};
+.for _MODGO_m in ${DISTFILES:Mgo_modules/*:C/{.*//}
+MODGO_SETUP_WORKSPACE += ${INSTALL} -D ${DISTDIR}/${_MODGO_m} ${WRKDIR}/${_MODGO_m};
+.endfor
 .endif
 
 INSTALL_STRIP =
