07cd827dd56f51251180e3b15c0297c93884dddb
ajacoutot <ajacoutot@openbsd.org>
2021-02-20 17:18:28 +0000

    'py-dot' => 'py3-dot'

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.1194 2021/02/20 17:09:02 sthen Exp $
+# $OpenBSD: Quirks.pm,v 1.1195 2021/02/20 17:18:28 ajacoutot Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -581,6 +581,7 @@ my $stem_extensions = {
 	'py-sphinx_rtd_theme' => 'py3-sphinx_rtd_theme',
 	'py-codestyle' => 'py3-codestyle',
 	'pyflakes3' => 'pyflakes',
+	'py-dot' => 'py3-dot',
 };
 
 my $obsolete_reason = {
