5ca327728826bbd3afcf70048bd31cd4d315bb36
naddy <naddy@openbsd.org>
2021-02-26 21:24:22 +0000

    remove x11/golem
    Our port of this old window manager is at version 0.0.5, which is
    from 2002 (19 years old).  At startup, there is a spew of errors
    about undefined symbols and the plugins can't be loaded.  Clearly,
    nobody has been using this.
    
    There is a newer development version 0.0.6 from 2007, but it relies
    on Linux-only code.
    
    ok kn@ ajacoutot@

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.1218 2021/02/26 13:00:43 sthen Exp $
+# $OpenBSD: Quirks.pm,v 1.1219 2021/02/26 21:24:22 naddy Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -2160,6 +2160,7 @@ my $obsolete_reason = {
 	'mingw' => 6,
 	'bsd-airtools' => 3,
 	'gtkpod' => 3,
+	'golem' => 0,
 };
 
 # reasons for obsolete packages
