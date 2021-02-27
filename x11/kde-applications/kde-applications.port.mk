2a60d43d7b01486a22abe2c5c5adcbcd7d015295
rsadowski <rsadowski@openbsd.org>
2021-02-27 07:15:56 +0000

    Add kde-applications.port.mk
    Just move some stuff from Makefile.inc to kde-applications.port.mk.

---
+++ x11/kde-applications/kde-applications.port.mk
@@ -0,0 +1,24 @@
+# $OpenBSD: kde-applications.port.mk,v 1.9 2021/02/27 07:15:56 rsadowski Exp $
+
+MODKDE_VERSION ?=		20.12.2
+
+# Set to 'yes' if there are .desktop files under share/release-service/.
+.if defined(MODKDE5_DESKTOP_FILE) && ${MODKDE5_DESKTOP_FILE:L} == "yes"
+MODKDE5_RUN_DEPENDS +=		devel/desktop-file-utils
+.endif
+
+# Set to 'yes' if there are icon files under share/icons/.
+.if defined(MODKDE5_ICON_CACHE) && ${MODKDE5_ICON_CACHE:L} == "yes"
+MODKDE5_RUN_DEPENDS +=		x11/gtk+3,-guic
+.endif
+
+# Set to 'yes' if there are icon files under share/locale/.
+.if defined(MODKDE5_TRANSLATIONS) && ${MODKDE5_TRANSLATIONS:L} == "yes"
+MODKDE5_BUILD_DEPENDS +=	devel/gettext,-tools
+.endif
+
+# Set to 'yes' if there are icon files under share/doc/.
+.if defined(MODKDE5_DOCS) && ${MODKDE5_DOCS:L} == "yes"
+MODKDE5_BUILD_DEPENDS +=	devel/kf5/kdoctools
+MODKDE5_RUN_DEPENDS +=		devel/kf5/kdoctools
+.endif
