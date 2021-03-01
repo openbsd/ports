826e630917a1908b4ddce2f521aa2e38f0bb4b13
naddy <naddy@openbsd.org>
2021-03-01 20:02:54 +0000

    remove multimedia/audiopreview
    No upstream, no master site, fails to build with -fno-common, depends
    on gstreamer-0.10 which we also want to get rid of.
    
    ok gnezdo@ kn@

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.1225 2021/03/01 17:34:34 tracey Exp $
+# $OpenBSD: Quirks.pm,v 1.1226 2021/03/01 20:02:54 naddy Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -2176,6 +2176,7 @@ my $obsolete_reason = {
 	'wmmultipop3' => 3,
 	'wmpop3' => 3,
 	'angst' => 3,
+	'audiopreview' => 3,
 };
 
 # reasons for obsolete packages
