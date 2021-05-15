51131062017ef4afcb7a89c9b471887a9660e2ad
rsadowski <rsadowski@openbsd.org>
2021-05-15 09:56:26 +0000

    Bugfix update KDE Gear to 21.04.1
    https://kde.org/announcements/changelogs/gear/21.04.1/

--- x11/kde-applications/kde-applications.port.mk
+++ x11/kde-applications/kde-applications.port.mk
@@ -1,6 +1,6 @@
-# $OpenBSD: kde-applications.port.mk,v 1.11 2021/04/27 05:26:55 rsadowski Exp $
+# $OpenBSD: kde-applications.port.mk,v 1.12 2021/05/15 09:56:27 rsadowski Exp $
 
-MODKDE_VERSION ?=		21.04.0
+MODKDE_VERSION ?=		21.04.1
 
 # Set to 'yes' if there are .desktop files under share/release-service/.
 .if defined(MODKDE5_DESKTOP_FILE) && ${MODKDE5_DESKTOP_FILE:L} == "yes"
