0bd996c76c153b11b2bbd99e630395633220d98d
kmos <kmos@openbsd.org>
2021-11-01 14:17:16 +0000

    Hook Python 3.10 into build and add the necessary bit to python.port.mk to accept Python 3.10

--- lang/python/python.port.mk
+++ lang/python/python.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: python.port.mk,v 1.133 2021/11/01 03:55:31 kmos Exp $
+# $OpenBSD: python.port.mk,v 1.134 2021/11/01 14:17:16 kmos Exp $
 #
 #	python.port.mk - Xavier Santolaria <xavier@santolaria.net>
 #	This file is in the public domain.
@@ -60,7 +60,8 @@ MODPY_VERSION ?=	${MODPY_DEFAULT_VERSION_2}
 # verify if MODPY_VERSION found is correct
 .if ${MODPY_VERSION} != "2.7" && \
       ${MODPY_VERSION} != "3.8" && \
-      ${MODPY_VERSION} != "3.9"
+      ${MODPY_VERSION} != "3.9" && \
+      ${MODPY_VERSION} != "3.10"
 ERRORS += "Fatal: unknown or unsupported MODPY_VERSION: ${MODPY_VERSION}"
 .endif
 
