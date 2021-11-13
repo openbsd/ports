fa70a9fd915ec458d9e3be5481a1e9a88428be30
sthen <sthen@openbsd.org>
2021-11-13 17:23:37 +0000

    update to mupdf-1.19.0

--- textproc/mupdf/pkg/PFRAG.js
+++ textproc/mupdf/pkg/PFRAG.js
@@ -1,4 +1,4 @@
-@comment $OpenBSD: PFRAG.js,v 1.1 2018/12/03 18:40:07 sthen Exp $
+@comment $OpenBSD: PFRAG.js,v 1.2 2021/11/13 17:23:38 sthen Exp $
 @bin bin/mupdf-gl
 share/examples/mupdf/bbox-device.js
 share/examples/mupdf/create-thumbnail.js
@@ -8,5 +8,9 @@ share/examples/mupdf/fix-s22pdf.js
 share/examples/mupdf/jpx-to-pdf.js
 share/examples/mupdf/pdf-create-lowlevel.js
 share/examples/mupdf/pdf-create.js
+share/examples/mupdf/pdf-dejpx.js
+share/examples/mupdf/pdf-extract-rich-media.js
 share/examples/mupdf/pdf-merge.js
+share/examples/mupdf/pdf-portfolio.js
+share/examples/mupdf/pdf-trace.js
 share/examples/mupdf/trace-device.js
