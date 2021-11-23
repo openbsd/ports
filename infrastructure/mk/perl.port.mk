bd756bd601bfcfa9ee7fc265d6c90c3536e51727
afresh1 <afresh1@openbsd.org>
2021-11-23 01:12:38 +0000

    Adjust perl.port.mk so options will be picked up in more places
    Export some ExtUtils::MakeMaker and Module::Build recognized environment
    variables so they are picked up even if we don't run the configure
    script directly.
    
    This is useful for things that are not perl modules themselves, but include
    perl modules, some of which seem to configure them at build time instead
    of configure.
    
    This is the first step to moving perl to have a vendor lib that
    ports install into that is separate from where CPAN will install things.
    
    Thanks to kmos@ for a bulk build that didn't turn up anything untoward

--- infrastructure/mk/perl.port.mk
+++ infrastructure/mk/perl.port.mk
@@ -1,11 +1,48 @@
 #-*- mode: Fundamental; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-# $OpenBSD: perl.port.mk,v 1.30 2017/12/03 11:10:10 espie Exp $
+# $OpenBSD: perl.port.mk,v 1.31 2021/11/23 01:12:38 afresh1 Exp $
 #	Based on bsd.port.mk, originally by Jordan K. Hubbard.
 #	This file is in the public domain.
 
 TEST_TARGET ?=	test
 MODPERL_BUILD ?= Build
+
+P5SITE = libdata/perl5/site_perl
+P5ARCH = ${P5SITE}/${MACHINE_ARCH}-openbsd
+SUBST_VARS += P5ARCH P5SITE
+
+# For autoconf/automake
+CONFIGURE_ENV +=	PERL_LIB="${LOCALBASE}/${P5SITE}" PERL_ARCH="${LOCALBASE}/${P5ARCH}"
+#MAKE_ENV +=		PERL_LIB="${LOCALBASE}/${P5SITE}" PERL_ARCH="${LOCALBASE}/${P5ARCH}"
+
+# http://www.gnu.org/software/autoconf-archive/ax_perl_ext.html
+CONFIGURE_ENV +=	PERL_EXT_LIB="${LOCALBASE}/${P5ARCH}"
+#MAKE_ENV +=		PERL_EXT_LIB="${LOCALBASE}/${P5ARCH}"
+
+PERL_MM_OPT =	INSTALLSITELIB="${LOCALBASE}/${P5SITE}" \
+		INSTALLSITEARCH="${LOCALBASE}/${P5ARCH}" \
+		INSTALLPRIVLIB="/usr/./libdata/perl5" \
+		INSTALLARCHLIB="\$${INSTALLPRIVLIB}/${MACHINE_ARCH}-openbsd" \
+		INSTALLMAN1DIR="${LOCALBASE}/man/man1" \
+		INSTALLMAN3DIR="${LOCALBASE}/man/man3p" \
+		INSTALLBIN="$${LOCALBASE}/bin" \
+		INSTALLSCRIPT="$${INSTALLBIN}"
+
+# For ExtUtils::MakeMaker
+# in MAKE_ENV because some ports that include modules run it late
+CONFIGURE_ENV +=	PERL_MM_OPT="${PERL_MM_OPT}" PERL_MM_USE_DEFAULT=1
+MAKE_ENV +=		PERL_MM_OPT="${PERL_MM_OPT}" PERL_MM_USE_DEFAULT=1
+
+PERL_MB_OPT =	--install_path=lib=${LOCALBASE}/${P5SITE} \
+		--install_path=arch=${LOCALBASE}/${P5ARCH} \
+		--install_path=libdoc="${LOCALBASE}/man/man3p" \
+		--install_path=bindoc="${LOCALBASE}/man/man1" \
+		--install_path=bin="${LOCALBASE}/bin" \
+		--install_path=script="${LOCALBASE}/bin"
+
+# For Module::Build and Module::Build::Tiny
+CONFIGURE_ENV +=	PERL_MB_OPT="${PERL_MB_OPT}"
+#MAKE_ENV +=		PERL_MB_OPT="${PERL_MB_OPT}"
 
 # set /usr/bin/perl for executable scripts
 MODPERL_BIN_ADJ =	perl -pi \
@@ -20,44 +57,17 @@ MODPERL_pre-configure = for f in ${MODPERL_ADJ_FILES}; do \
 .endif
 
 .if ${CONFIGURE_STYLE:L:Mmodbuild}
-.  if ${CONFIGURE_STYLE:L:Mtiny}
 MODPERL_configure = \
-	arch=`perl -e 'use Config; print $$Config{archname}, "\n";'`; \
     cd ${WRKSRC}; ${SETENV} ${CONFIGURE_ENV} \
-	perl Build.PL \
-		--install_path=lib="${PREFIX}/libdata/perl5/site_perl" \
-		--install_path=arch="${PREFIX}/libdata/perl5/site_perl/$$arch" \
-		--install_path=libdoc="${PREFIX}/man/man3p" \
-		--install_path=bindoc="${PREFIX}/man/man1" \
-		--install_path=bin="${PREFIX}/bin" \
-		--install_path=script="${PREFIX}/bin" ${CONFIGURE_ARGS}
-.  else
-MODPERL_configure = \
-	arch=`perl -e 'use Config; print $$Config{archname}, "\n";'`; \
-    cd ${WRKSRC}; ${SETENV} ${CONFIGURE_ENV} \
-	perl Build.PL \
-		install_path=lib="${PREFIX}/libdata/perl5/site_perl" \
-		install_path=arch="${PREFIX}/libdata/perl5/site_perl/$$arch" \
-		install_path=libdoc="${PREFIX}/man/man3p" \
-		install_path=bindoc="${PREFIX}/man/man1" \
-		install_path=bin="${PREFIX}/bin" \
-		install_path=script="${PREFIX}/bin" ${CONFIGURE_ARGS}
-.   endif
+    perl Build.PL ${CONFIGURE_ARGS}; \
+	if ! test -f ${WRKBUILD}/Build; then \
+		echo >&2 "Fatal: Build.PL did not produce a Build script"; \
+		exit 1; \
+	fi
 .else
 MODPERL_configure = ${_MODPERL_preconfig}; \
-	arch=`perl -e 'use Config; print $$Config{archname}, "\n";'`; \
      cd ${WRKSRC}; ${SETENV} ${CONFIGURE_ENV} \
-	 PERL_MM_USE_DEFAULT=Yes \
-     perl Makefile.PL \
-     	PREFIX='${PREFIX}' \
-		INSTALLSITELIB='${PREFIX}/libdata/perl5/site_perl' \
-		INSTALLSITEARCH="\$${INSTALLSITELIB}/$$arch" \
-		INSTALLPRIVLIB='/usr/./libdata/perl5' \
-		INSTALLARCHLIB="\$${INSTALLPRIVLIB}/$$arch" \
-		INSTALLMAN1DIR='${PREFIX}/man/man1' \
-		INSTALLMAN3DIR='${PREFIX}/man/man3p' \
-		INSTALLBIN='$${PREFIX}/bin' \
-		INSTALLSCRIPT='$${INSTALLBIN}' ${CONFIGURE_ARGS}; \
+     perl Makefile.PL PREFIX='${PREFIX}' ${CONFIGURE_ARGS}; \
 	if ! test -f ${WRKBUILD}/Makefile; then \
 		echo >&2 "Fatal: Makefile.PL did not produce a Makefile"; \
 		exit 1; \
@@ -72,8 +82,7 @@ _MODPERL_preconfig = :
 .  endif
 .endif
 
-MODPERL_pre-fake = \
-	mkdir -p ${WRKINST}`perl -e 'use Config; print $$Config{installarchlib}, "\n";'`
+MODPERL_pre-fake = mkdir -p ${WRKINST}${PERL_ARCH}/auto
 
 .if ${CONFIGURE_STYLE:L:Mmodbuild}
 .  if ${CONFIGURE_STYLE:L:Mtiny}
@@ -90,6 +99,7 @@ MODPERL_BUILD_TARGET = \
 MODPERL_TEST_TARGET = \
 	cd ${WRKSRC} && ${SETENV} ${MAKE_ENV} perl \
 		${MODPERL_BUILD} ${TEST_TARGET}
+
 MODPERL_INSTALL_TARGET = \
 	cd ${WRKSRC} && ${SETENV} ${MAKE_ENV} perl \
 		${MODPERL_BUILD} ${FAKE_TARGET} --destdir=${WRKINST}
@@ -107,7 +117,3 @@ do-install:
 	@${MODPERL_INSTALL_TARGET}
 .  endif
 .endif
-
-P5SITE = libdata/perl5/site_perl
-P5ARCH = ${P5SITE}/${MACHINE_ARCH}-openbsd
-SUBST_VARS += P5ARCH P5SITE
