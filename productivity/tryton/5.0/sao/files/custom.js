cac90237040f933d066c76821a193ed8e6a0c57f
semarie <semarie@openbsd.org>
2022-03-10 13:25:23 +0000

    productivity/tryton: unbreak after werkzeug update
    - backport werkzeug 2.0 support to 5.0 and 5.2 series
    - remove RCSTag while here

--- productivity/tryton/5.0/sao/files/custom.js
+++ productivity/tryton/5.0/sao/files/custom.js
@@ -1,4 +1,3 @@
-/* $OpenBSD: custom.js,v 1.1.1.1 2018/12/03 12:28:14 semarie Exp $ */
 /* 
  * use this file to add javascript in order to customize sao, like changing
  * Sao.config values, inject a plugin or implement a custom widget.
--- productivity/tryton/5.2/sao/files/custom.js
+++ productivity/tryton/5.2/sao/files/custom.js
@@ -1,4 +1,3 @@
-/* $OpenBSD: custom.js,v 1.1.1.1 2019/05/09 11:49:00 semarie Exp $ */
 /* 
  * use this file to add javascript in order to customize sao, like changing
  * Sao.config values, inject a plugin or implement a custom widget.
