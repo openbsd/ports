5bba7c308f7155b377e667dedd95a79678fcc531
naddy <naddy@openbsd.org>
2022-03-11 18:24:26 +0000

    drop RCS Ids

--- cad/fritzing/files/fritzing.sh
+++ cad/fritzing/files/fritzing.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 #
-# $OpenBSD: fritzing.sh,v 1.1.1.1 2010/09/15 07:14:18 jasper Exp $
 # Since Fritzing wants it's file stored in CWD or specified via --folder
 # we use the last. Lest to prevent ever changing patches.
 
