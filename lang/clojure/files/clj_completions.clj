66e6abf9035b7c0e6b15cfde959e36456cb102b7
op <op@openbsd.org>
2022-04-25 11:37:06 +0000

    update lang/clojure to 1.11.1.1107
    While here also set NO_BUILD and tweak the clj_completions.clj script.
    I'm taking the maintainership of the port too.
    
    ok solene@

--- lang/clojure/files/clj_completions.clj
+++ lang/clojure/files/clj_completions.clj
@@ -1,3 +1,7 @@
-(def completions (mapcat (comp keys ns-publics) (all-ns)))
-(with-open [f (java.io.BufferedWriter. (java.io.FileWriter. "files/clj_completions"))]
-	   (.write f (apply str (interpose \newline (sort (distinct completions))))))
+(def completions
+  (mapcat (comp keys ns-publics) (all-ns)))
+
+(with-open [f (java.io.FileWriter. "files/clj_completions")]
+  (binding [*out* f]
+    (doseq [c (-> completions sort distinct)]
+      (println c))))
