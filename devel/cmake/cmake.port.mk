b6a8a4809803943ab330e1628fde35ba5dbb1c2c
rsadowski <rsadowski@openbsd.org>
2022-05-28 06:18:52 +0000

    Update CMake to 3.23.1
    - Drop cmComputeLinkInformation.cxx patch
    I don't understand why we still have this "workaround". Everything works
    WITHOUT it. It comes from very old dark times and was never validated again.
    I'm happy to drop it!
    
    - cmGeneratorTarget.cxx patch
    cmValue needs heap variables, that fixes a possible crash. Nevertheless,
    I still don't like this solution because nothing deletes the string.
    I have to dig dipper in the CMake memory management system. Anyway, not
    perfect but better than now. I will discuss this with upstream.
    
    - Respect CFLAGS and CXXFLAGS
    cmake.port.mk now respects C/CXX flags. With is CMAKE_*_FLAGS_* is obsolete.
    
    - MODCMAKE_LDFLAGS
    cmake.port.mk now respects LDFLAGS flags when you set MODCMAKE_LDFLAGS
    https://cmake.org/cmake/help/latest/envvar/LDFLAGS.html
    
    Many thanks to tb and sthen. Bulk build by tb, final ok sthen

--- devel/cmake/cmake.port.mk
+++ devel/cmake/cmake.port.mk
@@ -99,6 +99,18 @@ CONFIGURE_ENV +=	MODTK_VERSION=${MODTK_VERSION} \
 			MODTK_LIB=${MODTK_LIB}
 .endif
 
+
+.if ! empty(MODCMAKE_LDFLAGS)
+# https://cmake.org/cmake/help/latest/envvar/LDFLAGS.html
+# Will only be used by CMake on the first configuration to determine the
+# default linker flags, after which the value for LDFLAGS is stored in the
+# cache as CMAKE_EXE_LINKER_FLAGS_INIT, CMAKE_SHARED_LINKER_FLAGS_INIT, and
+# CMAKE_MODULE_LINKER_FLAGS_INIT. For any configuration run (including the
+# first), the environment variable will be ignored if the equivalent
+# CMAKE_<TYPE>_LINKER_FLAGS_INIT variable is defined.
+CONFIGURE_ENV +=	LDFLAGS="${MODCMAKE_LDFLAGS}"
+.endif
+
 MODCMAKE_DEBUG ?=		No
 
 .if empty(CONFIGURE_STYLE)
