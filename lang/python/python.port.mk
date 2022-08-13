b884d8b9ee05358ba9873685f635b297da494a86
sthen <sthen@openbsd.org>
2022-08-13 17:16:37 +0000

    s/hatch/hatchling/ for MODPY_PEP517

--- lang/python/python.port.mk
+++ lang/python/python.port.mk
@@ -196,7 +196,7 @@ BUILD_DEPENDS +=	devel/py-installer${MODPY_FLAVOR} \
 BUILD_DEPENDS +=	devel/py-flit_core${MODPY_FLAVOR}
 .  elif ${MODPY_PEP517:L:Mflit}
 BUILD_DEPENDS +=	devel/py-flit${MODPY_FLAVOR}
-.  elif ${MODPY_PEP517:L:Mhatch}
+.  elif ${MODPY_PEP517:L:Mhatchling}
 BUILD_DEPENDS +=	devel/py-hatchling${MODPY_FLAVOR}
 .  endif
 .else
