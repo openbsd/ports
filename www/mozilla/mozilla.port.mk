e5e737a860aa99cbca05b7f2d29ae2d39e58d8df
landry <landry@openbsd.org>
2022-08-16 13:40:36 +0000

    www/mozilla: add wip support for PGO (profile guided optimizations)
    fetch & use llvm profiling data directly from mozilla's taskcluster
    not used yet, needs to specify MOZILLA_PROFDATA_TASKID in the consumer.

--- www/mozilla/mozilla.port.mk
+++ www/mozilla/mozilla.port.mk
@@ -52,6 +52,14 @@ DISTNAME ?=	${MOZILLA_DIST}-${MOZILLA_DIST_VERSION}.source
 EXTRACT_SUFX ?=	.tar.xz
 DIST_SUBDIR ?=	mozilla
 
+.if defined(MOZILLA_PROFDATA_TASKID)
+DISTFILES =	${MOZILLA_DIST}-${MOZILLA_DIST_VERSION}.source${EXTRACT_SUFX}
+DISTFILES +=	${DISTNAME}-profdata${EXTRACT_SUFX}{profdata${EXTRACT_SUFX}}:0
+MASTER_SITES0=https://firefox-ci-tc.services.mozilla.com/api/queue/v1/task/${MOZILLA_PROFDATA_TASKID}/runs/0/artifacts/public/build/
+CONFIGURE_ARGS +=	--enable-profile-use
+CONFIGURE_ARGS +=	--with-pgo-profile-path=${WRKDIR}/merged.profdata
+.endif
+
 MODMOZ_RUN_DEPENDS =	devel/desktop-file-utils
 # autoconf-2.13 isnt a real dependency since a while, but configure still checks for it
 MODMOZ_BUILD_DEPENDS =	devel/autoconf/2.13 \
