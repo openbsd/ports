633af1ae20ba74e38d60d4a4ddb45be45ba21a22
kmos <kmos@openbsd.org>
2022-10-31 22:06:28 +0000

    Add Python 3.11.0 to build and python.port.mk
    ok sthen@ tb@

--- lang/python/python.port.mk
+++ lang/python/python.port.mk
@@ -74,7 +74,8 @@ MODPY_VERSION ?=	${MODPY_DEFAULT_VERSION_2}
 # verify if MODPY_VERSION found is correct
 .if ${MODPY_VERSION} != "2.7" && \
       ${MODPY_VERSION} != "3.9" && \
-      ${MODPY_VERSION} != "3.10"
+      ${MODPY_VERSION} != "3.10" && \
+      ${MODPY_VERSION} != "3.11"
 ERRORS += "Fatal: unknown or unsupported MODPY_VERSION: ${MODPY_VERSION}"
 .endif
 
