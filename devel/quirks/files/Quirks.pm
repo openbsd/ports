895f3cac6ae1083cb48aa36b33905cecbc268d73
kn <kn@openbsd.org>
2022-11-23 19:51:55 +0000

    Remove security/libperseus
    WWW throws 404, no update since import in 2011, no consumers, only we
    package it.
    
    maintainer timeout
    OK jca

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.1463 2022/11/23 19:48:28 kn Exp $
+# $OpenBSD: Quirks.pm,v 1.1464 2022/11/23 19:51:55 kn Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -1750,6 +1750,7 @@ setup_obsolete_reason(
 	1 => 'smbsniff',
 	0 => 'fragroute',
 	52 => 'ikeman',
+	3 => 'libperseus',
 );
 
 # though it's not yet used, these should be pkgnames, so that eventually
