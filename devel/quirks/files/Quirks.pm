8c422bf745ecfa4f13180d3671e74a9710b28712
sthen <sthen@openbsd.org>
2022-12-06 13:22:31 +0000

    net/py-ipaddr is no longer required

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.1480 2022/12/06 13:13:50 sthen Exp $
+# $OpenBSD: Quirks.pm,v 1.1481 2022/12/06 13:22:32 sthen Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -1770,6 +1770,7 @@ setup_obsolete_reason(
 	31 => 'nmap-zenmap',
 	0 => 'uucpd',
 	6 => 'py-sqlite2',
+	31 => 'py-ipaddr',
 );
 
 # though it's not yet used, these should be pkgnames, so that eventually
