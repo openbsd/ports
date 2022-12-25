18514b670f0a018cd4ecff4a4a3fe520aad069e9
ajacoutot <ajacoutot@openbsd.org>
2022-12-25 10:19:01 +0000

    Update to meson-1.0.0.

--- devel/meson/meson.port.mk
+++ devel/meson/meson.port.mk
@@ -4,7 +4,7 @@
 # finding foo.cmake and junking it if cmake is found & used)
 DPB_PROPERTIES +=	noconfigurejunk
 
-BUILD_DEPENDS +=	devel/meson>=0.64.1v0
+BUILD_DEPENDS +=	devel/meson>=1.0.0v0
 SEPARATE_BUILD ?=	Yes
 
 .if empty(CONFIGURE_STYLE)
