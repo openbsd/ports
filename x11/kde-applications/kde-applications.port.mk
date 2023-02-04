df79cf103b6e3be67f3c58da4e182e0364fa69fb
rsadowski <rsadowski@openbsd.org>
2023-02-04 14:34:50 +0000

    Update KDE Gear to 22.12.2
    https://kde.org/announcements/changelogs/gear/22.12.2/

--- x11/kde-applications/kde-applications.port.mk
+++ x11/kde-applications/kde-applications.port.mk
@@ -1,4 +1,4 @@
-MODKDE_VERSION ?=		22.12.1
+MODKDE_VERSION ?=		22.12.2
 
 # Set to 'yes' if there are .desktop files under share/release-service/.
 .if defined(MODKDE5_DESKTOP_FILE) && ${MODKDE5_DESKTOP_FILE:L} == "yes"
