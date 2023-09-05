6be19c621ebf830db3ec04c16179a4fd816b30a9
kn <kn@openbsd.org>
2023-09-05 13:49:11 +0000

    Remove themes noone uses for a bootloadder we don't have/use
    OK rsadowski

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.1541 2023/09/05 13:44:44 volker Exp $
+# $OpenBSD: Quirks.pm,v 1.1542 2023/09/05 13:49:11 kn Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -1839,6 +1839,7 @@ setup_obsolete_reason(
 	3 => 'py3-hypatia',
 	1 => 'reprepro',
 	5 => 'ocaml-pcre',
+	1 => 'breeze-grub',
 );
 
 # though it's not yet used, these should be pkgnames, so that eventually
