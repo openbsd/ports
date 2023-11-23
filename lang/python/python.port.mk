68d5077ab5d1a4b0a41f428b846ee9eff163634c
sthen <sthen@openbsd.org>
2023-11-23 12:34:21 +0000

    handle MODPY_PYBUILD=flit_scm

--- lang/python/python.port.mk
+++ lang/python/python.port.mk
@@ -209,6 +209,8 @@ BUILD_DEPENDS +=	devel/py-build${MODPY_FLAVOR} \
 			devel/py-installer${MODPY_FLAVOR}
 .  if ${MODPY_PYBUILD} == flit_core
 BUILD_DEPENDS +=	devel/py-flit_core${MODPY_FLAVOR}
+.  elif ${MODPY_PYBUILD} == flit_scm
+BUILD_DEPENDS +=	devel/py-flit_scm${MODPY_FLAVOR}
 .  elif ${MODPY_PYBUILD} == flit
 BUILD_DEPENDS +=	devel/py-flit${MODPY_FLAVOR}
 .  elif ${MODPY_PYBUILD} == hatchling
@@ -224,7 +226,7 @@ BUILD_DEPENDS +=	devel/py-setuptools${MODPY_FLAVOR} \
 BUILD_DEPENDS +=	devel/py-setuptools_scm${MODPY_FLAVOR}
 .    endif
 .  elif !${MODPY_PYBUILD:L:Myes}
-ERRORS +=		"Fatal: unknown MODPY_PYBUILD value (flit_core, flit, hatchling, hatch-vcs, poetry-core, setuptools, setuptools_scm)"
+ERRORS +=		"Fatal: unknown MODPY_PYBUILD value (flit_core, flit_scm, flit, hatchling, hatch-vcs, poetry-core, setuptools, setuptools_scm)"
 .  endif
 .else
 # Try to detect the case where a port will build regardless of setuptools
