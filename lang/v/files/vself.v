e222f0d91142b64c95fa7b0b0c9a1311fc865e11
jasper <jasper@openbsd.org>
2024-01-13 15:41:09 +0000

    - update to v-0.4.4 - use standalone files for vup and vself instead of patching in our intended code   as it's too likely to need merges with every update

---
+++ lang/v/files/vself.v
@@ -0,0 +1,5 @@
+module main
+
+fn main() {
+	println('It is not recommended to use "v self" with this packaged version. Please use the source distribution of V if you rely on this functionality')
+}
---
+++ lang/v/files/vup.v
@@ -0,0 +1,6 @@
+module main
+
+fn main() {
+	println('Use "pkg_add -u v" to update V, or if you want more recent V versions, just from V from source, https://github.com/vlang/v#installing-v-from-source')
+	println('If the V package is outdated in OpenBSD -current, please notify the maintainer.')
+}
