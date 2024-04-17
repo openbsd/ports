aaf344ca9430bc367fcf3e8ec5d23984b2735084
sthen <sthen@openbsd.org>
2024-04-17 13:33:56 +0000

    rename MODPY_PYBUILD=bootstrap to MODPY_PYBUILD=other as there are some less common backends (like pdm-backend) which don't seem particularly widely used so it seems useful to have a generic mechanism for these.

--- lang/python/python.port.mk
+++ lang/python/python.port.mk
@@ -231,8 +231,8 @@ BUILD_DEPENDS +=	devel/py-setuptools${MODPY_FLAVOR} \
 .    if ${MODPY_PYBUILD} == setuptools_scm
 BUILD_DEPENDS +=	devel/py-setuptools_scm${MODPY_FLAVOR}
 .    endif
-.  elif !${MODPY_PYBUILD:L:Mbootstrap}
-ERRORS +=		"Fatal: unknown MODPY_PYBUILD value (bootstrap, flit_core, flit_scm, flit, hatchling, hatch-vcs, jupyter_packaging, maturin, poetry-core, setuptools, setuptools_scm)"
+.  elif !${MODPY_PYBUILD:L:Mother}
+ERRORS +=		"Fatal: unknown MODPY_PYBUILD value (flit_core, flit_scm, flit, hatchling, hatch-vcs, jupyter_packaging, maturin, other, poetry-core, setuptools, setuptools_scm)"
 .  endif
 .else
 # Try to detect the case where a port will build regardless of setuptools
