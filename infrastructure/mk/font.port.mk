5d03926ea9b22f38c40bc0eb58f4fcb6cac2af34
bentley <bentley@openbsd.org>
2024-07-09 13:03:39 +0000

    Add a new font module variable, MODFONT_DOCDIR, to simplify doc installation.
    This reduces the need for custom install targets in ports using the
    font module.
    
    ok sthen@

--- infrastructure/mk/font.port.mk
+++ infrastructure/mk/font.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: font.port.mk,v 1.7 2024/04/15 16:08:14 bentley Exp $
+# $OpenBSD: font.port.mk,v 1.8 2024/07/09 13:03:39 bentley Exp $
 
 CATEGORIES +=	fonts
 
@@ -12,8 +12,10 @@ PKGNAME ?=	${MODFONT_FAMILY}-${MODFONT_VERSION}
 .  endif
 
 MODFONT_DIR ?=	${PREFIX}/share/fonts/${MODFONT_FAMILY}
+MODFONT_DOCDIR ?=	${PREFIX}/share/doc/${MODFONT_FAMILY}
 
 MODFONT_TYPES ?=
+MODFONT_DOCFILES ?=
 
 MODFONT_do-install = ${INSTALL_DATA_DIR} ${MODFONT_DIR};
 
@@ -23,6 +25,11 @@ MODFONT_do-install = ${INSTALL_DATA_DIR} ${MODFONT_DIR};
 MODFONT_do-install += for t in otf ttf; do ${INSTALL_DATA} ${WRKSRC}/*.$$t ${MODFONT_DIR} && break; done
 .else
 MODFONT_do-install += for t in ${MODFONT_TYPES}; do ${INSTALL_DATA} ${WRKSRC}/*.$$t ${MODFONT_DIR}; done
+.endif
+
+.if !empty(MODFONT_DOCFILES)
+MODFONT_do-install += ; ${INSTALL_DATA_DIR} ${MODFONT_DOCDIR}
+MODFONT_do-install += ; for t in ${MODFONT_DOCFILES}; do ${INSTALL_DATA} ${WRKSRC}/$$t ${MODFONT_DOCDIR}; done
 .endif
 
 .  if !target(do-install)
