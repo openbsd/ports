634faabc18b763e7e1463554db1befc836db8fa4
rsadowski <rsadowski@openbsd.org>
2024-07-13 14:12:24 +0000

    Rename MODKDE5_ prefix to MODKDE_

--- x11/kde-applications/kde-applications.port.mk
+++ x11/kde-applications/kde-applications.port.mk
@@ -3,27 +3,27 @@ MODKDE_VERSION ?=		24.05.2
 MODKDE_KF5 ?=			No
 
 # Set to 'yes' if there are .desktop files under share/release-service/.
-.if defined(MODKDE5_DESKTOP_FILE) && ${MODKDE5_DESKTOP_FILE:L} == "yes"
-MODKDE5_RUN_DEPENDS +=		devel/desktop-file-utils
+.if defined(MODKDE_DESKTOP_FILE) && ${MODKDE_DESKTOP_FILE:L} == "yes"
+MODKDE_RUN_DEPENDS +=		devel/desktop-file-utils
 .endif
 
 # Set to 'yes' if there are icon files under share/icons/.
-.if defined(MODKDE5_ICON_CACHE) && ${MODKDE5_ICON_CACHE:L} == "yes"
-MODKDE5_RUN_DEPENDS +=		x11/gtk+4,-guic
+.if defined(MODKDE_ICON_CACHE) && ${MODKDE_ICON_CACHE:L} == "yes"
+MODKDE_RUN_DEPENDS +=		x11/gtk+4,-guic
 .endif
 
 # Set to 'yes' if there are icon files under share/locale/.
-.if defined(MODKDE5_TRANSLATIONS) && ${MODKDE5_TRANSLATIONS:L} == "yes"
-MODKDE5_BUILD_DEPENDS +=	devel/gettext,-tools
+.if defined(MODKDE_TRANSLATIONS) && ${MODKDE_TRANSLATIONS:L} == "yes"
+MODKDE_BUILD_DEPENDS +=		devel/gettext,-tools
 .endif
 
 # Set to 'yes' if there are icon files under share/doc/.
-.if defined(MODKDE5_DOCS) && ${MODKDE5_DOCS:L} == "yes"
+.if defined(MODKDE_DOCS) && ${MODKDE_DOCS:L} == "yes"
 .if ${MODKDE_KF5:L} == "yes"
-MODKDE5_BUILD_DEPENDS +=	devel/kf5/kdoctools
-MODKDE5_RUN_DEPENDS +=		devel/kf5/kdoctools
+MODKDE_BUILD_DEPENDS +=		devel/kf5/kdoctools
+MODKDE_RUN_DEPENDS +=		devel/kf5/kdoctools
 .else
-MODKDE5_BUILD_DEPENDS +=	devel/kf6/kdoctools
-MODKDE5_RUN_DEPENDS +=		devel/kf6/kdoctools
+MODKDE_BUILD_DEPENDS +=		devel/kf6/kdoctools
+MODKDE_RUN_DEPENDS +=		devel/kf6/kdoctools
 .endif
 .endif
