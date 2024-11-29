0a75684d08cf8cc9e5b3d59e7ed41a8dd4031a6d
semarie <semarie@openbsd.org>
2024-11-29 12:02:25 +0000

    update lang/rust to 1.83.0
    Announce: https://blog.rust-lang.org/2024/11/28/Rust-1.83.0.html
    Changelog: https://doc.rust-lang.org/stable/releases.html#version-1830-2024-11-28

--- lang/rust/rust.port.mk
+++ lang/rust/rust.port.mk
@@ -1,6 +1,6 @@
 # increment after rust compiler update to trigger updates of
 # all compiled rust packages (see arch-defines.mk)
-_SYSTEM_VERSION-rust =	9
+_SYSTEM_VERSION-rust =	10
 
 CATEGORIES +=		lang/rust
 
