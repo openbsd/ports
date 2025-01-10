5a9600c7e58d00d23fec55e3acd4917cd84f493d
jca <jca@openbsd.org>
2025-01-10 18:03:43 +0000

    Mark ports using lang/gcc/8,-f95 on arm64 as not BTI compliant
    To be reconsidered when we move to lang/gcc/11 or newer, which
    supports -mbranch-protection. The fix isn't complete since some ports
    may use BTI-incompatible fortran code through shared libraries.
    
    ok tb@ phessler@ pascal@ sthen@

--- infrastructure/mk/fortran.port.mk
+++ infrastructure/mk/fortran.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: fortran.port.mk,v 1.17 2019/08/27 14:30:38 jca Exp $
+# $OpenBSD: fortran.port.mk,v 1.18 2025/01/10 18:03:43 jca Exp $
 
 MODFORTRAN_COMPILER ?= gfortran
 
@@ -13,6 +13,8 @@ MODGCC4_LANGS += fortran
 MODFORTRAN_BUILD_DEPENDS += ${MODGCC4_FORTRANDEP}
 MODFORTRAN_LIB_DEPENDS += ${MODGCC4_FORTRANLIBDEP}
 MODFORTRAN_WANTLIB += ${MODGCC4_FORTRANWANTLIB}
+# XXX revisit when we move to lang/gcc/11
+USE_NOBTCFI-aarch64 ?=	Yes
 .elif ${MODFORTRAN_COMPILER:L} == "flang"
 MODFORTRAN_BUILD_DEPENDS += lang/flang/flang
 MODFORTRAN_LIB_DEPENDS += lang/flang/flang
