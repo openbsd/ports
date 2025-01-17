77f2e3a355584819a187cb5c4f9e8db7d587b1c5
jsing <jsing@openbsd.org>
2025-01-17 08:00:51 +0000

    Bump MODGO_SYSTEM_VERSION for update to lang/go.

--- lang/go/go.port.mk
+++ lang/go/go.port.mk
@@ -1,6 +1,6 @@
 # increment after a go compiler update to trigger updates of
 # compiled go packages (see arch-defines.mk)
-_MODGO_SYSTEM_VERSION =	19
+_MODGO_SYSTEM_VERSION =	20
 
 ONLY_FOR_ARCHS ?=	${GO_ARCHS}
 
