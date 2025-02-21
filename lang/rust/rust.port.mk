2abe9a94983e35acb13de26d455110f8391ab3e1
semarie <semarie@openbsd.org>
2025-02-21 10:18:30 +0000

    update lang/rust to 1.85.0 (missed rust.port.mk bump)

--- lang/rust/rust.port.mk
+++ lang/rust/rust.port.mk
@@ -1,6 +1,6 @@
 # increment after rust compiler update to trigger updates of
 # all compiled rust packages (see arch-defines.mk)
-_SYSTEM_VERSION-rust =	11
+_SYSTEM_VERSION-rust =	12
 
 CATEGORIES +=		lang/rust
 
