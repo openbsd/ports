77e499a774fd8fbdac996726fd35b2724a47ee11
sthen <sthen@openbsd.org>
2025-05-13 09:24:26 +0000

    update to mupdf-1.26.0
    - don't build/install "extra-apps" (muraster, which is a cut-down version
    of functionality of "mutool draw", and mupdf-x11-curl) - upstream no longer
    builds by default and they're not hugely useful
    
    - mupdf-gl was fixed so that it no longer depends on javascript, so include
    it in the main package again

--- textproc/mupdf/pkg/PFRAG.js
+++ textproc/mupdf/pkg/PFRAG.js
@@ -1,10 +1,12 @@
-@bin bin/mupdf-gl
 share/examples/mupdf/bbox-device.js
+share/examples/mupdf/copy-outline.js
 share/examples/mupdf/create-thumbnail.js
 share/examples/mupdf/draw-device.js
 share/examples/mupdf/draw-document.js
 share/examples/mupdf/fix-s22pdf.js
+share/examples/mupdf/import-fdf.js
 share/examples/mupdf/jpx-to-pdf.js
+share/examples/mupdf/pdf-bake.js
 share/examples/mupdf/pdf-create-lowlevel.js
 share/examples/mupdf/pdf-create.js
 share/examples/mupdf/pdf-dejpx.js
@@ -12,5 +14,6 @@ share/examples/mupdf/pdf-extract-rich-media.js
 share/examples/mupdf/pdf-merge.js
 share/examples/mupdf/pdf-portfolio.js
 share/examples/mupdf/pdf-trace.js
+share/examples/mupdf/reconvert-cmyk.js
 share/examples/mupdf/storytest.js
 share/examples/mupdf/trace-device.js
