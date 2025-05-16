658a9b071adc5cad1398650fc57401e970e3e4d3
semarie <semarie@openbsd.org>
2025-05-16 18:41:40 +0000

    update lang/rust to 1.87.0 (missing part)
    trigger updates of all compiled rust packages

--- lang/rust/rust.port.mk
+++ lang/rust/rust.port.mk
@@ -1,6 +1,6 @@
 # increment after rust compiler update to trigger updates of
 # all compiled rust packages (see arch-defines.mk)
-_SYSTEM_VERSION-rust =	13
+_SYSTEM_VERSION-rust =	14
 
 CATEGORIES +=		lang/rust
 
