49e573a47434af196f04276669464c47a74eb8b0
ajacoutot <ajacoutot@openbsd.org>
2025-05-31 18:50:20 +0000

    Replace totem with showtime.

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.1723 2025/05/31 13:46:49 ajacoutot Exp $
+# $OpenBSD: Quirks.pm,v 1.1724 2025/05/31 18:50:20 ajacoutot Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -626,6 +626,7 @@ my $stem_extensions = {
 	'c-icap-urlcheck' => 'c-icap-modules',
 # 7.8
 	'coq' => 'rocq',
+	'totem' => 'showtime',
 };
 
 my $obsolete_reason = {};
