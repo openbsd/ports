34b35e4c25863a8033dee958f35d0e41d3dd5d48
daniel <daniel@openbsd.org>
2025-06-22 12:28:32 +0000

    fold py2-opengl and py2-Pillow into py3 equivalent ports
    as suggested by sthen@

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.1728 2025/06/21 21:33:43 daniel Exp $
+# $OpenBSD: Quirks.pm,v 1.1729 2025/06/22 12:28:32 daniel Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -628,6 +628,8 @@ my $stem_extensions = {
 	'coq' => 'rocq',
 	'totem' => 'showtime',
 	'py-numpy' => 'py3-numpy',
+	'py-Pillow' => 'py3-Pillow',
+	'py-opengl' => 'py3-opengl',
 };
 
 my $obsolete_reason = {};
