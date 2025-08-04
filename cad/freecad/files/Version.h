9b97e4663f00210fcf508d0610c48f8e3aa5571e
jtt <jtt@openbsd.org>
2025-08-04 17:08:41 +0000

    Import cad/freecad
    ok jca@ and rsadowski@
    
    FreeCAD is an open-source parametric 3D modeler made primarily to
    design real-life objects of any size. Parametric modeling allows
    you to easily modify your design by going back into your model
    history and changing its parameters.

---
+++ cad/freecad/files/Version.h
@@ -0,0 +1,16 @@
+
+// Version Number
+#define FCVersionMajor  "1"
+#define FCVersionMinor  "0"
+#define FCVersionName   ""
+#define FCVersionPoint  "1"
+#define FCVersionSuffix ""
+// test: $Format:Hash (%H), Date: %ci$
+#define FCRevision      "39285 (Git)"      //Highest committed revision number
+#define FCRevisionDate  "2025/05/14 02:58:13"     //Date of highest committed revision
+#define FCRepositoryURL "Unknown"      //Repository URL of the working copy
+
+// Git relevant stuff
+#define FCRepositoryHash   "878f0b8c9c72c6f215833a99f2762bc3a3cf2abd"
+#define FCRepositoryBranch "tag: 1.0.1"
+
