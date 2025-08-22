81be6268450821bda1de8c1a8be4dd8b4bafe1ef
jtt <jtt@openbsd.org>
2025-08-22 12:03:26 +0000

    cad/freecad: update to 1.0.2
    This should unbreak the port with libc++19.
    
    ok tb@

--- cad/freecad/files/Version.h
+++ cad/freecad/files/Version.h
@@ -3,14 +3,14 @@
 #define FCVersionMajor  "1"
 #define FCVersionMinor  "0"
 #define FCVersionName   ""
-#define FCVersionPoint  "1"
+#define FCVersionPoint  "2"
 #define FCVersionSuffix ""
 // test: $Format:Hash (%H), Date: %ci$
-#define FCRevision      "39285 (Git)"      //Highest committed revision number
-#define FCRevisionDate  "2025/05/14 02:58:13"     //Date of highest committed revision
+#define FCRevision      "39319 (Git)"      //Highest committed revision number
+#define FCRevisionDate  "2025/08/05 20:19:07"     //Date of highest committed revision
 #define FCRepositoryURL "Unknown"      //Repository URL of the working copy
 
 // Git relevant stuff
-#define FCRepositoryHash   "878f0b8c9c72c6f215833a99f2762bc3a3cf2abd"
-#define FCRepositoryBranch "tag: 1.0.1"
+#define FCRepositoryHash   "256fc7eff3379911ab5daf88e10182c509aa8052"
+#define FCRepositoryBranch "tag: 1.0.2"
 
