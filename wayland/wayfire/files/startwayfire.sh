e3d866d1321f08e485f973cb555c01b21c53402f
landry <landry@openbsd.org>
2025-11-16 08:30:49 +0000

    wayland/wayfire: drop XDG_RUNTIME_DIR fiddling from wrapper
    now properly supported by the libc
    
    while here drop SEATD_BACKEND env var, default is now noop

--- wayland/wayfire/files/startwayfire.sh
+++ wayland/wayfire/files/startwayfire.sh
@@ -2,18 +2,10 @@
 set -eu
 
 : ${WLR_DRM_DEVICES:=/dev/dri/card0}
-: ${LIBSEAT_BACKEND:=noop}
 : ${XDG_CURRENT_DESKTOP:=wayfire}
-: ${XDG_RUNTIME_DIR:=${HOME}/.local/run}
 : ${QT_QPA_PLATFORM:=wayland}
 #: MOZ_ENABLE_WAYLAND:=1 # doesn't work yet
 
-export WLR_DRM_DEVICES LIBSEAT_BACKEND
-export XDG_CURRENT_DESKTOP XDG_RUNTIME_DIR
-export QT_QPA_PLATFORM
-
-if [ ! -d "${XDG_RUNTIME_DIR}" ]; then
-    mkdir -m 700 -p "${XDG_RUNTIME_DIR}"
-fi
+export WLR_DRM_DEVICES XDG_CURRENT_DESKTOP QT_QPA_PLATFORM
 
 exec /usr/local/bin/wayfire "$@"
