39dcd4c29fe3c3520f456e654fcde9f0c93efc43
kurt <kurt@openbsd.org>
2025-12-18 22:06:46 +0000

    Update to 19439 and jdk 17+. feedback and ok sthen@

--- geo/josm/files/josm.sh
+++ geo/josm/files/josm.sh
@@ -1,6 +1,9 @@
-#!/bin/ksh 
+#!/bin/ksh
 
 JAVA_CMD=$(javaPathHelper -c josm)
-JAVA_OPTIONS=-Xmx256M
+JAVA_OPTIONS="-Xmx256M \
+	--add-exports=java.base/sun.security.action=ALL-UNNAMED \
+	--add-exports=java.desktop/com.sun.imageio.plugins.jpeg=ALL-UNNAMED \
+	--add-exports=java.desktop/com.sun.imageio.spi=ALL-UNNAMED"
 
 ${JAVA_CMD} ${JAVA_OPTIONS} -jar ${TRUEPREFIX}/share/josm/josm-latest.jar $*
