e9c5ab5140217ea690353a7e182570cfbf409d9e
sthen <sthen@openbsd.org>
2026-01-09 15:12:27 +0000

    fix _MODPY_EXPECTED_BACKEND for scikit-build-core

--- lang/python/3/python.port.mk
+++ lang/python/3/python.port.mk
@@ -130,7 +130,7 @@ BUILD_DEPENDS +=	py3-poetry-core->=2.2.0:devel/py-poetry-core
 _MODPY_EXPECTED_BACKEND = poetry.core
 .  elif ${MODPY_PYBUILD} == scikit-build-core
 BUILD_DEPENDS +=	devel/py-scikit-build-core
-_MODPY_EXPECTED_BACKEND = poetry.core
+_MODPY_EXPECTED_BACKEND = scikit_build_core
 .  elif ${MODPY_PYBUILD} == setuptools || \
 	${MODPY_PYBUILD} == setuptools_scm || \
 	${MODPY_PYBUILD} == setuptools-rust
