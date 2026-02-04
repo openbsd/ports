1d4831e64720bc8ab594fdee2168d3708711b521
semarie <semarie@openbsd.org>
2026-02-04 19:40:04 +0000

    lang/rust: update to 1.93.0
    Announce: https://blog.rust-lang.org/2026/01/22/Rust-1.93.0/
    Release notes: https://doc.rust-lang.org/stable/releases.html#version-1930-2026-01-22

--- lang/rust/rust.port.mk
+++ lang/rust/rust.port.mk
@@ -1,6 +1,6 @@
 # increment after rust compiler update to trigger updates of
 # all compiled rust packages (see arch-defines.mk)
-_SYSTEM_VERSION-rust =	19
+_SYSTEM_VERSION-rust =	20
 
 CATEGORIES +=		lang/rust
 
