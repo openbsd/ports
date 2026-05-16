a41d843d4cf2a745d1f543dd9c6219a411b7f3de
jtt <jtt@openbsd.org>
2026-05-16 15:13:20 +0000

    cad/freecad: update to 1.1.0
    ok rsadowski@

--- cad/freecad/files/Version.h
+++ cad/freecad/files/Version.h
@@ -1,16 +1,18 @@
 
 // Version Number
 #define FCVersionMajor  "1"
-#define FCVersionMinor  "0"
+#define FCVersionMinor  "1"
 #define FCVersionName   ""
-#define FCVersionPoint  "2"
+#define FCVersionPoint  "0"
 #define FCVersionSuffix ""
+// Displayed Copyright Year (i.e. build year)
+#define FCCopyrightYear 2026
 // test: $Format:Hash (%H), Date: %ci$
-#define FCRevision      "39319 (Git)"      //Highest committed revision number
-#define FCRevisionDate  "2025/08/05 20:19:07"     //Date of highest committed revision
+#define FCRevision      "44795 (Git)"      //Highest committed revision number
+#define FCRevisionDate  "2026/03/25 02:17:53"     //Date of highest committed revision
 #define FCRepositoryURL "Unknown"      //Repository URL of the working copy
 
 // Git relevant stuff
-#define FCRepositoryHash   "256fc7eff3379911ab5daf88e10182c509aa8052"
-#define FCRepositoryBranch "tag: 1.0.2"
+#define FCRepositoryHash   "34a9716668b1ddeb55b914f1c5be644826bdbbbf"
+#define FCRepositoryBranch "tag: 1.1.0"
 
