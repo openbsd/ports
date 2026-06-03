988a78bc4c4bce50674f145c9e1224b33a2b3792
semarie <semarie@openbsd.org>
2026-06-03 11:15:20 +0000

    update lang/rust to 1.96.0
    Announce: https://blog.rust-lang.org/2026/05/28/Rust-1.96.0/
    Release notes: https://doc.rust-lang.org/stable/releases.html#version-1960-2026-05-28
    
    ok landry@

--- lang/rust/rust.port.mk
+++ lang/rust/rust.port.mk
@@ -1,6 +1,6 @@
 # increment after rust compiler update to trigger updates of
 # all compiled rust packages (see arch-defines.mk)
-_SYSTEM_VERSION-rust =	22
+_SYSTEM_VERSION-rust =	23
 
 CATEGORIES +=		lang/rust
 
