5fa653ee29a56ddd6eab723fcbd1f1ff4a5ec907
landry <landry@openbsd.org>
2026-06-08 16:14:42 +0000

    Import www/librewolf 151.0.3pl1, from MAINTAINER Leah Rowe
    LibreWolf is a modified version of Mozilla Firefox focused on
    privacy, security, and software freedom. It enhances protections
    against tracking and fingerprinting, and applies additional
    hardening compared to standard Firefox.
    
    ok caspar@

---
+++ www/librewolf/files/all-openbsd.js
@@ -0,0 +1,12 @@
+// OpenBSD-specific defaults overrides
+pref("media.autoplay.default", 1);
+pref("app.shield.optoutstudies.enabled", false);
+pref("app.normandy.enabled",false);
+pref("browser.safebrowsing.enabled", false);
+pref("browser.safebrowsing.malware.enabled", false);
+pref("spellchecker.dictionary_path", "${LOCALBASE}/share/mozilla-dicts/");
+pref("extensions.pocket.enabled", false);
+pref("browser.newtabpage.enabled", false);
+pref("browser.startup.homepage", "about:blank");
+pref("network.trr.mode", 5);
+pref("media.webrtc.hw.h264.enabled", true);
---
+++ www/librewolf/files/autoconfig.js
@@ -0,0 +1,2 @@
+pref("general.config.filename", "librewolf.cfg");
+pref("general.config.obscure_value", 0);
