36c0e59ba5a04fb81f7c559d9791b461704f19b4
afresh1 <afresh1@openbsd.org>
2026-09-20 23:43:52 +0000

    Add MODMARIADB_DEFAULTS_FILE to mariadb module
    The default sets innodb_buffer_pool_size_max=512m because otherwise
    none of the modules that try to use it for tests work as they want
    to allocate enough memory to run an LLM.
    
    While here, give mariadb a bit more time to start,
    my machine is apparently slow.
    
    OK Brad Smith <brad () comstyle ! com> (MAINTAINER)

--- databases/mariadb/mariadb.port.mk
+++ databases/mariadb/mariadb.port.mk
@@ -6,16 +6,18 @@ MODMARIADB_TEST_CMD ?= \
 MODMARIADB_TEST_DBNAME ?=
 MODMARIADB_TEST_SOCKET =	${WRKDIR}/mariadb.sock
 _MODMARIADB_TEST_DATA_DIR =	${WRKDIR}/testdb-mariadb
+_MODMARIADB_DEFAULTS_FILE =	${WRKDIR}/mariadb-default-my.cnf
+MODMARIADB_DEFAULTS_FILE ?=	${_MODMARIADB_DEFAULTS_FILE}
 
 MODMARIADB_SERVER_ARGS = \
-	--no-defaults \
+	--defaults-file=${MODMARIADB_DEFAULTS_FILE} \
 	--datadir=${_MODMARIADB_TEST_DATA_DIR} \
 	--skip-grant-tables \
 	--skip-networking \
 	--socket=${MODMARIADB_TEST_SOCKET}
 
 MODMARIADB_ADMIN_ARGS = \
-	--no-defaults \
+	--defaults-file=${MODMARIADB_DEFAULTS_FILE} \
 	--host=localhost \
 	--protocol=socket \
 	--socket=${MODMARIADB_TEST_SOCKET}
@@ -34,6 +36,8 @@ TEST_ENV += \
 
 MODMARIADB_TEST_TARGET = \
 	rm -Rf ${_MODMARIADB_TEST_DATA_DIR}; \
+	printf "[mysqld]\ninnodb_buffer_pool_size_max=512m\n" \
+	    > ${_MODMARIADB_DEFAULTS_FILE}; \
 	export ${ALL_TEST_ENV}; \
 	${LOCALBASE}/bin/mariadb-install-db \
 		--skip-name-resolve \
@@ -41,7 +45,7 @@ MODMARIADB_TEST_TARGET = \
 	${LOCALBASE}/libexec/mariadbd \
 		${MODMARIADB_SERVER_ARGS} & \
 	started=false; \
-	for i in $$(jot 10); do \
+	for i in $$(jot 30); do \
 		sleep 1; \
 		if mariadb-admin ${MODMARIADB_ADMIN_ARGS} \
 		    ping >/dev/null 2>&1; then \
